ab9da02a06ecdeaa7aea40fdb6749c6507336141
Florian Bienefelt <florian@e-potek.ch>
2016-11-26 17:41:22 +0100

    basic new step 1 layout

--- imports/ui/pages/user/Step1Page.jsx
+++ imports/ui/pages/user/Step1Page.jsx
@@ -1,133 +1,104 @@
 import React, { Component, PropTypes } from 'react';
-import { Meteor } from 'meteor/meteor';
 import { DocHead } from 'meteor/kadira:dochead';
-import { updateValues } from '/imports/api/creditrequests/methods.js';
 
-import TodoCardArray from '/imports/ui/components/general/TodoCardArray.jsx';
+import RaisedButton from 'material-ui/RaisedButton';
+import Checkbox from 'material-ui/Checkbox';
+
+import DropzoneInput from '/imports/ui/components/forms/DropzoneInput.jsx';
 
 
-const todoCards = [
-  {
-    title: 'Check-up initial',
-    duration: '5 min',
-  }, {
-    title: 'Mes Partenaires Financiers Particuliers',
-    duration: '20 sec',
-  }, {
-    title: 'Ma déclaration d\'impôts',
-    duration: '5 sec',
-  }, {
-    title: 'Évaluer mon bien immobilier',
-    duration: '2 min',
+const styles = {
+  mask: {
+    marginBottom: 40,
   },
-];
-
+  p: {
+    padding: 20,
+  },
+  checkbox: {
+    marginBottom: 20,
+  },
+  button: {
+    float: 'right',
+  },
+};
 
 export default class Step1Page extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      progress: this.getPercentages(),
+      isReady: false,
+      willUploadTaxes: true,
     };
+
+    this.handleCheck = this.handleCheck.bind(this);
   }
 
   componentDidMount() {
     DocHead.setTitle('Étape 1 - e-Potek');
   }
 
-
-  getPercentages() {
-    let part1 = 0;
-    let part2 = 0;
-    let part3 = 0;
-    let part4 = 0;
-    const r = this.props.creditRequest;
-
-    // all values from the forms
-    const part1Values = [
-      r.personalInfo.twoBuyers, r.personalInfo.age2, r.personalInfo.age1, r.propertyInfo.type,
-      r.financialInfo.salary, r.financialInfo.bonusExists, r.financialInfo.fortune,
-      r.financialInfo.insuranceFortune, r.propertyInfo.value,
-    ];
-    const part2Values = [
-      r.financialInfo.personalBank, r.financialInfo.corporateBankExists,
-      r.financialInfo.corporateBank, r.financialInfo.avoidLenderExists, r.financialInfo.avoidLender,
-    ];
-
-    let length1 = part1Values.length;
-    let length2 = part2Values.length;
-
-    if (r.personalInfo.twoBuyers === 'false') { length1 -= 1; } // Don't count age2
-    if (r.personalInfo.bonusExists === 'false') { length1 -= 1; } // Don't count bonus
-    if (r.financialInfo.corporateBankExists === 'false') { length2 -= 1; } // Don't count corporate bank
-    if (r.financialInfo.avoidLenderExists === 'false') { length2 -= 1; } // Don't count avoided lender
-
-
-    if (r.files.taxes) {
-      if (r.files.taxes.url) { part3 = 100; }
-
-    }
-    if (r.files.housePicture) {
-      if (r.files.housePicture.url) { part4 = 100; }
-
-    }
-    // if (r.files.taxes.url) { part3 = 100; }
-    // if (r.files.housePicture.url) { part4 = 100; }
-
-
-    // Filter out values
-    part1 = Math.round((part1Values.filter(this.filterFunc).length / length1) * 100);
-    part2 = Math.round((part2Values.filter(this.filterFunc).length / length2) * 100);
-
-    if (part1 >= 100 && part2 >= 100 && part3 >= 100 && part4 >= 100) {
-      this.setStepTo2();
-    }
-
-    return [
-      Math.min(part1, 100),
-      Math.min(part2, 100),
-      Math.min(part3, 100),
-      Math.min(part4, 100),
-    ];
+  handleCheck(event, isInputChecked) {
+    this.setState({ willUploadTaxes: !isInputChecked });
   }
-
-
-  setStepTo2() {
-    const id = this.props.creditRequest._id;
-    const object = {};
-    object['logic.step'] = 1;
-
-    updateValues.call({
-      object, id,
-    }, (error, result) => {
-      if (error) {
-        console.log(error.message);
-        throw new Meteor.Error(500, error.message);
-      } else {
-        return 'Step Increment Successful';
-      }
-    });
-  }
-
-
-  // Only return true for values which aren't equal an empty string
-  filterFunc(value) {
-    return value !== '' && value !== undefined && value !== null;
-  }
-
 
   render() {
     return (
-      <div>
-        <h1 className="stepTitle">1ère Étape <small>Préparez-vous pour les enchères</small></h1>
-        <TodoCardArray
-          cards={todoCards}
-          progress={this.state.progress}
+      <section>
+        <h1 className="stepTitle">
+          1ère Étape
+          <small> Encore quelques informations avant qu'on se mette au travail</small>
+        </h1>
+
+        <article className="mask2" style={styles.mask}>
+          <h3>Validez votre bien immobilier</h3>
+          <DropzoneInput
+            fileName="housePicture"
+            requestId={this.props.creditRequest._id}
+          />
+        </article>
+
+        <article className="mask2" style={styles.mask}>
+          <h3>Votre déclaration d'impôts</h3>
+          <p style={styles.p}>
+            Pour permettre au établissements financiers de vous faire une offre très précise,
+            nous pouvons leur faire savoir que vous nous avez déjà confié votre
+            déclaration d'impôts, sans la leur montrer.
+            <br />
+            Certains établissements refuseront de faire une offre, car ils considèreront que votre
+            dossier n'est pas assez sérieux.
+          </p>
+
+          <div className="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 secondary">
+            <Checkbox
+              label="J'uploaderai ma déclaration d'impôts plus tard"
+              style={styles.checkbox}
+              onCheck={this.handleCheck}
+            />
+          </div>
+
+          <div className="col-xs-12">
+            {this.state.willUploadTaxes &&
+              <DropzoneInput
+                fileName="taxes"
+                requestId={this.props.creditRequest._id}
+              />
+            }
+          </div>
+        </article>
+
+        <RaisedButton
+          label="Continuer"
+          disabled={!this.state.isReady}
+          primary
+          style={styles.button}
         />
-      </div>
+
+      </section>
     );
   }
 }
+
 
 Step1Page.propTypes = {
   creditRequest: PropTypes.objectOf(PropTypes.any).isRequired,
