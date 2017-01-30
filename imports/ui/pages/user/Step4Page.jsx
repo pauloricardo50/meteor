35fb7b9cca50b96e4e84bd45cbcdb26bd5ed6160
Florian Bienefelt <florian.bienefelt@hotmail.com>
2017-01-30 11:57:56 +0100

    round fix

--- imports/ui/pages/user/Step4Page.jsx
+++ imports/ui/pages/user/Step4Page.jsx
@@ -15,33 +15,36 @@ export default class Step4Page extends React.Component {
 
   render() {
     return (
-      <section className="mask1 animated fadeIn">
+      <div>
         <h1>4ème Étape <small>Enfin, le contrat</small></h1>
-        <img
-          src="/img/step4_icons.svg"
-          alt="Votre dossier => Banque"
-          style={{
-            width: '100%',
-            padding: 40,
-          }}
-        />
-        <p
-          className="col-sm-6 col-sm-offset-3"
-          style={{ textAlign: 'justify', padding: 20 }}
-        >
-          Vous y êtes presque! Si vous estimez que vos informations sont correctes,
-          envoyez votre dossier à la banque. Vous recevrez un contrat dans environ 7
-          jours ouvrables, que vous pourrez étudier en toute tranquilité.
-        </p><br />
-        <div className="col-xs-12">
-          <div className="form-group text-center">
-            <RaisedButton label="Envoyer mon dossier" primary />
+
+        <section className="mask1 animated fadeIn">
+          <img
+            src="/img/step4_icons.svg"
+            alt="Votre dossier => Banque"
+            style={{
+              width: '100%',
+              padding: 40,
+            }}
+          />
+          <p
+            className="col-sm-6 col-sm-offset-3"
+            style={{ textAlign: 'justify', padding: 20 }}
+          >
+            Vous y êtes presque! Si vous estimez que vos informations sont correctes,
+            envoyez votre dossier à la banque. Vous recevrez un contrat dans environ 7
+            jours ouvrables, que vous pourrez étudier en toute tranquilité.
+          </p><br />
+          <div className="col-xs-12">
+            <div className="form-group text-center">
+              <RaisedButton label="Envoyer mon dossier" primary />
+            </div>
+            <div className="form-group text-center">
+              <RaisedButton label="Pas encore" href="/step3" />
+            </div>
           </div>
-          <div className="form-group text-center">
-            <RaisedButton label="Pas encore" href="/step3" />
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     );
   }
 }
