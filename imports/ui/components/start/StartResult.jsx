478bf4a709d91d2743582a96181d0ff8d3ad4e37
Florian Bienefelt <florian.bienefelt@hotmail.com>
2017-03-13 18:20:33 +0100

    fix

--- imports/ui/components/start/StartResult.jsx
+++ imports/ui/components/start/StartResult.jsx
@@ -1,11 +1,11 @@
 import React, {PropTypes} from 'react';
 
-
 import RaisedButton from 'material-ui/RaisedButton';
 import ExpensesChart from '/imports/ui/components/charts/ExpensesChart.jsx';
 import Start2Recap from '/imports/ui/components/start/Start2Recap.jsx';
 
 import constants from '/imports/js/constants';
+import { toMoney } from '/imports/js/conversionFunctions';
 
 
 export default class StartResult extends React.Component {
@@ -23,11 +23,14 @@ export default class StartResult extends React.Component {
           <Start2Recap
             {...this.props}
           />
-          <ExpensesChart
-            interests={(loan * constants.interestsReal) / 12}
-            amortizing={(loan * constants.amortizing) / 12}
-            maintenance={(this.props.propAndWork * constants.maintenanceReal) / 12}
-          />
+          <div className="chart">
+            <h3>Votre emprunt: CHF {toMoney(loan)}</h3>
+            <ExpensesChart
+              interests={(loan * constants.interestsReal) / 12}
+              amortizing={(loan * constants.amortizing) / 12}
+              maintenance={(this.props.propAndWork * constants.maintenanceReal) / 12}
+            />
+          </div>
         </div>
 
         <div className="button">
