e3d8c35708e13e55437e973508e009966302ec91
Florian Bienefelt <Florian@Florians-MacBook-Pro.local>
2017-03-31 11:47:30 +0200

    added signup form

--- imports/ui/pages/public/PasswordPage.jsx
+++ imports/ui/pages/public/PasswordPage.jsx
@@ -1,56 +1,25 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 
-import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
-export default class PasswordPage extends React.Component {
-  constructor(props) {
-    super(props);
+import BetaAccess from './passwordPage/BetaAccess.jsx';
 
-    this.state = { error: '' };
+const PasswordPage = props => (
+  <main className="password-page animated fadeIn">
+    <img src="/img/logo_black.svg" alt="e-Potek" />
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    <h3>En Développement</h3>
 
-  handleSubmit(e) {
-    e.preventDefault();
-
-    console.log(this.password);
-
-    // If you read this, write to me at florian@e-potek.ch, we're hiring hackers!
-    if (this.password.input.value === 'goforlife') {
-      this.props.history.push('/home');
-    } else {
-      this.setState({ error: 'nope' });
-    }
-  }
-
-  render() {
-    return (
-      <main className="password-page animated fadeIn">
-        <img src="/img/logo_black.svg" alt="e-Potek" />
-
-        <h1>En Développement</h1>
-
-        <form action="submit" onSubmit={this.handleSubmit}>
-          <TextField
-            autoFocus
-            floatingLabelText="Mot de Passe"
-            type="password"
-            ref={r => {
-              this.password = r;
-            }}
-            errorText={this.state.error}
-          />
-          <RaisedButton
-            label="Accéder à la Beta"
-            primary
-            onTouchTap={this.handleSubmit}
-          />
-        </form>
-      </main>
-    );
-  }
-}
+    <RaisedButton
+      className="subscribe"
+      href="http://eepurl.com/cI56Sn"
+      primary
+      label="M'avertir du lancement"
+    />
+    <BetaAccess {...props} />
+  </main>
+);
 
 PasswordPage.propTypes = {};
+
+export default PasswordPage;
---
+++ imports/ui/pages/public/passwordPage/BetaAccess.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+export default class BetaAccess extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+      error: '',
+    };
+
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleOpen() {
+    this.setState({ open: true });
+  }
+
+  handleClose() {
+    this.setState({ open: false });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    // If you read this, write to me at florian@e-potek.ch, we're hiring curious people!
+    if (this.password.input.value === 'goforlife') {
+      this.props.history.push('/home');
+    } else {
+      this.setState({ error: 'Nope' });
+    }
+  }
+
+  render() {
+    const actions = [
+      <FlatButton label="Annuler" onTouchTap={this.handleClose} />,
+      <FlatButton label="Okay" primary onTouchTap={this.handleSubmit} />,
+    ];
+
+    return (
+      <div>
+        <RaisedButton label="Accéder à la Beta" onTouchTap={this.handleOpen} />
+        <Dialog
+          title="Accéder à la Beta"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          <form
+            action="submit"
+            onSubmit={this.handleSubmit}
+            className="text-center"
+          >
+            <TextField
+              autoFocus
+              floatingLabelText="Mot de passe"
+              type="password"
+              ref={r => {
+                this.password = r;
+              }}
+              style={{ width: 100 }}
+              errorText={this.state.error}
+            />
+          </form>
+        </Dialog>
+      </div>
+    );
+  }
+}
