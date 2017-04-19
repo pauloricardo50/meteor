e5a22e5164aa8e712039067b97c66693c65c80a9
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-04-19 03:33:41 +0200

    changed from rc-tooltip to react-bootstrap tooltip

--- imports/ui/components/general/AutoTooltip.jsx
+++ imports/ui/components/general/AutoTooltip.jsx
@@ -5,6 +5,9 @@ import { Meteor } from 'meteor/meteor';
 import RcTooltip from 'rc-tooltip';
 import reactStringReplace from 'react-string-replace';
 
+import Tooltip from 'react-bootstrap/lib/Tooltip';
+import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
+
 import tooltips from '/imports/js/arrays/tooltips';
 
 const styles = {
@@ -37,20 +40,34 @@ const AutoTooltip = props => {
         props.children,
         new RegExp(`(${Object.keys(tooltips).join('|')})`, 'gi'),
         (match, i) => (
-          <RcTooltip
-            placement={props.placement}
-            trigger={props.trigger}
+        // <RcTooltip
+        //   placement={props.placement}
+        //   trigger={props.trigger}
+        //   overlay={
+        //     <span style={styles.text}>{tooltips[match.toLowerCase()]}</span>
+        //   }
+        //   overlayStyle={styles.tooltip}
+        //   mouseLeaveDelay={0}
+        //   key={i}
+        // >
+        //   <span style={styles.span}>
+        //     {match}
+        //   </span>
+        // </RcTooltip>
+          <OverlayTrigger
+            placement="bottom"
             overlay={
-              <span style={styles.text}>{tooltips[match.toLowerCase()]}</span>
+              <Tooltip id="tooltip">
+                <span style={styles.text}>{tooltips[match.toLowerCase()]}</span>
+              </Tooltip>
             }
-            overlayStyle={styles.tooltip}
-            mouseLeaveDelay={0}
             key={i}
+            rootClose
           >
             <span style={styles.span}>
               {match}
             </span>
-          </RcTooltip>
+          </OverlayTrigger>
         ),
       )}
     </span>
