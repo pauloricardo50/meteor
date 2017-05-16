708bd62b270136da429478b702fdcead4323dd03
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-05-16 13:39:27 +0200

    fixed url v2

--- .deploy/staging/mup.js
+++ .deploy/staging/mup.js
@@ -17,7 +17,7 @@ module.exports = {
       serverOnly: true,
     },
     env: {
-      ROOT_URL: 'https://staging.e-potek.ch',
+      ROOT_URL: 'http://staging.e-potek.ch',
       // MONGO_URL: 'mongodb://admin1:password@gcp-europe-west1-cpu.0.dblayer.com:15119/e-potek-test?ssl=true',
       MONGO_URL: 'mongodb://admin1:password@aws-eu-central-1-portal.2.dblayer.com:15723,aws-eu-central-1-portal.0.dblayer.com:15723/e-potek?ssl=true',
     },
