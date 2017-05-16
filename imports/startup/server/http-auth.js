4263a2aec46fa2fc6336e82d992bef4223de0cf6
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-05-16 17:43:41 +0200

    staging properly set up!

--- imports/startup/server/http-auth.js
+++ imports/startup/server/http-auth.js
@@ -1,5 +1,6 @@
 import { HttpBasicAuth } from 'meteor/jabbslad:basic-auth';
 
+// adds http authentication for the entire app with given "username, password"
 const setupAuth = () => {
   const basicAuth = new HttpBasicAuth('epotek', 'goforlife');
   basicAuth.protect();
