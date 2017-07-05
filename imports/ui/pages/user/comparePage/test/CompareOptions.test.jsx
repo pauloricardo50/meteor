a612bf9b837b531ffcc40364d2474a5b2d8a5d4f
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-07-05 19:08:23 +0200

    fixed test

--- imports/ui/pages/user/comparePage/test/CompareOptions.test.jsx
+++ imports/ui/pages/user/comparePage/test/CompareOptions.test.jsx
@@ -7,7 +7,6 @@ import { Meteor } from 'meteor/meteor';
 import getMountedComponent from '/imports/js/helpers/testHelpers';
 import CompareOptions from '../CompareOptions.jsx';
 
-import RaisedButton from 'material-ui/RaisedButton';
 import DefaultOptions from '../DefaultOptions.jsx';
 import AdvancedOptions from '../AdvancedOptions.jsx';
 
@@ -17,32 +16,32 @@ describe('<CompareOptions />', () => {
   });
 
   it('renders', () => {
-    const wrapper = shallow(<CompareOptions />);
+    const wrapper = shallow(<CompareOptions options={{}} />);
 
     expect(wrapper.exists()).to.be.true;
     expect(wrapper.name()).to.equal('div');
   });
 
   it('has a mask1 class on it', () => {
-    const wrapper = shallow(<CompareOptions />);
+    const wrapper = shallow(<CompareOptions options={{}} />);
 
     expect(wrapper.hasClass('mask1')).to.be.true;
   });
 
   it('renders only the default options by default', () => {
-    const wrapper = shallow(<CompareOptions />);
+    const wrapper = shallow(<CompareOptions options={{}} />);
 
     expect(wrapper.find(DefaultOptions).exists()).to.be.true;
     expect(wrapper.find(AdvancedOptions).exists()).to.be.false;
   });
 
   it('has default state on start', () => {
-    const wrapper = shallow(<CompareOptions />);
+    const wrapper = shallow(<CompareOptions options={{}} />);
     expect(wrapper.state()).to.deep.equal({ showAdvanced: false });
   });
 
   it('changes state with handleClick', (done) => {
-    const wrapper = shallow(<CompareOptions />);
+    const wrapper = shallow(<CompareOptions options={{}} />);
     wrapper.instance().handleClick(() => {
       expect(wrapper.state()).to.deep.equal({ showAdvanced: true });
       done();
