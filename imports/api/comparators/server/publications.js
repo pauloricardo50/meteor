5f85c275fb56adfa4a235102fabf61971fbd8fcd
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-07-19 16:33:33 +0200

    edited publications

--- imports/api/comparators/server/publications.js
+++ imports/api/comparators/server/publications.js
@@ -4,6 +4,11 @@ import Comparators from '../comparators';
 
 // Get all comparators for the current user
 Meteor.publish('userComparators', function () {
+  // Verify if user is logged In
+  if (!this.userId) {
+    return this.ready();
+  }
+
   return Comparators.find({
     userId: this.userId,
   });
--- imports/api/properties/server/publications.js
+++ imports/api/properties/server/publications.js
@@ -4,6 +4,11 @@ import Properties from '../properties';
 
 // Get all properties for the current user
 Meteor.publish('userProperties', function () {
+  // Verify if user is logged In
+  if (!this.userId) {
+    return this.ready();
+  }
+
   return Properties.find({
     userId: this.userId,
   });
