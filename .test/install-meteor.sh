05faae7dfb974b66909cb97b389492f9ef497213
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-07-27 16:07:12 +0200

    made .sh file executable

--- .test/install-meteor.sh
+++ .test/install-meteor.sh
@@ -3,8 +3,8 @@
 if [ -f ~/.meteor/meteor ]
 then
     echo `~/.meteor/meteor --version` found, restoring symlink
-	sudo ln -s ~/.meteor/meteor /usr/local/bin/meteor
+  sudo ln -s ~/.meteor/meteor /usr/local/bin/meteor
 else
     echo Installing Meteor
-	curl https://install.meteor.com | sh
+  curl https://install.meteor.com | sh
 fi
