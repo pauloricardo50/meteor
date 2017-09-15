0a6cbe6d305e2deb0528c22b1fabfcfd4fc0f920
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-09-15 15:10:31 +0200

    fixed DashboardOffer

--- imports/ui/pages/user/DashboardPage/DashboardOffer.jsx
+++ imports/ui/pages/user/DashboardPage/DashboardOffer.jsx
@@ -1,81 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { T, IntlNumber } from '/imports/ui/components/general/Translation';
-import OfferToggle from '/imports/ui/components/general/OfferToggle';
 import ConditionsButton from '/imports/ui/components/general/ConditionsButton';
 import DashboardItem from './DashboardItem';
 
-export default class DashboardOffer extends Component {
-  constructor(props) {
-    super(props);
-    const offerId = this.props.loanRequest.logic.lender.offerId;
-    const offer = this.props.offers.find(o => o._id === offerId);
+const DashboardOffer = (props) => {
+  const { offers, loanRequest } = props;
+  const offer = offers.find(o => o._id === loanRequest.logic.lender.offerId);
+  const displayedOffer =
+    loanRequest.logic.lender.type === 'counterparts'
+      ? offer.counterpartOffer
+      : offer.standardOffer;
 
-    this.state = { toggle: false, offer };
-  }
-
-  handleToggle = () => this.setState(prev => ({ toggle: !prev.toggle }));
-
-  renderButton = () => {
-    const { toggle, offer } = this.state;
-
-    if (!toggle && offer.conditions && offer.conditions.length) {
-      // If showing conditions
-
-      return (
-        <div className="text-center" style={{ marginTop: 16 }}>
-          <ConditionsButton conditions={offer.conditions} counterparts={offer.counterparts} />
-        </div>
-      );
-    } else if (
-      toggle &&
-      ((offer.conditions && offer.conditions.length) ||
-        (offer.counterparts && offer.counterparts.length))
-    ) {
-      return (
-        <div className="text-center" style={{ marginTop: 16 }}>
-          <ConditionsButton conditions={offer.conditions} counterparts={offer.counterparts} />
-        </div>
-      );
-    }
-
-    return null;
-  };
-
-  render() {
-    const { toggle, offer } = this.state;
-
-    const displayedOffer = this.state.toggle ? offer.counterpartOffer : offer.standardOffer;
-
-    return (
-      <DashboardItem title={<T id="DashboardOffer.title" />}>
-        <OfferToggle
-          value={this.state.toggle}
-          handleToggle={this.handleToggle}
-          offers={this.props.offers}
-          short
+  return (
+    <DashboardItem title={<T id="DashboardOffer.title" />}>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        {Object.keys(displayedOffer).map(key => (
+          <div
+            style={{ display: 'flex', justifyContent: 'space-between' }}
+            key={key}
+          >
+            <span>
+              <T id={`offer.${key}`} />
+            </span>
+            <span>
+              <IntlNumber
+                value={displayedOffer[key]}
+                format={key === 'maxAmount' ? 'money' : 'percentage'}
+              />
+            </span>
+          </div>
+        ))}
+      </div>
+      <div className="text-center" style={{ marginTop: 16 }}>
+        <ConditionsButton
+          conditions={offer.conditions}
+          counterparts={offer.counterparts}
         />
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          {Object.keys(displayedOffer).map(key =>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }} key={key}>
-              <span><T id={`offer.${key}`} /></span>
-              <span>
-                <IntlNumber
-                  value={displayedOffer[key]}
-                  format={key === 'maxAmount' ? 'money' : 'percentage'}
-                />
-              </span>
-            </div>,
-          )}
-        </div>
-        {this.renderButton()}
-      </DashboardItem>
-    );
-  }
-}
+      </div>
+    </DashboardItem>
+  );
+};
 
 DashboardOffer.propTypes = {
   loanRequest: PropTypes.objectOf(PropTypes.any).isRequired,
   offers: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
+
+export default DashboardOffer;
