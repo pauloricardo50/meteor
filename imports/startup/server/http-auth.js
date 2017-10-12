ff3c1559d73ba629765e142428707ca9d8bf81b6
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-12 14:24:15 +0200

    removed a few unecessary packages

--- imports/startup/server/http-auth.js
+++ imports/startup/server/http-auth.js
@@ -1,9 +1,9 @@
-import { HttpBasicAuth } from 'meteor/jabbslad:basic-auth';
+// import { HttpBasicAuth } from 'meteor/jabbslad:basic-auth';
 
 // adds http authentication for the entire app with given "username, password"
 const setupAuth = () => {
-  const basicAuth = new HttpBasicAuth('epotek', 'goforlife');
-  basicAuth.protect();
+  // const basicAuth = new HttpBasicAuth('epotek', 'goforlife');
+  // basicAuth.protect();
 };
 
 export default setupAuth;
