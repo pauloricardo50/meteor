949b1bde7cfa4d5c0870e9f3bf6cf20a086ac248
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-12-18 17:18:44 +0100

    Remove symlink to private/ folders and make it a hard copy instead
    Bug currently in Meteor: https://github.com/meteor/meteor/issues/7013

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,19 +14,21 @@ for i in 'sAdmin' 'sApp' 'sLender' 'sWww'
     echo Creating symlinks
     ln -s ../../core ../$i/imports/core
     ln -s ../../core/assets/css ../$i/client/css
-    ln -s ../core/assets/private ../$i/private
     ln -s ../core/.babelrc ../$i/.babelrc
 
-    # public folder can't have any symlink: https://github.com/meteor/meteor/issues/7013
-    echo Creating public folder
+    # public and private folders can't have any symlink: https://github.com/meteor/meteor/issues/7013
+    echo Creating public/private folders
     rm -rf ../$i/public
     cp -a ../core/assets/public ../$i/public
+
+    rm -rf ../$i/private
+    cp -a ../core/assets/private ../$i/private
 
     echo Installing clean node_modules
     ( cd ../$i && rm -rf node_modules/ && meteor npm install )
 
     # This is the hard clean variant in case node_modules has integrity problems
-    ( cd ../$i && rm ./package-lock.json && rm -rf node_modules/ && npm cache clear --force && meteor npm install )
+    # ( cd ../$i && rm ./package-lock.json && rm -rf node_modules/ && npm cache clear --force && meteor npm install )
 
   done
 
