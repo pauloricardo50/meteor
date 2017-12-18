16cd00ef7529910cdecbe8f1e6ed9c555e6884a8
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-12-18 16:23:06 +0100

    Add run.sh script to start all apps

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 echo Running e-Potek...
+
+# Check if ttab is installed
+if ! type "ttab" > /dev/null; then
+  # install ttab if not
+  echo Installing ttab
+  npm i -g ttab
+fi
+
+# Start each app in its own tab
+for i in 'sApp' 'sWww' 'sAdmin' 'sLender'
+  do
+    # Make them all connect to the same mongoDB
+    ttab -t $i -d ../$i 'MONGO_URL=mongodb://localhost:3000; meteor npm start'
+  done
