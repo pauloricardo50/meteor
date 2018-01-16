9d97442429fad96783176e8845f1ecdef77abb95
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-01-16 22:51:27 +0100

    Make Lenders microservice start without errors again

--- microservices/lender/imports/startup/server/index.js
+++ microservices/lender/imports/startup/server/index.js
@@ -1,3 +1,12 @@
+import { Meteor } from 'meteor/meteor';
+
+import 'core/api/api';
 import 'core/api/api-server';
+import '../accounts-config';
+import setupMandrill from 'core/api/email/server/email-config';
+
+Meteor.startup(() => {
+  setupMandrill();
+});
 
 console.log('Hello World lender server');
