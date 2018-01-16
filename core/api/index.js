cb1a3a7a0c61a8b20dfd81d280ae0322bb7aeb12
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-01-16 16:26:15 +0100

    Make collections easier to import

---
+++ core/api/index.js
@@ -0,0 +1,7 @@
+import LoanRequests from './loanrequests';
+import Borrowers from './borrowers';
+import Offers from './offers';
+import Properties from './properties';
+import Comparators from './comparators';
+
+export { LoanRequests, Borrowers, Offers, Properties, Comparators };
