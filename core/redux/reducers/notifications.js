37f8dad0106e42c70417e6a56c16b9c6b077ba29
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-01-17 16:47:41 +0100

    Prepare notifications switch to redux

---
+++ core/redux/reducers/notifications.js
@@ -0,0 +1,17 @@
+const initialState = [];
+
+const ADD_NOTIFICATION = 'ADD_NOTIFICATION';
+const POP_NOTIFICATION = 'POP_NOTIFICATION';
+
+const notifications = (state = initialState, { type, notification }) => {
+  switch (type) {
+    case ADD_NOTIFICATION:
+      return [...state, notification];
+    case POP_NOTIFICATION:
+      return [...state.slice(1)];
+    default:
+      return state;
+  }
+};
+
+export default notifications;
