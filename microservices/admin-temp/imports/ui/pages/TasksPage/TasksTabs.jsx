1ebe88f2ef263b67b72e0db6144901bf6127f362
andra10 <andra.lazariuc@yahoo.com>
2018-02-13 15:37:25 +0200

    Fixed columnOptions on tab change

--- microservices/admin-temp/imports/ui/pages/TasksPage/TasksTabs.jsx
+++ microservices/admin-temp/imports/ui/pages/TasksPage/TasksTabs.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import Tabs from 'core/components/Tabs';
-import PropTypes from 'prop-types';
-import queryString from 'query-string';
-import TasksTabContainer from './TasksTabContainer';
-import { T } from '../../../core/components/Translation/';
+import React from "react";
+import Tabs from "core/components/Tabs";
+import PropTypes from "prop-types";
+import queryString from "query-string";
+import TasksTabContainer from "./TasksTabContainer";
+import { T } from "../../../core/components/Translation/";
 
 const getTabs = props => [
-  { id: 'myTasks', label: <T id={`TasksTabs.myTasks`} />, content: <TasksTabContainer {...props} userId={ Meteor.userId()} showAsignee={false}/> },
-  { id: 'allTasks', label: <T id={`TasksTabs.allTasks`} />, content: <TasksTabContainer {...props} showAsignee={true}/> },
+    {
+        id: "myTasks",
+        label: <T id={`TasksTabs.myTasks`} />,
+        content: (
+            <TasksTabContainer
+                {...props}
+                userId={Meteor.userId()}
+                showAsignee={false}
+                key="myTasks"
+            />
+        )
+    },
+    {
+        id: "allTasks",
+        label: <T id={`TasksTabs.allTasks`} />,
+        content: (
+            <TasksTabContainer {...props} showAsignee={true} key="allTasks" />
+        )
+    }
 ];
 
-const TasksTabs = (props) => {
-  const tabs = getTabs(props);
-  const initialTab = tabs.findIndex(tab => tab.id === queryString.parse(props.location.search).tab);
-  return <Tabs initialIndex={initialTab} tabs={tabs} />;
+const TasksTabs = props => {
+    const tabs = getTabs(props);
+    const initialTab = tabs.findIndex(
+        tab => tab.id === queryString.parse(props.location.search).tab
+    );
+    return <Tabs initialIndex={initialTab} tabs={tabs} />;
 };
-
 
 export default TasksTabs;
