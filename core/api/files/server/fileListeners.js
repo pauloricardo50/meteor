b4430a6bea30e913c105aab556b8f50efe904339
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-02-17 23:58:36 +0100

    Add boilerplate listeners for files

---
+++ core/api/files/server/fileListeners.js
@@ -0,0 +1,20 @@
+import { EventService, mutations } from '../..';
+import FileService from '../FileService';
+
+EventService.addMutationListener(
+  mutations.BORROWER_DELETE,
+  ({ borrowerId }) => {
+    // TODO: Delete all files related to this borrower
+  }
+);
+
+EventService.addMutationListener(mutations.LOAN_DELETE, ({ loanId }) => {
+  // TODO: Delete all files related to this borrower
+});
+
+EventService.addMutationListener(
+  mutations.PROPERTY_DELETE,
+  ({ propertyId }) => {
+    // TODO: Delete all files related to this property
+  }
+);
