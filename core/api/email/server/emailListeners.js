f21320863d4748037cab225ddbab3bbf0d5b05d6
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-02-17 16:15:09 +0100

    Add all required listeners as boilerplate

--- core/api/email/server/emailListeners.js
+++ core/api/email/server/emailListeners.js
@@ -1,6 +1,21 @@
 import { EventService, mutations } from '../..';
 import EmailService from '../EmailService';
 
+EventService.addMutationListener(
+  mutations.REQUEST_LOAN_VERIFICATION,
+  ({ loanId }) => {
+    EmailService.sendEmail({});
+  },
+);
+
 EventService.addMutationListener(mutations.START_AUCTION, ({ loanId }) => {
+  EmailService.sendEmail({});
+});
+
+EventService.addMutationListener(mutations.END_AUCTION, ({ loanId }) => {
+  EmailService.sendEmail({});
+});
+
+EventService.addMutationListener(mutations.CANCEL_AUCTION, ({ loanId }) => {
   EmailService.sendEmail({});
 });
