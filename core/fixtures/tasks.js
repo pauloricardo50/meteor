612d08a0b9e848659d9406902a9c0a7a9e354460
andraLazariuc <andra.lazariuc@yahoo.com>
2018-03-08 11:26:54 +0200

    Implement changes form review

--- core/fixtures/tasks.js
+++ core/fixtures/tasks.js
@@ -1,7 +1,7 @@
 import TaskService from 'core/api/tasks/TaskService';
 import { TASK_TYPE } from 'core/api/tasks/tasksConstants';
 
-const types = Object.values(TASK_TYPE);
+const types = Object.values(TASK_TYPE).filter(item => item !== TASK_TYPE.ADD_ASSIGNED_TO);
 
 const createFakeTasks = (loanId, assignedTo) => {
   const type = types[Math.floor(Math.random() * types.length)];
