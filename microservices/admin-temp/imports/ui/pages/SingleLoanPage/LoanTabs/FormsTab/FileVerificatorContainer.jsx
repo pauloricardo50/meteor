6cd84b8379222eddba97e48d96ded2f2bf027092
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-03-10 19:04:24 +0100

    Minor adjustments

--- microservices/admin-temp/imports/ui/pages/SingleLoanPage/LoanTabs/FormsTab/FileVerificatorContainer.jsx
+++ microservices/admin-temp/imports/ui/pages/SingleLoanPage/LoanTabs/FormsTab/FileVerificatorContainer.jsx
@@ -1,18 +1,10 @@
 import { createContainer, setFileStatus, setFileError } from 'core/api';
 
-const FileVerificatorContainer = createContainer(({ isBorrower, isProperty, docId, id: fileId }) => {
-  let collection = 'loans';
-  if (isBorrower) {
-    collection = 'borrowers';
-  } else if (isProperty) {
-    collection = 'properties';
-  }
-  return {
-    setFileStatus: (fileKey, newStatus) =>
-      setFileStatus.run({ collection, docId, fileId, fileKey, newStatus }),
-    setFileError: (fileKey, error) =>
-      setFileError.run({ collection, docId, fileId, fileKey, error }),
-  };
-});
+const FileVerificatorContainer = createContainer(({ collection, docId, id: fileId }) => ({
+  setFileStatus: (fileKey, newStatus) =>
+    setFileStatus.run({ collection, docId, fileId, fileKey, newStatus }),
+  setFileError: (fileKey, error) =>
+    setFileError.run({ collection, docId, fileId, fileKey, error }),
+}));
 
 export default FileVerificatorContainer;
