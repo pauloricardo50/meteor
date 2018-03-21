2d3040b6f32d33408cef0bbf04d1eea2fd085531
claudiut <claudiut@users.noreply.github.com>
2018-03-21 18:59:03 +0200

    EP-13 Export IMPERSONATE_METHOD constant for the impersonate method name and use it when defining the impersonate method.

--- core/api/impersonation/defs.js
+++ core/api/impersonation/defs.js
@@ -1,7 +1,8 @@
 import { Method } from '../methods/methods';
+import { IMPERSONATE_METHOD } from './impersonation';
 
 export const impersonateUser = new Method({
-  name: 'impersonateUser',
+  name: IMPERSONATE_METHOD,
   params: {
     authToken: String,
     userId: String,
