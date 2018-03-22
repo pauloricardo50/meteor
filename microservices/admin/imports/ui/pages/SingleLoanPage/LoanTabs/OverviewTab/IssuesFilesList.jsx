74d77567f797589242386369daa3596940a1ef61
andraLazariuc <andra.lazariuc@yahoo.com>
2018-03-22 22:03:49 +0200

    EP-39 Use documents key when documents label is undefined

--- microservices/admin/imports/ui/pages/SingleLoanPage/LoanTabs/OverviewTab/IssuesFilesList.jsx
+++ microservices/admin/imports/ui/pages/SingleLoanPage/LoanTabs/OverviewTab/IssuesFilesList.jsx
@@ -3,14 +3,14 @@ import { T } from 'core/components/Translation';
 
 export default ({ documents, checkFileErrors }) =>
   Object.keys(documents).map((key) => {
-    // documents.buyersContract: {files: Array(1), uploadCount: 1, isAdmin: false}
     if (checkFileErrors(documents[key].files)) {
       return (
         <div>
-          <p className="bold"> {documents[key].label || 'No label'}</p>
+          <p className="bold">
+            {documents[key].label || <T id={`files.${key}`} />}
+          </p>
           <ul>
             {documents[key].files.map((file) => {
-              // files[0]: {name: "fakeFile.pdf", initialName: "fakeFile.pdf", size: 10000, type: "application/pdf", url: "https://www.fake-url.com", …}
               if (file.error) {
                 return (
                   <li key={file.name}>
