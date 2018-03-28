632ebbfa3fb02a75c6325ce2d507502abc974548
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-03-28 11:33:06 +0200

    EP-79-80-81 Remove old dashboard and set structure for new one

---
+++ microservices/app/imports/ui/pages/DashboardPage/DashboardRecap/DashboardRecap.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const DashboardRecap = props => <div>Hello World</div>;
+
+DashboardRecap.propTypes = {};
+
+export default DashboardRecap;
