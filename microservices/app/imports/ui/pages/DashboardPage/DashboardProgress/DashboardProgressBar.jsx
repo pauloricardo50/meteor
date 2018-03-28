38f408759b59cd124481aede6fe90a84179a1611
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-03-28 11:18:08 +0200

    EP-79 First version of dashboard progress

--- microservices/app/imports/ui/pages/DashboardPage/DashboardProgress/DashboardProgressBar.jsx
+++ microservices/app/imports/ui/pages/DashboardPage/DashboardProgress/DashboardProgressBar.jsx
@@ -5,19 +5,22 @@ import DashboardProgressBarStep from './DashboardProgressBarStep';
 
 const DashboardProgressBar = ({ steps, currentStep }) => (
   <div className="dashboard-progress-bar">
-    {steps.map(step => (
-      <DashboardProgressBarStep
-        isDone={currentStep > step.nb}
-        isCurrentStep={currentStep === step.nb}
-        step={step}
-        key={step.nb}
-      />
-    ))}
-    <div className="absolute-lines">
+    <div className="steps">
+      {steps.map((step, index) => (
+        <DashboardProgressBarStep
+          isDone={currentStep > step.nb}
+          isCurrentStep={currentStep === step.nb}
+          step={step}
+          key={step.nb}
+          label={step.title}
+        />
+      ))}
+    </div>
+    {/* <div className="absolute-lines">
       {steps
         .slice(0, -1)
         .map((_, index) => <span className="line" key={index} />)}
-    </div>
+      </div> */}
   </div>
 );
 
--- microservices/app/imports/ui/pages/DashboardPage/DashboardProgress/DashboardProgressContainer.jsx
+++ microservices/app/imports/ui/pages/DashboardPage/DashboardProgress/DashboardProgressContainer.jsx
@@ -6,7 +6,13 @@ export default compose(
   withRouter,
   createContainer(({ loan, borrowers, property }) => {
     const steps = getSteps({ loan, borrowers, property });
-    const nextItem = steps[loan.logic.step - 1].items.find(subStep => !subStep.isDone());
+    let nextItem = steps[loan.logic.step - 1].items.find(subStep => !subStep.isDone());
+
+    if (!nextItem) {
+      // If all items are done, provide the last item of the step, and let
+      // the user click on next step in ProcessPageBar
+      nextItem = steps[loan.logic.step - 1].items.pop();
+    }
 
     return {
       steps,
