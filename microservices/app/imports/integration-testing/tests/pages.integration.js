b5bf6e8318e3b3d031c609067ac21adf311877d8
claudiut <claudiut@users.noreply.github.com>
2018-04-03 18:06:24 +0300

    EP-13 Add most of the App’s routes.

--- microservices/app/imports/integration-testing/tests/pages.integration.js
+++ microservices/app/imports/integration-testing/tests/pages.integration.js
@@ -2,10 +2,22 @@ import { expect } from 'chai';
 
 let appData;
 const appPages = {
-  Login: () => '/login',
-  App: () => '/',
+  Login: '/login',
+  App: '/',
   Loan: ({ loans }) => `/loans/${loans[0]._id}`,
   'Loan Contract': ({ loans }) => `/loans/${loans[0]._id}/contract`,
+  'Loan File': ({ loans }) => `/loans/${loans[0]._id}/files`,
+  'Loan Property': ({ loans }) => `/loans/${loans[0]._id}/property`,
+  'Loan Finance': ({ loans }) => `/loans/${loans[0]._id}/finance`,
+  'Loan Verification': ({ loans }) => `/loans/${loans[0]._id}/verification`,
+  'Loan Structure': ({ loans }) => `/loans/${loans[0]._id}/structure`,
+  'Loan Auction': ({ loans }) => `/loans/${loans[0]._id}/auction`,
+  'Loan Strategy': ({ loans }) => `/loans/${loans[0]._id}/strategy`,
+  'Loan Offerpicker': ({ loans }) => `/loans/${loans[0]._id}/offerpicker`,
+  'Loan Closing': ({ loans }) => `/loans/${loans[0]._id}/closing`,
+  'Add Loan': ({ loans }) => `/add-loan/${loans[0]._id}`,
+  Dev: '/dev',
+  Profile: '/profile',
 };
 const publicPages = ['Login'];
 
@@ -31,7 +43,10 @@ describe('App Pages', () => {
           cy.meteorLogin();
         }
 
-        const pageUri = appPages[pageName](appData);
+        const pageUri =
+          typeof appPages[pageName] === 'function'
+            ? appPages[pageName](appData)
+            : appPages[pageName];
 
         cy
           .visit(pageUri)
