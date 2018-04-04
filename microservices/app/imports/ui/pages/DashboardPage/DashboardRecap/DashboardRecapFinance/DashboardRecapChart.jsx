d803fe2299368cb2aa2854fc50882406a783b1ac
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-04-04 13:32:35 +0200

    EP-80 Plug real values into expenses chart

--- microservices/app/imports/ui/pages/DashboardPage/DashboardRecap/DashboardRecapFinance/DashboardRecapChart.jsx
+++ microservices/app/imports/ui/pages/DashboardPage/DashboardRecap/DashboardRecapFinance/DashboardRecapChart.jsx
@@ -3,24 +3,47 @@ import PropTypes from 'prop-types';
 
 import DonutChart from 'core/components/charts/DonutChart';
 import { getBorrowerIncome } from 'core/utils/borrowerFunctions';
-
+import { T } from 'core/components/Translation';
+import { getInterests, getAmortization } from 'core/utils/finance-math';
+import { getInterestsWithOffer } from 'core/utils/loanFunctions';
 import DashboardRecapChartInfo from './DashboardRecapChartInfo';
 import DashboardRecapChartLegend from './DashboardRecapChartLegend';
 
 const getRevenuePercent = (total, monthlyIncome) =>
   (100 * total / monthlyIncome).toFixed(2);
 
-const DashboardRecapChart = (props) => {
-  const data = [
-    { id: 'general.interests', value: 800 },
-    { id: 'general.amortization', value: 800 },
+const getChartData = (props) => {
+  const { loan, offer, interestRate } = props;
+  let interests = 0;
+
+  if (offer) {
+    interests = getInterestsWithOffer(props, false);
+  } else {
+    interests = getInterests(props, interestRate);
+  }
+
+  return [
+    {
+      id: 'general.interests',
+      value: interests,
+    },
+    {
+      id: 'general.amortization',
+      value: getAmortization(props).amortization,
+    },
     { id: 'general.buildingMaintenance', value: 800 },
-  ];
+  ].map(dataPoint => ({ ...dataPoint, value: Math.round(dataPoint.value) }));
+};
+
+const DashboardRecapChart = (props) => {
+  const data = getChartData(props);
   const total = data.reduce((sum, item) => sum + item.value, 0);
   const monthlyIncome = getBorrowerIncome({ borrowers: props.borrowers }) / 12;
   return (
     <div className="card-bottom dashboard-recap-chart">
-      <h3>Coût mensuel</h3>
+      <h3>
+        <T id="DashboardRecapChart.title" />
+      </h3>
 
       <div className="chart">
         <DonutChart
