2cdb295f44408f3f97e08fafae075907fc9ea2ba
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-04-05 13:58:47 +0200

    Strip unnecessary code

--- microservices/admin/imports/ui/pages/AdminProfilePage/AdminProfilePage.jsx
+++ microservices/admin/imports/ui/pages/AdminProfilePage/AdminProfilePage.jsx
@@ -1,13 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import SingleUserPage from '../SingleUserPage';
 
-const AdminProfilePage = props => (
+const AdminProfilePage = () => (
   <SingleUserPage match={{ params: { userId: Meteor.userId() } }} />
 );
-
-AdminProfilePage.propTypes = {};
 
 export default AdminProfilePage;
