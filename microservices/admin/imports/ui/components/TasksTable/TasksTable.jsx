b291717dd5fa555cc7408a0c9c24d5eed50af971
andraLazariuc <andra.lazariuc@yahoo.com>
2018-04-11 19:06:01 +0300

    EP-70 EP-99 Immplement changes from git reviews

--- microservices/admin/imports/ui/components/TasksTable/TasksTable.jsx
+++ microservices/admin/imports/ui/components/TasksTable/TasksTable.jsx
@@ -3,12 +3,16 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
+
 import Table from 'core/components/Table';
 import { T } from 'core/components/Translation';
+import { getBorrowerFullName } from 'core/utils/borrowerFunctions';
+import IconLink from 'core/components/IconLink';
+import Loading from 'core/components/Loading';
 import TaskAssignDropdown
   from '../../components/AssignAdminDropdown/TaskAssignDropdown';
 import TasksStatusDropdown from './TasksStatusDropdown';
-import LinkToDoc from '../LinkToDoc';
+
 
 const styles = {
   dropdownButtons: { display: 'inline', width: '50%' },
@@ -18,6 +22,54 @@ const formatDateTime = date =>
   moment(date).format('D MMM YY à HH:mm:ss');
 
 export default class TasksTable extends Component {
+  getRelatedDoc = ({ borrower, loan, property, user }) => {
+    if (borrower) {
+      const { _id, firstName, lastName } = borrower;
+
+      return {
+        link: `/borrowers/${_id}`,
+        icon: 'people',
+        text: getBorrowerFullName({ firstName, lastName }),
+        translationId: 'borrower',
+      };
+    }
+
+    if (loan) {
+      const { _id, name } = loan;
+
+      return {
+        link: `/loans/${_id}`,
+        icon: 'dollarSign',
+        text: name,
+        translationId: 'loan',
+      };
+    }
+
+    if (property) {
+      const { _id, address1 } = property;
+
+      return {
+        link: `/properties/${_id}`,
+        icon: 'building',
+        text: address1,
+        translationId: 'property',
+      };
+    }
+
+    if (user) {
+      const { _id, username, emails } = user;
+
+      return {
+        link: `/users/${_id}`,
+        icon: 'contactMail',
+        text: username || emails[0].address,
+        translationId: 'user',
+      };
+    }
+
+    return {};
+  };
+
   getColumnOptions = ({ showAssignee }) => {
     const columnOptions = [
       { id: '#', style: { width: 32, textAlign: 'left' } },
@@ -39,7 +91,7 @@ export default class TasksTable extends Component {
     return columnOptions;
   };
 
-  getColumns = ({ props, index, task }) => {
+  getColumns = ({ showAssignee, index, task }) => {
     const { type,
       status,
       createdAt,
@@ -51,6 +103,25 @@ export default class TasksTable extends Component {
       property,
       loan,
       assignedEmployee } = task;
+
+    const { link, icon, text, translationId } = this.getRelatedDoc({
+      borrower,
+      loan,
+      property,
+      user,
+    });
+
+    const relatedDoc = {
+      label: (
+        <IconLink
+          link={link}
+          icon={icon}
+          text={text || translationId}
+        />
+      ),
+      raw: text,
+    };
+
     const columns = [
       index + 1,
       <T id={`TasksStatusDropdown.${type}`} key="type" />,
@@ -59,71 +130,64 @@ export default class TasksTable extends Component {
       formatDateTime(updatedAt),
       formatDateTime(dueAt),
       formatDateTime(completedAt),
-      <LinkToDoc
-        loan={loan}
-        borrower={borrower}
-        property={property}
-        user={user}
-        key="relatedDoc"
-      />,
+      relatedDoc,
     ];
-    if (props.showAssignee) {
-      columns.push(assignedEmployee &&
-        <Link to={`/users/${assignedEmployee._id}`} >
-          {assignedEmployee.username || assignedEmployee.emails[0].address}
-        </Link>);
+    if (showAssignee) {
+      if (assignedEmployee) {
+        const cellText = assignedEmployee.username ||
+        assignedEmployee.emails[0].address;
+        columns.push({
+          label: <Link to={`/users/${assignedEmployee._id}`} >{cellText}</Link>,
+          raw: cellText,
+        });
+      } else {
+        columns.push('');
+      }
     }
 
-    columns.push(
-      <div style={{ display: 'flex' }}>
-        <TasksStatusDropdown
-          {...props}
-          currentUser={Meteor.user()}
-          taskId={task._id}
-          taskStatus={task.status}
-          styles={styles.dropdownButtons}
-        />
-        <TaskAssignDropdown doc={task} styles={styles.dropdownButtons} />
-      </div>);
+    columns.push(<div style={{ display: 'flex' }}>
+      <TasksStatusDropdown
+        currentUser={Meteor.user()}
+        taskId={task._id}
+        taskStatus={task.status}
+        styles={styles.dropdownButtons}
+      />
+      <TaskAssignDropdown doc={task} styles={styles.dropdownButtons} />
+                 </div>);
 
     return columns;
   };
 
-  setupRows = (props) => {
-    const tasks = props.data;
+  setupRows = ({ data, showAssignee }) =>
+    data.map((task, index) => ({
+      id: task._id,
+      columns: this.getColumns({ showAssignee, index, task }),
+    }));
 
-    if (tasks && tasks.length) {
-      this.rows = tasks.map((task, index) => ({
-        id: task._id,
-        columns: this.getColumns({
-          props,
-          index,
-          task,
-        }),
-      }));
-
-      return this.rows;
-    }
-
-    return [];
-  };
 
   render() {
-    const { isLoading } = this.props;
+    const { data, isLoading, showAssignee } = this.props;
 
-    if (!isLoading) {
-      return (
-        <Table
-          columnOptions={this.getColumnOptions(this.props)}
-          rows={this.setupRows(this.props)}
-          noIntl
-        />
-      );
+    if (isLoading) {
+      return <Loading />;
     }
-    return null;
+
+    return (
+      <Table
+        columnOptions={this.getColumnOptions({ showAssignee })}
+        rows={this.setupRows({ data, showAssignee })}
+        noIntl
+      />
+    );
   }
 }
 
 TasksTable.propTypes = {
   isLoading: PropTypes.bool.isRequired,
+  data: PropTypes.object.isRequired,
+  showAssignee: PropTypes.bool,
+};
+
+TasksTable.defaultProps = {
+  showAssignee: false,
 };
