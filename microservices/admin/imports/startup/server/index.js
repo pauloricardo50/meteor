e7ce7c96071cb437c7363b71314e9b942ea5f803
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-04-12 15:08:45 +0200

    Remove old loadingText

--- microservices/admin/imports/startup/server/index.js
+++ microservices/admin/imports/startup/server/index.js
@@ -10,20 +10,8 @@ import 'core/api/links';
 import '../accounts-config';
 
 Meteor.startup(() => {
-  // Do something on startup if necessary
-  // Add password protection
-  // if (Meteor.settings.public.environment === 'staging') {
-  //   setupAuth();
-  // }
-
   jc.startJobServer();
 });
 
 // Inject a loader before client is ready, is removed in the on startup function on the client
 Inject.rawHead('loader', Assets.getText('loader.html'));
-
-// Make sure this is passed a function, so that it gets random strings for
-// every client, instead of once on server-startup
-
-// FIXME: This gets picked up by google crawlers..! Very bad
-// Inject.rawHead('loadingText', getHtml);
