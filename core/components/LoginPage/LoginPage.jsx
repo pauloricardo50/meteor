9d20cbba9f4042d3d731959755a01c1929212264
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-04-19 12:40:15 +0200

    Adjust styles

--- core/components/LoginPage/LoginPage.jsx
+++ core/components/LoginPage/LoginPage.jsx
@@ -9,40 +9,29 @@ import { addUserTracking } from '../../utils/analytics';
 import { T } from '../Translation';
 import Accounts from './Accounts';
 
-const styles = {
-  section: {
-    width: '100%',
-    minHeight: 'calc(100% - 64px)',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    overflowY: 'scroll',
-    padding: '0 16px',
-  },
-};
-
 const LoginPage = ({ location: { search }, history: { push } }) => {
   const { path } = queryString.parse(search);
 
   return (
-    <section style={styles.section}>
-      <h1>
-        <T id="LoginPage.title" />
-      </h1>
-      <Accounts.ui.LoginForm
-        onSignedInHook={() => push(path || '/')}
-        onPostSignUpHook={() => {
-          push(path || '/');
-          sendVerificationLink.run({});
+    <section className="login-page">
+      <div>
+        <h1>
+          <T id="LoginPage.title" />
+        </h1>
+        <Accounts.ui.LoginForm
+          onSignedInHook={() => push(path || '/')}
+          onPostSignUpHook={() => {
+            push(path || '/');
+            sendVerificationLink.run({});
 
-          // Create user for analytics
-          addUserTracking(Meteor.userId(), {
-            email: Meteor.user().emails[0].address,
-            id: Meteor.userId(),
-          });
-        }}
-      />
+            // Create user for analytics
+            addUserTracking(Meteor.userId(), {
+              email: Meteor.user().emails[0].address,
+              id: Meteor.userId(),
+            });
+          }}
+        />
+      </div>
     </section>
   );
 };
