94780bb9be526d7b428f03898fba35b014171653
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-04-20 23:27:01 +0200

    Add some SEO to HomePage

--- microservices/www/imports/ui/pages/HomePage/HomePageDescription/HomePageDescription.jsx
+++ microservices/www/imports/ui/pages/HomePage/HomePageDescription/HomePageDescription.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { T } from 'core/components/Translation';
 import HomePageProgression from './HomePageProgression';
 import DescriptionItem from './DescriptionItem';
+import HomePageDescriptionSeo from './HomePageDescriptionSeo';
 
 const steps = [
   { id: 'eligibility' },
@@ -19,9 +20,7 @@ const HomePageDescription = () => (
         <T id="HomePageDescription.title" />
       </h2>
     </b>
-    <p className="description">
-      <T id="HomePageDescription.description" />
-    </p>
+    <HomePageDescriptionSeo />
     <div className="steps">
       {steps.map((step, index) => (
         <DescriptionItem nb={index + 1} key={step.id} step={step} />
