035eb88b69e03d55def78007feecd099f9ff3110
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-04-23 15:31:34 +0200

    Add lender count to widget 1

---
+++ microservices/www/imports/ui/pages/Widget1Page/Widget1Recap/Widget1LenderCount.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { T } from 'core/components/Translation';
+import { WARNING, ERROR, SUCCESS } from 'core/api/constants';
+
+const getLenderCount = (borrowStatus, incomeStatus) => {
+  if (borrowStatus !== ERROR && incomeStatus === SUCCESS) {
+    return 15;
+  } else if (incomeStatus === WARNING) {
+    return 4;
+  }
+  return '-';
+};
+
+const Widget1LenderCount = ({
+  borrowRule: { status: borrowStatus },
+  incomeRule: { status: incomeStatus },
+}) => {
+  const count = getLenderCount(borrowStatus, incomeStatus);
+
+  return (
+    <div className="widget1-lender-count">
+      <T id="Widget1LenderCount.label" />
+      <span>{count}</span>
+    </div>
+  );
+};
+
+Widget1LenderCount.propTypes = {
+  borrowRule: PropTypes.object.isRequired,
+  incomeRule: PropTypes.object.isRequired,
+};
+
+export default Widget1LenderCount;
