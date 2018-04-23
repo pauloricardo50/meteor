bf605c89b4594fcdc52ad560f51160bd8f3f3df0
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-04-23 20:30:05 +0200

    Only shuffle reviews once and not on every render

--- microservices/www/imports/ui/pages/HomePage/HomePageReviews/HomePageReviewsCarousel/HomePageReviewsCarousel.jsx
+++ microservices/www/imports/ui/pages/HomePage/HomePageReviews/HomePageReviewsCarousel/HomePageReviewsCarousel.jsx
@@ -9,6 +9,7 @@ import reviews from './reviews';
 const NEXT = 'NEXT';
 
 const shuffleArray = array => array.sort(() => 0.5 - Math.random());
+const shuffledReviews = shuffleArray(reviews);
 
 export default class HomePageReviewsCarousel extends Component {
   constructor(props) {
@@ -34,10 +35,7 @@ export default class HomePageReviewsCarousel extends Component {
         <Button onClick={this.handleClick} variant="fab" color="primary">
           <Icon type="left" />
         </Button>
-        <MotionCarousel
-          reviews={shuffleArray(reviews)}
-          currentIndex={currentIndex}
-        />
+        <MotionCarousel reviews={shuffledReviews} currentIndex={currentIndex} />
         <Button
           onClick={() => this.handleClick(NEXT)}
           variant="fab"
