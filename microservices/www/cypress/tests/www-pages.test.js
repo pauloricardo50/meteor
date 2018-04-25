a3728164267340beecadde6788b01ce79b30d688
claudiut <claudiut@users.noreply.github.com>
2018-04-25 16:33:00 +0300

    EP-75 Add end to end tests for all www routes, except the ones excluded in the LITE_VERSION feature.

--- microservices/www/cypress/tests/www-pages.test.js
+++ microservices/www/cypress/tests/www-pages.test.js
@@ -1,5 +1,11 @@
 const pages = {
   Home: '/',
+  Widget1: '/start/1',
+  About: '/about',
+  faq: '/faq',
+  Contact: '/contact',
+  Conditions: '/conditions',
+  'Not Found': '/an-inexistent-page',
 };
 
 describe('Www Pages', () => {
