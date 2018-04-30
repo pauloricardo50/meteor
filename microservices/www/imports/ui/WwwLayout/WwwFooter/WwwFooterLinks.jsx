8fa5b3513d62e5f8da2e1440e3c4c60a53d1ce6c
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-04-30 12:23:49 +0200

    Update togglePoint to use functional syntax

--- microservices/www/imports/ui/WwwLayout/WwwFooter/WwwFooterLinks.jsx
+++ microservices/www/imports/ui/WwwLayout/WwwFooter/WwwFooterLinks.jsx
@@ -4,14 +4,9 @@ import { Link } from 'react-router-dom';
 import { T } from 'core/components/Translation';
 import togglePoint, { TOGGLE_POINTS } from 'core/api/features/togglePoint';
 
-// const infoLinks = ['faq', 'contact', 'conditions'];
-const infoLinks = [
-  'contact',
-  ...togglePoint({
-    id: TOGGLE_POINTS.ROUTES_CONFIG_STRIPPED_IN_LITE_VERSION,
-    code: ['about', 'faq', 'conditions'],
-  }),
-];
+const modifier = togglePoint(TOGGLE_POINTS.ROUTES_CONFIG_STRIPPED_IN_LITE_VERSION);
+
+const infoLinks = ['contact', ...modifier(['about', 'faq', 'conditions'])];
 
 const WwwFooterLinks = () => (
   <div className="www-footer-links">
