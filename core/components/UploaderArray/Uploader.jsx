e47b32a51fafca6422d42d9afd31bbdac87da128
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-04-30 13:38:37 +0200

    Simplify Uploader component

--- core/components/UploaderArray/Uploader.jsx
+++ core/components/UploaderArray/Uploader.jsx
@@ -1,10 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl } from 'react-intl';
-
-import bert from 'core/utils/bert';
-import { allowedFileTypes, maxSize } from 'core/api/files/meteor-slingshot';
-import { FILE_STATUS } from '../../api/constants';
 
 import UploaderContainer from './UploaderContainer';
 import Title from './Title';
@@ -13,162 +9,68 @@ import TempFile from './TempFile';
 import FileAdder from './FileAdder';
 import FileDropper from './FileDropper.jsx';
 
-const checkFile = (file) => {
-  if (allowedFileTypes.indexOf(file.type) < 0) {
-    return 'fileType';
-  } else if (file.size > maxSize) {
-    return 'fileSize';
-  }
-  return true;
-};
+const Uploader = ({
+  fileMeta,
+  currentValue,
+  disabled,
+  docId,
+  collection,
+  userIsAdmin,
+  isOwnedByAdmin,
+  removeDocument,
+  tempFiles,
+  shouldDisableAdd,
+  handleAddFiles,
+  handleRemove,
+  handleSave,
+}) => {
+  const { id, uploadCount } = fileMeta;
+  const disableAdd = shouldDisableAdd();
 
-class Uploader extends Component {
-  constructor(props) {
-    super(props);
+  return (
+    <FileDropper
+      className="uploader"
+      handleAddFiles={handleAddFiles}
+      disabled={disableAdd}
+    >
+      <Title
+        {...fileMeta}
+        currentValue={currentValue}
+        userIsAdmin={userIsAdmin}
+        isOwnedByAdmin={isOwnedByAdmin}
+        removeDocument={removeDocument}
+      />
 
-    this.state = { tempFiles: [] };
-  }
-
-  // Remove temp files from state when they are saved to the DB, and appear in
-  // props.
-  // FIXME: This prevents someone from uploading a file with the same name twice
-  componentWillReceiveProps(nextProps) {
-    const { currentValue: nextValue } = nextProps;
-    const { currentValue } = this.props;
-
-    if (nextValue.length !== currentValue.length) {
-      const { tempFiles } = this.state;
-
-      if (tempFiles && tempFiles.length) {
-        nextValue.forEach((file) => {
-          tempFiles.forEach((temp) => {
-            if (temp.name === file.initialName) {
-              this.setState(prev => ({
-                tempFiles: prev.tempFiles.filter(f => f.name !== file.initialName),
-              }));
-            }
-          });
-        });
-      }
-    }
-  }
-
-  handleAddFiles = (files = []) => {
-    const fileArray = [];
-    let showError = false;
-
-    files.forEach((file) => {
-      const isValid = checkFile(file);
-      if (isValid === true) {
-        fileArray.push(file);
-      } else {
-        showError = isValid;
-      }
-    });
-
-    if (showError) {
-      const { intl } = this.props;
-      const { formatMessage: f } = intl;
-      bert(
-        f({ id: `error.${showError}.title` }),
-        f({ id: `error.${showError}.description` }),
-        'danger',
-      );
-      return;
-    }
-
-    this.setState(prev => ({ tempFiles: [...prev.tempFiles, ...files] }));
-  };
-
-  handleSave = (file, downloadUrl) => {
-    const { addFileToDoc } = this.props;
-
-    addFileToDoc({
-      initialName: file.name,
-      size: file.size,
-      type: file.type,
-      url: encodeURI(downloadUrl), // To avoid spaces and unallowed chars
-      key: downloadUrl.split('amazonaws.com/')[1],
-    });
-  };
-
-  handleRemove = (key) => {
-    const { deleteFile } = this.props;
-
-    deleteFile(key);
-  };
-
-  // If one of the files has an error, allow uploading even if form is disabled
-  shouldDisableAdd = () =>
-    this.props.currentValue.reduce(
-      (acc, f) => !(f.status === FILE_STATUS.ERROR),
-      true,
-    ) && this.props.disabled;
-
-  render() {
-    const {
-      fileMeta,
-      currentValue,
-      disabled,
-      docId,
-      collection,
-      userIsAdmin,
-      isOwnedByAdmin,
-      removeDocument,
-    } = this.props;
-    const { tempFiles } = this.state;
-    const { id, uploadCount } = fileMeta;
-    const disableAdd = this.shouldDisableAdd();
-
-    return (
-      <FileDropper
-        className="uploader"
-        handleAddFiles={this.handleAddFiles}
-        disabled={disableAdd}
-      >
-        <Title
-          {...fileMeta}
-          currentValue={currentValue}
-          userIsAdmin={userIsAdmin}
-          isOwnedByAdmin={isOwnedByAdmin}
-          removeDocument={removeDocument}
-        />
-
-        {currentValue
-          .sort((a, b) => a.fileCount > b.fileCount)
-          .map(f => (
-            <File
-              key={f.key}
-              file={f}
-              disabled={disabled}
-              handleRemove={this.handleRemove}
-            />
-          ))}
-
-        {tempFiles.map((f, i) => (
-          <TempFile
+      {currentValue
+        .sort((a, b) => a.fileCount > b.fileCount)
+        .map(f => (
+          <File
+            key={f.key}
             file={f}
-            key={f.name + i} // if the same file is uploaded twice there's a conflict
-            docId={docId}
-            collection={collection}
-            handleSave={this.handleSave}
-            id={id}
-            currentValue={currentValue}
-            uploadCount={uploadCount}
+            disabled={disabled}
+            handleRemove={handleRemove}
           />
         ))}
 
-        {!disableAdd && (
-          <FileAdder
-            id={fileMeta.id}
-            handleAddFiles={this.handleAddFiles}
-            docId={docId}
-          />
-        )}
-      </FileDropper>
-    );
-  }
-}
+      {tempFiles.map((f, i) => (
+        <TempFile
+          file={f}
+          key={f.name + i} // if the same file is uploaded twice there's a conflict
+          docId={docId}
+          collection={collection}
+          handleSave={handleSave}
+          id={id}
+          currentValue={currentValue}
+          uploadCount={uploadCount}
+        />
+      ))}
+
+      {!disableAdd && (
+        <FileAdder id={id} handleAddFiles={handleAddFiles} docId={docId} />
+      )}
+    </FileDropper>
+  );
+};
 
 Uploader.propTypes = {
   fileMeta: PropTypes.objectOf(PropTypes.any).isRequired,
@@ -181,6 +83,11 @@ Uploader.propTypes = {
   userIsAdmin: PropTypes.bool.isRequired,
   isOwnedByAdmin: PropTypes.bool.isRequired,
   removeDocument: PropTypes.func.isRequired,
+  tempFiles: PropTypes.array.isRequired,
+  shouldDisableAdd: PropTypes.func.isRequired,
+  handleAddFiles: PropTypes.func.isRequired,
+  handleSave: PropTypes.func.isRequired,
+  handleRemove: PropTypes.func.isRequired,
 };
 
 Uploader.defaultProps = {
