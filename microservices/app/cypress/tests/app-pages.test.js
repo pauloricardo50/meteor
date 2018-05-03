d680ec7ec6ed94b535842201b99afb5d2de7cbf6
claudiut <claudiut@users.noreply.github.com>
2018-05-03 15:59:41 +0300

    EP-153 Add tests for all pages in app.

--- microservices/app/cypress/tests/app-pages.test.js
+++ microservices/app/cypress/tests/app-pages.test.js
@@ -1,69 +1,88 @@
+import capitalize from 'lodash/capitalize';
+
 let testData;
 const pages = {
-  Login: '/login',
-  'Reset Password': '/reset-password/fakeToken',
-  'Enroll Account': '/enroll-account/fakeToken',
-  'Verify Email': '/verify-email/fakeToken',
-  
-  App: '/',
-  Profile: '/profile',
+  public: {
+    Login: '/login',
+    'Reset Password': '/reset-password/fakeToken',
+    'Enroll Account': '/enroll-account/fakeToken',
+    Impersonate: ({ userId, adminLoginToken }) =>
+      `/impersonate?userId=${userId}&authToken=${adminLoginToken}`,
+  },
 
-  Loan: ({ _id }) => `/loans/${_id}`,
-  'Loan Files': ({ _id }) => `/loans/${_id}/files`,
-  'Loan Property': ({ _id }) => `/loans/${_id}/property`,
-  'Loan Verification': ({ _id }) => `/loans/${_id}/verification`,
-  'Loan Structure': ({ _id }) => `/loans/${_id}/structure`,
-  'Loan Auction': ({ _id }) => `/loans/${_id}/auction`,
-  'Loan Strategy': ({ _id }) => `/loans/${_id}/strategy`,
-  'Loan Offerpicker': ({ _id }) => `/loans/${_id}/offerpicker`,
-  'Loan Contract': ({ _id }) => `/loans/${_id}/contract`,
-  'Loan Closing': ({ _id }) => `/loans/${_id}/closing`,
+  user: {
+    App: '/',
+    Profile: '/profile',
 
-  'Loan Borrower Personal': ({ _id, borrowers }) =>
-    `/loans/${_id}/borrowers/${borrowers[0]._id}/personal`,
-  'Loan Borrower Finance': ({ _id, borrowers }) =>
-    `/loans/${_id}/borrowers/${borrowers[0]._id}/finance`,
-  'Loan Borrower Files': ({ _id, borrowers }) =>
-    `/loans/${_id}/borrowers/${borrowers[0]._id}/files`,
+    'Verify Email (Invalid Token)': '/verify-email/fakeToken',
+    'Verify Email (Valid Token)': '/verify-email/user1VerificationToken',
 
-  'Not Found': '/a-page-that-does-not-exist',
+    Loan: ({ step3Loan: { _id } }) => `/loans/${_id}`,
+    'Add Loan': ({ unownedLoan: { _id } }) => `/add-loan/${_id}`,
+    'Loan Files': ({ step3Loan: { _id } }) => `/loans/${_id}/files`,
+    'Loan Property': ({ step3Loan: { _id } }) => `/loans/${_id}/property`,
+    //
+    'Loan Finance': ({ step3Loan: { _id } }) => `/loans/${_id}/finance`,
+    'Loan Verification': ({ step3Loan: { _id } }) =>
+      `/loans/${_id}/verification`,
+    'Loan Structure': ({ step3Loan: { _id } }) => `/loans/${_id}/structure`,
+    'Loan Auction': ({ step3Loan: { _id } }) => `/loans/${_id}/auction`,
+    'Loan Strategy': ({ step3Loan: { _id } }) => `/loans/${_id}/strategy`,
+    //
+    'Loan Offerpicker': ({ step3Loan: { _id } }) => `/loans/${_id}/offerpicker`,
+    'Loan Contract': ({ step3Loan: { _id } }) => `/loans/${_id}/contract`,
+    'Loan Closing': ({ step3Loan: { _id } }) => `/loans/${_id}/closing`,
+
+    'Borrower Personal': ({ step3Loan: { _id, borrowers } }) =>
+      `/loans/${_id}/borrowers/${borrowers[0]._id}/personal`,
+    'Borrower Finance': ({ step3Loan: { _id, borrowers } }) =>
+      `/loans/${_id}/borrowers/${borrowers[0]._id}/finance`,
+    'Borrower Files': ({ step3Loan: { _id, borrowers } }) =>
+      `/loans/${_id}/borrowers/${borrowers[0]._id}/files`,
+
+    'Not Found': '/a-page-that-does-not-exist',
+  },
+
+  dev: {
+    Dev: '/dev',
+  },
 };
-const publicPages = ['Login'];
 
 describe('App Pages', () => {
   before(() => {
     cy
       .eraseAndGenerateTestData()
-      .meteorLogoutAndLogin()
       .getTestData()
       .then((data) => {
         testData = data;
       });
   });
 
-  Object.keys(pages).forEach((pageName) => {
-    describe(`${pageName} Page`, () => {
-      it('should render', () => {
-        /**
-         * we login every time, as it seems that we're logged out again
-         * in each test, probably because a new window instance is
-         * used for every test, which results in us using new Meteor instance in every test
-         */
-        if (publicPages.includes(pageName)) {
-          cy.meteorLogout();
-        } else {
-          cy.meteorLogoutAndLogin();
-        }
+  Object.keys(pages).forEach((pageAuthentication) => {
+    describe(`${capitalize(pageAuthentication)} Pages`, () => {
+      Object.keys(pages[pageAuthentication]).forEach((pageName) => {
+        describe(`${pageName} Page`, () => {
+          it('should render', () => {
+            /**
+             * we login every time, as it seems that we're logged out again
+             * in each test, probably because a new window instance is
+             * used for every test, which results in us using new Meteor instance in every test
+             */
+            if (pageAuthentication === 'public') {
+              cy.meteorLogout();
+            } else {
+              cy.meteorLogoutAndLogin(`${pageAuthentication}-1@e-potek.ch`);
+            }
 
-        const pageUri =
-          typeof pages[pageName] === 'function'
-            ? pages[pageName](testData)
-            : pages[pageName];
+            const uri = pages[pageAuthentication][pageName];
+            const pageUri = typeof uri === 'function' ? uri(testData) : uri;
 
-        cy
-          .visit(pageUri)
-          .waitUntilLoads()
-          .shouldRenderWithoutErrors(pageUri);
+            cy
+              .visit(pageUri)
+              .waitUntilLoads()
+              .shouldRenderWithoutErrors(pageUri);
+          });
+        });
       });
     });
   });
