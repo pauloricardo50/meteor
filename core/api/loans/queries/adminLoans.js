353705373a6217be95d636ab668d6b12ae7ff006
claudiut <claudiut@users.noreply.github.com>
2018-05-04 12:47:02 +0300

    EP-158 Add the ability to filter admin loans by step and if it’s owned.

--- core/api/loans/queries/adminLoans.js
+++ core/api/loans/queries/adminLoans.js
@@ -4,9 +4,17 @@ import { LOAN_QUERIES } from '../loanConstants';
 import { createSearchFilters } from '../../helpers/mongoHelpers';
 
 export default Loans.createQuery(LOAN_QUERIES.ADMIN_LOANS, {
-  $filter({ filters, params: { searchQuery } }) {
+  $filter({ filters, params: { searchQuery, step, owned } }) {
     if (searchQuery) {
       Object.assign(filters, createSearchFilters(['name'], searchQuery));
+    }
+
+    if (step) {
+      filters['logic.step'] = step;
+    }
+
+    if (owned) {
+      filters.userId = { $exists: true };
     }
   },
   $options: {
