ca3569fa6c7ceaec5177c5eebabbc0aa257c644d
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-05-08 10:59:28 +0200

    Add InterestsPage boilerplate

---
+++ microservices/www/imports/ui/pages/InterestsPage/InterestsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const InterestsPage = props => (
+  <section className="interests-page">Hello World</section>
+);
+
+InterestsPage.propTypes = {};
+
+export default InterestsPage;
