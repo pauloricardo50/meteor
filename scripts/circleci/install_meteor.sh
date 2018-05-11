1c23e57ef778c12e0cfe535dbff9f2dc4307e94c
claudiut <claudiut@users.noreply.github.com>
2018-05-11 09:28:37 +0300

    Add better comments in install_meteor.sh and for now use a local bash variable to hold the meteor version.

--- scripts/circleci/install_meteor.sh
+++ scripts/circleci/install_meteor.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# If the cached meteor directory exists, link its' binary
+# If the cached meteor directory exists, link its binary
 # Else install meteor.
 if [ -e $HOME/.meteor/meteor ]; then
   echo "Meteor directory found. Symlinking binary..."
-  # meteor_path is the 'meteor' command path (if any available)
-  #  or a default binary path
+
+  # `meteor_path`` is the 'meteor' command's path (if any available)
   meteor_path=$(type -p meteor);
 
+  # If the `meteor_path` is empty (meaning 'meteor' command doesn't exist)
+  # a default binary path is set for it
   if [ -z $meteor_path ];
   then
     meteor_path=/usr/local/bin/meteor;
@@ -16,6 +18,7 @@ if [ -e $HOME/.meteor/meteor ]; then
   sudo ln -sf ~/.meteor/meteor $meteor_path;
   echo "Done."
 else
+  METEOR_VERSION=1.6.1.1
   echo "Meteor directory not found. Installing Meteor $METEOR_VERSION..."
   curl https://install.meteor.com?release=$METEOR_VERSION | /bin/sh
 fi
