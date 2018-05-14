f13ce237944e57f149739f3c38c1c77a47ecbb9d
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-05-14 16:53:38 +0200

    Formatting

--- core/utils/loadable.js
+++ core/utils/loadable.js
@@ -1,8 +1,9 @@
 import Loadable from 'react-loadable';
 import Loading from 'core/components/Loading';
 
-export default options => Loadable({
-  loading: Loading,
-  delay: 200, // Hides the loading component for 200ms, to avoid flickering
-  ...options,
-});
+export default options =>
+  Loadable({
+    loading: Loading,
+    delay: 200, // Hides the loading component for 200ms, to avoid flickering
+    ...options,
+  });
