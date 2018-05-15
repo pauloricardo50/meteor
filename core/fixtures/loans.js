81d97d091b8f9f946e3b9bee2b7de447494e567e
claudiut <claudiut@users.noreply.github.com>
2018-05-15 12:49:08 +0300

    Generate 3 step loan fixtures for all 3 auction status types.

--- core/fixtures/loans.js
+++ core/fixtures/loans.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 import LoanService from 'core/api/loans/LoanService';
-import { PURCHASE_TYPE } from 'core/api/loans/loanConstants';
+import { PURCHASE_TYPE, AUCTION_STATUS } from 'core/api/loans/loanConstants';
 import {
   fakeDocument,
   fakeDocumentWithLabel,
@@ -43,7 +43,7 @@ const logic3 = {
     comments: [],
   },
   auction: {
-    status: 'ENDED',
+    status: AUCTION_STATUS.ENDED,
     startTime: new Date(),
     endTime: new Date(),
   },
@@ -89,11 +89,12 @@ const fakeFiles = {
 
 const fakeFiles2 = {};
 
-export const createFakeLoan = (
+export const createFakeLoan = ({
   userId,
   step,
   completeFiles = Math.random() > 0.5,
-) => {
+  auctionStatus = AUCTION_STATUS.NONE,
+}) => {
   const borrowerIds = createFakeBorrowers(userId);
   const propertyId = createFakeProperty(userId);
   const loan = {
@@ -126,6 +127,22 @@ export const createFakeLoan = (
     break;
   default:
     loan.logic = logic1;
+  }
+
+  if (auctionStatus === AUCTION_STATUS.NONE) {
+    loan.logic.auction = {};
+  } else if (auctionStatus === AUCTION_STATUS.STARTED) {
+    loan.logic.auction = {
+      status: AUCTION_STATUS.STARTED,
+      startTime: new Date(Date.now() - 1000),
+      endTime: new Date(Date.now() + 60 * 60 * 1000),
+    };
+  } else if (auctionStatus === AUCTION_STATUS.ENDED) {
+    loan.logic.auction = {
+      status: AUCTION_STATUS.ENDED,
+      startTime: new Date(),
+      endTime: new Date(),
+    };
   }
 
   return LoanService.insert({ loan, userId });
