af6a9ee7fab3fbef2989ff78b790188ecd546b27
claudiut <claudiut@users.noreply.github.com>
2018-05-16 17:28:08 +0300

    EP-180 Since the meteor “binary” (actually symlink) cannot be restored from workspace, symlink the cached meteor using a shell script.

--- scripts/circleci/install_meteor.sh
+++ scripts/circleci/install_meteor.sh
@@ -3,20 +3,7 @@
 # If the cached meteor directory exists, link its binary
 # Else install meteor.
 if [ -e $HOME/.meteor/meteor ]; then
-  echo "Meteor directory found. Symlinking binary..."
-
-  # `METEOR_PATH` is the 'meteor' command's path (if any available)
-  METEOR_PATH=$(type -p meteor);
-
-  # If the `METEOR_PATH` is empty (meaning 'meteor' command doesn't exist)
-  # a default binary path is set for it
-  if [ -z $METEOR_PATH ];
-  then
-    METEOR_PATH=/usr/local/bin/meteor;
-  fi
-
-  sudo ln -sf ~/.meteor/meteor $METEOR_PATH;
-  echo "Done symlinking meteor binary."
+  ./symlink_cached_meteor_binary.sh
 else
   echo "Meteor directory not found. Installing Meteor $METEOR_VERSION..."
   curl https://install.meteor.com?release=$METEOR_VERSION | /bin/sh
---
+++ scripts/circleci/symlink_cached_meteor_binary.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# If the cached meteor directory exists, link its binary.
+if [ -e $HOME/.meteor/meteor ]; then
+  echo "Meteor directory found. Symlinking binary..."
+
+  # `METEOR_PATH` is the 'meteor' command's path (if any available)
+  METEOR_PATH=$(type -p meteor);
+
+  # If the `METEOR_PATH` is empty (meaning 'meteor' command doesn't exist)
+  # a default binary path is set for it
+  if [ -z $METEOR_PATH ];
+  then
+    METEOR_PATH=/usr/local/bin/meteor;
+  fi
+
+  sudo ln -sf ~/.meteor/meteor $METEOR_PATH;
+  echo "Done symlinking meteor binary."
+else
+  echo "Meteor directory not found.";
+fi
