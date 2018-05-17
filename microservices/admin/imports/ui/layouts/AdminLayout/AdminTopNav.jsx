00b6688cfbc587c76f4ecde378c1d42090605210
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-05-17 12:58:39 +0200

    Fix admin top-nav position bug

--- microservices/admin/imports/ui/layouts/AdminLayout/AdminTopNav.jsx
+++ microservices/admin/imports/ui/layouts/AdminLayout/AdminTopNav.jsx
@@ -4,7 +4,7 @@ import SearchIcon from '../../pages/SearchPage/SearchIcon';
 
 const AdminTopNav = props => (
   <div className="admin-top-nav">
-    <TopNav {...props} public={false} >
+    <TopNav {...props} public={false}>
       <SearchIcon />
     </TopNav>
   </div>
