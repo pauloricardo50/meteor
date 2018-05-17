0ee2a77f378830763601f3e889533187bd4fb8b6
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-05-17 17:51:29 +0200

    Adjust details

--- microservices/www/imports/ui/WwwLayout/WwwFooter/WwwFooterCopyright.jsx
+++ microservices/www/imports/ui/WwwLayout/WwwFooter/WwwFooterCopyright.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const WwwFooterCopyright = () => (
   <p className="copyright">
-    Copyright - e-Potek 2018<br />UID: CHE-405.084.029
+    Copyright - e-Potek 2018<br />UID : CHE-405.084.029
   </p>
 );
 
