172a26552004259e936ff609a5066c7829fc7bb7
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-05-23 13:58:07 +0200

    Reorder imports

--- microservices/www/imports/ui/pages/FaqPage/tests/FaqPageFaqs.spec.jsx
+++ microservices/www/imports/ui/pages/FaqPage/tests/FaqPageFaqs.spec.jsx
@@ -1,11 +1,11 @@
 /* eslint-env mocha */
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'core/utils/testHelpers/enzyme';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 
+import { shallow } from 'core/utils/testHelpers/enzyme';
 import { T } from 'core/components/Translation';
 import FaqPageFaqs from '../FaqPageFaqs';
 
