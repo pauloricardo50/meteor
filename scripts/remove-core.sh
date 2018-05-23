32a591b2a6b157755cd893dff31cdf593bb82d86
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-05-23 12:18:20 +0200

    Add script to remove-core temporarily

---
+++ scripts/remove-core.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script removes all copied files from core to the other microservices
+# It's helpful when trying to find code without the duplicates showing up
+
+# Remove all symlinks in the parent directory
+find .. -type l -exec unlink {} \;
+
+for i in 'admin' 'app' 'lender' 'www'
+  do
+    ( cd ../microservices/$i && rm -rf ./public && rm -rf ./private );
+  done
