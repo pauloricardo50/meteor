733aabc73a64a38cc7f78dd32e32e5ea5d2fa4d9
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-05-24 21:08:28 +0200

    Adjust styles to be consistent with FAQ

--- microservices/www/imports/ui/pages/CareersPage/CareersPageList.jsx
+++ microservices/www/imports/ui/pages/CareersPage/CareersPageList.jsx
@@ -8,9 +8,9 @@ const CareersPageList = () => (
   <div className="careers-page-list">
     {Object.keys(jobs).map(department => (
       <div className="careers-page-list-department" key={department}>
-        <h3 className="careers-page-list-department-title">
+        <h2 className="careers-page-list-department-title">
           <T id={`CareersPageList.${department}`} />
-        </h3>
+        </h2>
         {jobs[department].map((job, index) => (
           <CareersPageJob job={job} key={index} />
         ))}
