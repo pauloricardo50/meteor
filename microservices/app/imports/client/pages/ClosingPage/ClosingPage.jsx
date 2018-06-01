9325b161e318946164dc127c367132da7f00a1cc
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-06-01 11:40:58 +0200

    Change ui folder to be client
    This allows all client edits to only restart the client and not the server

---
+++ microservices/app/imports/client/pages/ClosingPage/ClosingPage.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import T from 'core/components/Translation';
+import ProcessPage from '../../components/ProcessPage';
+
+import Empty from './Empty';
+import Content from './Content';
+
+const ClosingPage = (props) => {
+  const { loan } = props;
+  return (
+    <ProcessPage {...props} stepNb={3} id="closing" showBottom={false}>
+      <section className="mask1 closing-page">
+        {loan.logic.closingSteps && loan.logic.closingSteps.length ? (
+          <Content
+            steps={loan.logic.closingSteps}
+            loan={loan}
+            disabled={loan.logic.step > 3}
+          />
+        ) : (
+          <Empty />
+        )}
+      </section>
+    </ProcessPage>
+  );
+};
+
+ClosingPage.propTypes = {
+  loan: PropTypes.objectOf(PropTypes.any).isRequired,
+  borrowers: PropTypes.arrayOf(PropTypes.object).isRequired,
+  offers: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default ClosingPage;
---
+++ microservices/app/imports/client/pages/VerificationPage/VerificationPage.jsx
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+import track from 'core/utils/analytics';
+import { requestLoanVerification } from 'core/api';
+import ProcessPage from '../../components/ProcessPage';
+import VerificationStart from './VerificationStart';
+import VerificationValidated from './VerificationValidated';
+import VerificationFailed from './VerificationFailed';
+
+export default class VerificationPage extends Component {
+  handleClick = () => {
+    requestLoanVerification
+      .run({ loanId: this.props.loan._id })
+      .then(() => track('requested verification'));
+  };
+
+  render() {
+    const { loan, borrowers } = this.props;
+    let content = null;
+    const verification = loan.logic.verification;
+
+    if (verification.validated === true) {
+      content = <VerificationValidated />;
+    } else if (verification.validated === false) {
+      content = (
+        <VerificationFailed
+          verification={verification}
+          onClick={this.handleClick}
+        />
+      );
+    } else {
+      content = (
+        <VerificationStart
+          verification={verification}
+          loanId={loan._id}
+          onClick={this.handleClick}
+        />
+      );
+    }
+
+    return (
+      <ProcessPage
+        {...this.props}
+        stepNb={1}
+        id="verification"
+        showBottom={false}
+      >
+        <section className="mask1 verification-page">{content}</section>
+      </ProcessPage>
+    );
+  }
+}
+
+VerificationPage.propTypes = {
+  loan: PropTypes.objectOf(PropTypes.any).isRequired,
+};
