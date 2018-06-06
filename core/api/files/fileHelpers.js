bd1abdf78e2bf03f2a12e2d36f4294fb92284953
claudiut <claudiut@users.noreply.github.com>
2018-06-06 17:35:24 +0300

    EP-197 Put back the extensions of the fake files’ `name` and `initialName` fields

--- core/api/files/fileHelpers.js
+++ core/api/files/fileHelpers.js
@@ -4,8 +4,8 @@ export const getUploadCountPrefix = lastUploadCount =>
   (lastUploadCount < 10 ? `0${lastUploadCount}` : `${lastUploadCount}`);
 
 export const fakeFile = {
-  name: 'fakeFile',
-  initialName: 'fakeFile',
+  name: 'fakeFile.pdf',
+  initialName: 'fakeFile.pdf',
   size: 10000,
   type: 'application/pdf',
   url: 'https://www.fake-url.com',
@@ -15,7 +15,7 @@ export const fakeFile = {
 
 export const validFakeFile = {
   ...fakeFile,
-  name: 'validFakeFile',
+  name: 'validFakeFile.pdf',
   key: 'asdf/fakeKey/validFakeFile.pdf',
   status: FILE_STATUS.VALID,
   error: '',
@@ -23,7 +23,7 @@ export const validFakeFile = {
 
 export const invalidFakeFile = {
   ...fakeFile,
-  name: 'invalidFakeFile',
+  name: 'invalidFakeFile.pdf',
   key: 'asdf/fakeKey/invalidFakeFile.pdf',
   error: 'Incorrect file format',
   status: FILE_STATUS.ERROR,
