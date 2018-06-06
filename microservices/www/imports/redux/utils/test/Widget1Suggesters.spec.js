3fe84b42ca85527a02272b268f9c76976dc2a3ee
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-06-06 12:11:32 +0200

    Add tests for suggesters module

---
+++ microservices/www/imports/redux/utils/test/Widget1Suggesters.spec.js
@@ -0,0 +1,50 @@
+/* eslint-env mocha */
+import { expect } from 'chai';
+import merge from 'lodash/merge';
+
+import { makeSuggestValue } from '../widget1Suggesters';
+import { PURCHASE_TYPE, SALARY } from '../../constants/widget1Constants';
+
+const value = 100;
+const prepareState = overrides => ({
+  widget1: merge(
+    {
+      purchaseType: PURCHASE_TYPE.ACQUISITION,
+      salary: { value, auto: false },
+      property: { value, auto: false },
+      fortune: { value, auto: false },
+    },
+    overrides,
+  ),
+});
+
+describe('suggestValue', () => {
+  let suggesters;
+  const createSuggestValue = () => makeSuggestValue(suggesters);
+
+  beforeEach(() => {
+    suggesters = {
+      [PURCHASE_TYPE.ACQUISITION]: {
+        [SALARY]: {},
+      },
+      [PURCHASE_TYPE.REFINANCING]: {},
+    };
+  });
+
+  it('should not suggest a value if it is currently manual', () => {
+    expect(createSuggestValue()('salary', prepareState())).to.equal(value);
+  });
+
+  it('should suggest a value with "all" if it is in auto and the others false', () => {
+    suggesters[PURCHASE_TYPE.ACQUISITION][SALARY].all = () => 1;
+    expect(createSuggestValue()('salary', prepareState({ salary: { auto: true } }))).to.equal(1);
+  });
+
+  it('should suggest a value with "fortune" if property is in auto', () => {
+    suggesters[PURCHASE_TYPE.ACQUISITION][SALARY].fortune = () => 1;
+    expect(createSuggestValue()(
+      'salary',
+      prepareState({ salary: { auto: true }, property: { auto: true } }),
+    )).to.equal(1);
+  });
+});
--- microservices/www/imports/redux/utils/widget1Suggesters.js
+++ microservices/www/imports/redux/utils/widget1Suggesters.js
@@ -1,6 +1,17 @@
 import { compose } from 'recompose';
-import { makeSelectValue, selectAutoValues } from '../reducers/widget1';
-import { SALARY, FORTUNE, PROPERTY } from '../constants/widget1Constants';
+import {
+  makeSelectValue,
+  selectAutoValues,
+  makeWidget1Selector,
+} from '../reducers/widget1';
+import {
+  SALARY,
+  FORTUNE,
+  PROPERTY,
+  CURRENT_LOAN,
+  WANTED_LOAN,
+  PURCHASE_TYPE,
+} from '../constants/widget1Constants';
 import {
   suggestSalary,
   suggestFortune,
@@ -15,18 +26,41 @@ import {
 // currently set to `auto` or not.
 // If both other values are set to `auto: false`, use `both` function,
 // otherwise use the 2nd or 3rd function
-const suggesters = {
+const acquisitionSuggesters = {
+  [SALARY]: {
+    all: suggestSalary,
+    [FORTUNE]: compose(fortuneToProperty, propertyToSalary),
+    [PROPERTY]: propertyToSalary,
+  },
+  [FORTUNE]: {
+    all: suggestFortune,
+    [SALARY]: compose(salaryToProperty, propertyToFortune),
+    [PROPERTY]: propertyToFortune,
+  },
+  [PROPERTY]: {
+    all: suggestProperty,
+    [SALARY]: salaryToProperty,
+    [FORTUNE]: fortuneToProperty,
+  },
+};
+
+const refinancingSuggesters = {
   [SALARY]: {
     both: suggestSalary,
     [FORTUNE]: compose(fortuneToProperty, propertyToSalary),
     [PROPERTY]: propertyToSalary,
   },
-  [FORTUNE]: {
-    both: suggestFortune,
-    [SALARY]: compose(salaryToProperty, propertyToFortune),
-    [PROPERTY]: propertyToFortune,
-  },
   [PROPERTY]: {
+    both: suggestProperty,
+    [SALARY]: salaryToProperty,
+    [FORTUNE]: fortuneToProperty,
+  },
+  [CURRENT_LOAN]: {
+    both: suggestProperty,
+    [SALARY]: salaryToProperty,
+    [FORTUNE]: fortuneToProperty,
+  },
+  [WANTED_LOAN]: {
     both: suggestProperty,
     [SALARY]: salaryToProperty,
     [FORTUNE]: fortuneToProperty,
@@ -34,9 +68,13 @@ const suggesters = {
 };
 
 // Pick the right suggester for `name`
-const makeValueSuggester = (name, [firstManualKey, secondManualKey]) => {
+const makeValueSuggester = (
+  suggesters,
+  name,
+  [firstManualKey, secondManualKey],
+) => {
   if (secondManualKey) {
-    return suggesters[name].both;
+    return suggesters[name].all;
   } else if (!firstManualKey) {
     // If all values are auto, just set them all to 0
     return () => 0;
@@ -44,9 +82,10 @@ const makeValueSuggester = (name, [firstManualKey, secondManualKey]) => {
   return suggesters[name][firstManualKey];
 };
 
-const suggestValue = (name, state) => {
+export const makeSuggestValue = suggesters => (name, state) => {
   // Get the number value for this name
   const value = makeSelectValue(name)(state);
+  const purchaseType = makeWidget1Selector('purchaseType')(state);
 
   // Get an object with all the auto values for every key
   // The auto value for the current name should be true
@@ -61,9 +100,16 @@ const suggestValue = (name, state) => {
   // the next value
   const manualValueKeys = Object.keys(autoValues).filter(key => !autoValues[key]);
   const manualValues = manualValueKeys.map(key => makeSelectValue(key)(state));
-  const suggester = makeValueSuggester(name, manualValueKeys);
+  const suggester = makeValueSuggester(
+    suggesters[purchaseType],
+    name,
+    manualValueKeys,
+  );
   const suggestedValue = suggester(...manualValues);
   return suggestedValue;
 };
 
-export default suggestValue;
+export default makeSuggestValue({
+  [PURCHASE_TYPE.REFINANCING]: refinancingSuggesters,
+  [PURCHASE_TYPE.ACQUISITION]: acquisitionSuggesters,
+});
