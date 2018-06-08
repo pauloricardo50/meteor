fdf5aef821e4aafdb5f3aa1d96851883a879b5cd
claudiut <claudiut@users.noreply.github.com>
2018-06-08 16:43:07 +0300

    EP-198 Fix some tests and skip the ones not ready.

--- core/api/tasks/TasksNotificationService.js
+++ core/api/tasks/TasksNotificationService.js
@@ -2,38 +2,61 @@ import { Bert } from 'meteor/themeteorchef:bert';
 import tasksQuery from './queries/tasks';
 import { TASK_STATUS, TASK_TYPE } from './taskConstants';
 import { FILE_STATUS } from '../constants';
+import Tasks from '.';
 
 if (typeof window !== 'undefined') window.tasksQuery = tasksQuery;
 class TaskNotificationService {
-  notifyTaskCompletedWhenFileStatusChanged = ({ fileKey, newStatus }) => {
-    if ([FILE_STATUS.ERROR, FILE_STATUS.VALID].includes(newStatus)) {
+  notifyTaskCompletedWhenFileStatusChanged = ({ fileKey, newStatus }) =>
+    new Promise((resolve) => {
+      if (![FILE_STATUS.ERROR, FILE_STATUS.VALID].includes(newStatus)) {
+        return resolve();
+      }
+
       tasksQuery
         .clone({ file: fileKey, status: TASK_STATUS.COMPLETED })
         .fetchOne((err, task) => {
-          this.notifyAdminOfCompletedTask({ task });
-        });
-    }
-  };
+          if (err) {
+            console.log(
+              'ERROR in notifyTaskCompletedWhenFileStatusChanged:',
+              err,
+            );
+          }
 
-  notifyTaskCompletedWhenAdminAssigned = ({ userId }) => {
-    tasksQuery
-      .clone({
-        user: userId,
-        type: TASK_TYPE.ADD_ASSIGNED_TO,
-        status: TASK_STATUS.COMPLETED,
-      })
-      .fetchOne((err, task) => {
-        this.notifyAdminOfCompletedTask({ task });
-      });
-  };
+          this.notifyAdminOfCompletedTask({ task });
+          resolve();
+        });
+    });
+
+  notifyTaskCompletedWhenAdminAssigned = ({ userId }) =>
+    new Promise((resolve) => {
+      if (!userId) {
+        return resolve();
+      }
+
+      tasksQuery
+        .clone({
+          user: userId,
+          type: TASK_TYPE.ADD_ASSIGNED_TO,
+          status: TASK_STATUS.COMPLETED,
+        })
+        .fetchOne((err, task) => {
+          if (err) {
+            console.log('ERROR in notifyTaskCompletedWhenAdminAssigned:', err);
+          }
+
+          this.notifyAdminOfCompletedTask({ task });
+          resolve();
+        });
+    });
 
   notifyAdminOfCompletedTask = ({ task }) => {
+    console.log('notifyAdminOfCompletedTask-->', task);
     if (task) {
       const { _id, status } = task;
 
       Bert.alert({
         title: 'Success!',
-        message: `The task ${_id} has been completed`,
+        message: `The task "${_id}" has been completed`,
         type: 'success',
         style: 'fixed-top',
       });
--- core/api/tasks/client/test/TaskNotificationService.test.js
+++ core/api/tasks/client/test/TaskNotificationService.test.js
@@ -13,68 +13,66 @@ import { TASK_STATUS, FILE_STATUS } from '../../../constants';
 let completedTask;
 
 describe('Task Client Service', () => {
-  beforeEach(() => {
-    stubCollections();
-    resetDatabase();
-
-    const admin = Factory.create('admin');
-
-    sinon.stub(Meteor, 'userId').callsFake(() => admin._id);
-    sinon.stub(Meteor, 'user').callsFake(() => admin);
-
-    sinon.stub(Bert, 'alert');
-
-    completedTask = Factory.create('task', { status: TASK_STATUS.COMPLETED });
-  });
-
-  afterEach(() => {
-    Bert.alert.restore();
-    stubCollections.restore();
-    Meteor.user.restore();
-    Meteor.userId.restore();
-  });
-
-  describe('notifyTaskCompletedWhenFileStatusChanged', () => {
+  describe('Methods that query data', () => {
     beforeEach(() => {
+      stubCollections();
+      resetDatabase();
       sinon.stub(TasksNotificationService, 'notifyAdminOfCompletedTask');
+
+      completedTask = Factory.create('task', { status: TASK_STATUS.COMPLETED });
     });
 
     afterEach(() => {
+      stubCollections.restore();
       TasksNotificationService.notifyAdminOfCompletedTask.restore();
     });
 
-    it.skip('calls `TaskNotificationService.notifyAdminOfCompletedTask` on the correct task', () => {
-      expect(TasksNotificationService.notifyAdminOfCompletedTask.called).to.equal(false);
-      TasksNotificationService.notifyTaskCompletedWhenFileStatusChanged({
-        fileKey: completedTask.fileKey,
-        newStatus: FILE_STATUS.VALID,
+    describe('notifyTaskCompletedWhenFileStatusChanged', () => {
+      // TODO
+      it('calls `TaskNotificationService.notifyAdminOfCompletedTask` on the correct task', async () => {
+        expect(TasksNotificationService.notifyAdminOfCompletedTask.called).to.equal(false);
+        await TasksNotificationService.notifyTaskCompletedWhenFileStatusChanged({
+          fileKey: completedTask.fileKey,
+          newStatus: FILE_STATUS.VALID,
+        });
+        expect(TasksNotificationService.notifyAdminOfCompletedTask.getCall(0).args).to.deep.equal([{ task: completedTask }]);
       });
-      expect(TasksNotificationService.notifyAdminOfCompletedTask.getCall(0).args).to.deep.equal([{ task: completedTask }]);
+
+      // check it actually fails, after the above test is working
+      it(`does not call \`TaskNotificationService.notifyAdminOfCompletedTask\` when file status none of \`${
+        FILE_STATUS.ERROR
+      }\` or \`${FILE_STATUS.VALID}\``, async () => {
+        await TasksNotificationService.notifyTaskCompletedWhenFileStatusChanged({
+          fileKey: completedTask.fileKey,
+          newStatus: FILE_STATUS.UNVERIFIED,
+        });
+        expect(TasksNotificationService.notifyAdminOfCompletedTask.called).to.equal(false);
+      });
     });
 
-    it.skip("does not call `TaskNotificationService.notifyAdminOfCompletedTask` when file status isn't error or valid", () => {
-      TasksNotificationService.notifyTaskCompletedWhenFileStatusChanged({
-        fileKey: completedTask.fileKey,
-        newStatus: FILE_STATUS.UNVERIFIED,
-      });
-      expect(TasksNotificationService.notifyAdminOfCompletedTask.called).to.equal(false);
-    });
+    describe('notifyTaskCompletedWhenAdminAssigned', () => {
+      // TODO
+      it('calls `TaskNotificationService.notifyAdminOfCompletedTask` on the correct task');
 
-    it.skip('does not call `TaskNotificationService.notifyAdminOfCompletedTask` when file-related task not completed', () => {
-      const activeTask = Factory.create('task', {
-        status: TASK_STATUS.ACTIVE,
+      // check it actually fails, after the above test is working
+      it('does not call `TaskNotificationService.notifyAdminOfCompletedTask` when `userId` is undefined', async () => {
+        await TasksNotificationService.notifyTaskCompletedWhenFileStatusChanged({
+          userId: undefined,
+        });
+        expect(TasksNotificationService.notifyAdminOfCompletedTask.called).to.equal(false);
       });
-
-      TasksNotificationService.notifyTaskCompletedWhenFileStatusChanged({
-        fileKey: activeTask.fileKey,
-        newStatus: FILE_STATUS.VALID,
-      });
-
-      expect(TasksNotificationService.notifyAdminOfCompletedTask.called).to.equal(false);
     });
   });
 
   describe('notifyAdminOfCompletedTask', () => {
+    beforeEach(() => {
+      sinon.stub(Bert, 'alert');
+    });
+
+    afterEach(() => {
+      Bert.alert.restore();
+    });
+
     it('triggers a bert alert', () => {
       const task = { _id: 'aTaskId', status: TASK_STATUS.COMPLETED };
 
@@ -83,11 +81,20 @@ describe('Task Client Service', () => {
       expect(Bert.alert.getCall(0).args).to.deep.equal([
         {
           title: 'Success!',
-          message: 'The task aTaskId has been completed',
+          message: 'The task "aTaskId" has been completed',
           type: 'success',
           style: 'fixed-top',
         },
       ]);
+    });
+
+    // check
+    it('does not trigger a bert alert for an undefined task', () => {
+      expect(Bert.alert.called).to.equal(false);
+      TasksNotificationService.notifyAdminOfCompletedTask({
+        task: undefined,
+      });
+      expect(Bert.alert.called).to.equal(false);
     });
   });
 });
