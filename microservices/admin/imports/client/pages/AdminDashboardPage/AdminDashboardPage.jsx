27544336931448e654c46f15c22cf7c987c05632
andraLazariuc <andra.lazariuc@yahoo.com>
2018-06-08 13:31:41 +0300

    Fix merge conflict

--- microservices/admin/imports/client/pages/AdminDashboardPage/AdminDashboardPage.jsx
+++ microservices/admin/imports/client/pages/AdminDashboardPage/AdminDashboardPage.jsx
@@ -6,13 +6,8 @@ import T from 'core/components/Translation/';
 import TasksTableWithData from '../../components/TasksTable/TasksTableWithData';
 import CreateUserDialogForm from './CreateUserDialogForm';
 
-<<<<<<< HEAD:microservices/admin/imports/ui/pages/AdminDashboardPage/AdminDashboardPage.jsx
-const AdminDashboardPage = () => (
-  <section className="mask1">
-=======
 const AdminDashboardPage = props => (
   <section className="mask1 admin-dashboard-page">
->>>>>>> 2153bd1e6c91b464aa09d141687942f5890299e4:microservices/admin/imports/client/pages/AdminDashboardPage/AdminDashboardPage.jsx
     <h1>Admin Dashboard</h1>
 
     <CreateUserDialogForm />
