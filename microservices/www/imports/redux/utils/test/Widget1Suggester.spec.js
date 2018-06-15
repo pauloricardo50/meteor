ed8e0d4a65db41b708a917685b4f6ee07fed2c98
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-06-15 16:33:29 +0200

    Fix broken tests

--- microservices/www/imports/redux/utils/test/Widget1Suggester.spec.js
+++ microservices/www/imports/redux/utils/test/Widget1Suggester.spec.js
@@ -6,10 +6,13 @@ import Widget1Suggester from '../Widget1Suggester';
 describe('Widget1SuggesterClass', () => {
   describe('getMaxPossibleLoan', () => {
     it('returns the hard cap if salary is not a problem', () => {
-      expect(Widget1Suggester.getMaxPossibleLoan({ property: 100, salary: 50 })).to.equal(80);
+      expect(Widget1Suggester.getMaxPossibleLoan({ property: 10000, salary: 5000 })).to.equal(8000);
     });
     it('returns a value modified by salary', () => {
-      expect(Widget1Suggester.getMaxPossibleLoan({ property: 1000, salary: 120 })).to.equal(600);
+      expect(Widget1Suggester.getMaxPossibleLoan({
+        property: 1000000,
+        salary: 120000,
+      })).to.equal(600000);
     });
   });
 });
