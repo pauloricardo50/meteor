d597ceefa93d4ea322f34b847d8b7dd92df0bd01
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-07-02 11:46:35 +0200

    Update interest rates

--- microservices/www/imports/ui/pages/InterestsPage/interestRates.js
+++ microservices/www/imports/ui/pages/InterestsPage/interestRates.js
@@ -15,20 +15,20 @@ const interestRates = [
   },
   {
     type: INTEREST_RATES.YEARS_5,
-    rateLow: 0.0075,
-    rateHigh: 0.0125,
-    trend: TRENDS.DOWN,
-  },
-  {
-    type: INTEREST_RATES.YEARS_10,
-    rateLow: 0.0121,
-    rateHigh: 0.0172,
+    rateLow: 0.0078,
+    rateHigh: 0.0115,
     trend: TRENDS.FLAT,
   },
   {
+    type: INTEREST_RATES.YEARS_10,
+    rateLow: 0.0109,
+    rateHigh: 0.0142,
+    trend: TRENDS.DOWN,
+  },
+  {
     type: INTEREST_RATES.YEARS_15,
-    rateLow: 0.0178,
-    rateHigh: 0.0195,
+    rateLow: 0.0162,
+    rateHigh: 0.0203,
     trend: TRENDS.DOWN,
   },
   {
