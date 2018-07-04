526755155189314d9cd7d27005edc5b99e25df38
andraLazariuc <andra.lazariuc@yahoo.com>
2018-07-04 20:46:01 +0300

    Remove comments

--- microservices/www/imports/ui/pages/InterestsPage/WwwInterestRatesTable.jsx
+++ microservices/www/imports/ui/pages/InterestsPage/WwwInterestRatesTable.jsx
@@ -1,17 +1,4 @@
-import React from 'react';
 import InterestRatesTable from 'core/components/InterestRatesTable';
+import WwwInterestRatesTableContainer from './WwwInterestRatesTableContainer.jsx';
 
-import { columnOptions, rows } from './wwwInterestsTableHelpers';
-
-const WwwInterestRatesTable = props => (
-  <InterestRatesTable {...props} columnOptions={columnOptions} rows={rows} />
-);
-
-export default WwwInterestRatesTable;
-// These are the 2 different implementations I mentioned
-// during the dailly call. Which is better?
-//
-// import InterestRatesTable from 'core/components/InterestRatesTable';
-// import WwwInterestRatesTableContainer from './WwwInterestRatesTableContainer.jsx';
-
-// export default WwwInterestRatesTableContainer(InterestRatesTable);
+export default WwwInterestRatesTableContainer(InterestRatesTable);
