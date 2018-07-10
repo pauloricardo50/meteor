fff04efa1dd7e07516bad2e0ee9b9110ffe4697d
claudiut <claudiut@users.noreply.github.com>
2018-07-10 15:03:48 +0300

    EP-230 Add more tests to the analytics HoC

--- core/containers/test/client/withAnalytics.spec.jsx
+++ core/containers/test/client/withAnalytics.spec.jsx
@@ -27,15 +27,16 @@ addEvent('OPENED_USER_PREFS', {
 });
 
 const trackedComponent = (trackerHoc, props) => {
-  const WrappedComponent = () => null;
-  const Component = trackerHoc(WrappedComponent);
+  const WrappedWithTrackedFunction = () => null;
+  const Component = trackerHoc(WrappedWithTrackedFunction);
 
   return shallow(<Component {...props} />);
 };
 
+let WrappedWithLifecyle = () => null;
 const mountedComponent = (trackerHoc, props) => {
-  const WrappedComponent = () => null;
-  const Component = trackerHoc(WrappedComponent);
+  WrappedWithLifecyle = () => null;
+  const Component = trackerHoc(WrappedWithLifecyle);
 
   return getMountedComponent({
     Component,
@@ -54,50 +55,67 @@ describe.only('withAnalytics', () => {
   });
 
   describe('Callback Tracker', () => {
-    it('calls `analytics.track` with the event name and metadata', () => {
+    let component;
+    let originalOnChange;
+    let returnValueOfTrackedFunction;
+
+    beforeEach(() => {
       const trackerHoc = withAnalytics(EVENTS.SUBMITTED_USER_FORM);
 
-      const originalOnChange = sinon.spy();
-      const component = trackedComponent(trackerHoc, {
+      originalOnChange = sinon.stub().callsFake(() => 'the return value');
+      component = trackedComponent(trackerHoc, {
         onChange: originalOnChange,
       });
 
-      component.prop('onChange')('my name is John');
+      returnValueOfTrackedFunction = component.prop('onChange')('my name is John');
+    });
 
+    it('calls `analytics.track` with the event name and metadata', () => {
       expect(analytics.track.lastCall.args).to.deep.equal([
         EVENTS.SUBMITTED_USER_FORM,
         'my name is John',
       ]);
+    });
 
+    it('calls the original function with all original arguments', () => {
       expect(originalOnChange.lastCall.args).to.deep.equal(['my name is John']);
     });
 
-    it('calls the original function with all original arguments');
+    it('returns the return value of the original function', () => {
+      const originallyReturnedValue =
+        originalOnChange.returnValues[originalOnChange.returnValues.length - 1];
 
-    it('returns the return value of the original function');
+      expect(originallyReturnedValue).to.equal('the return value');
 
-    it('tracks the event name only');
+      expect(returnValueOfTrackedFunction).to.equal(originallyReturnedValue);
+    });
   });
 
   describe('Lifecycle Tracker', () => {
+    let component;
+    let user;
+
     beforeEach(() => {
       getMountedComponent.reset();
+
+      const trackerHoc = withAnalytics(EVENTS.OPENED_USER_PREFS);
+
+      user = { email: 'user@test.com' };
+
+      component = mountedComponent(trackerHoc, {
+        user,
+      });
     });
 
     it('tracks a lifecycle method by event name and metadata', () => {
-      const trackerHoc = withAnalytics(EVENTS.OPENED_USER_PREFS);
-
-      const user = { email: 'user@test.com' };
-      mountedComponent(trackerHoc, {
-        user,
-      });
-
       expect(analytics.track.lastCall.args).to.deep.equal([
         EVENTS.OPENED_USER_PREFS,
         { user },
       ]);
     });
 
-    it('does not crash the original component');
+    it('does not crash the original component', () => {
+      expect(component.find(WrappedWithLifecyle).length).to.equal(1);
+    });
   });
 });
