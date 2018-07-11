9cbae86fb23d73915fd3c20cf642c51c81bc388b
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-07-11 14:16:04 +0200

    Create redux boilerplate

---
+++ core/redux/financingStructures/index.js
@@ -0,0 +1,11 @@
+// @flow
+import * as _financingStructuresSelectors from './financingStructuresSelectors';
+import * as _financingStructuresActions from './financingStructuresActions';
+import * as _financingStructuresConstants from './financingStructuresConstants';
+import * as _financingStructuresTypes from './financingStructuresTypes';
+
+export { default } from './financingStructuresReducer';
+export const financingStructuresSelectors = _financingStructuresSelectors;
+export const financingStructuresActions = _financingStructuresActions;
+export const financingStructuresConstants = _financingStructuresConstants;
+export const financingStructuresTypes = _financingStructuresTypes;
--- microservices/admin/imports/client/redux/reducers.js
+++ microservices/admin/imports/client/redux/reducers.js
@@ -1,9 +1,14 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
+import financingStructuresReducer from 'core/redux/financingStructures';
 import sidenavReducer from './sidenav';
 
 const createRootReducer = () =>
-  combineReducers({ sidenav: sidenavReducer, form: formReducer });
+  combineReducers({
+    sidenav: sidenavReducer,
+    financingStructures: financingStructuresReducer,
+    form: formReducer,
+  });
 
 export default createRootReducer;
