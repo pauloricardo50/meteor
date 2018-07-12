756a2fb517f3ec9b992adddd8d6673b352b4c50d
Quentin Herzig <quentin.herzig@gmail.com>
2018-07-12 17:15:44 +0200

    Add tmux in run.sh script (#219)
    * Remove unused content
    
    * Change run.sh to use tmux instead of ttab

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,21 +4,35 @@
 
 echo Running e-Potek...
 
-# Check if ttab is installed
-if ! type "ttab" > /dev/null; then
-  # install ttab if not
-  echo Installing ttab
-  meteor npm i -g ttab
+# Check if tmux is installed
+if ! type "tmux" > /dev/null; then
+  #if not tell user
+  echo You need to install tmux (brew: brew install tmux, apt: apt-get install tmux)
 fi
 
-# Start each app in its own tab
-for i in "www" "app" "admin"
-  do
-    # Make them all connect to the mongoDB instance created by the first app (epotek-www, running at localhost:3000)
-    if [[ $i == "www" ]]; then
-        ttab -t $i -d ../microservices/$i 'meteor npm start'
-    else
-      ttab -t $i -d ../microservices/$i 'export MONGO_URL=mongodb://localhost:3001/meteor; meteor npm start'
-    fi
+# Start tmux session
+SESSION="e-potek"
+if [[ "$TERM" =~ "screen".* ]]; then
+  #Already in tmux, so create a new window
+  tmux new-window -n $SESSION
+else
+  #Not in tmux, create a new session
+  tmux new-session -d -s $SESSION 
+fi
+tmux splitw -t 0 -h -p 50
+tmux splitw -t 0 -v -p 50
+tmux splitw -t 2 -v -p 50
 
-  done
+# Start each app in its own pane
+tmux select-pane -t 0
+tmux send-keys "cd ../microservices/www; meteor npm start" C-m
+tmux select-pane -t 1
+tmux send-keys "cd ../microservices/app; export MONGO_URL=mongodb://localhost:3001/meteor; meteor npm start" C-m
+tmux select-pane -t 2
+tmux send-keys "cd ../microservices/admin; export MONGO_URL=mongodb://localhost:3001/meteor; meteor npm start" C-m
+
+#Prepare 'meteor mongo' command for user in 4th pane with focus on it
+tmux select-pane -t 3
+tmux send-keys "cd ../microservices/www; meteor mongo" 
+
+tmux attach-session -t $SESSION
