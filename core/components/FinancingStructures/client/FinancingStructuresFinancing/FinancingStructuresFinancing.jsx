0960603491524398c725929b76d971a87b33ebc6
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-07-19 15:42:07 +0200

    Add Financing section

--- core/components/FinancingStructures/client/FinancingStructuresFinancing/FinancingStructuresFinancing.jsx
+++ core/components/FinancingStructures/client/FinancingStructuresFinancing/FinancingStructuresFinancing.jsx
@@ -1,8 +1,41 @@
 // @flow
 import React from 'react';
 
+import { AMORTIZATION_STRATEGY_PRESET } from '../../../../api/constants';
+import FinancingStructuresSection, {
+  InputAndSlider,
+  CalculatedValue,
+  RadioButtons,
+} from '../FinancingStructuresSection';
+
+const calculateLoan = ({ structure: { wantedLoan } }) => wantedLoan;
+
 type FinancingStructuresFinancingProps = {};
 
-const FinancingStructuresFinancing = (props: FinancingStructuresFinancingProps) => <div>Hello World</div>;
+const FinancingStructuresFinancing = (props: FinancingStructuresFinancingProps) => (
+  <FinancingStructuresSection
+    summaryConfig={[
+      {
+        id: 'mortgageLoan',
+        label: <h3 className="section-title">Prêt hypothécaire</h3>,
+        Component: CalculatedValue,
+        value: calculateLoan,
+        money: true,
+      },
+    ]}
+    detailConfig={[
+      { Component: InputAndSlider, id: 'wantedLoan' },
+      { Component: InputAndSlider, id: 'amortization' },
+      {
+        Component: RadioButtons,
+        id: 'amortizationType',
+        options: Object.values(AMORTIZATION_STRATEGY_PRESET).map(key => ({
+          id: key,
+          label: key,
+        })),
+      },
+    ]}
+  />
+);
 
 export default FinancingStructuresFinancing;
--- core/components/FinancingStructures/client/containers/StructuresContainer.js
+++ core/components/FinancingStructures/client/containers/StructuresContainer.js
@@ -1,10 +1,6 @@
 import { connect } from 'react-redux';
 import { selectStructuresArray } from '../../../../redux/financingStructures';
 
-export default connect((state) => {
-  console.log('structs?', selectStructuresArray(state));
-
-  return {
-    structures: selectStructuresArray(state),
-  };
-});
+export default connect(state => ({
+  structures: selectStructuresArray(state),
+}));
