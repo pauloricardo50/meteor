7580899d973f2cee2a2f195df5b4aa3fd6642262
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-07-23 17:24:27 +0200

    Add keys to iterative functions

--- core/components/FinancingStructures/client/FinancingStructuresSection/financingStructuresSectionHelpers.jsx
+++ core/components/FinancingStructures/client/FinancingStructuresSection/financingStructuresSectionHelpers.jsx
@@ -1,30 +1,26 @@
 import React from 'react';
 import cx from 'classnames';
 
-export const makeRenderSummary = configArray => ({ id: structureId }) =>
-  configArray.map(({ Component, id, ...props }) =>
-    (Component ? (
-      <Component
-        key={id}
-        structureId={structureId}
-        id={id}
-        className={id}
-        {...props}
-      />
-    ) : (
-      <div className={cx('empty-line', id)} />
-    )));
+export const makeRenderSummary = configArray => ({ id: structureId }) => configArray.map(({ Component, id, ...props }) => (Component ? (
+  <Component
+    key={id}
+    structureId={structureId}
+    id={id}
+    className={id}
+    {...props}
+  />
+) : (
+  <div className={cx('empty-line', id)} key={id} />
+)));
 
-export const makeRenderDetail = configArray => ({ id: structureId }) =>
-  configArray.map(({ Component, id, ...props }) =>
-    (Component ? (
-      <Component
-        key={id}
-        structureId={structureId}
-        id={id}
-        className={id}
-        {...props}
-      />
-    ) : (
-      <div className={cx('empty-line', id)} />
-    )));
+export const makeRenderDetail = configArray => ({ id: structureId }) => configArray.map(({ Component, id, ...props }) => (Component ? (
+  <Component
+    key={id}
+    structureId={structureId}
+    id={id}
+    className={id}
+    {...props}
+  />
+) : (
+  <div className={cx('empty-line', id)} key={id} />
+)));
