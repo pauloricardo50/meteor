730fc30faa733f26d46c7f73e6cde7867873a34f
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-07-26 15:06:40 +0200

    Use i18n to render label

--- core/components/FinancingStructures/client/FinancingStructuresSection/components/RadioButtons.jsx
+++ core/components/FinancingStructures/client/FinancingStructuresSection/components/RadioButtons.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import cx from 'classnames'
+import cx from 'classnames';
 
+import T from 'core/components/Translation';
 import StructureUpdateContainer from './StructureUpdateContainer';
 
 type RadioButtonsProps = {
@@ -14,15 +15,25 @@ type RadioButtonsProps = {
   className: string,
 };
 
-const RadioButtons = ({ value, handleChange, options, className }: RadioButtonsProps) => (
+const RadioButtons = ({
+  value,
+  handleChange,
+  options,
+  className,
+}: RadioButtonsProps) => (
   <RadioGroup
     aria-label="Gender"
     value={value}
     onChange={event => handleChange(event.target.value)}
-    className={cx("radio-buttons", className)}
+    className={cx('radio-buttons', className)}
   >
     {options.map(({ id, label }) => (
-      <FormControlLabel key={id} value={id} control={<Radio />} label={label} />
+      <FormControlLabel
+        key={id}
+        value={id}
+        control={<Radio />}
+        label={<T id={label} />}
+      />
     ))}
   </RadioGroup>
 );
