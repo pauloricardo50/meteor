95da0769db2a7f15ef4f8de484821b6c5cd9c327
Quentin Herzig <quentin@e-potek.ch>
2018-07-27 15:23:22 +0200

    Add more test to wuest service

--- core/api/wuest/server/WuestService.js
+++ core/api/wuest/server/WuestService.js
@@ -14,11 +14,12 @@ import {
   PROPERTY_TYPE,
   VOLUME_TYPE,
   AREA_TYPE,
-  ERRORS,
+  WUEST_ERRORS,
 } from '../wuestConstants';
 import Properties from '../../properties';
 import { PROPERTY_STYLE, USAGE_TYPE } from '../../constants';
 
+const valueIsDefined = value => (value === 0 ? true : !!value);
 class WuestService {
   constructor() {
     this.properties = [];
@@ -35,6 +36,7 @@ class WuestService {
     });
 
   addProperty(property) {
+    this.checkPropertySanity(property);
     let data;
     switch (property.type) {
     case PROPERTY_TYPE.HOUSE:
@@ -44,10 +46,242 @@ class WuestService {
       data = this.buildFlat(property);
       break;
     default:
-      return;
+      return null;
     }
     this.properties.push(data);
     return data;
+  }
+
+  getErrors(property = {}) {
+    const sanityChecks = [
+      {
+        sanityCheck: ({ data }) => !!data,
+        error: WUEST_ERRORS.NO_PROPERTY_DATA_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { address } }) => !!address,
+        error: WUEST_ERRORS.NO_ADDRESS_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { address } }) =>
+          !!address && !!address.addressLine1,
+        error: WUEST_ERRORS.NO_ADDRESS_LINE_1_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { address } }) => !!address && !!address.zipCode,
+        error: WUEST_ERRORS.NO_ZIPCODE_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { address } }) => !!address && !!address.place,
+        error: WUEST_ERRORS.NO_CITY_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { address } }) =>
+          !!address && !!address.countryIsoCode,
+        error: WUEST_ERRORS.NO_COUNTRY_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { numberOfRooms } }) => !!numberOfRooms,
+        error: WUEST_ERRORS.NO_NUMBER_OF_ROOMS_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { constructionYear } }) => !!constructionYear,
+        error: WUEST_ERRORS.NO_CONSTRUCTION_YEAR_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { minergieCertificate } }) =>
+          !!minergieCertificate,
+        error: WUEST_ERRORS.NO_MINERGIE_CERTIFICATE_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { minergieCertificate } }) =>
+          Object.values(MINERGIE_CERTIFICATE).includes(minergieCertificate),
+        error: WUEST_ERRORS.INVALID_MINERGIE_CERTIFICATE,
+      },
+      {
+        sanityCheck: ({ data: { parking } }) => !!parking,
+        error: WUEST_ERRORS.NO_PARKING_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { parking } }) =>
+          !!parking && valueIsDefined(parking.indoor),
+        error: WUEST_ERRORS.NO_INSIDE_PARKING_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { parking } }) =>
+          !!parking && valueIsDefined(parking.outdoor),
+        error: WUEST_ERRORS.NO_OUTSIDE_PARKING_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { qualityProfile } }) => !!qualityProfile,
+        error: WUEST_ERRORS.NO_QUALITY_PROFILE_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { qualityProfile } }) =>
+          !!qualityProfile && !!qualityProfile.standard,
+        error: WUEST_ERRORS.NO_QUALITY_PROFILE_STANDARD_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { qualityProfile } }) =>
+          !!qualityProfile &&
+          Object.values(QUALITY.STANDARD).includes(qualityProfile.standard),
+        error: WUEST_ERRORS.INVALID_QUALITY_PROFILE_STANDARD,
+      },
+      {
+        sanityCheck: ({ data: { qualityProfile } }) =>
+          !!qualityProfile && !!qualityProfile.condition,
+        error: WUEST_ERRORS.NO_QUALITY_PROFILE_CONDITION_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { qualityProfile } }) =>
+          !!qualityProfile &&
+          Object.values(QUALITY.CONDITION).includes(qualityProfile.condition),
+        error: WUEST_ERRORS.INVALID_QUALITY_PROFILE_CONDITION,
+      },
+      {
+        sanityCheck: ({ data: { qualityProfile } }) =>
+          !!qualityProfile && !!qualityProfile.situation,
+        error: WUEST_ERRORS.NO_QUALITY_PROFILE_SITUATION_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { qualityProfile } }) =>
+          !!qualityProfile &&
+          Object.values(QUALITY.SITUATION).includes(qualityProfile.situation),
+        error: WUEST_ERRORS.INVALID_QUALITY_PROFILE_SITUATION,
+      },
+      {
+        sanityCheck: ({ data: { residenceType } }) => !!residenceType,
+        error: WUEST_ERRORS.NO_RESIDENCE_TYPE_PROVIDED,
+      },
+      {
+        sanityCheck: ({ data: { residenceType } }) =>
+          Object.values(RESIDENCE_TYPE).includes(residenceType),
+        error: WUEST_ERRORS.INVALID_RESIDENCE_TYPE,
+      },
+      {
+        sanityCheck: ({ type, data: { houseType } }) =>
+          (type === PROPERTY_TYPE.FLAT ? true : !!houseType),
+        error: WUEST_ERRORS.NO_HOUSE_TYPE_PROVIDED,
+      },
+      {
+        sanityCheck: ({ type, data: { houseType } }) =>
+          (type === PROPERTY_TYPE.FLAT
+            ? true
+            : Object.values(HOUSE_TYPE).includes(houseType)),
+        error: WUEST_ERRORS.INVALID_HOUSE_TYPE,
+      },
+      {
+        sanityCheck: ({ type, data: { buildingVolume } }) =>
+          (type === PROPERTY_TYPE.FLAT ? true : !!buildingVolume),
+        error: WUEST_ERRORS.NO_BUILDING_VOLUME_PROVIDED,
+      },
+      {
+        sanityCheck: ({ type, data: { buildingVolume } }) =>
+          (type === PROPERTY_TYPE.FLAT
+            ? true
+            : !!buildingVolume && !!buildingVolume.value),
+        error: WUEST_ERRORS.NO_BUILDING_VOLUME_VALUE_PROVIDED,
+      },
+      {
+        sanityCheck: ({ type, data: { buildingVolume } }) =>
+          (type === PROPERTY_TYPE.FLAT
+            ? true
+            : !!buildingVolume && !!buildingVolume.type),
+        error: WUEST_ERRORS.NO_BUILDING_VOLUME_TYPE_PROVIDED,
+      },
+      {
+        sanityCheck: ({ type, data: { buildingVolume } }) =>
+          (type === PROPERTY_TYPE.FLAT
+            ? true
+            : !!buildingVolume &&
+              Object.values(VOLUME_TYPE).includes(buildingVolume.type)),
+        error: WUEST_ERRORS.INVALID_BUILDING_VOLUME_TYPE,
+      },
+      {
+        sanityCheck: ({ type, data: { landPlotArea } }) =>
+          (type === PROPERTY_TYPE.FLAT ? true : !!landPlotArea),
+        error: WUEST_ERRORS.NO_LANDPLOT_AREA_PROVIDED,
+      },
+      {
+        sanityCheck: ({ type, data: { floorType } }) =>
+          (type === PROPERTY_TYPE.HOUSE ? true : !!floorType),
+        error: WUEST_ERRORS.NO_FLOOR_NUMBER_PROVIDED,
+      },
+      {
+        sanityCheck: ({ type, data: { floorType } }) =>
+          (type === PROPERTY_TYPE.HOUSE
+            ? true
+            : FLOOR_NUMBER.includes(floorType)),
+        error: WUEST_ERRORS.INVALID_FLOOR_NUMBER,
+      },
+      {
+        sanityCheck: ({ type, data: { flatType } }) =>
+          (type === PROPERTY_TYPE.HOUSE ? true : !!flatType),
+        error: WUEST_ERRORS.NO_FLAT_TYPE_PROVIDED,
+      },
+      {
+        sanityCheck: ({ type, data: { flatType } }) =>
+          (type === PROPERTY_TYPE.HOUSE
+            ? true
+            : Object.values(FLAT_TYPE).includes(flatType)),
+        error: WUEST_ERRORS.INVALID_FLAT_TYPE,
+      },
+      {
+        sanityCheck: ({ type, data: { numberOfFloors } }) =>
+          (type === PROPERTY_TYPE.HOUSE ? true : !!numberOfFloors),
+        error: WUEST_ERRORS.NO_NUMBER_OF_FLOORS_PROVIDED,
+      },
+      {
+        sanityCheck: ({ type, data: { numberOfFloors, floorType } }) =>
+          (type === PROPERTY_TYPE.HOUSE
+            ? true
+            : FLOOR_NUMBER.indexOf(floorType) <= numberOfFloors),
+        error: WUEST_ERRORS.FLOOR_NUMBER_EXCEEDS_TOTAL_NUMBER_OF_FLOORS,
+      },
+      {
+        sanityCheck: ({ type, data: { usableArea } }) =>
+          (type === PROPERTY_TYPE.HOUSE ? true : !!usableArea),
+        error: WUEST_ERRORS.NO_USABLE_AREA_PROVIDED,
+      },
+      {
+        sanityCheck: ({ type, data: { usableArea } }) =>
+          (type === PROPERTY_TYPE.HOUSE
+            ? true
+            : !!usableArea && !!usableArea.value),
+        error: WUEST_ERRORS.NO_USABLE_AREA_VALUE_PROVIDED,
+      },
+      {
+        sanityCheck: ({ type, data: { usableArea } }) =>
+          (type === PROPERTY_TYPE.HOUSE
+            ? true
+            : !!usableArea && !!usableArea.type),
+        error: WUEST_ERRORS.NO_USABLE_AREA_TYPE_PROVIDED,
+      },
+      {
+        sanityCheck: ({ type, data: { usableArea } }) =>
+          (type === PROPERTY_TYPE.HOUSE
+            ? true
+            : !!usableArea &&
+              Object.values(AREA_TYPE).includes(usableArea.type)),
+        error: WUEST_ERRORS.INVALID_USABLE_AREA_TYPE,
+      },
+    ];
+
+    const errors = sanityChecks.reduce(
+      (acc, { sanityCheck, error }) =>
+        (sanityCheck(property) ? acc : [...acc, error]),
+      [],
+    );
+
+    return errors;
+  }
+
+  checkPropertySanity(property) {
+    const errors = this.getErrors(property);
+
+    if (errors.length > 0) {
+      throw new Meteor.Error(errors[0]);
+    }
   }
 
   buildHouse(property) {
@@ -123,7 +357,7 @@ class WuestService {
     const property = Properties.findOne(propertyId);
 
     if (!property) {
-      throw new Meteor.Error(ERRORS.NO_PROPERTY_FOUND);
+      throw new Meteor.Error(WUEST_ERRORS.NO_PROPERTY_FOUND);
     }
 
     switch (property.style) {
@@ -134,7 +368,7 @@ class WuestService {
         data: {
           address: {
             addressLine1: property.address1,
-            zipCode: property.zipCode.toString(),
+            zipCode: `${property.zipCode}`,
             place: property.city,
             countryIsoCode: 'CH',
           },
@@ -145,7 +379,7 @@ class WuestService {
           flatType: FLAT_TYPE.SINGLE_FLOOR,
           numberOfRooms: property.roomCount,
           numberOfFloors: 4,
-          floorType: FLOOR_NUMBER[2],
+          floorType: FLOOR_NUMBER[property.floorNumber || 2],
           usableArea: {
             type: AREA_TYPE.NET,
             value: property.insideArea,
@@ -175,7 +409,7 @@ class WuestService {
         data: {
           address: {
             addressLine1: property.address1,
-            zipCode: property.zipCode.toString(),
+            zipCode: `${property.zipCode}`,
             place: property.city,
             countryIsoCode: 'CH',
           },
@@ -208,7 +442,7 @@ class WuestService {
       break;
 
     default:
-      return;
+      return null;
     }
     return data;
   }
@@ -256,6 +490,7 @@ class WuestService {
         .then(this.handleResult)
         .then(this.formatResult);
     });
+    this.properties = [];
     return Promise.all(promises).then(this.cleanUpResults);
   }
 }
--- core/api/wuest/server/test/WuestService.test.js
+++ core/api/wuest/server/test/WuestService.test.js
@@ -4,20 +4,361 @@ import { resetDatabase } from 'meteor/xolvio:cleaner';
 import { Factory } from 'meteor/dburles:factory';
 
 import WuestService from '../WuestService';
-import { ERRORS } from '../../wuestConstants';
+import {
+  WUEST_ERRORS,
+  FLOOR_NUMBER,
+  PROPERTY_TYPE,
+} from '../../wuestConstants';
+import { PROPERTY_STYLE } from '../../../properties/propertyConstants';
 
-describe('WuestService', () => {
+describe.only('WuestService', () => {
   beforeEach(() => {
     resetDatabase();
   });
 
-  describe('evaluateById', () => {
-    it('throws if it can not find the property', () => {
-      expect(() => WuestService.evaluateById('test')).to.throw(ERRORS.NO_PROPERTY_FOUND);
+  describe('createPropertyFromCollection', () => {
+    it('throws an error if it can not find the property', () => {
+      expect(() => WuestService.createPropertyFromCollection('test')).to.throw(WUEST_ERRORS.NO_PROPERTY_FOUND);
+    });
+  });
+
+  describe('getErrors returns an error when ', () => {
+    it('floor number is not provided', () => {
+      const property = {
+        type: PROPERTY_TYPE.FLAT,
+        data: { floorType: null },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_FLOOR_NUMBER_PROVIDED);
     });
 
+    it('floor number exceeds 20', () => {
+      const property = {
+        type: PROPERTY_TYPE.FLAT,
+        data: { floorType: FLOOR_NUMBER[21] },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.INVALID_FLOOR_NUMBER);
+    });
+
+    it('no address is provided', () => {
+      const property = {
+        data: {
+          address: null,
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_ADDRESS_PROVIDED);
+    });
+
+    it('no address line 1 is provided', () => {
+      const property = { data: { address: { addressLine1: '' } } };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_ADDRESS_LINE_1_PROVIDED);
+    });
+
+    it('no zipCode is provided', () => {
+      const property = { data: { address: { zipCode: '' } } };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_ZIPCODE_PROVIDED);
+    });
+
+    it('no city is provided', () => {
+      const property = { data: { address: { place: '' } } };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_CITY_PROVIDED);
+    });
+
+    it('no country is provided', () => {
+      const property = { data: { address: { countryIsoCode: '' } } };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_COUNTRY_PROVIDED);
+    });
+
+    it('no residence type is provided', () => {
+      const property = {
+        data: {
+          residenceType: null,
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_RESIDENCE_TYPE_PROVIDED);
+    });
+
+    it('residence type is invalid', () => {
+      const property = {
+        data: { redidenceType: 'not known' },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.INVALID_RESIDENCE_TYPE);
+    });
+
+    it('no number of rooms is provided', () => {
+      const property = {
+        data: { numberOfRooms: null },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_NUMBER_OF_ROOMS_PROVIDED);
+    });
+
+    it('no construction year is provided', () => {
+      const property = {
+        data: { constructionYear: null },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_CONSTRUCTION_YEAR_PROVIDED);
+    });
+
+    it('no minergie certificate is provided', () => {
+      const property = {
+        data: { minergieCertificate: null },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_MINERGIE_CERTIFICATE_PROVIDED);
+    });
+
+    it('minergie certificate is invalid', () => {
+      const property = {
+        data: { minergieCertificate: 'not known' },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.INVALID_MINERGIE_CERTIFICATE);
+    });
+
+    it('no parking is provided', () => {
+      const property = {
+        data: { parking: null },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_PARKING_PROVIDED);
+    });
+
+    it('no inside parking is provided', () => {
+      const property = {
+        data: {
+          parking: {
+            indoor: null,
+          },
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_INSIDE_PARKING_PROVIDED);
+    });
+
+    it('no outside parking is provided', () => {
+      const property = {
+        data: {
+          parking: {
+            outdoor: null,
+          },
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_OUTSIDE_PARKING_PROVIDED);
+    });
+
+    it('no house type is provided', () => {
+      const property = { type: PROPERTY_TYPE.HOUSE, data: { houseType: '' } };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_HOUSE_TYPE_PROVIDED);
+    });
+
+    it('house type is invalid', () => {
+      const property = {
+        type: PROPERTY_TYPE.HOUSE,
+        data: { houseType: 'not known' },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.INVALID_HOUSE_TYPE);
+    });
+
+    it('no building volume is provided', () => {
+      const property = {
+        type: PROPERTY_TYPE.HOUSE,
+        data: { buildingVolume: {} },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_HOUSE_TYPE_PROVIDED);
+    });
+
+    it('no building volume value is provided', () => {
+      const property = {
+        type: PROPERTY_TYPE.HOUSE,
+        data: { buildingVolume: { value: null } },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_BUILDING_VOLUME_VALUE_PROVIDED);
+    });
+
+    it('no building volume type is provided', () => {
+      const property = {
+        type: PROPERTY_TYPE.HOUSE,
+        data: { buildingVolume: { type: '' } },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_BUILDING_VOLUME_TYPE_PROVIDED);
+    });
+
+    it('building volume type is invalid', () => {
+      const property = {
+        type: PROPERTY_TYPE.HOUSE,
+        data: { buildingVolume: { type: 'not known' } },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.INVALID_BUILDING_VOLUME_TYPE);
+    });
+
+    it('no landplot area is provided', () => {
+      const property = {
+        type: PROPERTY_TYPE.HOUSE,
+        data: { landPlotAres: null },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_LANDPLOT_AREA_PROVIDED);
+    });
+
+    it('no quality profile is provided', () => {
+      const property = {
+        data: { qualityProfile: null },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_QUALITY_PROFILE_PROVIDED);
+    });
+
+    it('no quality profile standard is provided', () => {
+      const property = {
+        data: {
+          qualityProfile: {
+            standard: null,
+          },
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_QUALITY_PROFILE_STANDARD_PROVIDED);
+    });
+
+    it('no quality profile condition is provided', () => {
+      const property = {
+        data: {
+          qualityProfile: {
+            condition: null,
+          },
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_QUALITY_PROFILE_CONDITION_PROVIDED);
+    });
+
+    it('no quality profile situation is provided', () => {
+      const property = {
+        data: {
+          qualityProfile: {
+            situation: null,
+          },
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_QUALITY_PROFILE_SITUATION_PROVIDED);
+    });
+
+    it('quality profile standard is invalid', () => {
+      const property = {
+        data: {
+          qualityProfile: {
+            standard: 'not known',
+          },
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.INVALID_QUALITY_PROFILE_STANDARD);
+    });
+
+    it('quality profile condition is invalid', () => {
+      const property = {
+        data: {
+          qualityProfile: {
+            condition: 'not known',
+          },
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.INVALID_QUALITY_PROFILE_CONDITION);
+    });
+
+    it('quality profile situation is invalid', () => {
+      const property = {
+        data: {
+          qualityProfile: {
+            situation: 'not known',
+          },
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.INVALID_QUALITY_PROFILE_SITUATION);
+    });
+
+    it('no flat type is provided', () => {
+      const property = {
+        type: PROPERTY_TYPE.FLAT,
+        data: {
+          flatType: null,
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_FLAT_TYPE_PROVIDED);
+    });
+
+    it('flat type is invalid', () => {
+      const property = {
+        type: PROPERTY_TYPE.FLAT,
+        data: {
+          flatType: 'not known',
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.INVALID_FLAT_TYPE);
+    });
+
+    it('no number of floors is provided', () => {
+      const property = {
+        type: PROPERTY_TYPE.FLAT,
+        data: {
+          numberOfFloors: null,
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_NUMBER_OF_FLOORS_PROVIDED);
+    });
+
+    it('floor number exceeds total number of floors', () => {
+      const property = {
+        type: PROPERTY_TYPE.FLAT,
+        data: {
+          numberOfFloors: 5,
+          floorType: FLOOR_NUMBER[6],
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.FLOOR_NUMBER_EXCEEDS_TOTAL_NUMBER_OF_FLOORS);
+    });
+
+    it('no usable area is provided', () => {
+      const property = {
+        type: PROPERTY_TYPE.FLAT,
+        data: {
+          usableArea: null,
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_USABLE_AREA_PROVIDED);
+    });
+
+    it('no usable area value is provided', () => {
+      const property = {
+        type: PROPERTY_TYPE.FLAT,
+        data: {
+          usableArea: {
+            value: null,
+          },
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_USABLE_AREA_VALUE_PROVIDED);
+    });
+
+    it('no usable area type is provided', () => {
+      const property = {
+        type: PROPERTY_TYPE.FLAT,
+        data: {
+          usableArea: {
+            type: null,
+          },
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.NO_USABLE_AREA_TYPE_PROVIDED);
+    });
+
+    it('usable area type is invalid', () => {
+      const property = {
+        type: PROPERTY_TYPE.FLAT,
+        data: {
+          usableArea: {
+            type: 'not known',
+          },
+        },
+      };
+      expect(WuestService.getErrors(property)).to.include(WUEST_ERRORS.INVALID_USABLE_AREA_TYPE);
+    });
+  });
+
+  describe('evaluateById', () => {
     it('returns min and max', () => {
       const propertyId = Factory.create('property', {
+        style: PROPERTY_STYLE.FLAT,
         address1: 'rue du four 2',
         zipCode: '1400',
         city: 'Yverdon-les-Bains',
