89eccb6dd443bc265049bb74d47049a69abe0d01
Quentin Herzig <quentin@e-potek.ch>
2018-07-30 16:18:58 +0200

    Add valuation to dashboard

---
+++ microservices/app/imports/client/pages/DashboardPage/DashboardRecap/DashboardRecapProperty/test/DashboardRecapProperty.spec.jsx
@@ -0,0 +1,45 @@
+/* eslint-env mocha */
+import { expect } from 'chai';
+import React from 'react';
+
+import { EXPERTISE_STATUS } from 'core/api/properties/propertyConstants';
+import { toMoney } from 'core/utils/conversionFunctions';
+import { getRecapArray } from '../DashboardRecapProperty';
+
+describe('getRecapArray', () => {
+  it('returns min and max if valuation is done', () => {
+    // Test code
+    const min = 12345;
+    const max = 123456;
+    const status = EXPERTISE_STATUS.DONE;
+    const property = {
+      address1: 'Rue du test 12',
+      zipCode: 1201,
+      city: 'Genève',
+      landArea: 129,
+      insideArea: 123,
+      valuation: {
+        status,
+        min,
+        max,
+      },
+    };
+    expect(getRecapArray(property)[3].value.props.children).to.contain(toMoney(min));
+    expect(getRecapArray(property)[3].value.props.children).to.contain(toMoney(max));
+  });
+
+  it('returns expertise status if valuation is not done', () => {
+    const status = EXPERTISE_STATUS.NONE;
+    const property = {
+      address1: 'Rue du test 12',
+      zipCode: 1201,
+      city: 'Genève',
+      landArea: 129,
+      insideArea: 123,
+      valuation: {
+        status,
+      },
+    };
+    expect(getRecapArray(property)[3].value.props.id).to.equal('property.expertiseStatus.NONE');
+  });
+});
