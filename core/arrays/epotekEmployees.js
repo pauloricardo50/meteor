358560f62826b8279efe6062f5fb7d69da3676df
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-08-02 11:30:19 +0200

    Remove jean-luc

--- core/arrays/epotekEmployees.js
+++ core/arrays/epotekEmployees.js
@@ -40,13 +40,14 @@ const employees = [
     title: 'Conseiller en financement',
     phone: '+41 22 566 82 90',
   },
-  {
-    email: 'jeanluc@e-potek.ch',
-    src: '/img/team/jeanluc.jpg',
-    name: 'Jean-Luc Kringel',
-    title: 'Responsable Assurances',
-    phone: '+41 22 566 82 94',
-  },
+  // Attendre début emploiement de Jean-Luc pour éviter problèmes avec chomage
+  // {
+  //   email: 'jeanluc@e-potek.ch',
+  //   src: '/img/team/jeanluc.jpg',
+  //   name: 'Jean-Luc Kringel',
+  //   title: 'Responsable Assurances',
+  //   phone: '+41 22 566 82 94',
+  // },
   {
     email: '',
     src: '/img/team/tarik.jpg',
