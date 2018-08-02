09b9c0f62bb53991cc1c5cd2f1d10355e496f5b0
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-08-02 15:02:19 +0200

    Copy over propertyFunctions

--- core/utils/LoanCalculator/PropertyCalculator.js
+++ core/utils/LoanCalculator/PropertyCalculator.js
@@ -1,8 +1,28 @@
 // @flow
-import { FinanceCalculator } from '../FinanceCalculator';
+import {
+  getPropertyArray,
+  getPropertyLoanArray,
+} from '../../arrays/PropertyFormArray';
+import { getPercent } from '../general';
+import { getCountedArray } from '../formArrayHelpers';
+import { FinanceCalculator } from '../../FinanceCalculator';
 
 export const withPropertyCalculator = (SuperClass = class {}) =>
-  class extends SuperClass {};
+  class extends SuperClass {
+    propertyPercent = ({ loan, borrowers, property }) => {
+      const formArray1 = getPropertyArray({ loan, borrowers, property });
+      const formArray2 = getPropertyLoanArray({
+        loan,
+        borrowers,
+        property,
+      });
+
+      let a = getCountedArray(formArray1, property);
+      a = [...a, getCountedArray(formArray2, loan)];
+
+      return getPercent(a);
+    };
+  };
 
 export const PropertyCalculator = withPropertyCalculator(FinanceCalculator);
 
