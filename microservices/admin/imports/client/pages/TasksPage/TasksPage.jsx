c7cec08b217387b8734eba0399b6ba35ac29da4d
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-08-06 11:08:04 +0200

    Add better assignedTo filter

--- microservices/admin/imports/client/pages/TasksPage/TasksPage.jsx
+++ microservices/admin/imports/client/pages/TasksPage/TasksPage.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import T from 'core/components/Translation';
 import { TASK_TYPE, TASK_STATUS } from 'core/api/constants';
+import adminsQuery from 'core/api/users/queries/admins';
 import TasksTableWithData from '../../components/TasksTable/TasksTableWithData';
 
+const getAdminsEmails = async () => {
+  const admins = await adminsQuery.clone().fetchSync();
+  const adminsEmails = admins.map(({ emails: [{ address }] }) => address);
+  return [...adminsEmails, undefined];
+};
+
 const tasksTableFilters = {
-  filters: { type: true, status: true },
+  filters: {
+    type: true,
+    status: true,
+    assignedEmployee: { emails: [{ address: true }] },
+  },
   options: {
     type: Object.values(TASK_TYPE),
     status: Object.values(TASK_STATUS),
+    address: getAdminsEmails(),
   },
 };
 
