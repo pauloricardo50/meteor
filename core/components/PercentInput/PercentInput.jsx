1384c26164990c33b2d38b0226015495f5a3f268
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-08-07 14:04:45 +0200

    Add PercentInput

---
+++ core/components/PercentInput/PercentInput.jsx
@@ -0,0 +1,24 @@
+// @flow
+import React from 'react';
+import Input from '@material-ui/core/Input';
+import MaskedInput from 'react-text-mask';
+import { percentMask } from '../../utils/textMasks';
+import { percentFormatters } from '../Form/formHelpers';
+
+type PercentInputProps = {
+  onChange: Function,
+  value: number,
+};
+
+const PercentInput = ({ onChange, value, ...props }: PercentInputProps) => (
+  <Input
+    onChange={event => onChange(percentFormatters.parse(event.target.value))}
+    value={percentFormatters.format(value)}
+    inputComponent={MaskedInput}
+    inputProps={{ mask: percentMask }}
+    type="tel"
+    {...props}
+  />
+);
+
+export default PercentInput;
