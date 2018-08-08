38621d45f33fa21d984cdae3c8655645bcd6e9f4
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-08-08 18:04:32 +0200

    Display loadable errors with better loading component

--- core/utils/loadable.js
+++ core/utils/loadable.js
@@ -1,9 +1,24 @@
+import React from 'react';
 import Loadable from 'react-loadable';
 import Loading from '../components/Loading';
 
+const LoadableLoading = ({ error, retry, pastDelay }) => {
+  if (error) {
+    return (
+      <div>
+        Error: {error.message} <button onClick={retry}>Retry</button>
+      </div>
+    );
+  }
+  if (pastDelay) {
+    return <Loading />;
+  }
+  return null;
+};
+
 export default options =>
   Loadable({
-    loading: Loading,
+    loading: LoadableLoading,
     delay: 200, // Hides the loading component for 200ms, to avoid flickering
     ...options,
   });
