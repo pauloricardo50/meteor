99f492967f4f31ccbf3b897bfa379e9744f18638
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-08-09 15:11:17 +0200

    Clean up component

--- core/components/UploaderArray/Uploader/TempFile.jsx
+++ core/components/UploaderArray/Uploader/TempFile.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Tracker } from 'meteor/tracker';
 import { Slingshot } from 'meteor/edgee:slingshot';
-
 import CircularProgress from '@material-ui/core/CircularProgress';
 import LinearProgress from '@material-ui/core/LinearProgress';
+
+import { SLINGSHOT_DIRECTIVE_NAME } from '../../../api/constants';
 
 export default class TempFile extends Component {
   constructor(props) {
@@ -13,33 +14,45 @@ export default class TempFile extends Component {
   }
 
   componentDidMount() {
-    this.uploader = new Slingshot.Upload('myFileUploads', {
-      currentValue: this.props.currentValue,
-      collection: this.props.collection,
-      docId: this.props.docId,
-      id: this.props.id,
-      uploadCount: this.props.uploadCount,
+    const {
+      currentValue,
+      collection,
+      docId,
+      id,
+      uploadCount,
+      file,
+      handleSave,
+    } = this.props;
+
+    this.uploader = new Slingshot.Upload(SLINGSHOT_DIRECTIVE_NAME, {
+      currentValue,
+      collection,
+      docId,
+      id,
+      uploadCount,
     });
 
     const progressSetter = Tracker.autorun(() => {
       this.setState({ progress: this.uploader.progress() });
     });
 
-    this.uploader.send(this.props.file, (error, downloadUrl) => {
+    this.uploader.send(file, (error, downloadUrl) => {
       progressSetter.stop();
       if (error) {
-        // TODO: Show an error here
-        console.log('TempFile error', error);
         this.setState({ error: error.message });
       } else {
-        this.props.handleSave(this.props.file, downloadUrl);
+        handleSave(file, downloadUrl);
       }
     });
   }
 
   render() {
-    const { name } = this.props.file;
+    const {
+      file: { name },
+    } = this.props;
     const { progress, error } = this.state;
+
+    const fileIsUploading = !Number.isNaN(progress);
 
     return (
       <div className="flex-col">
@@ -48,31 +61,32 @@ export default class TempFile extends Component {
           style={{ height: 48, justifyContent: 'flex-start' }}
         >
           <h5 className="secondary bold">{name}</h5>
-          {!Number.isNaN(progress) ? (
-            <div style={{ paddingLeft: 16, flexGrow: 1 }}>
-              <LinearProgress
-                color="primary"
-                variant="determinate"
-                value={Math.round(progress * 100)}
-              />
-            </div>
-          ) : (
-            <div style={{ paddingLeft: 16 }}>
-              <CircularProgress size={24} color="primary" />
-            </div>
-          )}
+          {!error
+            && (fileIsUploading ? (
+              <div style={{ paddingLeft: 16, flexGrow: 1 }}>
+                <LinearProgress
+                  color="primary"
+                  variant="determinate"
+                  value={Math.round(progress * 100)}
+                />
+              </div>
+            ) : (
+              <div style={{ paddingLeft: 16 }}>
+                <CircularProgress size={24} color="primary" />
+              </div>
+            ))}
         </div>
-        {!error && <p className="error">{error}</p>}
+        {error && <p className="error">{error}</p>}
       </div>
     );
   }
 }
 
 TempFile.propTypes = {
+  collection: PropTypes.string.isRequired,
+  currentValue: PropTypes.arrayOf(PropTypes.object).isRequired,
+  docId: PropTypes.string.isRequired,
   file: PropTypes.objectOf(PropTypes.any).isRequired,
   handleSave: PropTypes.func.isRequired,
-  docId: PropTypes.string.isRequired,
-  collection: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
-  currentValue: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
