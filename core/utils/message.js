6059183cde23d2fb2b3fac355267680c9cc80aff
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-08-14 16:39:00 +0200

    Add link to docs for antd API

--- core/utils/message.js
+++ core/utils/message.js
@@ -1,4 +1,6 @@
 import message from 'antd/lib/message';
 import 'antd/lib/message/style/index.css';
 
+// API: https://ant.design/components/message/
+
 export default message;
--- core/utils/notification.js
+++ core/utils/notification.js
@@ -1,4 +1,6 @@
 import notification from 'antd/lib/notification';
 import 'antd/lib/notification/style/index.css';
 
+// API: https://ant.design/components/notification/
+
 export default notification;
