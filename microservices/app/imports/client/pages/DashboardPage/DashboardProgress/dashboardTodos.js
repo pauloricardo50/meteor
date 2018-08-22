fe5707f0e454012a47b340853b454794990f213a
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-08-22 10:31:50 +0200

    Reorder todos

--- microservices/app/imports/client/pages/DashboardPage/DashboardProgress/dashboardTodos.js
+++ microservices/app/imports/client/pages/DashboardPage/DashboardProgress/dashboardTodos.js
@@ -20,15 +20,19 @@ const createSinglePropertyLink = ({ _id: loanId, structure: { propertyId } }) =>
 
 export const dashboardTodosArray = [
   {
-    id: 'createStructure',
-    condition: ({ structures }) => structures.length === 0,
-    link: createFinancingLink,
-  },
-  {
-    id: 'addProperty',
-    condition: ({ properties }) => properties.length === 0,
+    id: 'completeBorrowers',
+    condition: ({ borrowers }) => {
+      const percentages = borrowers.map(borrower =>
+        BorrowerCalculator.personalInfoPercent({ borrowers: borrower }));
+
+      if (percentages.some(percent => percent < 1)) {
+        return true;
+      }
+
+      return false;
+    },
     link: ({ _id: loanId }) =>
-      createRoute(PROPERTIES_PAGE, { ':loanId': loanId }),
+      createRoute(BORROWERS_PAGE, { ':loanId': loanId, ':tabId': 'personal' }),
   },
   {
     id: 'completeProperty',
@@ -58,31 +62,21 @@ export const dashboardTodosArray = [
     link: createSinglePropertyLink,
   },
   {
-    id: 'completeBorrowers',
-    condition: ({ borrowers }) => {
-      const percentages = borrowers.map(borrower =>
-        BorrowerCalculator.personalInfoPercent({ borrowers: borrower }));
-
-      if (percentages.some(percent => percent < 1)) {
-        return true;
-      }
-
-      return false;
-    },
-    link: ({ _id: loanId }) =>
-      createRoute(BORROWERS_PAGE, { ':loanId': loanId, ':tabId': 'personal' }),
-  },
-  {
-    id: 'chooseOffer',
-    condition: ({ offers, structure: { offer } }) =>
-      offers.length > 0 && !offer,
-    hide: ({ offers }) => offers.length === 0,
+    id: 'createStructure',
+    condition: ({ structures }) => structures.length === 0,
     link: createFinancingLink,
   },
   {
     id: 'createSecondStructure',
     condition: ({ structures }) => structures.length === 1,
     hide: ({ structures }) => structures.length === 0,
+    link: createFinancingLink,
+  },
+  {
+    id: 'chooseOffer',
+    condition: ({ offers, structure: { offer } }) =>
+      offers.length > 0 && !offer,
+    hide: ({ offers }) => offers.length === 0,
     link: createFinancingLink,
   },
   {
