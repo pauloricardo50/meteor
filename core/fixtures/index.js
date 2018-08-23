87c99da9215c833f09dab90ee0ba2f1cc70bd57d
Quentin Herzig <quentin@e-potek.ch>
2018-08-23 12:46:26 +0200

    Add demo fixture

--- core/fixtures/index.js
+++ core/fixtures/index.js
@@ -2,9 +2,14 @@ import { Meteor } from 'meteor/meteor';
 import { ROLES } from 'core/api/constants';
 import { createFakeUsers } from './userFixtures';
 import { DEV_COUNT } from './fixtureConfig';
+import { createYannisUser } from './demoFixtures';
 
 Meteor.startup(() => {
   if (Meteor.users.find({ roles: { $in: [ROLES.DEV] } }).count() === 0) {
     createFakeUsers(DEV_COUNT, ROLES.DEV);
+  }
+
+  if (Meteor.users.find({ 'emails.address': 'y@nnis.ch' }).count() === 0) {
+    createYannisUser();
   }
 });
