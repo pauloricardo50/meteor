b58a5df8c26ec7a86d8fea98718cc9aa5ae6e6e7
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-09-03 19:09:59 +0200

    Use proper service method

--- core/fixtures/offerFixtures.js
+++ core/fixtures/offerFixtures.js
@@ -10,5 +10,5 @@ export const createFakeOffer = (loanId, userId) => {
     true,
   );
 
-  return OfferService.insertAdminOffer({ offer, loanId: loan._id, userId });
+  return OfferService.offerInsert({ offer, loanId: loan._id, userId });
 };
