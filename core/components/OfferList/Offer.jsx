2d21dbb58ad3da19a32c3a8035d7886bac93778e
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-09-03 09:48:24 +0200

    Refactor Offer

--- core/components/OfferList/Offer.jsx
+++ core/components/OfferList/Offer.jsx
@@ -2,34 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { offerDelete } from '../../api';
-import { T, IntlNumber } from '../Translation';
 import ConfirmMethod from '../ConfirmMethod';
+import OfferField from './OfferField';
 
 const Offer = ({ offer, offerValues }) => (
   <div className="offer-list-item">
     <h3>{offer.organization}</h3>
     <div className="offer-list-item-detail">
-      {offerValues.map(({ key, component, format, value, id }) => {
-        if (component) {
-          return (
-            <div className="offer-list-item-value" key={id || key}>
-              {component}
-            </div>
-          );
-        }
-
-        return (
-          <div className="value offer-list-item-value" key={id || key}>
-            <label className="secondary" htmlFor={id || key}>
-              <T id={`offer.${id || key}`} />
-            </label>
-            <h4 className="bold" id={id || key}>
-              {/* if it has an id, it has a value */}
-              {value || <IntlNumber value={offer[key]} format={format} />}
-            </h4>
-          </div>
-        );
-      })}
+      {offerValues.map(offerValue => (
+        <OfferField key={offer._id} offerValue={offerValue} offer={offer} />
+      ))}
       <ConfirmMethod
         label="Supprimer"
         keyword="Supprimer"
---
+++ core/components/OfferList/OfferField.jsx
@@ -0,0 +1,29 @@
+// @flow
+import React from 'react';
+
+import { T, IntlNumber } from '../Translation';
+
+type OfferFieldProps = {};
+
+const OfferField = ({
+  offer,
+  offerValue: { key, component, format, value, id },
+}: OfferFieldProps) => {
+  if (component) {
+    return <div className="offer-list-item-value">{component}</div>;
+  }
+
+  return (
+    <div className="value offer-list-item-value">
+      <label className="secondary" htmlFor={id || key}>
+        <T id={`offer.${id || key}`} />
+      </label>
+      <h4 className="bold" id={id || key}>
+        {/* if it has an id, it has a value */}
+        {value || <IntlNumber value={offer[key]} format={format} />}
+      </h4>
+    </div>
+  );
+};
+
+export default OfferField;
