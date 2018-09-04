2f02b87571563d262d662ef7473e3daeaa4ada82
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-09-04 17:09:06 +0200

    Improve passwordresetpage styling

--- microservices/app/imports/client/pages/PasswordResetPage/PasswordResetPage.jsx
+++ microservices/app/imports/client/pages/PasswordResetPage/PasswordResetPage.jsx
@@ -1,5 +1,7 @@
 // @flow
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/pro-light-svg-icons';
 
 import TextField from 'core/components/Material/TextField';
 import Loading from 'core/components/Loading/Loading';
@@ -37,38 +39,43 @@ export const PasswordResetPage = ({
       id="password-reset-page"
       className="password-reset-page"
     >
-      <h1>
-        <T
-          id="PasswordResetPage.title"
-          values={{ name: getUserDisplayName(user) }}
+      <div className="card1 card-top">
+        <div className="top">
+          <div className="text">
+            <FontAwesomeIcon icon={faUserCircle} className="icon" />
+            <h1>{getUserDisplayName(user)}</h1>
+          </div>
+          <h3 className="secondary">
+            <T id="PasswordResetPage.description" />
+          </h3>
+        </div>
+        <TextField
+          label={<T id="PasswordResetPage.password" />}
+          floatingLabelFixed
+          type="password"
+          value={newPassword}
+          onChange={e => handleChange(e, 'newPassword')}
+          className="password-reset-page-input"
         />
-      </h1>
-      <TextField
-        label={<T id="PasswordResetPage.password" />}
-        floatingLabelFixed
-        type="password"
-        value={newPassword}
-        onChange={e => handleChange(e, 'newPassword')}
-        className="password-reset-page-input"
-      />
-      <TextField
-        label={<T id="PasswordResetPage.confirmPassword" />}
-        floatingLabelFixed
-        type="password"
-        value={newPassword2}
-        onChange={e => handleChange(e, 'newPassword2')}
-        className="password-reset-page-input"
-      />
+        <TextField
+          label={<T id="PasswordResetPage.confirmPassword" />}
+          floatingLabelFixed
+          type="password"
+          value={newPassword2}
+          onChange={e => handleChange(e, 'newPassword2')}
+          className="password-reset-page-input"
+        />
 
-      <div className="password-reset-page-button">
-        <Button
-          raised
-          label={<T id="PasswordResetPage.CTA" />}
-          disabled={!isValid}
-          type="submit"
-          primary
-          loading={submitting}
-        />
+        <div className="password-reset-page-button">
+          <Button
+            raised
+            label={<T id="PasswordResetPage.CTA" />}
+            disabled={!isValid}
+            type="submit"
+            primary
+            loading={submitting}
+          />
+        </div>
       </div>
     </form>
   );
