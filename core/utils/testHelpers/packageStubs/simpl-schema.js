e48b5e9a6ddcc13ef5c86a022308bca1f06f5b9d
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-09-07 16:07:26 +0200

    Add stub for simpl-schema in wallaby
    Because simpl-schema depends on meteor packages like deny and unique

---
+++ core/utils/testHelpers/packageStubs/simpl-schema.js
@@ -0,0 +1,7 @@
+// This mock is necessary because simple schema is extended by small meteor
+// packages that wallaby does not know about, like aldeed:schema-index and aldeed: schema - deny
+class SimpleSchema {
+  static RegEx = { Email: undefined };
+}
+
+export default SimpleSchema;
