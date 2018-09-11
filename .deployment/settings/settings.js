81c9fcf0e4fccb6372db23f483359026fdd0e381
Quentin Herzig <quentin@e-potek.ch>
2018-09-11 09:41:14 +0200

    Add environment variables for deployment

--- .deployment/settings/settings.js
+++ .deployment/settings/settings.js
@@ -21,6 +21,7 @@ import {
   SPACES,
   TMUXINATOR_SESSION_NAME,
   TMUXINATOR_YML,
+  APP_ENV_VARIABLES,
 } from './config.js';
 
 import { readJSONFile, formatOptionsArray } from '../utils/helpers';
@@ -91,6 +92,9 @@ export const appManifestYAMLData = ({
       instances,
       buildpack: APP_BUILDPACK,
       services: [service],
+      ...(APP_ENV_VARIABLES[environment][applicationName] !== {}
+        ? { env: APP_ENV_VARIABLES[environment][applicationName] }
+        : null),
     },
   ],
 });
