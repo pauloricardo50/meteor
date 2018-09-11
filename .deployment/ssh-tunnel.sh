87a2505136fa13ca340c1c2762c5ef72d1fdeb8c
Quentin Herzig <quentin@e-potek.ch>
2018-09-11 14:44:05 +0200

    Run ssh-tunnel in a tmux environment

--- .deployment/ssh-tunnel.sh
+++ .deployment/ssh-tunnel.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+# Installs dependencies
+$SCRIPTPATH/../scripts/installTmuxinator.sh
+$SCRIPTPATH/checkPackage.sh mongodb install
+
 ../scripts/box_out.sh "Establishing a SSH tunnel with args:" "$*"
-babel-node -- ./ssh-tunnel/createSSHTunnel.js "$@" 
+babel-node -- ./ssh-tunnel/prepareSSHTunnel.js "$@" 
+tmuxinator start ssh-tunnel -n ssh-tunnel -p ./ssh-tunnel/ssh-tunnel.yml
+rm ./ssh-tunnel/ssh-tunnel.yml
