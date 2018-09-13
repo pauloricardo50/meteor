f9cb072ce18d82f6ed5c6d45923f6e878766a645
Quentin Herzig <quentin@e-potek.ch>
2018-09-13 14:05:55 +0200

    Remove mongo service creation

--- .deployment/CloudFoundry/CloudFoundryService.js
+++ .deployment/CloudFoundry/CloudFoundryService.js
@@ -9,26 +9,6 @@ class CloudFoundryService {
     return executeCommand(cloudFoundryCommands.selectSpace(space));
   };
 
-  createMongoDBService = ({ plan, serviceInstance }) => {
-    return this.checkIfServiceInstanceExists(serviceInstance).then(
-      serviceExists =>
-        !serviceExists &&
-        executeCommand(
-          cloudFoundryCommands.createService({
-            plan,
-            serviceInstance,
-            service: CLOUDFOUNDRY_MARKETPLACE.MONGO_DB.service,
-          }),
-        ),
-    );
-  };
-
-  checkIfServiceInstanceExists = serviceInstance => {
-    return executeCommand(cloudFoundryCommands.listServices()).then(
-      servicesList => servicesList.includes(serviceInstance),
-    );
-  };
-
   getScaleApplicationCommand = ({ space, applicationName, config }) => {
     return this.selectSpace(space).then(() =>
       cloudFoundryCommands.scale({ appName: applicationName, ...config }),
