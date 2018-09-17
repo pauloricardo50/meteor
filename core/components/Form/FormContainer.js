92edea7480c650dd8cd70e94bb981a4414967a2e
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-09-17 11:17:31 +0200

    Pass onSubmit function with error handling

--- core/components/Form/FormContainer.js
+++ core/components/Form/FormContainer.js
@@ -1,16 +1,20 @@
 import { compose, withProps } from 'recompose';
 import { injectIntl } from 'react-intl';
-import { reduxForm } from 'redux-form';
+import { reduxForm, SubmissionError } from 'redux-form';
 
 export default compose(
   injectIntl,
-  withProps(({ formArray, intl: { formatMessage }, intlPrefix }) => ({
+  withProps(({ formArray, intl: { formatMessage }, intlPrefix, onSubmit }) => ({
     formArray: formArray.map(field => ({
       ...field,
       placeholder: field.placeholder
         ? formatMessage({ id: `${intlPrefix}.${field.id}.placeholder` })
         : undefined,
     })),
+    onSubmit: (...args) =>
+      onSubmit(...args).catch((submitError) => {
+        throw new SubmissionError({ _error: submitError.message });
+      }),
   })),
   reduxForm(),
 );
