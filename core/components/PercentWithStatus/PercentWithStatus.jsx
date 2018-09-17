93a752db746381164e1d9821d415752676f82128
Quentin Herzig <quentin@e-potek.ch>
2018-09-17 11:53:01 +0200

    Add tooltips on status icons

--- core/components/PercentWithStatus/PercentWithStatus.jsx
+++ core/components/PercentWithStatus/PercentWithStatus.jsx
@@ -7,17 +7,27 @@ import { SUCCESS, ERROR } from '../../api/constants';
 
 type PercentWithStatusProps = {
   value: number,
-  status?: string,
+  status?: String,
+  tooltip?: String,
+  id?: String,
 };
 
 const PercentWithStatus = ({
   value,
   status = value >= 1 ? SUCCESS : ERROR,
+  id,
+  tooltip,
 }: PercentWithStatusProps) => (
   <React.Fragment>
     {Number.isNaN(value) ? '-' : <Percent value={value} />}
     {!Number.isNaN(value) && (
-      <StatusIcon status={status} className="icon" style={{ marginLeft: 4 }} />
+      <StatusIcon
+        status={status}
+        className="icon"
+        style={{ marginLeft: 4 }}
+        tooltip={tooltip}
+        id={id}
+      />
     )}
   </React.Fragment>
 );
