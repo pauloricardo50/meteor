0998c7c4ab913b7d96db9c0ac6861f2fbbb5a4f5
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-09-23 20:23:51 +0200

    Fix www tests
    React-helmet has a nasty bug, but there's a fix

--- core/cypress/plugins.js
+++ core/cypress/plugins.js
@@ -1,9 +1,17 @@
+const browserify = require('@cypress/browserify-preprocessor');
+
 module.exports = (on, config) => {
   console.log('---------------');
   console.log('Cypress Config');
   console.log('---------------');
   console.log(config);
 
+  // Fix for flow types issue: https://github.com/cypress-io/cypress/issues/2350#issuecomment-419772427
+  const options = browserify.defaultOptions;
+  options.browserifyOptions.transform[1][1].presets.push('@babel/preset-flow');
+  on('file:preprocessor', browserify(options));
+
+  // Store stuff properly when running in Circle CI
   if (!process.env.CIRCLE_CI) {
     config.screenshotsFolder = 'cypress/logs/screenshots';
     config.videosFolder = 'cypress/logs/videos';
