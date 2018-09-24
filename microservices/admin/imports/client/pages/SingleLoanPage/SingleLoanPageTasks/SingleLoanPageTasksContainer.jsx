040eb7806383067e4e09961b3baebe7707571984
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-09-24 11:18:17 +0200

    Reduce amount of reactive queries

--- microservices/admin/imports/client/pages/SingleLoanPage/SingleLoanPageTasks/SingleLoanPageTasksContainer.jsx
+++ microservices/admin/imports/client/pages/SingleLoanPage/SingleLoanPageTasks/SingleLoanPageTasksContainer.jsx
@@ -2,13 +2,12 @@ import { withSmartQuery } from 'core/api';
 import { compose } from 'recompose';
 import loanTasks from 'core/api/tasks/queries/loanTasks';
 import withTableFilters from 'core/containers/withTableFilters';
-import TasksTable from '../../../components/TasksTable/TasksTable';
 
 export default compose(
   withSmartQuery({
-    query: ({ borrowerIds, loanId, propertyIds }) =>
+    query: ({ loan: { _id: loanId, propertyIds, borrowerIds } }) =>
       loanTasks.clone({ borrowerIds, loanId, propertyIds }),
-    queryOptions: { reactive: true },
+    queryOptions: { reactive: false },
   }),
   withTableFilters,
-)(TasksTable);
+);
