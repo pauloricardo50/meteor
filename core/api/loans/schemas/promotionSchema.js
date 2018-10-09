e52656a896af0cda154021a0fd6720237b8ab7b7
Quentin Herzig <quentin@e-potek.ch>
2018-10-09 16:06:32 +0200

    Add empty array as default value for promotion links in loans

--- core/api/loans/schemas/promotionSchema.js
+++ core/api/loans/schemas/promotionSchema.js
@@ -1,10 +1,10 @@
 const promotionSchema = {
-  promotionLinks: { type: Array, optional: true },
+  promotionLinks: { type: Array, defaultValue: [] },
   'promotionLinks.$': Object,
   'promotionLinks.$._id': String,
   'promotionLinks.$.priorityOrder': { type: Array, optional: true },
   'promotionLinks.$.priorityOrder.$': String,
-  promotionOptionLinks: { type: Array, optional: true },
+  promotionOptionLinks: { type: Array, defaultValue: [] },
   'promotionOptionLinks.$': Object,
   'promotionOptionLinks.$._id': String,
 };
