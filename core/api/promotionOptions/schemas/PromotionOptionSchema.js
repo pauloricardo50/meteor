bcd52c4d1477a0d1ed2c24d67e138ea325dde496
Quentin Herzig <quentin@e-potek.ch>
2018-10-09 16:08:55 +0200

    Add empty array as default value for links in promotion options

--- core/api/promotionOptions/schemas/PromotionOptionSchema.js
+++ core/api/promotionOptions/schemas/PromotionOptionSchema.js
@@ -2,7 +2,8 @@ import SimpleSchema from 'simpl-schema';
 import { PROMOTION_OPTION_STATUS } from '../promotionOptionConstants';
 
 const PromotionOptionSchema = new SimpleSchema({
-  promotionLotLinks: [Object],
+  promotionLotLinks: { type: Array, defaultValue: [] },
+  'promotionLotLinks.$': Object,
   'promotionLotLinks.$._id': String,
   lotsOverrideLinks: { type: Array, optional: true },
   'lotsOverrideLinks.$': Object,
