20354278c1d3988c88a6ac17dae9ef70d7dbfedb
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-10-09 12:09:29 +0200

    Move email verification page to core

--- microservices/pro/imports/startup/client/ProRouter.jsx
+++ microservices/pro/imports/startup/client/ProRouter.jsx
@@ -12,6 +12,8 @@ import { getUserLocale, getFormats } from 'core/utils/localization';
 import DevPage from 'core/components/DevPage';
 import ImpersonatePage from 'core/components/Impersonate/ImpersonatePage';
 import { IMPERSONATE_ROUTE } from 'core/api/impersonation/impersonation';
+import PasswordResetPage from 'core/components/PasswordResetPage';
+import EmailVerificationPage from 'core/components/EmailVerificationPage';
 import ProLayout from '../../client/layout';
 import messagesFR from '../../../lang/fr.json';
 import * as ROUTES from './proRoutes';
@@ -27,6 +29,18 @@ const ProRouter = (props: ProRouterProps) => (
     <ProLayout>
       <Switch>
         <Route path={ROUTES.DEV_PAGE} component={DevPage} />
+        <Route
+          path={ROUTES.PASSWORD_RESET_PAGE}
+          component={PasswordResetPage}
+        />
+        <Route
+          path={ROUTES.ENROLL_ACCOUNT_PAGE}
+          component={PasswordResetPage}
+        />
+        <Route
+          path={ROUTES.EMAIL_VERIFICATION_PAGE}
+          component={EmailVerificationPage}
+        />
         <Route exact path={IMPERSONATE_ROUTE} component={ImpersonatePage} />
         <Route component={NotFound} />
       </Switch>
--- microservices/pro/imports/startup/client/proRoutes.js
+++ microservices/pro/imports/startup/client/proRoutes.js
@@ -1,1 +1,6 @@
+export const ACCOUNT_PAGE = '/account';
 export const DEV_PAGE = '/dev';
+export const EMAIL_VERIFICATION_PAGE = '/verify-email/:token';
+export const ENROLL_ACCOUNT_PAGE = '/enroll-account/:token';
+export const PASSWORD_RESET_PAGE = '/reset-password/:token';
+export const PRO_DASHBOARD_PAGE = '/';
