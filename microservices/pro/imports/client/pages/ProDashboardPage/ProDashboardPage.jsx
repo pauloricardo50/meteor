9ba814a7f1a8d37527baac70e30563651b1599e8
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-10-09 18:37:58 +0200

    Add some titles

--- microservices/pro/imports/client/pages/ProDashboardPage/ProDashboardPage.jsx
+++ microservices/pro/imports/client/pages/ProDashboardPage/ProDashboardPage.jsx
@@ -3,26 +3,25 @@ import React from 'react';
 
 import Button from 'core/components/Button';
 import T from 'core/components/Translation';
-import {
-  NEW_PROMOTION_PAGE,
-  NEW_PROPERTY_PAGE,
-} from 'imports/startup/client/proRoutes';
+import { NEW_PROMOTION_PAGE } from 'imports/startup/client/proRoutes';
 import PromotionsTable from './PromotionsTable';
 
 type ProDashboardPageProps = {};
 
 const ProDashboardPage = ({ currentUser }: ProDashboardPageProps) => (
   <div className="card1 pro-dashboard-page">
-    <h1>Hello Pro</h1>
+    <h1>
+      <T id="ProDashboardPage.title" />
+    </h1>
     <div className="buttons">
-      {/* <Button raised primary link to={NEW_PROPERTY_PAGE}>
-        <T id="ProDashboardPage.addProperty" />
-      </Button> */}
       <Button raised primary link to={NEW_PROMOTION_PAGE}>
         <T id="ProDashboardPage.addPromotion" />
       </Button>
     </div>
 
+    <h3 className="text-center">
+      <T id="ProDashboardPage.promotions" />
+    </h3>
     <PromotionsTable currentUser={currentUser} />
   </div>
 );
