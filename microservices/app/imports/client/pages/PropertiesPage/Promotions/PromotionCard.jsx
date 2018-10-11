13862f6c46fb0b1ce99a21b5f22d6134c96664de
Quentin Herzig <quentin@e-potek.ch>
2018-10-11 12:22:35 +0200

    Working on app promotions

---
+++ microservices/app/imports/client/pages/PropertiesPage/Promotions/PromotionCard.jsx
@@ -0,0 +1,43 @@
+// @flow
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { createRoute } from 'core/utils/routerUtils';
+import { APP_PROMOTION_PAGE } from 'imports/startup/client/appRoutes';
+import promotionFiles from 'core/api/promotions/queries/promotionFiles';
+import mergeFilesWithQuery from 'core/api/files/mergeFilesWithQuery';
+
+type PromotionCardProps = {
+  promotion: Object,
+  loanId: string,
+};
+
+const PromotionCard = ({ promotion, loanId }: PromotionCardProps) => {
+  const { name, documents } = promotion;
+  console.log('promotion', JSON.stringify(promotion, null, 2));
+
+  const { promotionImage = [{ url: '/img/placeholder.png' }] } = documents || {};
+  return (
+    <Link
+      to={createRoute(APP_PROMOTION_PAGE, {
+        ':promotionId': promotion._id,
+        ':loanId': loanId,
+      })}
+      className="card1 card-hover promotion-card"
+    >
+      <React.Fragment>
+        <span
+          style={{ backgroundImage: `url(${promotionImage[0].url})` }}
+          className="promotion-image"
+        />
+        <h2>{name}</h2>
+        <h3 className="secondary">{promotion.address}</h3>
+      </React.Fragment>
+    </Link>
+  );
+};
+
+export default mergeFilesWithQuery(
+  promotionFiles,
+  ({ promotion: { _id: promotionId } }) => ({ promotionId }),
+  'promotion',
+)(PromotionCard);
--- microservices/app/imports/client/pages/PropertiesPage/PropertiesPage.jsx
+++ microservices/app/imports/client/pages/PropertiesPage/PropertiesPage.jsx
@@ -5,35 +5,51 @@ import T from 'core/components/Translation';
 import { PropertyAdder } from 'core/components/PropertyForm';
 import Page from 'core/components/Page';
 import PropertiesPageDetail from './PropertiesPageDetail';
+import PromotionDetail from './Promotions/PromotionDetail';
 
-const PropertiesPage = ({ loan: { _id: loanId, properties } }) => (
-  <Page id="PropertiesPage" titleId="PropertiesPage.title">
-    <section className="card1 card-top properties-page">
-      <div className="properties">
-        {properties.map(property => (
-          <PropertiesPageDetail
-            property={property}
+const PropertiesPage = ({ loan: { _id: loanId, properties, promotions } }) => {
+  console.log('promotions', promotions);
+
+  return (
+    <Page id="PropertiesPage" titleId="PropertiesPage.title">
+      <section className="card1 card-top properties-page">
+        <div className="promotions">
+          {promotions
+            && promotions.map(promotion => (
+              <PromotionDetail
+                promotion={promotion}
+                loanId={loanId}
+                key={promotion._id}
+              />
+            ))}
+        </div>
+        <hr />
+        <div className="properties">
+          {properties.map(property => (
+            <PropertiesPageDetail
+              property={property}
+              loanId={loanId}
+              key={property._id}
+            />
+          ))}
+
+          <PropertyAdder
             loanId={loanId}
-            key={property._id}
+            button={(
+              <div className="property-adder-button">
+                <span className="plus">+</span>
+                <h3>
+                  <T id="PropertyForm.adderLabel" />
+                </h3>
+              </div>
+            )}
+            className="properties-page-detail card1 card1-top card-hover"
           />
-        ))}
-        
-        <PropertyAdder
-          loanId={loanId}
-          button={(
-            <div className="property-adder-button">
-              <span className="plus">+</span>
-              <h3>
-                <T id="PropertyForm.adderLabel" />
-              </h3>
-            </div>
-          )}
-          className="properties-page-detail card1 card1-top card-hover"
-        />
-      </div>
-    </section>
-  </Page>
-);
+        </div>
+      </section>
+    </Page>
+  );
+};
 
 PropertiesPage.propTypes = {
   loan: PropTypes.objectOf(PropTypes.any).isRequired,
