b10f4e0c1da0acdafae7bcd555c1a1e838ae3f4c
Quentin Herzig <quentin@e-potek.ch>
2018-10-19 13:22:20 +0200

    Add label overrides

--- core/components/PromotionPage/client/utils.js
+++ core/components/PromotionPage/client/utils.js
@@ -1,9 +1,19 @@
 import { PROMOTION_LOT_STATUS } from '../../../api/constants';
+import colors from '../../../config/colors';
 
-export const getLabelSuffix = ({ attributedToMe, status }) => {
-  if (!attributedToMe) {
-    return '';
+export const getLabelOtherProps = ({ attributedToMe, status }) => {
+  switch (status) {
+  case PROMOTION_LOT_STATUS.BOOKED:
+    return attributedToMe
+      ? { suffix: ' pour moi' }
+      : { labelOverride: 'Non disponible', colorOverride: colors.warning };
+  case PROMOTION_LOT_STATUS.AVAILABLE:
+    return {};
+  case PROMOTION_LOT_STATUS.SOLD:
+    return attributedToMe
+      ? { suffix: ' à moi', colorOverride: colors.success }
+      : {};
+  default:
+    return {};
   }
-
-  return `${status === PROMOTION_LOT_STATUS.BOOKED ? ' pour' : ' à'} moi`;
 };
