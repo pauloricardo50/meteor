2367b2a54e786e5191e9cbb6aef6838ffe1aed57
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-10-19 18:26:09 +0200

    Display verificationStatus on PRO

--- microservices/pro/imports/client/pages/ProPromotionLotPage/PromotionProgress.jsx
+++ microservices/pro/imports/client/pages/ProPromotionLotPage/PromotionProgress.jsx
@@ -4,46 +4,61 @@ import React from 'react';
 import { Percent } from 'core/components/Translation';
 import Icon from 'core/components/Icon';
 import Tooltip from 'core/components/Material/Tooltip';
-// import { LOAN_VERIFICATION_STATUS } from 'core/api/constants';
+import { LOAN_VERIFICATION_STATUS } from 'core/api/constants';
 
 type PromotionProgressProps = {};
 
-const getIcon = (status) => {
+const getVerificationData = (status) => {
   switch (status) {
-  // case LOAN_VERIFICATION_STATUS.NONE:
-  //   return 'close';
-  // case LOAN_VERIFICATION_STATUS.OK:
-  //   return 'check';
-  // case LOAN_VERIFICATION_STATUS.ERROR:
-  //   return 'close';
-  // case LOAN_VERIFICATION_STATUS.NONE:
-  //   return 'close';
+  case LOAN_VERIFICATION_STATUS.OK:
+    return {
+      icon: 'check',
+      className: 'success',
+      text: 'Vérification réussie',
+    };
+  case LOAN_VERIFICATION_STATUS.ERROR:
+    return {
+      icon: 'close',
+      className: 'error',
+      text: 'Vérification échouée',
+    };
+  case LOAN_VERIFICATION_STATUS.NONE:
+    return { icon: 'close', text: 'Vérification pas demandée' };
+  case LOAN_VERIFICATION_STATUS.REQUESTED:
+    return {
+      icon: 'waiting',
+      className: 'warning',
+      text: 'Vérification demandée',
+    };
 
   default:
-    return 'close';
+    return { icon: 'close', text: 'Vérification pas demandée' };
   }
 };
 
 const PromotionProgress = ({
   promotionProgress: { info, documents, verificationStatus },
-}: PromotionProgressProps) => (
-  <div className="promotion-progress">
-    <Tooltip title="Formulaires remplis">
-      <span>
-        <Percent value={info} rounded />
-      </span>
-    </Tooltip>
+}: PromotionProgressProps) => {
+  const { icon, text, className = '' } = getVerificationData(verificationStatus);
+  return (
+    <div className="promotion-progress">
+      <Tooltip title="Formulaires remplis">
+        <span>
+          <Percent value={info} rounded />
+        </span>
+      </Tooltip>
 
-    <Tooltip title="Documents uploadés">
-      <span>
-        <Percent value={documents} rounded />
-      </span>
-    </Tooltip>
+      <Tooltip title="Documents uploadés">
+        <span>
+          <Percent value={documents} rounded />
+        </span>
+      </Tooltip>
 
-    <Tooltip title="Vérifié par e-Potek">
-      <Icon type={getIcon(verificationStatus)} />
-    </Tooltip>
-  </div>
-);
+      <Tooltip title={text}>
+        <Icon type={icon} className={className} />
+      </Tooltip>
+    </div>
+  );
+};
 
 export default PromotionProgress;
