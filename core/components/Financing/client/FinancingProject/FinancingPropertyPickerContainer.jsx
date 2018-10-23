4d0e2224b63634f7141453d65aab9de05c6340b3
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-10-23 17:37:06 +0200

    Remove redux from update fields

--- core/components/Financing/client/FinancingProject/FinancingPropertyPickerContainer.jsx
+++ core/components/Financing/client/FinancingProject/FinancingPropertyPickerContainer.jsx
@@ -1,33 +1,27 @@
 // @flow
 import React from 'react';
-import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { compose, mapProps } from 'recompose';
+
 import T from '../../../Translation';
 import SingleStructureContainer from '../containers/SingleStructureContainer';
-import StructureUpdateContainer from '../containers/StructureUpdateContainer';
+import { updateStructure } from '../../../../api';
+import FinancingDataContainer from '../containers/FinancingDataContainer';
 
 const FinancingPropertyPickerContainer = compose(
-  StructureUpdateContainer,
+  FinancingDataContainer(),
   SingleStructureContainer,
-  connect(({ financing: { properties, loan: { _id: loanId } } }) => ({
-    properties,
-    loanId,
-  })),
   withRouter,
   mapProps(({
     properties,
     loanId,
-    structure: { propertyId },
-    handleChange,
+    structure: { id: structureId, propertyId },
     history: { push },
   }) => ({
     options: [
-      ...Object.values(properties).map(({ _id, address1 }) => ({
+      ...properties.map(({ _id, address1 }) => ({
         id: _id,
-        label: address1 || (
-          <T id="FinancingPropertyPicker.placeholder" />
-        ),
+        label: address1 || <T id="FinancingPropertyPicker.placeholder" />,
       })),
       {
         id: 'add',
@@ -41,7 +35,11 @@ const FinancingPropertyPickerContainer = compose(
       if (value === 'add') {
         push(`/loans/${loanId}/properties`);
       } else {
-        handleChange(value);
+        updateStructure.run({
+          loanId,
+          structureId,
+          structure: { propertyId: value },
+        });
       }
     },
   })),
