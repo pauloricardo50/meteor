8eedb5be8bcaf0e37c2f372b38bb1e8524730565
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-10-23 19:08:08 +0200

    Update interest rates

--- core/components/InterestRatesTable/interestRates.js
+++ core/components/InterestRatesTable/interestRates.js
@@ -16,27 +16,27 @@ const interestRates = [
   },
   {
     type: INTEREST_RATES.YEARS_5,
-    rateLow: 0.0106,
+    rateLow: 0.0099,
     rateHigh: 0.013,
-    trend: TRENDS.UP,
-  },
-  {
-    type: INTEREST_RATES.YEARS_10,
-    rateLow: 0.0147,
-    rateHigh: 0.0183,
-    trend: TRENDS.UP,
-  },
-  {
-    type: INTEREST_RATES.YEARS_15,
-    rateLow: 0.0185,
-    rateHigh: 0.0225,
     trend: TRENDS.FLAT,
   },
   {
+    type: INTEREST_RATES.YEARS_10,
+    rateLow: 0.0141,
+    rateHigh: 0.0177,
+    trend: TRENDS.DOWN,
+  },
+  {
+    type: INTEREST_RATES.YEARS_15,
+    rateLow: 0.018,
+    rateHigh: 0.0222,
+    trend: TRENDS.DOWN,
+  },
+  {
     type: INTEREST_RATES.YEARS_20,
-    rateLow: 0.0203,
-    rateHigh: 0.025,
-    trend: TRENDS.UP,
+    rateLow: 0.0198,
+    rateHigh: 0.0245,
+    trend: TRENDS.DOWN,
   },
   // {
   //   type: INTEREST_RATES.YEARS_25,
