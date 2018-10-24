00fe7eee8329a5f02a279c08e4d216724062a5fe
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-10-24 15:09:04 +0200

    Refresh all needed data

--- core/components/PromotionLotPage/PromotionLotAttributer/PromotionLotAttributerContainer.js
+++ core/components/PromotionLotPage/PromotionLotAttributer/PromotionLotAttributerContainer.js
@@ -6,13 +6,18 @@ import {
   sellPromotionLot,
 } from 'core/api';
 import ClientEventService from 'core/api/events/ClientEventService';
-import { PROMOTION_OPTION_QUERIES } from 'core/api/constants';
+import {
+  PROMOTION_OPTION_QUERIES,
+  PROMOTION_LOT_QUERIES,
+} from 'core/api/constants';
 
 export default compose(
   withState('isLoading', 'setLoading', false),
   withProps(({ promotionLotId, loanId }) => {
-    const refresh = () =>
+    const refresh = () => {
       ClientEventService.emit(PROMOTION_OPTION_QUERIES.PRO_PROMOTION_OPTIONS);
+      ClientEventService.emit(PROMOTION_LOT_QUERIES.PRO_PROMOTION_LOT);
+    };
     return {
       bookPromotionLot: () =>
         bookPromotionLot.run({ promotionLotId, loanId }).then(refresh),
