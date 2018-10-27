ba489672d12ad77e7093bbec14c5d452d72cc143
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-10-27 14:30:41 +0200

    Add shortcuts to admin

--- microservices/admin/imports/client/layouts/AdminLayout/AdminLayoutContainer.js
+++ microservices/admin/imports/client/layouts/AdminLayout/AdminLayoutContainer.js
@@ -1,11 +1,23 @@
 // @flow
+import { compose } from 'recompose';
+import { withHotKeys } from 'react-hotkeys';
 import query from 'core/api/users/queries/currentUser';
 import { withSmartQuery } from 'core/api/containerToolkit';
 
-export default withSmartQuery({
-  query,
-  params: () => ({}),
-  queryOptions: { reactive: true, single: true },
-  dataName: 'currentUser',
-  renderMissingDoc: false,
-});
+export default compose(
+  // withHotKeys(
+  //   { search: 'down' },
+  //   {
+  //     focused: true,
+  //     attach: window,
+  //     handlers: { up: () => console.log('yo') },
+  //   },
+  // ),
+  withSmartQuery({
+    query,
+    params: () => ({}),
+    queryOptions: { reactive: true, single: true },
+    dataName: 'currentUser',
+    renderMissingDoc: false,
+  }),
+);
