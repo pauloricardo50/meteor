ce33812bb1d2cb0b3f959898f79e58d9d736c2ad
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-10-30 15:48:07 +0100

    Refresh user page on assign admin

--- microservices/admin/imports/client/components/AssignAdminDropdown/UserAssignDropdown.jsx
+++ microservices/admin/imports/client/components/AssignAdminDropdown/UserAssignDropdown.jsx
@@ -1,20 +1,27 @@
 import { withProps } from 'recompose';
 import { assignAdminToUser, assignAdminToNewUser } from 'core/api/methods';
+import ClientEventService from 'core/api/events/ClientEventService/index';
+import { USER_QUERIES } from 'core/api/constants';
 import AssignAdminDropdown from './AssignAdminDropdown';
 
 const changeAssignedUser = ({ newAdmin, user, oldAdminId }) => {
+  const refresh = () => ClientEventService.emit(USER_QUERIES.ADMIN_USER);
   if (oldAdminId) {
     // change of a previously assigned employee
-    assignAdminToUser.run({
-      userId: user._id,
-      adminId: newAdmin._id,
-    });
+    assignAdminToUser
+      .run({
+        userId: user._id,
+        adminId: newAdmin._id,
+      })
+      .then(refresh);
   } else {
     // first assignment for that user
-    assignAdminToNewUser.run({
-      userId: user._id,
-      adminId: newAdmin._id,
-    });
+    assignAdminToNewUser
+      .run({
+        userId: user._id,
+        adminId: newAdmin._id,
+      })
+      .then(refresh);
   }
 };
 
