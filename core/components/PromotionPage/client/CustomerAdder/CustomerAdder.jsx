0e13d6f2159d6631ec9d1778068dd02be2432abb
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-10-31 15:24:01 +0100

    Improve success message

--- core/components/PromotionPage/client/CustomerAdder/CustomerAdder.jsx
+++ core/components/PromotionPage/client/CustomerAdder/CustomerAdder.jsx
@@ -19,24 +19,31 @@ const CustomerAdderUserSchema = new SimpleSchema({
   phoneNumber: String,
 });
 
+const onSuccessMessage = ({ email }) => `Invitation envoyée à ${email}`;
+
 const CustomerAdder = ({
   promotionId,
   promotionStatus,
-}: CustomerAdderProps) => (
-  <AutoFormDialog
-    buttonProps={{
-      raised: true,
-      primary: true,
-      label: <T id="PromotionPage.addCustomer" />,
-      disabled: promotionStatus !== PROMOTION_STATUS.OPEN,
-      tooltip:
-        'Vous ne pouvez ajouter des clients que lorsque la promotion est en cours, contactez e-Potek pour changer le statut de la promotion',
-    }}
-    schema={CustomerAdderUserSchema}
-    onSubmit={user => inviteUserToPromotion.run({ user, promotionId })}
-    title="Inviter un client"
-    description="Invitez un utilisateur à la promotion avec son addresse email. Il recevra un mail avec un lien pour se connecter à e-Potek."
-  />
-);
+}: CustomerAdderProps) => {
+  const disabled = promotionStatus !== PROMOTION_STATUS.OPEN;
+  return (
+    <AutoFormDialog
+      buttonProps={{
+        raised: true,
+        primary: true,
+        label: <T id="PromotionPage.addCustomer" />,
+        disabled,
+        tooltip:
+          disabled
+          && 'Vous ne pouvez ajouter des clients que lorsque la promotion est en cours, contactez e-Potek pour changer le statut de la promotion',
+      }}
+      schema={CustomerAdderUserSchema}
+      onSubmit={user => inviteUserToPromotion.run({ user, promotionId })}
+      title="Inviter un client"
+      description="Invitez un utilisateur à la promotion avec son addresse email. Il recevra un mail avec un lien pour se connecter à e-Potek."
+      onSuccessMessage={onSuccessMessage}
+    />
+  );
+};
 
 export default CustomerAdder;
