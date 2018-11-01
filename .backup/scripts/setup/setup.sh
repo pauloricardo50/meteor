92c9be5dcf385d0050786dce27aa5eb44b666bcc
Quentin Herzig <quentin@e-potek.ch>
2018-11-01 10:10:00 +0100

    Add cron job to delete 14 days old backups

--- .backup/scripts/setup/setup.sh
+++ .backup/scripts/setup/setup.sh
@@ -10,6 +10,9 @@ sudo unlink /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Etc/UTC /etc/localtime
 
 # ADD BACKUP SCRIPT TO CRONTAB
-echo "0 */1 * * * curl https://cronitor.link/aOsIJY/run -m 10 ; ~/scripts/backup_db/backup_db.sh && curl https://cronitor.link/aOsIJY/complete -m 10" > backupCron
+cat > backupCron << EOL 
+0 */1 * * * curl https://cronitor.link/aOsIJY/run -m 10 ; ~/scripts/backup_db/backup_db.sh && curl https://cronitor.link/aOsIJY/complete -m 10
+30 12 * * * curl https://cronitor.link/qDOCtx/run -m 10 ; s3cmd ls s3://production-backups | sort | awk -F' ' '{print $NF}' | grep -E  `date +%Y-%m-%d -d "14 day ago"` | xargs -n 1 s3cmd del -r && curl https://cronitor.link/qDOCtx/complete -m 10
+EOL
 crontab backupCron
 rm backupCron
