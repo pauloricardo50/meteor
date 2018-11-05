10699d7f7ff3c8834ee5928d0b33780999ec593d
Quentin Herzig <quentin@e-potek.ch>
2018-11-05 16:46:42 +0100

    Remove steps for documents

--- core/api/files/documents.js
+++ core/api/files/documents.js
@@ -3,8 +3,6 @@ import {
   RESIDENCE_TYPE,
   PROPERTY_TYPE,
   CIVIL_STATUS,
-  STEPS,
-  STEP_ORDER,
   PROPERTIES_COLLECTION,
   BORROWERS_COLLECTION,
   LOANS_COLLECTION,
@@ -19,96 +17,74 @@ export const borrowerDocuments = (b = {}, calc) => {
     calculator = require('../../utils/Calculator').default;
   }
 
-  return {
-    [STEPS.PREPARATION]: [
-      { id: DOCUMENTS.IDENTITY },
-      { id: DOCUMENTS.RESIDENCY_PERMIT, condition: !b.isSwiss },
-      // TODO: implement married couple logic
-      { id: DOCUMENTS.TAXES },
-      { id: DOCUMENTS.SALARY_CERTIFICATE },
-      {
-        id: DOCUMENTS.BONUSES,
-        condition: !!b.bonus && Object.keys(b.bonus).length > 0,
-      },
-      {
-        id: DOCUMENTS.OTHER_INCOME_JUSTIFICATION,
-        condition: b.otherIncome && !!(b.otherIncome.length > 0),
-      },
-      {
-        id: DOCUMENTS.OWN_COMPANY_FINANCIAL_STATEMENTS,
-        condition: !!b.worksForOwnCompany,
-      },
-      {
-        id: DOCUMENTS.DIVORCE_RULING,
-        condition: !b.civilStatus === CIVIL_STATUS.DIVORCED,
-        noTooltips: true,
-      },
-      {
-        id: DOCUMENTS.EXPENSES_JUSTIFICATION,
-        condition: !!b.expenses && !!(b.expenses.length > 0),
-      },
-    ],
-    [STEPS.GET_CONTRACT]: [
-      { id: DOCUMENTS.DEBT_COLLECTION_REGISTER_EXTRACT, doubleTooltip: true },
-      { id: DOCUMENTS.LAST_SALARIES, noTooltips: true },
-      {
-        id: DOCUMENTS.CURRENT_MORTGAGES,
-        condition: !!b.realEstate && !!b.realEstate.length > 0,
-      },
-      {
-        id: DOCUMENTS.PENSION_FUND_YEARLY_STATEMENT,
-        condition: calculator.getInsurance2({ borrowers: b }) > 0,
-        doubleTooltip: true,
-      },
-      {
-        id: DOCUMENTS.THIRD_PILLAR_ACCOUNTS,
-        condition:
-          calculator.getInsurance3A({ borrowers: b }) > 0
-          || calculator.getInsurance3B({ borrowers: b }) > 0
-          || calculator.getBank3A({ borrowers: b }) > 0,
-        doubleTooltip: true,
-      },
-    ],
-    [STEPS.CLOSING]: [],
-    all() {
-      return [
-        ...this[STEPS.PREPARATION],
-        ...this[STEPS.GET_CONTRACT],
-        ...this[STEPS.CLOSING],
-      ];
+  return [
+    { id: DOCUMENTS.IDENTITY },
+    { id: DOCUMENTS.RESIDENCY_PERMIT, condition: !b.isSwiss },
+    // TODO: implement married couple logic
+    { id: DOCUMENTS.TAXES },
+    { id: DOCUMENTS.SALARY_CERTIFICATE },
+    {
+      id: DOCUMENTS.BONUSES,
+      condition: !!b.bonus && Object.keys(b.bonus).length > 0,
     },
-  };
+    {
+      id: DOCUMENTS.OTHER_INCOME_JUSTIFICATION,
+      condition: b.otherIncome && !!(b.otherIncome.length > 0),
+    },
+    {
+      id: DOCUMENTS.OWN_COMPANY_FINANCIAL_STATEMENTS,
+      condition: !!b.worksForOwnCompany,
+    },
+    {
+      id: DOCUMENTS.DIVORCE_RULING,
+      condition: !b.civilStatus === CIVIL_STATUS.DIVORCED,
+      noTooltips: true,
+    },
+    {
+      id: DOCUMENTS.EXPENSES_JUSTIFICATION,
+      condition: !!b.expenses && !!(b.expenses.length > 0),
+    },
+    { id: DOCUMENTS.DEBT_COLLECTION_REGISTER_EXTRACT, doubleTooltip: true },
+    { id: DOCUMENTS.LAST_SALARIES, noTooltips: true },
+    {
+      id: DOCUMENTS.CURRENT_MORTGAGES,
+      condition: !!b.realEstate && !!b.realEstate.length > 0,
+    },
+    {
+      id: DOCUMENTS.PENSION_FUND_YEARLY_STATEMENT,
+      condition: calculator.getInsurance2({ borrowers: b }) > 0,
+      doubleTooltip: true,
+    },
+    {
+      id: DOCUMENTS.THIRD_PILLAR_ACCOUNTS,
+      condition:
+        calculator.getInsurance3A({ borrowers: b }) > 0
+        || calculator.getInsurance3B({ borrowers: b }) > 0
+        || calculator.getBank3A({ borrowers: b }) > 0,
+      doubleTooltip: true,
+    },
+  ];
 };
 
 export const loanDocuments = ({ general: { purchaseType }, hasPromotion }) => {
   const isRefinancing = purchaseType === PURCHASE_TYPE.REFINANCING;
-  return {
-    [STEPS.PREPARATION]: hasPromotion
+  return [
+    ...(hasPromotion
       ? []
-      : [{ id: DOCUMENTS.SIGNED_MANDATE, noTooltips: true }],
-    [STEPS.GET_CONTRACT]: [
-      {
-        id: DOCUMENTS.BUYERS_CONTRACT,
-        tooltipSuffix: isRefinancing ? 'a' : 'b',
-      },
-      {
-        id: DOCUMENTS.REIMBURSEMENT_STATEMENT,
-        condition: isRefinancing,
-      },
-    ],
-    [STEPS.CLOSING]: [],
-    all() {
-      return [
-        ...this[STEPS.PREPARATION],
-        ...this[STEPS.GET_CONTRACT],
-        ...this[STEPS.CLOSING],
-      ];
+      : [{ id: DOCUMENTS.SIGNED_MANDATE, noTooltips: true }]),
+    {
+      id: DOCUMENTS.BUYERS_CONTRACT,
+      tooltipSuffix: isRefinancing ? 'a' : 'b',
     },
-  };
+    {
+      id: DOCUMENTS.REIMBURSEMENT_STATEMENT,
+      condition: isRefinancing,
+    },
+  ];
 };
 
-export const propertyDocuments = (property = {}, loan = {}) => ({
-  [STEPS.PREPARATION]: loan.hasPromotion
+export const propertyDocuments = (property = {}, loan = {}) =>
+  (loan.hasPromotion
     ? []
     : [
       { id: DOCUMENTS.PROPERTY_PLANS },
@@ -127,10 +103,6 @@ export const propertyDocuments = (property = {}, loan = {}) => ({
         ),
         required: false,
       },
-    ],
-  [STEPS.GET_CONTRACT]: loan.hasPromotion
-    ? []
-    : [
       {
         id: DOCUMENTS.INVESTMENT_PROPERTY_RENT_JUSTIFICATION,
         condition:
@@ -150,34 +122,7 @@ export const propertyDocuments = (property = {}, loan = {}) => ({
         doubleTooltip: true,
       },
       { id: DOCUMENTS.FIRE_AND_WATER_INSURANCE, condition: !!property.isNew },
-    ],
-  [STEPS.CLOSING]: [],
-  all() {
-    return [
-      ...this[STEPS.PREPARATION],
-      ...this[STEPS.GET_CONTRACT],
-      ...this[STEPS.CLOSING],
-    ];
-  },
-});
-
-export const getDocumentArrayByStep = (func, step) => [
-  ...func()[step],
-  { id: DOCUMENTS.OTHER, required: false, noTooltips: true },
-];
-
-const getKeysFromArray = (order, key) => order.slice(0, order.indexOf(key) + 1);
-
-const flattenDocumentsByStep = (documentsObject, step) => {
-  const previousSteps = getKeysFromArray(STEP_ORDER, step);
-  return previousSteps.reduce(
-    (documents, s) => [
-      ...documents,
-      ...(documentsObject[s] ? documentsObject[s] : []),
-    ],
-    [],
-  );
-};
+    ]);
 
 const makeGetDocuments = collection => ({ loan, id }, ...args) => {
   let documents;
@@ -187,19 +132,20 @@ const makeGetDocuments = collection => ({ loan, id }, ...args) => {
   }
 
   const doc = !isLoans && loan[collection].find(({ _id }) => _id === id);
-  const { step } = loan.logic;
 
-  if (collection === PROPERTIES_COLLECTION) {
-    const allPropertyDocuments = propertyDocuments(doc, loan);
-    documents = flattenDocumentsByStep(allPropertyDocuments, step);
-  }
-  if (collection === BORROWERS_COLLECTION) {
-    const allBorrowerDocuments = borrowerDocuments(doc, ...args);
-    documents = flattenDocumentsByStep(allBorrowerDocuments, step);
-  }
-  if (collection === LOANS_COLLECTION) {
-    const allLoanDocuments = loanDocuments(loan);
-    documents = flattenDocumentsByStep(allLoanDocuments, step);
+  switch (collection) {
+  case PROPERTIES_COLLECTION:
+    documents = propertyDocuments(doc, loan);
+    break;
+  case BORROWERS_COLLECTION:
+    documents = borrowerDocuments(doc, ...args);
+    break;
+  case LOANS_COLLECTION:
+    documents = loanDocuments(loan);
+    break;
+  default:
+    documents = [];
+    break;
   }
 
   return [
