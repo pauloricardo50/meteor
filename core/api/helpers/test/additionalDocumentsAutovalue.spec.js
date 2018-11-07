d13ac4a42fbb6f0e31c3d50dcb0473a3403e7b3d
Quentin Herzig <quentin@e-potek.ch>
2018-11-07 11:02:37 +0100

    Working on tests

--- core/api/helpers/test/additionalDocumentsAutovalue.spec.js
+++ core/api/helpers/test/additionalDocumentsAutovalue.spec.js
@@ -27,30 +27,39 @@ describe.only('additionalDocumentsAutovalue', () => {
     },
   };
 
+  const CONDITIONAL_DOCUMENTS = {
+    CONDITIONAL_DOC_1: 'CONDITIONAL_DOC_1',
+    CONDITIONAL_DOC_2: 'CONDITIONAL_DOC_2',
+    CONDITIONAL_DOC_3: 'CONDITIONAL_DOC_3',
+    CONDITIONAL_DOC_4: 'CONDITIONAL_DOC_4',
+  };
+
   const conditionalDocuments = [
     {
-      id: 'conditionalDocument1',
+      id: CONDITIONAL_DOCUMENTS.CONDITIONAL_DOC_1,
       condition: ({ doc, context }) =>
         doc[DOC_KEYS.KEY1.name] !== DOC_KEYS.KEY1.values[0]
         && context.field(DOC_KEYS.KEY1.name).value === DOC_KEYS.KEY1.values[0],
     },
     {
-      id: 'conditionalDocument2',
+      id: CONDITIONAL_DOCUMENTS.CONDITIONAL_DOC_2,
       condition: ({ doc, context }) =>
         doc[DOC_KEYS.KEY1.name] !== DOC_KEYS.KEY1.values[1]
         && context.field(DOC_KEYS.KEY1.name).value === DOC_KEYS.KEY1.values[1],
     },
     {
-      id: 'conditionalDocument3',
+      id: CONDITIONAL_DOCUMENTS.CONDITIONAL_DOC_3,
       condition: ({ doc, context }) =>
-        doc[DOC_KEYS.KEY2.name] !== DOC_KEYS.KEY2.values[0]
-        && context.field(DOC_KEYS.KEY2.name).value === DOC_KEYS.KEY2.values[0],
+        // doc[DOC_KEYS.KEY2.name] !== DOC_KEYS.KEY2.values[0]
+        // &&
+        context.field(DOC_KEYS.KEY2.name).value === DOC_KEYS.KEY2.values[0],
     },
     {
-      id: 'conditionalDocument3',
+      id: CONDITIONAL_DOCUMENTS.CONDITIONAL_DOC_4,
       condition: ({ doc, context }) =>
-        doc[DOC_KEYS.KEY2.name] !== DOC_KEYS.KEY2.values[1]
-        && context.field(DOC_KEYS.KEY2.name).value === DOC_KEYS.KEY2.values[1],
+        // doc[DOC_KEYS.KEY2.name] !== DOC_KEYS.KEY2.values[1]
+        // &&
+        context.field(DOC_KEYS.KEY2.name).value === DOC_KEYS.KEY2.values[1],
     },
   ];
 
@@ -75,7 +84,7 @@ describe.only('additionalDocumentsAutovalue', () => {
       conditionalDocuments,
       initialDocuments,
       context: autovalueContext,
-    })).to.deep.contain(...initialDocuments);
+    })).to.deep.equal(initialDocuments);
   });
 
   it('should not remove initial documents if doc is updated', () => {
@@ -89,6 +98,89 @@ describe.only('additionalDocumentsAutovalue', () => {
       conditionalDocuments,
       initialDocuments,
       context: autovalueContext,
-    })).to.deep.contain(...initialDocuments);
+    })).to.deep.equal(initialDocuments);
+  });
+
+  it('should add additional documents if condition is met', () => {
+    autovalueContext.isInsert = true;
+    autovalueContext.fields = {
+      [DOC_KEYS.KEY1.name]: DOC_KEYS.KEY1.values[0],
+      [DOC_KEYS.KEY2.name]: DOC_KEYS.KEY2.values[0],
+    };
+
+    const doc = {
+      additionalDocuments: [],
+    };
+
+    expect(additionalDocumentsAutovalue({
+      doc,
+      conditionalDocuments,
+      initialDocuments,
+      context: autovalueContext,
+    })).to.deep.equal([
+      ...initialDocuments,
+      { id: CONDITIONAL_DOCUMENTS.CONDITIONAL_DOC_1 },
+      { id: CONDITIONAL_DOCUMENTS.CONDITIONAL_DOC_3 },
+    ]);
+  });
+
+  it('should remove old additional documents and add new ones if condition result has changed', () => {
+    autovalueContext.isInsert = false;
+    autovalueContext.fields = {
+      [DOC_KEYS.KEY1.name]: DOC_KEYS.KEY1.values[1],
+      [DOC_KEYS.KEY2.name]: DOC_KEYS.KEY2.values[1],
+    };
+
+    const doc = {
+      additionalDocuments: [
+        { id: CONDITIONAL_DOCUMENTS.CONDITIONAL_DOC_1 },
+        { id: CONDITIONAL_DOCUMENTS.CONDITIONAL_DOC_3 },
+      ],
+      [DOC_KEYS.KEY1.name]: DOC_KEYS.KEY1.values[0],
+      [DOC_KEYS.KEY2.name]: DOC_KEYS.KEY2.values[0],
+    };
+
+    expect(additionalDocumentsAutovalue({
+      doc,
+      conditionalDocuments,
+      initialDocuments,
+      context: autovalueContext,
+    })).to.deep.equal([
+      { id: CONDITIONAL_DOCUMENTS.CONDITIONAL_DOC_2 },
+      { id: CONDITIONAL_DOCUMENTS.CONDITIONAL_DOC_4 },
+    ]);
+  });
+
+  it('should keep additional documents if condition result has not changed', () => {
+    autovalueContext.isInsert = false;
+    autovalueContext.fields = {
+      [DOC_KEYS.KEY1.name]: DOC_KEYS.KEY1.values[1],
+      [DOC_KEYS.KEY2.name]: DOC_KEYS.KEY2.values[1],
+    };
+
+    const doc = {
+      additionalDocuments: [
+        { id: CONDITIONAL_DOCUMENTS.CONDITIONAL_DOC_2 },
+        { id: CONDITIONAL_DOCUMENTS.CONDITIONAL_DOC_4 },
+      ],
+      [DOC_KEYS.KEY1.name]: DOC_KEYS.KEY1.values[1],
+      [DOC_KEYS.KEY2.name]: DOC_KEYS.KEY2.values[1],
+    };
+
+    console.log(additionalDocumentsAutovalue({
+      doc,
+      conditionalDocuments,
+      initialDocuments,
+      context: autovalueContext,
+    }));
+    expect(additionalDocumentsAutovalue({
+      doc,
+      conditionalDocuments,
+      initialDocuments,
+      context: autovalueContext,
+    })).to.deep.equal([
+      { id: CONDITIONAL_DOCUMENTS.CONDITIONAL_DOC_2 },
+      { id: CONDITIONAL_DOCUMENTS.CONDITIONAL_DOC_4 },
+    ]);
   });
 });
