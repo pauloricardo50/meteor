68569cc2564670baab6679ddde31cbca32e3d449
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-11-09 12:09:41 +0100

    Don't run tests in separate its

--- microservices/pro/cypress/tests/pro-promotion.test.js
+++ microservices/pro/cypress/tests/pro-promotion.test.js
@@ -2,28 +2,23 @@
 import { PRO_EMAIL, PRO_PASSWORD } from '../constants';
 
 describe('Pro', () => {
-  before(() => {
+  beforeEach(() => {
     cy.visit('/');
     cy.callMethod('resetDatabase');
     cy.callMethod('generateProFixtures');
-  });
 
-  it('should login', () => {
     cy.url().should('include', 'login');
     cy.get('input[name=email]').type(PRO_EMAIL);
     cy.get('input[name=password]').type(`${PRO_PASSWORD}{enter}`);
     cy.location('pathname').should('eq', '/');
   });
 
-  it('should start with an empty dashboard', () => {
-    cy.get('.pro-dashboard-page').contains('Rien à afficher');
-  });
-
   it('should add a promotion', () => {
+    cy.get('.pro-dashboard-page').contains('Rien à afficher');
+
     cy.get('.buttons > a').click();
     cy.location('pathname').should('eq', '/promotions/new');
 
-    cy.wait(1000);
     cy.location('pathname').should('eq', '/promotions/new');
   });
 });
