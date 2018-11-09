e6e4741ccab5222c428b9ce1919ce6989e2eccfd
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-11-09 16:28:03 +0100

    Plug calculator with loan for demo

--- core/utils/notaryFees/notaryFees.js
+++ core/utils/notaryFees/notaryFees.js
@@ -1,1 +1,8 @@
-const notaryFees = () => {};
+import NotaryFeesCalculator from './NotaryFeesCalculator';
+
+const notaryFees = ({ loan }) => {
+  const calculator = new NotaryFeesCalculator({ canton: 'GE' });
+  return calculator.getNotaryFeesForLoan(loan);
+};
+
+export default notaryFees;
