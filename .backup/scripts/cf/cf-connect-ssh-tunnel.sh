23b328b2088f29dabbdc50752e38ef704c9b3976
Quentin Herzig <quentin@e-potek.ch>
2018-11-13 10:44:12 +0100

    Append random ID to backup manager ssh tunnel

--- .backup/scripts/cf/cf-connect-ssh-tunnel.sh
+++ .backup/scripts/cf/cf-connect-ssh-tunnel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-cf ssh -L $1:kubernetes-service-node.service.consul:$1 e-potek-ssh-tunnel-backup-manager
+cf ssh -L $1:kubernetes-service-node.service.consul:$1 e-potek-ssh-tunnel-backup-manager-$2
