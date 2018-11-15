599e4a6e42b0a3b9ca35ef96574256952db9b25d
Quentin Herzig <quentin@e-potek.ch>
2018-11-15 08:30:02 +0100

    Add comment to cf-deploy-ssh-tunnel

--- .backup/scripts/cf/cf-deploy-ssh-tunnel.sh
+++ .backup/scripts/cf/cf-deploy-ssh-tunnel.sh
@@ -2,6 +2,8 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+# Change application name in manifest
+# Appends the random id to the end
 sed -i "s/  - name:.*/  - name: e-potek-backup-manager-$1/" $SCRIPTPATH/ssh-tunnel/manifest.yml
 
 
