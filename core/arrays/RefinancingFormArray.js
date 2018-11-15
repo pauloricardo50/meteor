404178fb99f7c555bd4c8a052007a79ca6f36bd1
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-11-15 17:58:45 +0100

    Add complete refinancing form on app and i18n

--- core/arrays/RefinancingFormArray.js
+++ core/arrays/RefinancingFormArray.js
@@ -1,7 +1,27 @@
+import { INTEREST_RATES } from '../api/constants';
+
 const getRefinancingFormArray = ({ loan }) => [
   {
     id: 'previousLender',
     type: 'textInput',
+  },
+  {
+    id: 'previousLoanTranches',
+    type: 'arrayInput',
+    inputs: [
+      {
+        id: 'type',
+        type: 'selectInput',
+        options: Object.values(INTEREST_RATES),
+      },
+      { id: 'value', type: 'textInput', money: true },
+      { id: 'dueDate', type: 'dateInput' },
+    ],
+  },
+  {
+    id: 'mortgageNotes',
+    type: 'arrayInput',
+    inputs: [{ id: 'value', type: 'textInput', money: true }],
   },
 ];
 
