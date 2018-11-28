96378b137b386751afcafa08f32ba071b993b980
Quentin Herzig <quentin@e-potek.ch>
2018-11-28 17:35:36 +0100

    Add disbursement date to loan fragment

--- core/api/loans/queries/loanFragments/combinedLoanFragments.js
+++ core/api/loans/queries/loanFragments/combinedLoanFragments.js
@@ -29,6 +29,7 @@ export const adminLoanFragment = {
   status: 1,
   properties: adminPropertyFragment,
   signingDate: 1,
+  disbursementDate: 1,
 };
 
 export const adminLoansFragment = {
@@ -38,6 +39,7 @@ export const adminLoansFragment = {
   properties: { value: 1, address1: 1 },
   user: { assignedEmployee: { email: 1 }, name: 1 },
   signingDate: 1,
+  disbursementDate: 1,
 };
 
 export const proLoansFragment = {
