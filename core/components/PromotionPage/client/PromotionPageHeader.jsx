10ab84de15009a57c068e68333f32b983f39d156
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-12-04 18:44:32 +0100

    Improve logo alignment

--- core/components/PromotionPage/client/PromotionPageHeader.jsx
+++ core/components/PromotionPage/client/PromotionPageHeader.jsx
@@ -53,7 +53,9 @@ const PromotionPageHeader = ({
             <div className="list">
               {logos.map(logo => (
                 <div className="logo" key={logo.Key}>
-                  <img src={logo.url} alt="" />
+                  <div className="logo-wrapper">
+                    <img src={logo.url} alt="" />
+                  </div>
                   <p className="text-center bold">{logo.name.split('.')[0]}</p>
                 </div>
               ))}
