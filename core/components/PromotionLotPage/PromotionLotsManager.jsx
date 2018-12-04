de757d04c760bbf0dc562a44d322d51a26c9c452
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-12-04 18:35:20 +0100

    Hide additional lots title if no lots are present

--- core/components/PromotionLotPage/PromotionLotsManager.jsx
+++ core/components/PromotionLotPage/PromotionLotsManager.jsx
@@ -18,6 +18,12 @@ const PromotionLotsManager = ({
   canModify,
 }: PromotionLotsManagerProps) => {
   const allowEdit = canModify && status === PROMOTION_LOT_STATUS.AVAILABLE;
+
+  if (!allowEdit && lots.length === 0) {
+    // Don't display an empty title for users who can't even add a new lot
+    return null;
+  }
+
   return (
     <div className="promotion-lots-manager">
       {lots.map(lot => (
