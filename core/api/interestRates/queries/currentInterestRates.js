d67bc0c63c1ab05796871e692df4d824a196005c
Quentin Herzig <quentin@e-potek.ch>
2018-12-07 14:30:19 +0100

    Sort rates from libor to 25 years

--- core/api/interestRates/queries/currentInterestRates.js
+++ core/api/interestRates/queries/currentInterestRates.js
@@ -1,21 +1,24 @@
 import moment from 'moment';
 import InterestRates from '../interestRates';
 import { currentInterestRatesFragment } from './interestRatesFragments';
-import { INTEREST_RATES_QUERIES } from '../interestRatesConstants';
+import {
+  INTEREST_RATES_QUERIES,
+  INTEREST_RATES,
+} from '../interestRatesConstants';
 
 export default InterestRates.createQuery(
   INTEREST_RATES_QUERIES.CURRENT_INTEREST_RATES,
   {
-    $filter({ filters, params }) {
+    $filter({ filters }) {
       filters.date = {
         $lte: moment().toDate(),
       };
     },
     ...currentInterestRatesFragment,
     $options: { sort: { date: -1 }, limit: 1 },
-    $postFilter(results, params) {
+    $postFilter(results) {
       const interestRates = results.length > 0 && results[0];
-      const rates = Object.keys(interestRates)
+      const cleanedRates = Object.keys(interestRates)
         .filter(type =>
           interestRates[type].rateLow
             && interestRates[type].rateHigh
@@ -25,15 +28,20 @@ export default InterestRates.createQuery(
           rateLow: interestRates[type].rateLow,
           rateHigh: interestRates[type].rateHigh,
           trend: interestRates[type].trend,
-        }));
-      const averageRates = rates.reduce(
+        }))
+        .sort(({ type: a }, { type: b }) =>
+          Object.values(INTEREST_RATES).indexOf(a)
+            - Object.values(INTEREST_RATES).indexOf(b));
+
+      const averageRates = cleanedRates.reduce(
         (avgRates, { type, rateLow, rateHigh }) => ({
           ...avgRates,
           [type]: (rateLow + rateHigh) / 2,
         }),
         {},
       );
-      return { rates, averageRates, date: interestRates.date };
+
+      return { rates: cleanedRates, averageRates, date: interestRates.date };
     },
   },
 );
