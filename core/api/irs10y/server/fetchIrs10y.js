23e509f7752b52626c7b3214946b73b2ce70c940
Quentin Herzig <quentin@e-potek.ch>
2018-12-07 17:29:16 +0100

    Add method to fetch irs10y

---
+++ core/api/irs10y/server/fetchIrs10y.js
@@ -0,0 +1,21 @@
+import { Meteor } from 'meteor/meteor';
+
+const cheerio = require('cheerio');
+const request = require('request');
+
+console.log('request', request);
+
+const URL = 'https://kurse.vermoegenszentrum.ch/vermoegenszentrum/details/details.jsp?listingId=211372,433,1';
+
+export const fetchIrs10y = () =>
+  new Promise((resolve, reject) => {
+    request(URL, (error, response, html) => {
+      if (!error && response.statusCode === 200) {
+        const $ = cheerio.load(html);
+        let result = $('.kum-bht-value').text();
+        result = result.split('%')[0] / 100;
+        resolve(result);
+      }
+      reject(new Meteor.Error(error));
+    });
+  });
--- core/api/irs10y/server/methods.js
+++ core/api/irs10y/server/methods.js
@@ -1,6 +1,8 @@
+import { Method } from '../../methods/methods';
 import SecurityService from '../../security';
 import Irs10yService from '../Irs10yService';
 import { irs10yInsert, irs10yRemove, irs10yUpdate } from '../methodDefinitions';
+import { fetchIrs10y } from './fetchIrs10y';
 
 irs10yInsert.setHandler((context, { irs10y }) => {
   SecurityService.checkCurrentUserIsAdmin();
@@ -16,3 +18,14 @@ irs10yUpdate.setHandler((context, { irs10yId, object }) => {
   SecurityService.checkCurrentUserIsAdmin();
   return Irs10yService._update({ id: irs10yId, object });
 });
+
+export const irs10yFetch = new Method({
+  name: 'irs10yFetch',
+});
+
+irs10yFetch.setHandler(() =>
+  fetchIrs10y()
+    .then(irs10y => Irs10yService.insert({ date: new Date(), rate: irs10y }))
+    .catch((error) => {
+      throw error;
+    }));
