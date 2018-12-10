86fbc36cbcf192968005e8e5edcb19e8ca2170c9
Quentin Herzig <quentin@e-potek.ch>
2018-12-10 12:32:34 +0100

    Remove job before inserting it again

--- microservices/admin/imports/startup/server/fecthIrs10yCron.js
+++ microservices/admin/imports/startup/server/fecthIrs10yCron.js
@@ -1,12 +1,13 @@
-import { Meteor } from 'meteor/meteor';
 import { SyncedCron } from 'meteor/littledata:synced-cron';
 import { irs10yFetch } from 'core/api/irs10y/server/methods';
 
 const getRandomMinute = () => Math.floor(Math.random() * 59);
+const jobName = 'Fetch IRS 10Y';
 
-Meteor.startup(() => {
+try {
+  SyncedCron.remove(jobName);
   SyncedCron.add({
-    name: 'Fetch IRS 10Y',
+    name: jobName,
     schedule(parser) {
       return parser.text(`at 6:${getRandomMinute()}`);
     },
@@ -14,5 +15,7 @@ Meteor.startup(() => {
       irs10yFetch.run({});
     },
   });
-  SyncedCron.start();
-});
+} catch (error) {
+  console.error(error);
+}
+SyncedCron.start();
