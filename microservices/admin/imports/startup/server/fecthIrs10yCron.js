7309e3a5230e4dd962644ade3dd0c1f445983cae
Quentin Herzig <quentin@e-potek.ch>
2018-12-10 14:19:47 +0100

    Stop synced cron in meteor startup

--- microservices/admin/imports/startup/server/fecthIrs10yCron.js
+++ microservices/admin/imports/startup/server/fecthIrs10yCron.js
@@ -1,21 +1,24 @@
+import { Meteor } from 'meteor/meteor';
 import { SyncedCron } from 'meteor/littledata:synced-cron';
 import { irs10yFetch } from 'core/api/irs10y/server/methods';
 
 const getRandomMinute = () => Math.floor(Math.random() * 59);
 const jobName = 'Fetch IRS 10Y';
 
-try {
-  SyncedCron.remove(jobName);
-  SyncedCron.add({
-    name: jobName,
-    schedule(parser) {
-      return parser.text(`at 6:${getRandomMinute()}`);
-    },
-    job() {
-      irs10yFetch.run({});
-    },
-  });
-} catch (error) {
-  console.error(error);
-}
-SyncedCron.start();
+Meteor.startup(() => {
+  try {
+    SyncedCron.stop();
+    SyncedCron.add({
+      name: jobName,
+      schedule(parser) {
+        return parser.text(`at 6:${getRandomMinute()}`);
+      },
+      job() {
+        irs10yFetch.run({});
+      },
+    });
+    SyncedCron.start();
+  } catch (error) {
+    console.error(error);
+  }
+});
