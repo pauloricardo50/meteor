45e2f094c1e74dd5e521804e71ce9c2b63e7cc4b
Quentin Herzig <quentin@e-potek.ch>
2018-12-10 17:29:27 +0100

    Fix fetch irs10y cron

--- microservices/admin/imports/startup/server/index.js
+++ microservices/admin/imports/startup/server/index.js
@@ -9,7 +9,7 @@ import 'core/api/api-server';
 import '../accounts-config';
 import './kadira';
 
-import './fecthIrs10yCron';
+import './fetchIrs10yCron';
 
 // Inject a loader before client is ready, is removed in the on startup function on the client
 Inject.rawHead('loader', Assets.getText('loader.html'));
