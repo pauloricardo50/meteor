92c9aab8f9ceef062043379da98dfbc4066ecc81
Quentin Herzig <quentin@e-potek.ch>
2018-12-10 17:14:32 +0100

    Fix fetch irs10y cron

--- microservices/admin/imports/startup/server/fecthIrs10yCron.js
+++ microservices/admin/imports/startup/server/fecthIrs10yCron.js
@@ -1,22 +1,26 @@
+import { Meteor } from 'meteor/meteor';
 import { SyncedCron } from 'meteor/littledata:synced-cron';
 import { irs10yFetch } from 'core/api/irs10y/server/methods';
 
-const getRandomMinute = () => Math.floor(Math.random() * 59);
+const getRandomMinute = () => Math.floor(Math.random() * 49) + 10;
 const jobName = 'Fetch IRS 10Y';
 
-try {
-  SyncedCron.stop();
-  SyncedCron.remove(jobName);
-  SyncedCron.add({
-    name: jobName,
-    schedule(parser) {
-      return parser.text(`at 6:${getRandomMinute()}`);
-    },
-    job() {
-      irs10yFetch.run({});
-    },
-  });
-  SyncedCron.start();
-} catch (error) {
-  console.error(error);
-}
+Meteor.startup(() => {
+  try {
+    SyncedCron.stop();
+    SyncedCron.remove(jobName);
+    SyncedCron.add({
+      name: jobName,
+      schedule(parser) {
+        const randomMinute = getRandomMinute();
+        return parser.text(`at 6:${randomMinute}`);
+      },
+      job() {
+        irs10yFetch.run({});
+      },
+    });
+    SyncedCron.start();
+  } catch (error) {
+    console.error(error);
+  }
+});
