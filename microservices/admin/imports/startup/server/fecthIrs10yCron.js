f9c8b88e50fdfd6b144fa44ac2685036e42d5a48
Quentin Herzig <quentin@e-potek.ch>
2018-12-10 16:53:59 +0100

    Add residence type setter on app promotion page

--- microservices/admin/imports/startup/server/fecthIrs10yCron.js
+++ microservices/admin/imports/startup/server/fecthIrs10yCron.js
@@ -1,25 +1,22 @@
-import { Meteor } from 'meteor/meteor';
 import { SyncedCron } from 'meteor/littledata:synced-cron';
 import { irs10yFetch } from 'core/api/irs10y/server/methods';
 
 const getRandomMinute = () => Math.floor(Math.random() * 59);
 const jobName = 'Fetch IRS 10Y';
 
-Meteor.startup(() => {
-  try {
-    SyncedCron.stop();
-    SyncedCron.remove(jobName);
-    SyncedCron.add({
-      name: jobName,
-      schedule(parser) {
-        return parser.text(`at 6:${getRandomMinute()}`);
-      },
-      job() {
-        irs10yFetch.run({});
-      },
-    });
-    SyncedCron.start();
-  } catch (error) {
-    console.error(error);
-  }
-});
+try {
+  SyncedCron.stop();
+  SyncedCron.remove(jobName);
+  SyncedCron.add({
+    name: jobName,
+    schedule(parser) {
+      return parser.text(`at 6:${getRandomMinute()}`);
+    },
+    job() {
+      irs10yFetch.run({});
+    },
+  });
+  SyncedCron.start();
+} catch (error) {
+  console.error(error);
+}
