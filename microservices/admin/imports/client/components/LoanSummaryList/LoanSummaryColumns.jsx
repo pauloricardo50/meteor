bbb524ce43428db98adcbfb7c8f4639becfb53ad
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-12-12 17:34:10 +0100

    Simplify LoanSummaryColumns

--- microservices/admin/imports/client/components/LoanSummaryList/LoanSummaryColumns.jsx
+++ microservices/admin/imports/client/components/LoanSummaryList/LoanSummaryColumns.jsx
@@ -1,17 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
-import LoanSummaryColumn from './LoanSummaryColumn';
-import { getLoanSummaryColumns } from './loanSummaryHelpers';
+import T, { Money } from 'core/components/Translation';
+import StatusLabel from 'core/components/StatusLabel';
+import Calculator from 'core/utils/Calculator';
+import { LOANS_COLLECTION } from 'core/api/constants';
+
+const getLoanSummaryColumns = ({ status, createdAt, updatedAt, ...loan }) => {
+  const ownFunds = Calculator.getNonPledgedOwnFunds({ loan });
+  const ownFundsPledged = Calculator.getTotalPledged({ loan });
+  const loanValue = Calculator.selectLoanValue({ loan });
+  const propertyValue = Calculator.selectPropertyValue({ loan });
+  return [
+    {
+      translationId: 'LoanSummaryColumn.status',
+      content: <StatusLabel status={status} collection={LOANS_COLLECTION} />,
+    },
+    {
+      translationId: 'LoanSummaryColumn.createdAt',
+      content: moment(createdAt).format('D MMM YY à HH:mm:ss'),
+    },
+    {
+      translationId: 'LoanSummaryColumn.updatedAt',
+      content: moment(updatedAt).format('D MMM YY à HH:mm:ss'),
+    },
+    {
+      translationId: 'LoanSummaryColumn.propertyValue',
+      content: <Money value={propertyValue} />,
+    },
+    {
+      translationId: 'general.mortgageLoan',
+      content: <Money value={loanValue} />,
+    },
+    {
+      translationId: 'LoanSummaryColumn.ownFunds',
+      content: <Money value={ownFunds} />,
+    },
+    {
+      translationId: 'LoanSummaryColumn.ownFundsPledged',
+      content: <Money value={ownFundsPledged} />,
+    },
+  ];
+};
 
 const LoanSummaryColumns = ({ loan }) => (
   <div className="flex admin-loan">
     {getLoanSummaryColumns(loan).map(({ translationId, content }) => (
-      <LoanSummaryColumn
-        translationId={translationId}
-        content={content}
-        key={translationId}
-      />
+      <div className="flex-col" key={translationId}>
+        <label>
+          <T id={translationId} />
+        </label>
+        <p>{content}</p>
+      </div>
     ))}
   </div>
 );
