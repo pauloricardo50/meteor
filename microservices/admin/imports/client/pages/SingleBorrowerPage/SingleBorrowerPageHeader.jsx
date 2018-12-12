581b1f9c1028634effcb2e21807d199747237e2c
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-12-12 10:44:23 +0100

    Improve info on SingleBorrowerPage

--- microservices/admin/imports/client/pages/SingleBorrowerPage/SingleBorrowerPageHeader.jsx
+++ microservices/admin/imports/client/pages/SingleBorrowerPage/SingleBorrowerPageHeader.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from 'core/components/Link';
 
 import T from 'core/components/Translation';
 import FullDate from 'core/components/dateComponents/FullDate';
+import { USERS_COLLECTION } from 'core/api/constants';
+import { CollectionIconLink } from 'core/components/IconLink';
 
 const SingleBorrowerHeader = ({
-  borrower: { address1, age, gender, name, createdAt, user },
+  borrower: { address1, age, gender, name, createdAt, updatedAt, user },
 }) => (
   <div className="single-borrower-page-header">
     <div className="top">
       <h1>{name || <T id="general.borrower" />}</h1>
     </div>
 
-    {gender
-      && age
-      && address1 && (
+    {gender && age && address1 && (
       <p className="secondary">
         {`${gender}, `}
         <T id="SingleBorrowerPageHeader.age" values={{ value: age }} />
@@ -25,27 +24,31 @@ const SingleBorrowerHeader = ({
 
     <div className="bottom">
       <p className="created-at">
-        {user && [
-          <T id="SingleBorrowerPageHeader.createdBy" key="createdBy" />,
-          ' ',
-          <Link to={`/users/${user._id}`} key="userLink">
-            {user.email}
-          </Link>,
-          ', ',
-        ]}
-
-        <FullDate date={createdAt} />
-
-        {user
-          && user.assignedEmployee && (
+        {user && (
+          <CollectionIconLink
+            relatedDoc={{ ...user, collection: USERS_COLLECTION }}
+          />
+        )}
+        {user && user.assignedEmployee && (
           <span>
-            {' - '}
-            <T id="SingleBorrowerPageHeader.assignedTo" />{' '}
-            <Link to={`/users/${user.assignedEmployee._id}`}>
-              {user.assignedEmployee.email}
-            </Link>
+            <T id="SingleBorrowerPageHeader.assignedTo" />
+            &nbsp;
+            <CollectionIconLink
+              relatedDoc={{
+                ...user.assignedEmployee,
+                collection: USERS_COLLECTION,
+              }}
+            />
           </span>
         )}
+        <span>
+          Créé à &nbsp;
+          <FullDate date={createdAt} />
+        </span>
+        <span>
+          Modifié à &nbsp;
+          <FullDate date={updatedAt} />
+        </span>
       </p>
     </div>
   </div>
