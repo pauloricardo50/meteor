253291e18b56d8fdd7d5ef8e89b5eb63fc259651
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-12-27 11:19:32 +0100

    Make sure required own funds uses real notary fees

--- core/components/Financing/client/FinancingOwnFunds/test/ownFundsHelpers.spec.js
+++ core/components/Financing/client/FinancingOwnFunds/test/ownFundsHelpers.spec.js
@@ -9,21 +9,35 @@ describe('ownFundsHelpers', () => {
   describe('calculateMissingOwnFunds', () => {
     it('returns a standard amount', () => {
       expect(calculateMissingOwnFunds({
+        properties: [{ _id: 'propertyId', value: 1000000 }],
+        loan: {
+          properties: [{ _id: 'propertyId', value: 1000000 }],
+          structures: [
+            {
+              id: 'struct1',
+              wantedLoan: 800000,
+              propertyId: 'propertyId',
+              propertyWork: 0,
+              ownFunds: [],
+            },
+          ],
+          borrowers: [{}],
+        },
+        structureId: 'struct1',
         structure: {
+          id: 'struct1',
           wantedLoan: 800000,
           propertyId: 'propertyId',
           propertyWork: 0,
           ownFunds: [],
         },
-        properties: [{ _id: 'propertyId', value: 1000000 }],
-        loan: {},
-        borrowers: [{}],
       })).to.equal(250000);
     });
 
     it('overrides notaryFees if provided', () => {
       expect(calculateMissingOwnFunds({
         structure: {
+          id: 'struct1',
           wantedLoan: 800000,
           propertyId: 'propertyId',
           propertyWork: 0,
@@ -31,42 +45,82 @@ describe('ownFundsHelpers', () => {
           notaryFees: 0,
         },
         properties: [{ _id: 'propertyId', value: 1000000 }],
-        loan: {},
+        loan: {
+          properties: [{ _id: 'propertyId', value: 1000000 }],
+          structures: [
+            {
+              id: 'struct1',
+              wantedLoan: 800000,
+              propertyId: 'propertyId',
+              propertyWork: 0,
+              ownFunds: [],
+              notaryFees: 0,
+            },
+          ],
+        },
         borrowers: [{}],
+        structureId: 'struct1',
       })).to.equal(200000);
     });
 
     it('uses property work', () => {
       expect(calculateMissingOwnFunds({
         structure: {
+          id: 'struct1',
           wantedLoan: 800000,
           propertyId: 'propertyId',
           propertyWork: 100000,
           ownFunds: [],
         },
         properties: [{ _id: 'propertyId', value: 900000 }],
-        loan: {},
-        borrowers: [{}],
+        loan: {
+          properties: [{ _id: 'propertyId', value: 900000 }],
+          structures: [
+            {
+              id: 'struct1',
+              wantedLoan: 800000,
+              propertyId: 'propertyId',
+              propertyWork: 100000,
+              ownFunds: [],
+            },
+          ],
+          borrowers: [{}],
+        },
+        structureId: 'struct1',
       })).to.equal(245000);
     });
 
     it('subtracts used own funds', () => {
       expect(calculateMissingOwnFunds({
         structure: {
+          id: 'struct1',
           wantedLoan: 800000,
           propertyId: 'propertyId',
           propertyWork: 0,
           ownFunds: [{ value: 100000 }],
         },
         properties: [{ _id: 'propertyId', value: 1000000 }],
-        loan: {},
+        loan: {
+          properties: [{ _id: 'propertyId', value: 1000000 }],
+          structures: [
+            {
+              id: 'struct1',
+              wantedLoan: 800000,
+              propertyId: 'propertyId',
+              propertyWork: 0,
+              ownFunds: [{ value: 100000 }],
+            },
+          ],
+        },
         borrowers: [{}],
+        structureId: 'struct1',
       })).to.equal(150000);
     });
 
     it('does not count pledge as own funds', () => {
       expect(calculateMissingOwnFunds({
         structure: {
+          id: 'struct1',
           wantedLoan: 800000,
           propertyId: 'propertyId',
           propertyWork: 0,
@@ -76,9 +130,51 @@ describe('ownFundsHelpers', () => {
           ],
         },
         properties: [{ _id: 'propertyId', value: 1000000 }],
-        loan: {},
+        loan: {
+          properties: [{ _id: 'propertyId', value: 1000000 }],
+          structures: [
+            {
+              id: 'struct1',
+              wantedLoan: 800000,
+              propertyId: 'propertyId',
+              propertyWork: 0,
+              ownFunds: [
+                { value: 100000, usageType: OWN_FUNDS_USAGE_TYPES.PLEDGE },
+                { value: 100000, usageType: OWN_FUNDS_USAGE_TYPES.WITHDRAW },
+              ],
+            },
+          ],
+        },
         borrowers: [{}],
+        structureId: 'struct1',
       })).to.equal(150000);
+    });
+
+    it('uses calculated notary fees and rounds them', () => {
+      expect(calculateMissingOwnFunds({
+        structure: {
+          id: 'struct1',
+          wantedLoan: 1179750,
+          propertyId: 'propertyId',
+          propertyWork: 165000,
+          ownFunds: [],
+        },
+        properties: [{ _id: 'propertyId', value: 1650000, canton: 'GE' }],
+        loan: {
+          properties: [{ _id: 'propertyId', value: 1650000, canton: 'GE' }],
+          structures: [
+            {
+              id: 'struct1',
+              wantedLoan: 1179750,
+              propertyId: 'propertyId',
+              propertyWork: 165000,
+              ownFunds: [],
+            },
+          ],
+          borrowers: [{}],
+        },
+        structureId: 'struct1',
+      })).to.equal(720848);
     });
   });
 });
