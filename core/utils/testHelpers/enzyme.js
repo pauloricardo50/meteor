398243d1f7b774ce2ee62d60230205400ec3cf85
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-01-04 17:21:23 +0100

    Somehow enzyme makes wallaby crash when it tries to access HTMLInputElement

--- core/utils/testHelpers/enzyme.js
+++ core/utils/testHelpers/enzyme.js
@@ -7,6 +7,10 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
+// Avoid weird wallaby bug, inspired from here
+// https://github.com/wallabyjs/public/issues/1487
+global.HTMLInputElement = () => {};
+
 export default defaultEnzyme;
 export const mount = defaultMount;
 export const shallow = defaultShallow;
