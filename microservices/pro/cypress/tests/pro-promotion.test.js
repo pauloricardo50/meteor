42786a068a68093ecb47c08f44e4d672a53e899c
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-01-10 17:04:11 +0100

    Add another wait to avoid focus problems

--- microservices/pro/cypress/tests/pro-promotion.test.js
+++ microservices/pro/cypress/tests/pro-promotion.test.js
@@ -92,6 +92,8 @@ describe('Pro', () => {
           .should('not.exist');
 
         cy.contains('Lot 1').click();
+        cy.wait(2000); // Try to wait for focus to settle
+
         cy.get('input[name=name]').clear();
         cy.get('input[name=name]').type('Lot 2');
         cy.setSelect('type', 'BASEMENT');
