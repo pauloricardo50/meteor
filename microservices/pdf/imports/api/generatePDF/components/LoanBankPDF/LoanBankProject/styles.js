6cc98f37c78b96161f064a3c2124c16a35eb0076
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-01-14 14:42:21 +0100

    Adjust styles to make tables go full width

--- microservices/pdf/imports/api/generatePDF/components/LoanBankPDF/LoanBankProject/styles.js
+++ microservices/pdf/imports/api/generatePDF/components/LoanBankPDF/LoanBankProject/styles.js
@@ -1,16 +1,12 @@
-import * as styles from '../../utils/styleHelpers';
-
 const stylesheet = `
     .project-table {
-        ${styles.flex(['FLEX_ROW'])}
+        display: flex;
         justify-content: space-between;
     }
 
-    .project-table .structure-table,
-    .project-table .property-table {
-        ${styles.flex(['FLEX_COLUMN'])}
+    .project-table table {
         width: 48%;
-        box-sizing: border-box;
+        //box-sizing: border-box;
     }
 `;
 
