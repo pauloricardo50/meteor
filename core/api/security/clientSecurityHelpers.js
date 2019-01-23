a2c7f029c1f53a5d2f401a4e4535e13ecdc25faf
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-01-23 17:46:17 +0100

    Make sure promotion security does not crash if there are no userLinks

--- core/api/security/clientSecurityHelpers.js
+++ core/api/security/clientSecurityHelpers.js
@@ -1,5 +1,20 @@
-import { PROMOTION_STATUS } from '../constants';
+import { PROMOTION_STATUS, ROLES } from '../constants';
 import SecurityService from './Security';
 
-export const isAllowedToModifyPromotion = promotion =>
-  [PROMOTION_STATUS.OPEN, PROMOTION_STATUS.PREPARATION].includes(promotion.status) && SecurityService.canModifyDoc(promotion);
+export const isAllowedToModifyPromotion = (promotion) => {
+  if (
+    [PROMOTION_STATUS.OPEN, PROMOTION_STATUS.PREPARATION].includes(promotion.status)
+  ) {
+    return true;
+  }
+
+  if (SecurityService.minimumRole(ROLES.ADMIN)) {
+    return true;
+  }
+
+  if (SecurityService.canModifyDoc(promotion)) {
+    return true;
+  }
+
+  return false;
+};
