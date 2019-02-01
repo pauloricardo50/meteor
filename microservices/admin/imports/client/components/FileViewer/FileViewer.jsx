872d2c5c8781313e920523b3e991cc53877b680d
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-02-01 18:19:56 +0100

    Add basic file-viewer to admin

---
+++ microservices/admin/imports/client/components/FileViewer/FileViewer.jsx
@@ -0,0 +1,31 @@
+// @flow
+import React from 'react';
+import ReactFileViewer from 'react-file-viewer';
+
+import IconButton from 'core/components/IconButton';
+import { withFileViewerContext } from './FileViewerContext';
+
+type FileViewerProps = {};
+
+const FileViewer = ({
+  filePath,
+  fileType,
+  hideFileViewer,
+}: FileViewerProps) => {
+  if (!filePath) {
+    return null;
+  }
+
+  return (
+    <div className="file-viewer">
+      <IconButton
+        onClick={hideFileViewer}
+        type="close"
+        className="file-viewer-closer"
+      />
+      <ReactFileViewer fileType={fileType} filePath={filePath} />
+    </div>
+  );
+};
+
+export default withFileViewerContext(FileViewer);
