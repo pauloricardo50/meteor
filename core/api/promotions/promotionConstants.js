96621e0f7e1d13a4af2dee0525459a5a38c0433b
Quentin Herzig <quentin@e-potek.ch>
2019-02-05 16:16:39 +0100

    Add full access permissions constant

--- core/api/promotions/promotionConstants.js
+++ core/api/promotions/promotionConstants.js
@@ -1,3 +1,4 @@
+import merge from 'lodash/merge';
 import { PROMOTION_LOT_STATUS } from '../promotionLots/promotionLotConstants';
 
 export const PROMOTIONS_COLLECTION = 'promotions';
@@ -43,7 +44,9 @@ export const PROMOTION_PERMISSIONS_PACKAGES = {
     canSeeCustomers: true,
     canPreBookLots: true,
     displayCustomerNames: {
-      forLotStatus: Object.values(PROMOTION_PERMISSIONS.DISPLAY_CUSTOMER_NAMES.FOR_LOT_STATUS),
+      forLotStatus: Object.values(
+        PROMOTION_PERMISSIONS.DISPLAY_CUSTOMER_NAMES.FOR_LOT_STATUS,
+      ),
       invitedBy:
         PROMOTION_PERMISSIONS.DISPLAY_CUSTOMER_NAMES.INVITED_BY.ORGANISATION,
     },
@@ -68,4 +71,21 @@ export const PROMOTION_PERMISSIONS_PACKAGES = {
   BOOKING: settings => ({
     canBookLots: true,
   }),
+};
+
+export const PROMOTION_PERMISSIONS_FULL_ACCESS = () => {
+  const settings = {
+    consultation: {
+      forLotStatus: Object.values(
+        PROMOTION_PERMISSIONS.DISPLAY_CUSTOMER_NAMES.FOR_LOT_STATUS,
+      ),
+      invitedBy: PROMOTION_PERMISSIONS.DISPLAY_CUSTOMER_NAMES.INVITED_BY.ANY,
+    },
+  };
+
+  return Object.keys(PROMOTION_PERMISSIONS_PACKAGES).reduce(
+    (packages, package) =>
+      merge({}, packages, PROMOTION_PERMISSIONS_PACKAGES[package](settings)),
+    {},
+  );
 };
