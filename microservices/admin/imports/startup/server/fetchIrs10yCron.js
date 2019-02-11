a78f8e3fb8c2b27efb5d2c6a66efb6c15cfbdc3f
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-02-11 15:54:40 +0100

    When rescheduling IRS cron, schedule it for tomorrow

--- microservices/admin/imports/startup/server/fetchIrs10yCron.js
+++ microservices/admin/imports/startup/server/fetchIrs10yCron.js
@@ -1,9 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 import { SyncedCron } from 'meteor/littledata:synced-cron';
-import { irs10yFetch } from 'core/api/irs10y/server/methods';
+
+import moment from 'moment';
+
+import { logError, irs10yFetch } from 'core/api/methods';
 
 const getRandomMinute = () => Math.floor(Math.random() * 49) + 10;
 const jobName = 'Fetch IRS 10Y';
+
+SyncedCron.config({
+  logger: ({ level, message, tag }) => {
+    if (Meteor.isProduction) {
+      console.log('---------------------- CRON LOG ----------------------');
+      console.log('Level', level);
+      console.log('Message', message);
+      console.log('Tag', tag);
+    }
+  },
+});
 
 Meteor.startup(() => {
   try {
@@ -13,7 +27,10 @@ Meteor.startup(() => {
       name: jobName,
       schedule(parser) {
         const randomMinute = getRandomMinute();
-        return parser.text(`at 6:${randomMinute}`);
+        const tomorrow = moment()
+          .add(1, 'days')
+          .format('dddd');
+        return parser.text(`at 6:${randomMinute} on ${tomorrow}`);
       },
       job() {
         irs10yFetch.run({});
@@ -21,6 +38,6 @@ Meteor.startup(() => {
     });
     SyncedCron.start();
   } catch (error) {
-    console.error(error);
+    logError.run({ error });
   }
 });
