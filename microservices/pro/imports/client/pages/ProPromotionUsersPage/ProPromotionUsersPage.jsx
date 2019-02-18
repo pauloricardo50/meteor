28d1d20bbab8955c6cf5304ec262ab17db1e8f99
Quentin Herzig <quentin@e-potek.ch>
2019-02-18 16:38:20 +0100

    Clean up imports

--- microservices/pro/imports/client/pages/ProPromotionUsersPage/ProPromotionUsersPage.jsx
+++ microservices/pro/imports/client/pages/ProPromotionUsersPage/ProPromotionUsersPage.jsx
@@ -1,15 +1,15 @@
 // @flow
 import React from 'react';
+import { compose, withProps } from 'recompose';
 
+import proLoans from 'core/api/loans/queries/proLoans';
+import { isAllowedToModifyPromotion } from 'core/api/security/clientSecurityHelpers';
+import { withSmartQuery } from 'core/api';
 import PromotionUsersPage from 'core/components/PromotionUsersPage';
 import Button from 'core/components/Button';
 import T from 'core/components/Translation';
-import { withSmartQuery } from 'core/api';
-import proLoans from 'core/api/loans/queries/proLoans';
-import { compose, withProps } from 'recompose';
 import withMatchParam from 'core/containers/withMatchParam';
 import { createRoute } from 'core/utils/routerUtils';
-import { isAllowedToModifyPromotion } from 'core/api/security/clientSecurityHelpers';
 import { PRO_PROMOTION_PAGE } from '../../../startup/client/proRoutes';
 
 type ProPromotionUsersPageProps = {};
