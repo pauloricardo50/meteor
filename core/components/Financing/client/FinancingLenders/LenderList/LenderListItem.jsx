7b55ca7f401cb217ba8b151afe80c30377f614b4
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-02-22 12:51:49 +0100

    Display structures solvency in lenderPicker

--- core/components/Financing/client/FinancingLenders/LenderList/LenderListItem.jsx
+++ core/components/Financing/client/FinancingLenders/LenderList/LenderListItem.jsx
@@ -1,79 +1,23 @@
 // @flow
 import React from 'react';
 
-import Icon from '../../../../Icon';
-import { Calculator } from '../../../../../utils/Calculator';
-import T, { Money, Percent } from '../../../../Translation';
-import { ERROR, SUCCESS } from '../../../../../api/constants';
-import StatusIcon from '../../../../StatusIcon';
+import LenderListItemRules from './LenderListItemRules';
 
 type LenderListItemProps = {};
 
 const LenderListItem = ({
-  organisation: { name, lenderRules, logo },
+  organisation,
   loan,
   structureId,
 }: LenderListItemProps) => {
-  const calc = new Calculator({ loan, structureId, lenderRules });
-  const incomeRatio = calc.getIncomeRatio({ loan, structureId });
-  const borrowRatio = calc.getBorrowRatio({ loan, structureId });
-  const totalIncome = calc.getTotalIncome({ loan });
-  const expenses = calc.getTheoreticalMonthly({ loan, structureId }) * 12;
-
+  const { name } = organisation;
   return (
     <div className="lender-list-item">
       <h4>{name}</h4>
-      <Icon
-        type="info"
-        className="icon"
-        tooltip={(
-          <div>
-            <div>
-              <span>
-                <T id="FinancingLenders.consideredIncome" /> Revenus
-              </span>
-              :&nbsp;
-              <Money value={totalIncome} />
-            </div>
-            <div>
-              <span>
-                <T id="FinancingLenders.consideredExpenses" />
-              </span>
-              :&nbsp;
-              <Money value={expenses} />
-            </div>
-          </div>
-        )}
-      />
-      <StatusIcon
-        status={incomeRatio > calc.maxIncomeRatio ? ERROR : SUCCESS}
-        tooltip={(
-          <span>
-            <T id="Financing.incomeRatio" />
-            <br />
-            <Percent value={incomeRatio} />
-            <br />
-            <T
-              id="PercentWithStatus.max"
-              values={{ max: <Percent value={calc.maxIncomeRatio} /> }}
-            />
-          </span>
-        )}
-      />
-      <StatusIcon
-        status={borrowRatio > calc.maxBorrowRatio ? ERROR : SUCCESS}
-        tooltip={(
-          <span>
-            <T id="Financing.borrowRatio" />
-            <br />
-            <Percent value={borrowRatio} />
-            <br />
-            <T
-              id="PercentWithStatus.max"
-              values={{ max: <Percent value={calc.maxBorrowRatio} /> }}
-            />
-          </span>
-        )}
+      <LenderListItemRules
+        organisation={organisation}
+        loan={loan}
+        structureId={structureId}
       />
     </div>
   );
