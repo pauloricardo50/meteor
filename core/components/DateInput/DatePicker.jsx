8e5043a0b029eb7a5fa1e30179f38e39d6bd2017
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-03-01 14:01:48 +0100

    Allow picking month and year in date picker

--- core/components/DateInput/DatePicker.jsx
+++ core/components/DateInput/DatePicker.jsx
@@ -12,6 +12,39 @@ const setDateRange = (minDate = undefined, maxDate = undefined) => day =>
   (minDate && !isInclusivelyAfterDay(day, moment(minDate)))
   || (maxDate && isInclusivelyAfterDay(day, moment(maxDate)));
 
+const returnYears = () => {
+  const years = [];
+  for (let i = moment().year() - 100; i <= moment().year(); i++) {
+    years.push(<option value={i}>{i}</option>);
+  }
+  return years;
+};
+
+const renderMonthElement = ({ month, onMonthSelect, onYearSelect }) => (
+  <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <div>
+      <select
+        value={month.month()}
+        onChange={e => onMonthSelect(month, e.target.value)}
+      >
+        {moment.months().map((label, value) => (
+          <option key={label} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </div>
+    <div>
+      <select
+        value={month.year()}
+        onChange={e => onYearSelect(month, e.target.value)}
+      >
+        {returnYears()}
+      </select>
+    </div>
+  </div>
+);
+
 const DatePicker = ({ minDate, maxDate, value, ...props }) => {
   // To prevent prop warnings
   const otherProps = omit(props, [
@@ -42,6 +75,7 @@ const DatePicker = ({ minDate, maxDate, value, ...props }) => {
         minDate || maxDate ? setDateRange(minDate, maxDate) : () => false
       }
       displayFormat="D MMMM YYYY"
+      renderMonthElement={renderMonthElement}
     />
   );
 };
