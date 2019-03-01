755c50122e19eae6bab34dc323037c4d165eed91
Quentin Herzig <quentin@e-potek.ch>
2019-03-01 17:20:40 +0100

    Working on pro property security helpers

--- core/api/security/clientSecurityHelpers/proPropertySecurityHelpers.js
+++ core/api/security/clientSecurityHelpers/proPropertySecurityHelpers.js
@@ -1,11 +1,32 @@
 import { hasMinimumRole } from './generalSecurityHelpers';
 import { ROLES } from '../../users/userConstants';
+import SecurityService from '../Security';
+import { PROPERTY_STATUS } from '../../properties/propertyConstants';
+import {
+  getCurrentUserPermissionsForProProperty,
+  shouldAnonymize,
+} from '../../properties/propertyClientHelper';
 
-export const isPropertyOwner = ({ property, currentUser }) => {
-  const { _id: userId } = currentUser;
-  const { userId: propertyOwner } = property;
+const checkProPropertyPermissions = ({
+  property,
+  userId,
+  permissions,
+  propertyStatus = [],
+}) => {
+  if (propertyStatus.length && !propertyStatus.includes(property.status)) {
+    return false;
+  }
 
-  return propertyOwner === userId;
+  if (hasMinimumRole({ role: ROLES.ADMIN, userId })) {
+    return true;
+  }
+
+  try {
+    SecurityService.hasPermissionOnDoc({ doc: property, permissions, userId });
+    return true;
+  } catch (error) {
+    return false;
+  }
 };
 
 export const isAllowedToViewProProperty = ({ property, currentUser }) => {
@@ -15,7 +36,77 @@ export const isAllowedToViewProProperty = ({ property, currentUser }) => {
     return true;
   }
 
-  if (isPropertyOwner({ property, currentUser })) {
+  const { userLinks = [], users = [], loans = [] } = property;
+  const userLoans = loans
+    .reduce((users, { user }) => [...users, user], [])
+    .filter(x => x);
+
+  const user = userLinks.find(({ _id }) => _id === userId)
+    || users.find(({ _id }) => _id === userId)
+    || userLoans.find(({ _id }) => _id === userId);
+
+  if (!user) {
+    return false;
+  }
+
+  return true;
+};
+
+export const isAllowedToInviteCustomersToProProperty = ({
+  property,
+  currentUser,
+}) => {
+  const { _id: userId } = currentUser;
+  const permissions = { canInviteCustomers: true };
+
+  return checkProPropertyPermissions({
+    property,
+    userId,
+    permissions,
+    propertyStatus: [PROPERTY_STATUS.FOR_SALE, PROPERTY_STATUS.BOOKED],
+  });
+};
+
+export const isAllowedToRemoveCustomerFromProProperty = ({
+  property,
+  currentUser,
+  customerOwnerType,
+}) => {
+  const { _id: userId } = currentUser;
+  if (hasMinimumRole({ role: ROLES.ADMIN, userId })) {
+    return true;
+  }
+  const { _id: propertyId } = property;
+  const permissions = getCurrentUserPermissionsForProProperty({
+    currentUser,
+    propertyId,
+  });
+
+  return (
+    isAllowedToInviteCustomersToProProperty({ property, currentUser })
+    && !shouldAnonymize({ customerOwnerType, permissions })
+  );
+};
+
+export const isAllowedToModifyProProperty = ({ property, currentUser }) => {
+  const { _id: userId } = currentUser;
+  const permissions = { canModifyProperty: true };
+
+  return checkProPropertyPermissions({
+    property,
+    userId,
+    permissions,
+    propertyStatus: [PROPERTY_STATUS.FOR_SALE],
+  });
+};
+
+export const isAllowedToSeeProPropertyCustomers = ({
+  property,
+  currentUser,
+}) => {
+  const { _id: userId } = currentUser;
+
+  if (hasMinimumRole({ role: ROLES.ADMIN, userId })) {
     return true;
   }
 
@@ -28,5 +119,18 @@ export const isAllowedToViewProProperty = ({ property, currentUser }) => {
     return false;
   }
 
+  const userPermissions = user.permissions || user.$metadata.permissions;
+
+  if (userPermissions.displayCustomerNames === false) {
+    return false;
+  }
+
   return true;
+};
+
+export const isPropertyOwner = ({ property, currentUser }) => {
+  const { _id: userId } = currentUser;
+  const { userId: propertyOwner } = property;
+
+  return propertyOwner === userId;
 };
