be64cc9525249e63da34d7bc14fb9440b7a71797
Quentin Herzig <quentin@e-potek.ch>
2019-03-01 10:48:43 +0100

    Add pro property status modifier

--- core/components/ProPropertyPage/ProPropertyPageHeader.jsx
+++ core/components/ProPropertyPage/ProPropertyPageHeader.jsx
@@ -5,25 +5,38 @@ import { Money } from 'core/components/Translation';
 import { ProPropertyModifier } from './ProPropertyForm';
 import PropertyDocumentsManager from './PropertyDocumentsManager';
 import PropertyCustomerAdder from './PropertyCustomerAdder';
+import StatusLabel from '../StatusLabel';
+import { PROPERTIES_COLLECTION } from '../../api/constants';
 
 type ProPropertyPageHeaderProps = {
   property: Object,
 };
 
 const ProPropertyPageHeader = ({ property }: ProPropertyPageHeaderProps) => {
-  const { address1, totalValue, _id: propertyId } = property;
+  const { address1, totalValue, _id: propertyId, status } = property;
 
   return (
     <div className="pro-property-page-header">
       <div className="flex-col">
-        <h1>{address1}</h1>
+        <div className="flex-row center space-children">
+          <h1>{address1}</h1>
+          <StatusLabel
+            status={status}
+            collection={PROPERTIES_COLLECTION}
+            allowModify
+            docId={propertyId}
+          />
+        </div>
         <h3 className="secondary">
           <Money value={totalValue} />
         </h3>
       </div>
-      <ProPropertyModifier property={property} />
-      <PropertyDocumentsManager property={property} />
-      <PropertyCustomerAdder propertyId={propertyId} />
+
+      <div className="pro-property-page-header-buttons">
+        <ProPropertyModifier property={property} />
+        <PropertyDocumentsManager property={property} />
+        <PropertyCustomerAdder propertyId={propertyId} />
+      </div>
     </div>
   );
 };
