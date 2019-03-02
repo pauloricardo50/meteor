7b7a32fb4217eab5dc6dff432a54392cba702697
zodern <zodern@users.noreply.github.com>
2019-03-01 20:18:43 -0600

    Use backend microservice in admin

--- microservices/admin/imports/startup/server/index.js
+++ microservices/admin/imports/startup/server/index.js
@@ -1,15 +1,5 @@
-import '../shared-startup';
-
 import { Inject } from 'meteor/meteorhacks:inject-initial';
-
-import 'core/api/api-server';
-import 'core/api/api';
-import 'core/fixtures';
-
-import '../accounts-config';
 import './kadira';
-
-import './fetchIrs10yCron';
 
 // Inject a loader before client is ready, is removed in the on startup function on the client
 Inject.rawHead('loader', Assets.getText('loader.html'));
