4f84c53296225e9041652f25a4185a54d0d785e8
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-03-07 20:55:27 +0100

    Disable structure suggestion when structure is locked

--- core/components/Financing/client/FinancingOwnFunds/RequiredOwnFunds.jsx
+++ core/components/Financing/client/FinancingOwnFunds/RequiredOwnFunds.jsx
@@ -1,61 +1,21 @@
 // @flow
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagic } from '@fortawesome/pro-light-svg-icons/faMagic';
 
-import { SUCCESS, ERROR } from '../../../../api/constants';
-import StatusIcon from '../../../StatusIcon';
-import T from '../../../Translation';
-import { toMoney } from '../../../../utils/conversionFunctions';
-import { OWN_FUNDS_ROUNDING_AMOUNT } from '../../../../config/financeConstants';
 import { CalculatedValue } from '../FinancingSection';
-import IconButton from '../../../IconButton';
 import StructureUpdateContainer from '../containers/StructureUpdateContainer';
 import Calculator from '../../../../utils/Calculator';
+import RequiredOwnFundsBody from './RequiredOwnFundsBody';
 
 type RequiredOwnFundsProps = {};
 
-const getLabel = (value) => {
-  if (value > OWN_FUNDS_ROUNDING_AMOUNT) {
-    return 'Financing.requiredOwnFunds.low';
-  }
-  if (value < -OWN_FUNDS_ROUNDING_AMOUNT) {
-    return 'Financing.requiredOwnFunds.high';
-  }
-
-  return 'Financing.requiredOwnFunds.valid';
-};
-
-export const RequiredOwnFundsBody = ({ value, suggestStructure }) => {
-  console.log('value', value);
-
-  return (
-    <div className="requiredOwnFunds-component-body">
-      <div className="text-and-value">
-        <span className="text">
-          <T id={getLabel(value)} />
-        </span>
-        <div className="value">
-          <span className="chf">CHF</span>
-          {toMoney(value)}
-          <StatusIcon
-            status={getLabel(value).endsWith('valid') ? SUCCESS : ERROR}
-            style={{ marginLeft: 8 }}
-          />
-        </div>
-      </div>
-      <IconButton
-        type={<FontAwesomeIcon icon={faMagic} />}
-        onClick={suggestStructure}
-        tooltip="Suggérer"
-      />
-    </div>
-  );
-};
-
 const RequiredOwnFunds = (props: RequiredOwnFundsProps) => {
-  const { updateStructure, loan, structureId, calculateValue } = props;
-  console.log('calculateValue:', calculateValue);
+  const {
+    updateStructure,
+    loan,
+    structureId,
+    calculateValue,
+    structure: { disableForms },
+  } = props;
   return (
     <CalculatedValue
       {...props}
@@ -72,6 +32,7 @@ const RequiredOwnFunds = (props: RequiredOwnFundsProps) => {
             });
             updateStructure({ ownFunds });
           }}
+          disableForms={disableForms}
         />
       )}
     </CalculatedValue>
