8e40a354008cec8da3ee9eda54b7ca4137675a97
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-03-10 21:59:59 +0100

    Watch updates on loans and properties

--- core/api/properties/server/hooks.js
+++ core/api/properties/server/hooks.js
@@ -1,12 +1,15 @@
 import LoanService from '../../loans/server/LoanService';
 import Properties from '..';
-
+import SecurityService from '../../security';
 import { additionalDocumentsHook } from '../../helpers/sharedHooks';
 import { PROPERTIES_COLLECTION } from '../propertyConstants';
 import {
   initialDocuments,
   conditionalDocuments,
 } from '../propertiesAdditionalDocuments';
+import UpdateWatcherService from '../../updateWatchers/server/UpdateWatcherService';
+import { ROLES } from '../../constants';
+import { PropertySchemaAdmin } from '../schemas/PropertySchema';
 
 Properties.before.remove((userId, { _id: propertyId }) => {
   // Remove all references to this property on the loan
@@ -37,3 +40,11 @@ Properties.after.update(additionalDocumentsHook({
   initialDocuments,
   conditionalDocuments,
 }));
+
+UpdateWatcherService.addUpdateWatching({
+  collection: Properties,
+  fields: PropertySchemaAdmin._schemaKeys,
+  shouldWatch: ({ userId }) =>
+    SecurityService.hasRole(userId, ROLES.USER)
+    || SecurityService.hasRole(userId, ROLES.PRO),
+});
