c32b3edfc73771d532b442a46aa215141a541d9d
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-03-12 18:51:10 +0100

    Add progres percent in single loan page tabs

--- microservices/admin/imports/client/pages/SingleLoanPage/LoanTabs/PropertiesTab.jsx
+++ microservices/admin/imports/client/pages/SingleLoanPage/LoanTabs/PropertiesTab.jsx
@@ -3,37 +3,51 @@ import React from 'react';
 
 import Tabs from 'core/components/Tabs';
 import { PropertyAdder, PropertyReuser } from 'core/components/PropertyForm';
+import Calculator from 'core/utils/Calculator';
+import { percentFormatters } from 'core/utils/formHelpers';
 import SinglePropertyPage from '../../SinglePropertyPage/SinglePropertyPage';
 
 type PropertiesTabProps = {
   loan: Object,
 };
 
-const PropertiesTab = ({
-  loan: { properties, userId, _id: loanId, residenceType: loanResidenceType },
-}: PropertiesTabProps) => (
-  <div className="properties-tab">
-    <div className="buttons">
-      <PropertyAdder loanId={loanId} propertyUserId={userId} />
-      <PropertyReuser loanId={loanId} propertyUserId={userId} />
+const propertiesTabLabel = (loan, property, index) => {
+  const progress = Calculator.propertyPercent({ loan, property });
+  return `${property.address1
+    || `Bien immo ${index + 1}`} - ${percentFormatters.format(progress)}%`;
+};
+
+const PropertiesTab = ({ loan }: PropertiesTabProps) => {
+  const {
+    properties,
+    userId,
+    _id: loanId,
+    residenceType: loanResidenceType,
+  } = loan;
+  return (
+    <div className="properties-tab">
+      <div className="buttons">
+        <PropertyAdder loanId={loanId} propertyUserId={userId} />
+        <PropertyReuser loanId={loanId} propertyUserId={userId} />
+      </div>
+      {properties && properties.length > 0 && (
+        <Tabs
+          tabs={properties.map((property, index) => ({
+            id: property._id,
+            label: propertiesTabLabel(loan, property, index),
+            content: (
+              <SinglePropertyPage
+                propertyId={property._id}
+                displayLoans={false}
+                loanResidenceType={loanResidenceType}
+                loanId={loanId}
+              />
+            ),
+          }))}
+        />
+      )}
     </div>
-    {properties && properties.length > 0 && (
-      <Tabs
-        tabs={properties.map((property, index) => ({
-          id: property._id,
-          label: property.address1 || `Bien immo ${index + 1}`,
-          content: (
-            <SinglePropertyPage
-              propertyId={property._id}
-              displayLoans={false}
-              loanResidenceType={loanResidenceType}
-              loanId={loanId}
-            />
-          ),
-        }))}
-      />
-    )}
-  </div>
-);
+  );
+};
 
 export default PropertiesTab;
