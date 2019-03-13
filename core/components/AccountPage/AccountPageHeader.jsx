873920e2225912b00017940620990de487282f80
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-03-13 18:38:59 +0100

    Improve account page styling

---
+++ core/components/AccountPage/AccountPageHeader.jsx
@@ -0,0 +1,21 @@
+// @flow
+import React from 'react';
+
+import { getUserDisplayName } from '../../utils/userFunctions';
+import T from '../Translation';
+import Icon from '../Icon';
+
+const AccountPageHeader = ({ currentUser }) => {
+  const { organisations } = currentUser;
+
+  return (
+    <div className="account-page-header">
+      <Icon type="accountCircle" className="icon" />
+      <h1>{getUserDisplayName(currentUser)}</h1>
+      {organisations && organisations.length > 0 && (
+        <h3 className="secondary">{organisations[0].name}</h3>
+      )}
+    </div>
+  );
+};
+export default AccountPageHeader;
