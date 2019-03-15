5fac10195f0fdea3d9a85eae1a7f6426192abad2
Quentin Herzig <quentin@e-potek.ch>
2019-03-15 10:26:25 +0100

    Add referred by organisation assign dropdown

---
+++ microservices/admin/imports/client/pages/SingleUserPage/ReferredByOrganisationAssignDropdownContainer.js
@@ -0,0 +1,39 @@
+import { withProps, compose } from 'recompose';
+
+import { setUserReferredByOrganisation } from 'core/api';
+import { withSmartQuery } from 'core/api/containerToolkit';
+import query from 'core/api/organisations/queries/adminOrganisations';
+import { ORGANISATION_FEATURES } from 'core/api/constants';
+
+const getMenuItems = ({
+  organisations,
+  referredByOrganisation: { referredByOrganisationId } = {},
+  userId,
+}) =>
+  organisations.map((organisation) => {
+    const { _id: organisationId, name } = organisation;
+    return {
+      id: organisationId,
+      show: organisationId !== referredByOrganisationId,
+      label: name,
+      link: false,
+      onClick: () =>
+        setUserReferredByOrganisation.run({ userId, organisationId }),
+    };
+  });
+
+export default compose(
+  withSmartQuery({
+    query,
+    queryOptions: { reactive: false },
+    params: () => ({ features: ORGANISATION_FEATURES.PRO }),
+    dataName: 'organisations',
+  }),
+  withProps(({
+    organisations = [],
+    user: { _id: userId, referredByOrganisation },
+  }) => ({
+    options: getMenuItems({ organisations, referredByOrganisation, userId }),
+    referredByOrganisation,
+  })),
+);
