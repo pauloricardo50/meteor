aa300150e587c56f1e3cdbc21d896793ca4b9613
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-04-04 11:38:01 +0200

    Update import paths for react-bootstrap v1

--- core/components/tooltips/TooltipOverlay.jsx
+++ core/components/tooltips/TooltipOverlay.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 import { tooltips } from 'core/arrays/tooltips';
 import track from 'core/utils/analytics';
