109bddc01464808d35ac413bc1054893d76f310f
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-04-17 11:39:01 +0200

    Change port for production DB

--- .deployment/ssh-tunnel/openSSHTunnel.js
+++ .deployment/ssh-tunnel/openSSHTunnel.js
@@ -86,7 +86,7 @@ const openSSHTunnel = ({ sshIdNumber = 0, environmentOverride } = {}) => {
       const parsedCredentials = JSON.parse(`{${credentials}}`);
       return {
         ...parsedCredentials,
-        mongoPort: Number(parsedCredentials.ports.split(',')[1]),
+        mongoPort: Number(parsedCredentials.ports.split(',')[2]),
         sshId: SSH_ID,
         environment,
       };
