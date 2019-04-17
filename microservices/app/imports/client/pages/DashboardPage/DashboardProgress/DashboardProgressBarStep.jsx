2f83a725268facaeb42022663ecd951dfce72416
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-04-17 14:36:55 +0200

    Add tooltips to dashboard progress

--- microservices/app/imports/client/pages/DashboardPage/DashboardProgress/DashboardProgressBarStep.jsx
+++ microservices/app/imports/client/pages/DashboardPage/DashboardProgress/DashboardProgressBarStep.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import T from 'core/components/Translation';
+import Icon from 'core/components/Icon';
+import { STEPS } from 'core/api/constants';
 
 const DashboardProgressBarStep = ({ id, isDone, nb, displayLabel }) => (
-  <div key={nb} className="dashboard-progress-bar-step">
-    <span className={classnames({ step: true, done: isDone })}>{nb}</span>
-    {displayLabel && (
-      <span className="step-name">
-        <T id={`Forms.step.${id}`} />
-      </span>
-    )}
-  </div>
+  <Tooltip
+    title={
+      id === STEPS.CLOSING ? (
+        <Icon type="monetizationOn" className="heart-beat" size={40} />
+      ) : (
+        <T id={`Forms.step.${id}.tooltip`} />
+      )
+    }
+  >
+    <div key={nb} className="dashboard-progress-bar-step">
+      <span className={classnames({ step: true, done: isDone })}>{nb}</span>
+      {displayLabel && (
+        <span className="step-name">
+          <T id={`Forms.step.${id}`} />
+        </span>
+      )}
+    </div>
+  </Tooltip>
 );
 
 DashboardProgressBarStep.propTypes = {
