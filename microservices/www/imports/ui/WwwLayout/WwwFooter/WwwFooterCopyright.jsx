a3b5b2963fd31a254147d51050ab528bcd040ace
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-04-26 12:41:29 +0200

    Add privacy policy in www

--- microservices/www/imports/ui/WwwLayout/WwwFooter/WwwFooterCopyright.jsx
+++ microservices/www/imports/ui/WwwLayout/WwwFooter/WwwFooterCopyright.jsx
@@ -1,3 +1,5 @@
+import { Meteor } from 'meteor/meteor';
+
 import React from 'react';
 
 import Icon from 'core/components/Icon';
@@ -7,7 +9,14 @@ const WwwFooterCopyright = () => (
     Copyright - e-Potek {new Date().getFullYear()} &bull; Hébergé et sécurisé en{' '}
     <Icon type="health" className="switzerland" />
     <br />
-    UID : CHE-405.084.029 -{' '}
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href={`${Meteor.settings.public.subdomains.www}/files/Privacy Policy - e-Potek.pdf`}
+    >
+      Privacy policy
+    </a>{' '}&bull;{' '}
+    UID : CHE-405.084.029 &bull;{' '}
     <a
       target="_blank"
       rel="noopener noreferrer"
