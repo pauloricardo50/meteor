6783342801ca6fd4c1bd676bc854d5bbef00a19e
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-04-26 18:09:38 +0200

    Remove the filesReducer

--- core/api/organisations/server/serverReducers.js
+++ core/api/organisations/server/serverReducers.js
@@ -1,10 +1,8 @@
 import Organisations from '..';
-import filesReducer from '../../reducers/filesReducer';
 import RevenueService from '../../revenues/server/RevenueService';
 import { getCurrentRate } from '../helpers';
 
 Organisations.addReducers({
-  ...filesReducer,
   generatedRevenues: {
     body: { _id: 1 },
     reduce: ({ _id: organisationId }) =>
