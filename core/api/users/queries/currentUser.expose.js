2107a4363fcbbea20d87bbbbf9c0f1d946bb8d81
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-05-01 22:12:38 +0200

    Improve query

--- core/api/users/queries/currentUser.expose.js
+++ core/api/users/queries/currentUser.expose.js
@@ -1,17 +1,16 @@
 import { Match } from 'meteor/check';
 
-import SecurityService from '../../security';
 import query from './currentUser';
 
 query.expose({
   firewall(userId, params) {
-    params.userId = userId;
+    params._id = userId;
   },
-  validateParams: { userId: Match.Maybe(String), $body: Match.Maybe(Object) },
+  validateParams: { _id: Match.Maybe(String), $body: Match.Maybe(Object) },
   embody: {
     // This will deepExtend your body
     $filter({ filters, params }) {
-      filters._id = params.userId;
+      filters._id = params._id;
     },
   },
 });
