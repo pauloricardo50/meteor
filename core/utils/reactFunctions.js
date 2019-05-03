35121d83b53d37be8cc7ae60463e8cf8f299a1f1
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-05-03 12:32:43 +0200

    Add optimization function for easy shouldComponentUpdate

--- core/utils/reactFunctions.js
+++ core/utils/reactFunctions.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { renderComponent } from 'recompose';
+import get from 'lodash/get';
 
 export const childrenToComponent = children => props =>
   React.Children.map(children, child => React.cloneElement(child, props));
@@ -8,3 +9,28 @@ export const renderObjectOrFunction = objectOrFunction =>
   renderComponent(typeof objectOrFunction === 'function'
     ? objectOrFunction
     : () => objectOrFunction);
+
+const isEqual = (a, b) => {
+  if (Array.isArray(a) || Array.isArray(b)) {
+    if (a.length !== b.length) {
+      return false;
+    }
+
+    return a.every((item, i) => isEqual(item, b[i]));
+  }
+
+  if (
+    (typeof a === 'object' && a !== null)
+    || (typeof b === 'object' && b !== null)
+  ) {
+    throw new Error('Should not compare objects between each other, use a more nested key');
+  }
+
+  return a === b;
+};
+
+export const arePathsEqual = paths => (object1, object2) =>
+  paths.every(path => isEqual(get(object1, path), get(object2, path)));
+
+export const arePathsUnequal = paths => (...args) =>
+  !arePathsEqual(paths)(...args);
---
+++ core/utils/test/reactFunctions.spec.js
@@ -0,0 +1,38 @@
+/* eslint-env mocha */
+import { expect } from 'chai';
+
+import { arePathsUnequal } from '../reactFunctions';
+
+describe.only('reactFunctions', () => {
+  describe('arePathsUnequal', () => {
+    it('returns true or false if paths are unequal', () => {
+      const func = arePathsUnequal(['a']);
+      expect(func({ a: 1 }, { a: 2 })).to.equal(true);
+      expect(func({ a: 1 }, { a: 1 })).to.equal(false);
+    });
+
+    it('does not fail with nested objects', () => {
+      const func = arePathsUnequal(['a.b']);
+      expect(func({ a: 1 }, { a: { b: 1 } })).to.equal(true);
+      expect(func({ a: 1 }, { a: 1 })).to.equal(false);
+      expect(func({ a: { b: 1 } }, { a: { b: 1 } })).to.equal(false);
+    });
+
+    it('compares arrays', () => {
+      const func = arePathsUnequal(['a']);
+      expect(func({ a: [1] }, { a: [1] })).to.equal(false);
+      expect(func({ a: [1] }, { a: [] })).to.equal(true);
+    });
+
+    it('throws if trying to compare objects', () => {
+      const func = arePathsUnequal(['a']);
+      expect(() => func({ a: {} }, { a: {} })).to.throw('Should not');
+    });
+
+    it('works if one value is null', () => {
+      const func = arePathsUnequal(['a']);
+      expect(func({ a: null }, { a: 2 })).to.equal(true);
+      expect(func({ a: 2 }, { a: null })).to.equal(true);
+    });
+  });
+});
