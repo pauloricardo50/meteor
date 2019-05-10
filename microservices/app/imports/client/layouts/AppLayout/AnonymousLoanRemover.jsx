583c6c4dde47e7bcaf7fa223ccdc6c01fa275729
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-05-10 19:23:34 +0200

    Remove component in production

--- microservices/app/imports/client/layouts/AppLayout/AnonymousLoanRemover.jsx
+++ microservices/app/imports/client/layouts/AppLayout/AnonymousLoanRemover.jsx
@@ -1,35 +1,33 @@
 // @flow
-import { Meteor } from 'meteor/meteor';
-
 import React from 'react';
 import { LOCAL_STORAGE_ANONYMOUS_LOAN } from 'core/api/constants';
 
-type AnonymousLoanRemoverProps = {};
+let AnonymousLoanRemover;
 
-const AnonymousLoanRemover = (props: AnonymousLoanRemoverProps) => {
-  if (!Meteor.isDevelopment) {
-    return null;
-  }
+if (process.env.NODE_ENV !== 'production') {
+  AnonymousLoanRemover = () => {
+    const loanId = localStorage.getItem(LOCAL_STORAGE_ANONYMOUS_LOAN);
 
-  const loanId = localStorage.getItem(LOCAL_STORAGE_ANONYMOUS_LOAN);
+    if (!loanId) {
+      return null;
+    }
 
-  if (!loanId) {
-    return null;
-  }
+    return (
+      <button
+        style={{ position: 'absolute', bottom: 0, right: 0, opacity: 0.5 }}
+        type="button"
+        onClick={() => {
+          localStorage.removeItem(LOCAL_STORAGE_ANONYMOUS_LOAN);
+          // Reload page for react to know about new localStorage
+          window.location.reload();
+        }}
+      >
+        Remove anon loan
+      </button>
+    );
+  };
+}
 
-  return (
-    <button
-      style={{ position: 'absolute', bottom: 0, right: 0, opacity: 0.5 }}
-      type="button"
-      onClick={() => {
-        localStorage.removeItem(LOCAL_STORAGE_ANONYMOUS_LOAN);
-        // Reload page for react to know about new localStorage
-        window.location.reload();
-      }}
-    >
-      Remove anon loan
-    </button>
-  );
-};
+AnonymousLoanRemover = () => null;
 
 export default AnonymousLoanRemover;
