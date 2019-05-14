56e53e052789abe1b98d1f0b0c51eb407389a628
zodern <zodern@users.noreply.github.com>
2019-05-13 20:09:03 -0500

    Add run-with-backend.js script

---
+++ scripts/run-with-backend.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-console */
+
+const {
+  spawn,
+} = require('child_process');
+const net = require('net');
+const path = require('path');
+
+const BACKEND_PORT = 5500;
+
+const [
+  microservice,
+] = process.argv.slice(2);
+
+if (!microservice) {
+  throw new Error('Microservice argument not provided. Usage: "node run-with-backend.js <microservice>" ');
+}
+
+function startMeteor(_microservice) {
+  spawn(
+    'npm',
+    ['run', 'start'],
+    {
+      cwd: path.resolve(__dirname, `../microservices/${_microservice}`),
+      stdio: 'inherit',
+    },
+  );
+}
+
+// Check if microservice is running by checking its port is in use
+const listener = net.createServer()
+  .once('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+      console.log('=> Backend already running');
+    }
+
+    console.error('=> Unable to check if Backend is running', err.code);
+  })
+  .once('listening', () => {
+    listener.once('close', () => {
+      startMeteor('backend');
+    });
+
+    listener.close();
+  })
+  .listen(BACKEND_PORT);
+
+startMeteor(microservice);
