a23c560228e26352dce4ec145b13f3d5d76cd9c9
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-05-14 12:09:31 +0200

    Fix test

--- core/api/users/server/test/UserService.test.js
+++ core/api/users/server/test/UserService.test.js
@@ -818,7 +818,6 @@ describe('UserService', function () {
         promotionIds: ['promotionId'],
         proUserId: 'proId',
         adminId: 'adminId',
-        shareSolvency,
       }).then(() => {
         const { loans } = UserService.fetchOne({
           $filters: { _id: 'userId' },
