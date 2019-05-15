2c033d1d881da63c825f74e8526186d9fb77fe4a
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-05-15 12:38:42 +0200

    Prevent creating accounts on login page

--- core/components/LoginPage/LoginPageRight.jsx
+++ core/components/LoginPage/LoginPageRight.jsx
@@ -1,23 +1,55 @@
 // @flow
+import { Meteor } from 'meteor/meteor';
+
 import React from 'react';
 
 import { sendVerificationLink } from 'core/api/methods';
 import Accounts from './Accounts';
+import Link from '../Link';
+import T from '../Translation';
 
 type LoginPageRightProps = {};
 
-const LoginPageRight = ({ path, push }: LoginPageRightProps) => (
-  <div className="right">
-    <div className="content">
-      <Accounts.ui.LoginForm
-        onSignedInHook={() => push(path || '/')}
-        onPostSignUpHook={() => {
-          push(path || '/');
-          sendVerificationLink.run({});
-        }}
-      />
+const LoginPageRight = ({ path, push }: LoginPageRightProps) => {
+  const isApp = Meteor.microservice === 'app';
+  return (
+    <div className="right">
+      <div className="content">
+        <span className="no-account">
+          {isApp ? (
+            <T
+              id="LoginPage.noAccountApp"
+              values={{
+                link: (
+                  <Link to="/" className="color">
+                    <T id="general.here" />
+                  </Link>
+                ),
+              }}
+            />
+          ) : (
+            <T
+              id="LoginPage.noAccount"
+              values={{
+                contactUs: (
+                  <a href={`${Meteor.settings.public.subdomains.www}/contact`}>
+                    <T id="LoginPage.contactUs" />
+                  </a>
+                ),
+              }}
+            />
+          )}
+        </span>
+        <Accounts.ui.LoginForm
+          onSignedInHook={() => push(path || '/')}
+          onPostSignUpHook={() => {
+            push(path || '/');
+            sendVerificationLink.run({});
+          }}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default LoginPageRight;
