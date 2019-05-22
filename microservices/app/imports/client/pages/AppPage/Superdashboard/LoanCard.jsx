6164a59cb77eaf9fa436a6a5fce1ddfe11c9356b
Quentin Herzig <quentin@e-potek.ch>
2019-05-22 14:47:38 +0200

    Add loan card

--- microservices/app/imports/client/pages/AppPage/Superdashboard/LoanCard.jsx
+++ microservices/app/imports/client/pages/AppPage/Superdashboard/LoanCard.jsx
@@ -1,14 +1,95 @@
 // @flow
 import React from 'react';
+import BorrowersSummary from 'core/components/BorrowersSummary';
+import LoanRenamer from '../LoanRenamer';
 
 type LoanCardProps = {
   loan: Object,
 };
 
-const LoanCard = ({ loan = {} }: LoanCardProps) => {
-  console.log('loan:', loan);
+const getLoanTitle = (name, customName) => {
+  if (customName) {
+    return `${name} - ${customName}`;
+  }
 
-  return <div className="loancard">{loan.name}</div>;
+  return name;
+};
+
+const getAddress = ({ promotions, properties, hasPromotion }) =>
+  (hasPromotion
+    ? promotions[0].address
+    : properties.length
+      ? properties[0].address
+      : '');
+
+const getImage = ({ promotions, properties, hasPromotion, hasProProperty }) => {
+  if (hasPromotion) {
+    const promotion = promotions[0];
+    const { documents: { promotionImage = [] } = {} } = promotion;
+    return promotionImage.length ? promotionImage[0].url : null;
+  }
+  if (hasProProperty) {
+    const property = properties[0];
+    const { documents: { propertyImages = [] } = {} } = property;
+    return propertyImages.length ? propertyImages[0].url : null;
+  }
+
+  return null;
+};
+
+const LoanCard = ({ loan = {} }: LoanCardProps) => {
+  const {
+    _id: loanId,
+    borrowers = [],
+    customName,
+    name,
+    hasPromotion,
+    hasProProperty,
+    promotions = [],
+    properties = [],
+    purchaseType,
+    step,
+  } = loan;
+
+  const image = getImage({
+    promotions,
+    properties,
+    hasPromotion,
+    hasProProperty,
+  });
+
+  return (
+    <div className="loancard">
+      <div className="loancard-header">
+        {image && (
+          <div className="loancard-header-cover">
+            <div className="gradient" />
+            <img src={image} />
+          </div>
+        )}
+        <div className="loancard-header-title">
+          <h4>
+            <small>{purchaseType}</small>
+          </h4>
+          <h2>{getLoanTitle(name, customName)}</h2>
+          <p>{getAddress({ promotions, properties, hasPromotion })}</p>
+        </div>
+        <LoanRenamer
+          loanId={loanId}
+          customName={customName}
+          className="loan-renamer"
+        />
+      </div>
+      <div className="loancard-body">
+        <div className="borrowers">
+          <h4 className="secondary">Emprunteurs</h4>
+          <BorrowersSummary borrowers={borrowers} showTitle={false} />
+        </div>
+        {/* <p>{step}</p> */}
+        <p>{step}</p>
+      </div>
+    </div>
+  );
 };
 
 export default LoanCard;
