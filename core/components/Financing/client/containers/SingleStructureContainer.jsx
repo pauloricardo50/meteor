242f1f854ea03ebb22f460bc0065ec598b0121c9
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-06-13 09:58:57 +0200

    Make sure ownFunds state is never out of sync

--- core/components/Financing/client/containers/SingleStructureContainer.jsx
+++ core/components/Financing/client/containers/SingleStructureContainer.jsx
@@ -4,24 +4,15 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { Consumer } from './loan-context';
 
-export default Component =>
-  React.memo((props) => {
-    const { structureId } = props;
-    return (
-      <Consumer>
-        {({ loan }) => {
-          const structure = loan.structures.find(({ id }) => id === structureId);
-          return (
-            <Component
-              {...props}
-              structure={{
-                ...structure,
-                disableForms:
-                  Meteor.microservice === 'admin' ? false : structure.disabled,
-              }}
-            />
-          );
-        }}
-      </Consumer>
-    );
-  });
+export default Component => (props) => {
+  const { structureId } = props;
+  return (
+    <Consumer>
+      {({ loan }) => {
+        const structure = loan.structures.find(({ id }) => id === structureId);
+        structure.disableForms = Meteor.microservice === 'admin' ? false : structure.disabled;
+        return <Component {...props} structure={structure} />;
+      }}
+    </Consumer>
+  );
+};
