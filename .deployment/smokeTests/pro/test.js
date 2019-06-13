96c4f543b7bec6d3c0e3f0d8a394a1f41d546091
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-06-13 16:51:31 +0200

    Clean up smoke tests

--- .deployment/smokeTests/pro/test.js
+++ .deployment/smokeTests/pro/test.js
@@ -1,26 +1,3 @@
-const Nightmare = require('nightmare');
-const cfenv = require('cfenv');
-const assert = require('assert');
+import { assertTitle } from '../smokeTestHelpers';
 
-const appEnv = cfenv.getAppEnv();
-
-const nightmare = new Nightmare({});
-
-const TITLE = 'Accédez à votre compte e-Potek.';
-
-const checkTitle = title => {
-  assert(title === TITLE, `Wrong title: expected "${TITLE}", got "${title}"`);
-};
-
-nightmare
-  .goto(`https://${appEnv.name}.scapp.io`)
-  .wait(500)
-  .evaluate(() => document.querySelector('h1').textContent)
-  .end()
-  .then(checkTitle)
-  .then(() => console.log('Smoke test success!'))
-  .catch(e => {
-    const error = `Smoke test failed. Reason:\n${e}`;
-    console.error(error);
-    throw error;
-  });
+assertTitle('Accédez à votre compte e-Potek.');
