6ecd616d95227ae84d793530429d49706903cef7
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-06-15 18:42:26 +0200

    Make token random, to never bbe duplicate

--- core/components/PasswordResetPage/test/client/PasswordResetPage.test.jsx
+++ core/components/PasswordResetPage/test/client/PasswordResetPage.test.jsx
@@ -1,9 +1,11 @@
 // @flow
 /* eslint-env mocha */
+import { resetDatabase } from 'meteor/xolvio:cleaner';
+import { Random } from 'meteor/random';
+
 import React from 'react';
 import { expect } from 'chai';
 import { Redirect } from 'react-router-dom';
-import { resetDatabase } from 'meteor/xolvio:cleaner';
 
 import { testCreateUser } from '../../../../api';
 import {
@@ -29,7 +31,7 @@ describe('PasswordResetPage', () => {
   beforeEach(() => {
     resetDatabase();
     getMountedComponent.reset();
-    props = { token: 'token12345', email: 'john.doe@test.com' };
+    props = { token: Random.id(), email: 'john.doe@test.com' };
   });
 
   it('renders the name', () => {
@@ -56,7 +58,7 @@ describe('PasswordResetPage', () => {
 
   it('Redirects to the login page if there is an error', () => {
     props.error = { message: 'Test error' };
-    
+
     expect(shallowComponent().find(Redirect).length).to.equal(1);
   });
 
