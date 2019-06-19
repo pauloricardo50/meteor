8a9e84ebba3db00e96baeefa0380d16a57a3b405
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-06-19 22:25:44 +0200

    Allow querying activity by type

--- core/api/activities/server/exposures.js
+++ core/api/activities/server/exposures.js
@@ -5,7 +5,7 @@ exposeQuery({
   query: adminActivities,
   overrides: {
     embody: (body) => {
-      body.$filter = ({ filters, params: { _id, loanId } }) => {
+      body.$filter = ({ filters, params: { _id, loanId, type } }) => {
         if (_id) {
           filters._id = _id;
         }
@@ -13,11 +13,16 @@ exposeQuery({
         if (loanId) {
           filters['loanLink._id'] = loanId;
         }
+
+        if (type) {
+          filters.type = type;
+        }
       };
     },
     validateParams: {
       _id: Match.Maybe(String),
       loanId: Match.Maybe(String),
+      type: Match.Maybe(Match.OneOf(Object, String)),
     },
   },
 });
