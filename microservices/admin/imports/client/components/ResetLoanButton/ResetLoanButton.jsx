6eb0e7a501b4574a70e29bee0b29cc90c8d38157
Quentin Herzig <quentin@e-potek.ch>
2019-06-19 15:50:17 +0200

    Better confirm message

--- microservices/admin/imports/client/components/ResetLoanButton/ResetLoanButton.jsx
+++ microservices/admin/imports/client/components/ResetLoanButton/ResetLoanButton.jsx
@@ -8,7 +8,7 @@ import { adminLoanReset } from 'imports/core/api/methods/index';
 type ResetLoanButtonProps = {};
 
 const ResetLoanButton = ({ loading, onClick }: ResetLoanButtonProps) => (
-  <Button onClick={onClick} raised error disabled={loading}>
+  <Button onClick={onClick} outlined error disabled={loading}>
     Réinitialiser le dossier
   </Button>
 );
@@ -18,7 +18,14 @@ export default compose(
   withProps(({ loan, setLoading }) => ({
     onClick: () => {
       const { _id: loanId } = loan;
-      const confirm = window.confirm('Réinitialiser le dossier ?');
+      const confirm = window.confirm(`Réinitialiser le dossier ? Les informations suivantes seront modifées:\n
+      - Statut -> "Test"
+      - Etape du dossier -> "Attestation de financement"
+      - Interface -> "Simplifiée"
+      - Suppression des plans financiers
+      - Suppression de la capacité d'achat
+      - Suppression des informations financières des emprunteurs
+      `);
       if (confirm) {
         setLoading(true);
         return adminLoanReset.run({ loanId }).then(() => setLoading(false));
