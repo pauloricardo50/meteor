ebd7156c597a2d1d5be328e34cdf413cbb894a0b
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-06-20 13:27:14 +0200

    Allow adding tasks from top nab

--- microservices/admin/imports/client/layouts/AdminLayout/AdminTopNav.jsx
+++ microservices/admin/imports/client/layouts/AdminLayout/AdminTopNav.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import updateForProps from 'core/containers/updateForProps';
 import TopNav from 'core/components/TopNav';
 import Search from '../../components/Search';
+import TaskAdder from '../../components/TasksTable/TaskAdder';
 
 const AdminTopNav = ({ setOpenSearch, openSearch, ...props }) => (
   <div className="admin-top-nav">
     <TopNav {...props}>
+      <TaskAdder />
       <Search openSearch={openSearch} setOpenSearch={setOpenSearch} />
     </TopNav>
   </div>
