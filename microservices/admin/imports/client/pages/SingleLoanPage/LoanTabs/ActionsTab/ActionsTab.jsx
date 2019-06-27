400cab1cc2c095db8eb4c5e533585b3d4f600f6f
Quentin Herzig <quentin@e-potek.ch>
2019-06-27 14:58:23 +0200

    Disable submit button when trying to change loan name to its actual value

--- microservices/admin/imports/client/pages/SingleLoanPage/LoanTabs/ActionsTab/ActionsTab.jsx
+++ microservices/admin/imports/client/pages/SingleLoanPage/LoanTabs/ActionsTab/ActionsTab.jsx
@@ -1,38 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import AutoForm from 'uniforms-material/AutoForm';
-import pick from 'lodash/pick';
 
 import ConfirmMethod from 'core/components/ConfirmMethod';
 import UserAssigner from 'core/components/UserAssigner';
-import { loanDelete, loanUpdate, assignLoanToUser } from 'core/api';
-import LoanSchema from 'core/api/loans/schemas/LoanSchema';
-import { adminLoans } from 'core/api/loans/queries';
+import { loanDelete, assignLoanToUser } from 'core/api';
+import LoanRenamer from './LoanRenamer';
 
 const ActionsTab = ({ loan }) => (
   <div className="actions-tab">
-    <AutoForm
-      className="name-form"
-      schema={LoanSchema.pick('name')}
-      model={loan}
-      onSubmit={doc =>
-        adminLoans
-          .clone({ name: doc.name, $body: { _id: 1 } })
-          .fetchOneSync()
-          .then((result) => {
-            if (result) {
-              import('../../../../../core/utils/message').then(({ default: message }) => {
-                message.error('Ce numéro de dossier existe déjà');
-              });
-            } else {
-              loanUpdate.run({
-                loanId: loan._id,
-                object: pick(doc, ['name']),
-              });
-            }
-          })
-      }
-    />
+    <LoanRenamer loan={loan} />
+
     <ConfirmMethod
       label="Supprimer la demande"
       keyword="SUPPRIMER"
