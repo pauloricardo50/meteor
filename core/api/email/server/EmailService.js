641351aad75112d1ede0f42d95e594d568fa5646
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-07-04 12:20:28 +0200

    Skip emails in dev environment

--- core/api/email/server/EmailService.js
+++ core/api/email/server/EmailService.js
@@ -12,7 +12,7 @@ import {
 import { FROM_NAME, FROM_EMAIL } from '../emailConstants';
 
 export const isEmailTestEnv = Meteor.isTest || Meteor.isAppTest;
-export const skipEmails = (Meteor.isDevelopment || Meteor.isStaging) && !isEmailTestEnv;
+export const skipEmails = (Meteor.isDevelopment || Meteor.isStaging || Meteor.isDevEnvironment) && !isEmailTestEnv;
 // export const skipEmails = false;
 
 class EmailService {
