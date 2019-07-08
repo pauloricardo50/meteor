94de3d83339a8fe8e08c23990da1c13a392aee8f
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-07-08 14:07:24 +0200

    Make fileviewer size more reliable

--- microservices/admin/imports/client/components/FileViewer/FileViewer.jsx
+++ microservices/admin/imports/client/components/FileViewer/FileViewer.jsx
@@ -1,5 +1,6 @@
 // @flow
 import React, { Component } from 'react';
+import debounce from 'lodash/debounce';
 
 import Loadable from 'core/utils/loadable';
 import IconButton from 'core/components/IconButton';
@@ -11,15 +12,25 @@ const ReactFileViewer = Loadable({ loader: () => import('react-file-viewer') });
 
 type FileViewerProps = {};
 
+const minWidth = 300;
+
 class FileViewer extends Component {
   constructor() {
     super();
     this.state = { isResizing: false, width: 1000 };
+    this.handleResize();
   }
 
   componentDidMount() {
     document.addEventListener('mousemove', this.handleMouseMove);
     document.addEventListener('mouseup', this.handleMouseUp);
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('mousemove', this.handleMouseMove);
+    window.removeEventListener('mouseup', this.handleMouseUp);
+    window.removeEventListener('resize', this.handleResize);
   }
 
   handleMouseDown = () => this.setState({ isResizing: true });
@@ -29,16 +40,34 @@ class FileViewer extends Component {
     if (!this.state.isResizing) {
       return;
     }
+    const { maxWidth } = this.state;
+    const width = document.body.offsetWidth - e.clientX;
 
-    const offsetRight = document.body.offsetWidth - (e.clientX - document.body.offsetLeft);
-    const minWidth = 300;
-    const maxWidth = 1500;
-    if (offsetRight > minWidth && offsetRight < maxWidth) {
-      this.setState({ width: offsetRight });
+    if (width < minWidth) {
+      return this.setState({ width: minWidth });
     }
+
+    if (width > maxWidth) {
+      return this.setState({ width: maxWidth });
+    }
+
+    return this.setState({ width });
   };
 
   handleMouseUp = e => this.setState({ isResizing: false });
+
+  handleResize = debounce(() => {
+    // Only set state on window width change, otherwise it's too costly
+    if (window.innerWidth !== this.state.windowWidth) {
+      const { width } = this.state;
+      const maxWidth = 0.8 * window.innerWidth;
+      this.setState({ maxWidth });
+
+      if (width > maxWidth) {
+        this.setState({ width: maxWidth });
+      }
+    }
+  }, 500);
 
   render() {
     const { filePath, fileType, hideFileViewer } = this.props;
