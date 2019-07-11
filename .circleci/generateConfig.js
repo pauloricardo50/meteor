0f88cfd3884aeadee09dbdb1b8c2ce8266e07886
Quentin Herzig <quentin@e-potek.ch>
2019-07-11 14:07:35 +0200

    Update circleci cache version

--- .circleci/generateConfig.js
+++ .circleci/generateConfig.js
@@ -1,7 +1,7 @@
 import { writeYAML } from '../.deployment/utils';
 
 const WORKING_DIRECTORY = '~/app';
-const CACHE_VERSION = 14;
+const CACHE_VERSION = 15;
 
 const defaultJobValues = {
   working_directory: WORKING_DIRECTORY,
