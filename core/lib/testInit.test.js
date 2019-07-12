dcf3999c5be40c413664e792107987b936d9679f
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-07-12 19:24:20 +0200

    Init simpleschema's uniforms key in tests

--- core/lib/testInit.test.js
+++ core/lib/testInit.test.js
@@ -1,10 +1,15 @@
 // Somehow the .app-test files importing this can't use import statements???
 // Leave this imported here for autoforms to work
+require('uniforms-bridge-simple-schema-2');
 const SimpleSchema = require('simpl-schema').default;
 
 const uniforms = require('uniforms-material');
 
-SimpleSchema.extendOptions(['condition', 'customAllowedValues', 'customAutoValue']);
+SimpleSchema.extendOptions([
+  'condition',
+  'customAllowedValues',
+  'customAutoValue',
+]);
 
 process.on('uncaughtException', (error) => {
   console.log('uncaughtException error', error);
