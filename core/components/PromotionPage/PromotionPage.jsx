c5a23c2a878c0b1218fbf8f87f40a64b51ca4b01
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-07-15 18:20:25 +0200

    Add new promotion page boilerplate

---
+++ core/components/PromotionPage/PromotionPage.jsx
@@ -0,0 +1,10 @@
+// @flow
+import React from 'react';
+
+type PromotionPageProps = {};
+
+const PromotionPage = (props: PromotionPageProps) => (
+  <div>Hello from PromotionPage</div>
+);
+
+export default PromotionPage;
