95b6a73cfd6ddbfa05afd4a4d50bfdb56ab16448
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-07-15 09:57:58 +0200

    Update shortcuts for react-hotkeys v2

---
+++ microservices/admin/imports/client/layouts/AdminLayout/AdminKeyboardShortcuts.jsx
@@ -0,0 +1,27 @@
+// @flow
+import React from 'react';
+import { GlobalHotKeys } from 'react-hotkeys';
+
+type AdminKeyboardShortcutsProps = {};
+
+const keyMap = {
+  SEARCH: 'space',
+};
+
+const makeHandlers = ({ setOpenSearch }) => ({
+  SEARCH: e => {
+    // Prevent the space key to be sent to the search input
+    e.preventDefault();
+    setOpenSearch(true);
+  },
+});
+
+const AdminKeyboardShortcuts = ({
+  setOpenSearch,
+}: AdminKeyboardShortcutsProps) => {
+  return (
+    <GlobalHotKeys keyMap={keyMap} handlers={makeHandlers({ setOpenSearch })} />
+  );
+};
+
+export default AdminKeyboardShortcuts;
