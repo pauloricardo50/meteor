b6152aeb098507349713200941e667e8c88ec2ff
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-07-22 18:15:35 +0200

    Use hooks for context

--- core/components/ContactButton/ContactButtonContainer.js
+++ core/components/ContactButton/ContactButtonContainer.js
@@ -1,23 +1,19 @@
 import { withProps, compose } from 'recompose';
 
 import employees from 'core/arrays/epotekEmployees';
-import { withContactButtonContext } from './ContactButtonContext';
 
 const getStaffByEmail = email =>
   employees.find(employee => employee.email === email);
 
-const ContactButtonContainer = compose(
-  withProps(({ currentUser }) => {
-    let staff;
-    if (currentUser && currentUser.assignedEmployee) {
-      staff = getStaffByEmail(currentUser.assignedEmployee.email);
-    }
-    if (!staff) {
-      staff = getStaffByEmail('yannis@e-potek.ch');
-    }
-    return { staff };
-  }),
-  withContactButtonContext,
-);
+const ContactButtonContainer = compose(withProps(({ currentUser }) => {
+  let staff;
+  if (currentUser && currentUser.assignedEmployee) {
+    staff = getStaffByEmail(currentUser.assignedEmployee.email);
+  }
+  if (!staff) {
+    staff = getStaffByEmail('yannis@e-potek.ch');
+  }
+  return { staff };
+}));
 
 export default ContactButtonContainer;
