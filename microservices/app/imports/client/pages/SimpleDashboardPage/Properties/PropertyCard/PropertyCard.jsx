cbe0d114f9ef4a48b79a279b12332306b91d4861
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-07-24 09:48:13 +0200

    Remove button in mobile

--- microservices/app/imports/client/pages/SimpleDashboardPage/Properties/PropertyCard/PropertyCard.jsx
+++ microservices/app/imports/client/pages/SimpleDashboardPage/Properties/PropertyCard/PropertyCard.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 
 import Link from 'core/components/Link';
-import Button from 'core/components/Button';
 import useMedia from 'core/hooks/useMedia';
 import PropertyCardPromotionOptions from './PropertyCardPromotionOptions';
 import PropertyCardInfos from './PropertyCardInfos';
@@ -18,7 +17,7 @@ type PropertyCardProps = {
 };
 
 const PropertyCard = (props: PropertyCardProps) => {
-  const { buttonLabel, onClick, loan, route, image } = props;
+  const { loan, route } = props;
 
   const isMobile = useMedia({ maxWidth: 1200 });
 
@@ -26,13 +25,6 @@ const PropertyCard = (props: PropertyCardProps) => {
     <Link className="card1 card-hover property-card" to={route}>
       <div className="top">
         <PropertyCardInfos isMobile={isMobile} {...props} />
-        {isMobile && (
-          <div className="property-card-actions">
-            <Button className="button" onClick={onClick} secondary raised>
-              {buttonLabel}
-            </Button>
-          </div>
-        )}
       </div>
       {loan.promotionOptions && loan.promotionOptions.length > 0 && (
         <div className="bottom">
