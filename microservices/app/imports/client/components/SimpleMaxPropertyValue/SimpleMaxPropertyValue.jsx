1d71d895acfac33fd7038cb544d33dbb71d206ce
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-08-05 16:45:15 +0200

    Unify loading of maxPropertyValue

--- microservices/app/imports/client/components/SimpleMaxPropertyValue/SimpleMaxPropertyValue.jsx
+++ microservices/app/imports/client/components/SimpleMaxPropertyValue/SimpleMaxPropertyValue.jsx
@@ -2,9 +2,8 @@
 import React from 'react';
 import cx from 'classnames';
 
-import T from 'core/components/Translation';
-import Loading from 'core/components/Loading';
 import MaxPropertyValueResults from 'core/components/MaxPropertyValue/MaxPropertyValueResults';
+import MaxPropertyValueLoading from 'core/components/MaxPropertyValue/MaxPropertyValueLoading';
 import MaxPropertyValueContainer, {
   STATE,
 } from 'core/components/MaxPropertyValue/MaxPropertyValueContainer';
@@ -13,21 +12,13 @@ import SimpleMaxPropertyValueEmptyState from './SimpleMaxPropertyValueEmptyState
 
 type SimpleMaxPropertyValueProps = {};
 
-export const SimpleMaxPropertyValue = (props: MaxPropertyValueProps) => {
+export const SimpleMaxPropertyValue = (props: SimpleMaxPropertyValueProps) => {
   const { blue, state, loading, loan, fixed = false } = props;
 
   if (loading) {
     return (
       <div className={cx('simple-max-property-value loading', { fixed })}>
-        <div className="animated fadeIn">
-          <Loading />
-          <h4>
-            <T id="MaxPropertyValue.loading1" />
-          </h4>
-          <p>
-            <T id="MaxPropertyValue.loading1" />
-          </p>
-        </div>
+        <MaxPropertyValueLoading />
       </div>
     );
   }
