fa0a102afd9a539fe12fabe4056dc7050f31a5f8
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-08-06 18:20:23 +0200

    Allow deleting proProperties as a dev

--- core/components/ProPropertyPage/ProPropertyPageHeader.jsx
+++ core/components/ProPropertyPage/ProPropertyPageHeader.jsx
@@ -1,11 +1,15 @@
 // @flow
+import { Meteor } from 'meteor/meteor';
+
 import React from 'react';
 
 import { Money } from 'core/components/Translation';
+import { propertyDelete } from 'core/api/methods/index';
 import { ProPropertyModifier } from './ProPropertyForm';
 import PropertyDocumentsManager from './PropertyDocumentsManager';
 import StatusLabel from '../StatusLabel';
-import { PROPERTIES_COLLECTION } from '../../api/constants';
+import { PROPERTIES_COLLECTION, ROLES } from '../../api/constants';
+import ConfirmMethod from '../ConfirmMethod';
 
 type ProPropertyPageHeaderProps = {
   property: Object,
@@ -17,6 +21,7 @@ const ProPropertyPageHeader = ({
 }: ProPropertyPageHeaderProps) => {
   const { address1, totalValue, _id: propertyId, status } = property;
   const { canModifyProperty } = permissions;
+  const isDev = Meteor.user().roles.includes(ROLES.DEV);
 
   return (
     <div className="pro-property-page-header">
@@ -38,6 +43,17 @@ const ProPropertyPageHeader = ({
       <div className="pro-property-page-header-buttons">
         {canModifyProperty && (
           <>
+            {isDev && (
+              <ConfirmMethod
+                buttonProps={{
+                  label: 'Supprimer',
+                  color: 'error',
+                  outlined: true,
+                }}
+                method={() => propertyDelete.run({ propertyId })}
+                keyword="SUPPRIMER"
+              />
+            )}
             <ProPropertyModifier property={property} />
             <PropertyDocumentsManager property={property} />
           </>
