8a7fa4665aaf99a7a43926500232fd189da22c9b
Quentin Herzig <quentin@e-potek.ch>
2019-08-06 09:02:12 +0200

    Use curl instead of nc in wait port port

--- scripts/npm-scripts/scripts/wait-port.sh
+++ scripts/npm-scripts/scripts/wait-port.sh
@@ -2,9 +2,11 @@
 port="$1"
 script="${@:2}"
 
-while ! nc -z localhost $port
+until $(curl --output /dev/null --silent --head --fail http://localhost:$port);
     do
         sleep 1
     done
 
+echo "Port $port is open"
+echo "Running $script"
 eval ${script}
