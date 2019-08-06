a766c50566a6168f5be84497c6bcafa76a2a748d
Quentin Herzig <quentin@e-potek.ch>
2019-08-06 13:13:00 +0200

    Fix scriptpath

--- scripts/circleci/build_backend.sh
+++ scripts/circleci/build_backend.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-cd $SCRIPTPATH/../../microservices/backend
 
 if [ ! -d ./.meteor/local ]; then
 
-    meteor npm run start &
+    cd $SCRIPTPATH/../../microservices/backend && meteor npm run start &
 
     until $(curl --output /dev/null --silent --head --fail http://localhost:5500);
         do 
