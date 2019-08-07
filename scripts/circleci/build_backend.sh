766adc355acf5025cb4a14e088b992cff223794d
Quentin Herzig <quentin@e-potek.ch>
2019-08-07 12:34:35 +0200

    Fix build backend relative path again

--- scripts/circleci/build_backend.sh
+++ scripts/circleci/build_backend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-cd $SCRIPTPATH/../../microservices/backend && meteor npm run start & ./$SCRIPTPATH/wait-port.sh killall node
+cd $SCRIPTPATH/../../microservices/backend && meteor npm run start & $SCRIPTPATH/wait-port.sh killall node
 
 exit 0
