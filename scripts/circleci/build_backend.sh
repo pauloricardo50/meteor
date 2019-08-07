0787e1daf805cc235ccfee1da182aee0b6387d06
Quentin Herzig <quentin@e-potek.ch>
2019-08-07 12:35:36 +0200

    Add port in wait port script

--- scripts/circleci/build_backend.sh
+++ scripts/circleci/build_backend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-cd $SCRIPTPATH/../../microservices/backend && meteor npm run start & $SCRIPTPATH/wait-port.sh killall node
+cd $SCRIPTPATH/../../microservices/backend && meteor npm run start & $SCRIPTPATH/wait-port.sh 5500 killall node
 
 exit 0
