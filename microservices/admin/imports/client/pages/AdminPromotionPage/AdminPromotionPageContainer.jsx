b36f06c33d7930b26c8a20058e30de77f88b585d
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-08-14 08:38:32 +0200

    Reduce fragment usage

--- microservices/admin/imports/client/pages/AdminPromotionPage/AdminPromotionPageContainer.jsx
+++ microservices/admin/imports/client/pages/AdminPromotionPage/AdminPromotionPageContainer.jsx
@@ -2,19 +2,29 @@ import { Meteor } from 'meteor/meteor';
 
 import React from 'react';
 import { compose } from 'recompose';
+import omit from 'lodash/omit';
 
 import { proPromotions } from 'core/api/promotions/queries';
 import { withSmartQuery } from 'core/api';
+import { proPromotion } from 'core/api/fragments';
 import withMatchParam from 'core/containers/withMatchParam';
 import { injectPromotionPermissions } from 'core/components/PromotionPage/client/PromotionPermissions';
 import { ROLES } from 'core/api/constants';
+
+const promotionFragment = {
+  ...omit(proPromotion(), ['promotionLots']),
+  promotionLots: { _id: 1, status: 1 },
+};
 
 export default compose(
   withMatchParam('promotionId'),
   Component => props => <Component {...props} key={props.promotionId} />,
   withSmartQuery({
     query: proPromotions,
-    params: ({ promotionId }) => ({ _id: promotionId }),
+    params: ({ promotionId }) => ({
+      _id: promotionId,
+      $body: promotionFragment,
+    }),
     queryOptions: { reactive: false, single: true },
     dataName: 'promotion',
   }),
