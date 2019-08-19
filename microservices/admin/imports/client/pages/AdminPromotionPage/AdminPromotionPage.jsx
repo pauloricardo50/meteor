c840db79fd18ac711607e37a55be35dc5b2a5bc1
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-08-19 14:53:35 +0200

    Simplify PromotionPage containers

--- microservices/admin/imports/client/pages/AdminPromotionPage/AdminPromotionPage.jsx
+++ microservices/admin/imports/client/pages/AdminPromotionPage/AdminPromotionPage.jsx
@@ -1,13 +1,52 @@
 // @flow
+import { Meteor } from 'meteor/meteor';
+
 import React from 'react';
+import { compose, withProps } from 'recompose';
+import omit from 'lodash/omit';
 
+import { proPromotions } from 'core/api/promotions/queries';
+import { withSmartQuery } from 'core/api';
+import { proPromotion } from 'core/api/fragments';
+import withMatchParam from 'core/containers/withMatchParam';
+import { injectPromotionMetadata } from 'core/components/PromotionPage/client/PromotionMetadata';
+import { ROLES } from 'core/api/constants';
+import ADMIN_ROUTES from 'imports/startup/client/adminRoutes';
 import PromotionPage from 'core/components/PromotionPage/client';
-import AdminPromotionPageContainer from './AdminPromotionPageContainer';
 
-type AdminPromotionPageProps = {};
+const promotionFragment = {
+  ...omit(proPromotion(), ['promotionLots']),
+  promotionLots: { _id: 1, status: 1, name: 1 },
+};
 
-const AdminPromotionPage = (props: AdminPromotionPageProps) => (
-  <PromotionPage {...props} />
+const AdminPromotionPageContainer = compose(
+  withMatchParam('promotionId'),
+  Component => props => <Component {...props} key={props.promotionId} />,
+  withSmartQuery({
+    query: proPromotions,
+    params: ({ promotionId }) => ({
+      _id: promotionId,
+      $body: promotionFragment,
+    }),
+    queryOptions: { reactive: false, single: true },
+    dataName: 'promotion',
+  }),
+  injectPromotionMetadata({
+    permissions: {
+      canModifyPromotion: true,
+      canInviteCustomers: true,
+      canManageDocuments: true,
+      canSeeCustomers: true,
+      canSeeUsers: true,
+      canAddLots: true,
+      canModifyLots: true,
+      canRemoveLots: true,
+      canAddPros: true,
+      canRemovePromotion: Meteor.user().roles.includes(ROLES.DEV),
+      canChangeTimeline: true,
+    },
+  }),
+  withProps({ route: ADMIN_ROUTES.ADMIN_PROMOTION_PAGE.path }),
 );
 
-export default AdminPromotionPageContainer(AdminPromotionPage);
+export default AdminPromotionPageContainer(PromotionPage);
--- microservices/app/imports/client/pages/AppPromotionPage/AppPromotionPage.jsx
+++ microservices/app/imports/client/pages/AppPromotionPage/AppPromotionPage.jsx
@@ -1,13 +1,38 @@
 // @flow
-import React from 'react';
+import { compose, withProps } from 'recompose';
 
+import { appPromotion } from 'core/api/promotions/queries';
+import { withSmartQuery } from 'core/api';
+import withMatchParam from 'core/containers/withMatchParam';
+import { createRoute } from 'core/utils/routerUtils';
 import PromotionPage from 'core/components/PromotionPage/client';
-import AppPromotionPageContainer from './AppPromotionPageContainer';
+import appRoutes from '../../../startup/client/appRoutes';
+import withSimpleAppPage from '../../components/SimpleAppPage/SimpleAppPage';
 
-type AppPromotionPageProps = {};
+const getInvitedByUser = ({ promotion, promotionId, loan }) => {
+  const { promotions = [] } = loan;
+  const { $metadata = {} } = promotions.find(({ _id }) => _id === promotionId) || {};
+  const { invitedBy } = $metadata;
+  const { users = [] } = promotion;
+  return users.find(({ _id }) => _id === invitedBy);
+};
 
-const AppPromotionPage = (props: AppPromotionPageProps) => (
-  <PromotionPage {...props} />
+const AppPromotionPageContainer = compose(
+  withMatchParam('promotionId'),
+  withSmartQuery({
+    query: appPromotion,
+    params: ({ promotionId, loan: { _id: loanId } }) => ({
+      promotionId,
+      loanId,
+    }),
+    queryOptions: { reactive: false, single: true },
+    dataName: 'promotion',
+  }),
+  withProps(({ promotion, promotionId, loan }) => ({
+    invitedByUser: getInvitedByUser({ promotion, promotionId, loan }),
+    route: createRoute(appRoutes.APP_PROMOTION_PAGE.path, { loanId: loan._id }),
+  })),
+  withSimpleAppPage,
 );
 
-export default AppPromotionPageContainer(AppPromotionPage);
+export default AppPromotionPageContainer(PromotionPage);
