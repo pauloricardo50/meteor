e552286df8be8187702f8cfa74df96a4dfe810f2
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-08-21 11:56:07 +0200

    Toggle contactButton when clicking outside

--- core/components/ContactButton/ContactButton.jsx
+++ core/components/ContactButton/ContactButton.jsx
@@ -1,9 +1,8 @@
 // @flow
 import React, { useContext } from 'react';
-
 import Fab from '@material-ui/core/Fab';
-import Icon from 'core/components/Icon';
 
+import Icon from 'core/components/Icon';
 import ContactButtonContainer from './ContactButtonContainer';
 import ContactButtonOverlay from './ContactButtonOverlay';
 import { ContactButtonContext } from './ContactButtonContext';
@@ -13,12 +12,26 @@ type ContactButtonProps = {};
 export const ContactButton = ({ ...props }: ContactButtonProps) => {
   const { openContact, toggleOpenContact } = useContext(ContactButtonContext);
 
+  const handleCloseContact = () => toggleOpenContact(false);
+
   return (
     <div className="contact-button">
-      <Fab onClick={() => toggleOpenContact(!openContact)} color="primary">
+      <Fab
+        onClick={(event) => {
+          // Allow onClickAwayListener to work properly
+          event.preventDefault();
+          toggleOpenContact(!openContact);
+        }}
+        color="primary"
+      >
         {openContact ? <Icon type="close" /> : <Icon type="forum" />}
       </Fab>
-      <ContactButtonOverlay {...props} openContact={openContact} />
+
+      <ContactButtonOverlay
+        {...props}
+        openContact={openContact}
+        handleCloseContact={handleCloseContact}
+      />
     </div>
   );
 };
