742f82f1ceff9c16836aa548dea6972d5e851890
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-08-23 23:36:01 +0200

    Make Loading fullScreen when requested

--- core/components/Loading/Loading.jsx
+++ core/components/Loading/Loading.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import cx from 'classnames';
 
 // http://codepen.io/TaniaLD/pen/oKxep
-const Loading = ({ small }) => (
+const Loading = ({ small, fullScreen }) => (
   // Fade it in so that the loader doesn't show up if loading is super fast
-  <div className={cx('loading-container animated fadeIn', { small })}>
+  <div className={cx('loading-container animated fadeIn', { small, fullScreen })}>
     <div className="loading-box">
       <div className="loader2" />
     </div>
