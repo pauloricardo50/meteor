ce0b23fa42dadea27c05fd56184da2dc37ec00ce
Quentin Herzig <quentin@e-potek.ch>
2019-09-12 14:31:49 +0200

    Delete exoscale vpn

--- core/api/analytics/analyticsConstants.js
+++ core/api/analytics/analyticsConstants.js
@@ -3,4 +3,4 @@ export const TRACKING_COOKIE = 'epotek_trackingid';
 // e-Potek's office and VPN IPs
 // Those addresses are blacklisted from 'app' and 'pro' tracked events
 // to avoid wrong tracking when impersonating users
-export const LOGIN_IP_BLACKLIST = ['185.19.31.79', '213.3.47.70'];
+export const LOGIN_IP_BLACKLIST = ['213.3.47.70'];
--- core/api/analytics/server/analyticsHelpers.js
+++ core/api/analytics/server/analyticsHelpers.js
@@ -4,13 +4,16 @@ export const impersonateMiddleware = context => () => next => (...args) => {
   const {
     connection: {
       clientAddress,
-      httpHeaders: { host },
+      httpHeaders: { host, 'x-real-ip': realIp },
     },
   } = context;
 
   // Don't track login events when impersonating
   if (!host.includes('admin')) {
-    if (LOGIN_IP_BLACKLIST.includes(clientAddress)) {
+    if (
+      LOGIN_IP_BLACKLIST.includes(clientAddress)
+      || LOGIN_IP_BLACKLIST.includes(realIp)
+    ) {
       return;
     }
   }
