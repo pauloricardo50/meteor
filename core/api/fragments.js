09f534ac457a4f37c80a712b7ddaa542e84a398a
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-09-18 13:51:46 +0200

    Show full property address in customer adder form

--- core/api/fragments.js
+++ core/api/fragments.js
@@ -857,7 +857,7 @@ export const proPropertySummary = () => ({
   loanCount: 1,
   country: 1,
   userLinks: 1,
-  users: {name: 1}
+  users: { name: 1 },
 });
 
 export const proProperty = ({ withSort } = {}) => ({
@@ -979,9 +979,11 @@ export const proUser = () => ({
   proProperties: {
     _id: 1,
     address1: 1,
+    city: 1,
     permissions: 1,
     status: 1,
     users: { _id: 1 },
+    zipCode: 1,
   },
 });
 
