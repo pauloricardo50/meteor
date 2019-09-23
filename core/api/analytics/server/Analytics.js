e5bbd49edba555c5fbf1bc05c4bca5cebeb6377b
Quentin Herzig <quentin@e-potek.ch>
2019-09-23 18:05:45 +0200

    Disable Analytics in dev

--- core/api/analytics/server/Analytics.js
+++ core/api/analytics/server/Analytics.js
@@ -41,11 +41,7 @@ class Analytics {
 
   init(context) {
     this.events = EVENTS_CONFIG;
-    if (
-      Meteor.isTest
-      || Meteor.isAppTest
-      // || Meteor.isDevelopment
-    ) {
+    if (Meteor.isTest || Meteor.isAppTest || Meteor.isDevelopment) {
       this.analytics = new TestAnalytics();
     } else {
       this.analytics = new NodeAnalytics(key);
