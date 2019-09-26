8d10cf95dc80fc58344820701d490b415bc4007f
Quentin Herzig <quentin@e-potek.ch>
2019-09-26 15:07:57 +0200

    Make firstTab the default route

--- core/components/PromotionPage/client/PromotionPageContent.jsx
+++ core/components/PromotionPage/client/PromotionPageContent.jsx
@@ -12,49 +12,43 @@ import PromotionCustomers from './PromotionCustomers/loadable';
 import PromotionMap from './PromotionMap/loadable';
 import PromotionManagement from './PromotionManagement/loadable';
 
+const TABS_COMPONENTS = {
+  map: PromotionMap,
+  partners: PromotionPartners,
+  files: PromotionFiles,
+  users: PromotionUsers,
+  customers: PromotionCustomers,
+  management: PromotionManagement,
+  overview: PromotionPageOverview,
+};
+
+const getRoutes = ({ tabs, route, props }) => {
+  const [firstTab, ...otherTabs] = tabs;
+  return [
+    ...otherTabs.map(({ id }) => (
+      <Route
+        key={id}
+        path={createRoute(route, { tabId: id })}
+        component={TABS_COMPONENTS[id]}
+        {...props}
+      />
+    )),
+    <Route
+      key={firstTab.id}
+      path={createRoute(route, { tabId: '' })}
+      component={TABS_COMPONENTS[firstTab.id]}
+      {...props}
+    />,
+  ];
+};
+
 type PromotionPageContentProps = {};
 
 const PromotionPageContent = ({
   route,
+  tabs,
   ...props
 }: PromotionPageContentProps) => (
-  <Switch>
-    <Route
-      path={createRoute(route, { tabId: 'map' })}
-      component={PromotionMap}
-      {...props}
-    />
-    <Route
-      path={createRoute(route, { tabId: 'partners' })}
-      component={PromotionPartners}
-      {...props}
-    />
-    <Route
-      path={createRoute(route, { tabId: 'files' })}
-      component={PromotionFiles}
-      {...props}
-    />
-    <Route
-      path={createRoute(route, { tabId: 'users' })}
-      component={PromotionUsers}
-      {...props}
-    />
-    <Route
-      path={createRoute(route, { tabId: 'customers' })}
-      component={PromotionCustomers}
-      {...props}
-    />
-    <Route
-      path={createRoute(route, { tabId: 'management' })}
-      component={PromotionManagement}
-      {...props}
-    />
-    <Route
-      path={[createRoute(route, { tabId: '' })]}
-      component={PromotionPageOverview}
-      {...props}
-    />
-  </Switch>
+  <Switch>{getRoutes({ tabs, route, props })}</Switch>
 );
-
 export default PromotionPageContent;
--- core/components/PromotionPage/client/PromotionPageTabs.jsx
+++ core/components/PromotionPage/client/PromotionPageTabs.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { makeStyles } from '@material-ui/core/styles';
@@ -8,48 +8,10 @@ import Link from 'core/components/Link/Link';
 import { createRoute } from 'core/utils/routerUtils';
 import withMatchParam from 'core/containers/withMatchParam';
 import T from '../../Translation';
-import PromotionMetadataContext from './PromotionMetadata';
 
 type PromotionPageTabsProps = {};
 
 const useStyles = makeStyles({ root: { backgroundColor: 'white' } });
-
-const getTabs = ({
-  permissions: { canSeeCustomers, canSeeUsers, canSeeManagement },
-  promotion: { users = [], loans = [], documents },
-}) =>
-  [
-    { id: 'management', shouldDisplay: canSeeManagement },
-    { id: 'overview', shouldDisplay: true },
-    { id: 'map', shouldDisplay: true },
-    { id: 'partners', shouldDisplay: true },
-    {
-      id: 'files',
-      shouldDisplay:
-        documents
-        && documents.promotionDocuments
-        && documents.promotionDocuments.length > 0,
-    },
-    {
-      id: 'customers',
-      label: (
-        <T id="PromotionPageTabs.customers" values={{ count: loans.length }} />
-      ),
-      shouldDisplay: canSeeCustomers,
-    },
-    {
-      id: 'users',
-      label: (
-        <T id="PromotionPageTabs.users" values={{ count: users.length }} />
-      ),
-      shouldDisplay: canSeeUsers,
-    },
-  ]
-    .filter(({ shouldDisplay }) => shouldDisplay)
-    .map(tab => ({
-      ...tab,
-      label: tab.label || <T id={`PromotionPageTabs.${tab.id}`} />,
-    }));
 
 const getInitialTab = (tabs, tabId) => {
   const index = tabs.findIndex(({ id }) => id === tabId);
@@ -65,11 +27,10 @@ const PromotionPageTabs = ({
   promotion,
   route,
   tabId,
+  tabs = [],
 }: PromotionPageTabsProps) => {
   const { _id: promotionId, users, loans } = promotion;
   const classes = useStyles();
-  const { permissions } = useContext(PromotionMetadataContext);
-  const tabs = getTabs({ promotion, permissions });
   const [value, setValue] = useState(getInitialTab(tabs, tabId));
 
   const handleChange = (event, newValue) => {
