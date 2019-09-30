10769ee67ee01b7d20350e597a705127eed8a6a3
Quentin Herzig <quentin@e-potek.ch>
2019-09-30 15:29:36 +0200

    Clean client unit test mocha output

--- scripts/circleci/test-ci.sh
+++ scripts/circleci/test-ci.sh
@@ -2,6 +2,6 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 BACKEND_PORT="5505"
-TEST_CMD="DDP_DEFAULT_CONNECTION_URL=http://localhost:$BACKEND_PORT METEOR_PACKAGE_DIRS="packages:../../meteorPackages" SERVER_TEST_REPORTER=xunit SERVER_MOCHA_OUTPUT=~/app/results/unit-server.xml CLIENT_MOCHA_OUTPUT=~/app/results/unit-client.xml TEST_BROWSER_DRIVER=nightmare meteor test --once --driver-package meteortesting:mocha --settings settings-dev.json"
+TEST_CMD="DDP_DEFAULT_CONNECTION_URL=http://localhost:$BACKEND_PORT METEOR_PACKAGE_DIRS="packages:../../meteorPackages" SERVER_TEST_REPORTER=xunit CLIENT_TEST_REPORTER=xunit SERVER_MOCHA_OUTPUT=~/app/results/unit-server.xml CLIENT_MOCHA_OUTPUT=~/app/results/unit-client.xml TEST_BROWSER_DRIVER=nightmare meteor test --once --driver-package meteortesting:mocha --settings settings-dev.json"
 
 ${SCRIPTPATH}/run-backend-ci.sh --test & ${SCRIPTPATH}/wait-port.sh $BACKEND_PORT $TEST_CMD
