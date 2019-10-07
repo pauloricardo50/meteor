e4964524c9fd5af812b3048919a71fd0be2133dd
Quentin Herzig <quentin@e-potek.ch>
2019-10-07 17:45:54 +0200

    Update promotion reservation insert

--- core/api/promotionReservations/server/PromotionReservationService.js
+++ core/api/promotionReservations/server/PromotionReservationService.js
@@ -1,6 +1,14 @@
+import { Meteor } from 'meteor/meteor';
+import moment from 'moment';
+
+import { LOAN_VERIFICATION_STATUS } from 'core/api/loans/loanConstants';
 import PromotionReservations from '../promotionReservations';
 import CollectionService from '../../helpers/CollectionService';
 import PromotionOptionService from '../../promotionOptions/server/PromotionOptionService';
+import {
+  PROMOTION_RESERVATION_STATUS,
+  PROMOTION_RESERVATION_MORTGAGE_CERTIFICATION_STATUS,
+} from '../promotionReservationConstants';
 
 class PromotionReservationService extends CollectionService {
   constructor() {
@@ -8,24 +16,63 @@ class PromotionReservationService extends CollectionService {
   }
 
   insert({ promotionReservation, promotionOptionId }) {
-    const promotionReservationId = this.collection.insert(promotionReservation);
-
     const {
       promotionLots = [],
       loan = {},
     } = PromotionOptionService.safeFetchOne({
       $filters: { _id: promotionOptionId },
-      promotionLots: { _id: 1, promotionCache: { _id: 1 } },
-      loan: { _id: 1 },
+      promotionLots: {
+        _id: 1,
+        promotionReservations: { status: 1, expirationDate: 1 },
+        promotionCache: { _id: 1, agreementDuration: 1 },
+      },
+      loan: { _id: 1, maxPropertyValue: { date: 1 }, verificationStatus: 1 },
     });
 
     const [
       {
         _id: promotionLotId,
-        promotionCache: { _id: promotionId },
+        promotionCache: { _id: promotionId, agreementDuration },
+        promotionReservations = [],
       },
     ] = promotionLots;
-    const { _id: loanId } = loan;
+
+    if (promotionReservations.length) {
+      const activePromotionReservation = promotionReservations.find(({ status }) => status === PROMOTION_RESERVATION_STATUS.ACTIVE);
+      if (activePromotionReservation) {
+        const { expirationDate } = activePromotionReservation;
+        throw new Meteor.Error(`Ce lot est déjà réservé jusqu'au ${moment(expirationDate).format('D MMM YYYY')}`);
+      }
+    }
+
+    if (!agreementDuration) {
+      throw new Meteor.Error('Aucun délai de réservation configuré pour cette promotion');
+    }
+
+    const { startDate } = promotionReservation;
+
+    const expirationDate = moment(startDate)
+      .add(agreementDuration, 'days')
+      .toDate();
+
+    const {
+      _id: loanId,
+      maxPropertyValue: { date: maxPropertyValueDate } = {},
+      verificationStatus,
+    } = loan;
+
+    const promotionReservationId = this.collection.insert({
+      ...promotionReservation,
+      expirationDate,
+      mortgageCertification: {
+        status: maxPropertyValueDate
+          ? verificationStatus === LOAN_VERIFICATION_STATUS.OK
+            ? PROMOTION_RESERVATION_MORTGAGE_CERTIFICATION_STATUS.VALIDATED
+            : PROMOTION_RESERVATION_MORTGAGE_CERTIFICATION_STATUS.CALCULATED
+          : PROMOTION_RESERVATION_MORTGAGE_CERTIFICATION_STATUS.NONE,
+        date: maxPropertyValueDate,
+      },
+    });
 
     this.addLink({
       id: promotionReservationId,
