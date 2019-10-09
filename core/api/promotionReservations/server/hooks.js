afc011f098db56c3ed89a7b8941a7c6f7852ef4b
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-10-09 14:07:07 +0200

    Add file delete hook

--- core/api/promotionReservations/server/hooks.js
+++ core/api/promotionReservations/server/hooks.js
@@ -1,4 +1,4 @@
-import PromotionReservations from '../PromotionReservations';
+import PromotionReservations from '../promotionReservations';
 import FileService from '../../files/server/FileService';
 
 PromotionReservations.after.remove((userId, { _id }) =>
