037d1b5afbc44e5d846ae8594022f405dc410210
sapna-nx <sapna@nodexperts.com>
2019-10-14 16:12:36 +0530

    Changes in range slider

--- core/components/Slider/Range.jsx
+++ core/components/Slider/Range.jsx
@@ -1,12 +1,39 @@
 // @flow
 import React, { useState } from 'react';
-import RCSlider from 'rc-slider';
+//import RCSlider from 'rc-slider';
+import { Slider as MuiSlider } from '@material-ui/core';
 import { useDebounce } from 'react-use';
+import Tooltip from '@material-ui/core/Tooltip'
 
-const { createSliderWithTooltip } = RCSlider;
-const RangeSlider = createSliderWithTooltip(RCSlider.Range);
+//const { createSliderWithTooltip } = RCSlider;
+//const RangeSlider = createSliderWithTooltip(RCSlider.Range);
 
 type RangeProps = {};
+
+function ValueLabelComponent(props) {
+  const { children, open, value } = props;
+
+  const popperRef = React.useRef(null);
+  React.useEffect(() => {
+    if (popperRef.current) {
+      popperRef.current.update();
+    }
+  });
+
+  return (
+    <Tooltip
+      PopperProps={{
+        popperRef,
+      }}
+      open={open}
+      enterTouchDelay={0}
+      placement="top"
+      title={value}
+    >
+      {children}
+    </Tooltip>
+  );
+}
 
 const Range = ({
   min = 0,
@@ -25,15 +52,20 @@ const Range = ({
   // is passed through other components that are expensive to update
   useDebounce(() => debounce && onChange(fastValue), 300, [fastValue]);
 
+  const handleChange = (event, newValue) => {
+    setFastValue(newValue);
+  };
+  
   return (
-    <RangeSlider
+    <MuiSlider
       min={min}
       max={max}
       defaultValue={defaultValue}
       value={debounce ? fastValue : value}
-      onChange={debounce ? setFastValue : onChange}
+      onChange={debounce ? handleChange : (event, value) => onChange(value)}
       allowCross={false}
       pushable
+      ValueLabelComponent={ValueLabelComponent}
       tipFormatter={tipFormatter}
       {...rest}
     />
