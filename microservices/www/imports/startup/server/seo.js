ef41c3bd471d7eba90053c1bb89ff1a653989a29
sapna <sapna@nodexperts.com>
2019-10-16 12:11:46 +0530

    updated og:url

--- microservices/www/imports/startup/server/seo.js
+++ microservices/www/imports/startup/server/seo.js
@@ -20,7 +20,7 @@ const setBlogHeaders = (sink, url) => {
 
       const { title, excerpt, post_thumbnail } = post;
       sink.appendToHead(renderToString(BlogPostSeo({ ...post })));
-      sink.appendToHead('<meta property="og:url" content="http://localhost:3000" />');
+      sink.appendToHead(`<meta property="og:url" content="https://www.e-potek.ch${url}" />`);
       sink.appendToHead(`<meta property="og:title" content="${title}" />`);
       sink.appendToHead('<meta property="og:type" content="website" />');
       sink.appendToHead('<meta property="fb:app_id" content="1868218996582233" />');
@@ -28,7 +28,7 @@ const setBlogHeaders = (sink, url) => {
       sink.appendToHead(`<meta property="og:image" content="${post_thumbnail.URL}" />`);
       sink.appendToHead(`<meta property="og:image:height" content="${
         post_thumbnail.height
-      }" />`);
+        }" />`);
       sink.appendToHead(`<meta property="og:image:width" content="${post_thumbnail.width}" />`);
     });
   }
