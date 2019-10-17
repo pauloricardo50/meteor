89c19bae1f0d5e1609c7721631160fe3db30f011
Quentin Herzig <quentin@e-potek.ch>
2019-10-17 18:29:15 +0200

    Fix migration 25

--- core/api/migrations/server/25.js
+++ core/api/migrations/server/25.js
@@ -1,105 +1,174 @@
-// import { Migrations } from 'meteor/percolate:migrations';
+import { Migrations } from 'meteor/percolate:migrations';
 
-// import { PROMOTION_LOT_STATUS } from '../../promotionLots/promotionLotConstants';
-// import PromotionReservationService from '../../promotionReservations/server/PromotionReservationService';
-// import PromotionService from '../../promotions/server/PromotionService';
-// import {
-//   AGREEMENT_STATUSES,
-//   PROMOTION_RESERVATION_STATUS,
-//   PROMOTION_RESERVATION_BANK_STATUS,
-//   DEPOSIT_STATUSES,
-// } from '../../promotionReservations/promotionReservationConstants';
-// import PromotionLotService from '../../promotionLots/server/PromotionLotService';
-// import PromotionReservations from '../../promotionReservations';
+import PromotionOptions from 'core/api/promotionOptions';
+import { PROMOTION_LOT_STATUS } from '../../promotionLots/promotionLotConstants';
+import PromotionOptionService from '../../promotionOptions/server/PromotionOptionService';
+import PromotionService from '../../promotions/server/PromotionService';
+import {
+  AGREEMENT_STATUSES,
+  PROMOTION_OPTION_STATUS,
+  PROMOTION_OPTION_BANK_STATUS,
+  DEPOSIT_STATUSES,
+} from '../../promotionOptions/promotionOptionConstants';
+import PromotionLotService from '../../promotionLots/server/PromotionLotService';
 
-// const handlePromotions = async () => {
-//   await PromotionService.collection
-//     .rawCollection()
-//     .update({}, { $set: { agreementDuration: 30 } }, { multi: true });
-// };
+const handlePromotions = async () => {
+  await PromotionService.collection
+    .rawCollection()
+    .update({}, { $set: { agreementDuration: 30 } }, { multi: true });
+};
 
-// const handleBookedLots = async () => {
-//   const bookedPromotionLots = PromotionLotService.fetch({
-//     $filters: { status: PROMOTION_LOT_STATUS.BOOKED },
-//     attributedTo: {
-//       _id: 1,
-//       promotionOptions: { promotionLotLinks: 1, createdAt: 1 },
-//     },
-//   });
+const handleBookedLots = async () => {
+  const bookedPromotionLots = PromotionLotService.fetch({
+    $filters: { status: PROMOTION_LOT_STATUS.BOOKED },
+    attributedTo: {
+      _id: 1,
+      promotionOptions: {
+        promotionLotLinks: 1,
+        createdAt: 1,
+        loan: { promotionLinks: { _id: 1 } },
+      },
+    },
+  });
 
-//   return Promise.all(bookedPromotionLots.map(async ({ _id, attributedTo: { promotionOptions = [] } }) => {
-//     const promotionOption = promotionOptions.find(({ promotionLotLinks }) =>
-//       promotionLotLinks[0] && promotionLotLinks[0]._id === _id);
-//     if (promotionOption) {
-//       const id = await PromotionReservationService.insert({
-//         promotionOptionId: promotionOption._id,
-//         promotionReservation: { startDate: new Date() },
-//         withAgreement: false,
-//       });
+  return Promise.all(bookedPromotionLots.map(async ({ _id, attributedTo: { promotionOptions = [] } }) => {
+    const promotionOption = promotionOptions.find(({ promotionLotLinks }) =>
+      promotionLotLinks[0] && promotionLotLinks[0]._id === _id);
+    if (promotionOption) {
+      const id = await PromotionOptionService.activateReservation({
+        promotionOptionId: promotionOption._id,
+        startDate: new Date(),
+        withAgreement: false,
+      });
 
-//       PromotionReservationService.baseUpdate(id, {
-//         $set: {
-//           status: PROMOTION_RESERVATION_STATUS.ACTIVE,
-//           reservationAgreement: {
-//             status: AGREEMENT_STATUSES.WAITING,
-//             date: new Date(),
-//           },
-//         },
-//       });
-//     }
-//   }));
-// };
+      const {
+        loan: {
+          _id: loanId,
+          promotionLinks: { _id: promotions = [] } = {},
+        },
+      } = promotionOption;
 
-// const handleSoldLots = async () => {
-//   const soldPromotionLots = PromotionLotService.fetch({
-//     $filters: { status: PROMOTION_LOT_STATUS.SOLD },
-//     attributedTo: { promotionOptions: { promotionLotLinks: 1, createdAt: 1 } },
-//   });
+      const [promotionId] = promotions;
 
-//   return Promise.all(soldPromotionLots.map(async ({ _id, attributedTo: { promotionOptions = [] } }) => {
-//     const promotionOption = promotionOptions.find(({ promotionLotLinks }) =>
-//       promotionLotLinks[0] && promotionLotLinks[0]._id === _id);
-//     if (promotionOption) {
-//       const id = await PromotionReservationService.insert({
-//         promotionOptionId: promotionOption._id,
-//         promotionReservation: { startDate: new Date() },
-//         withAgreement: false,
-//       });
+      PromotionOptionService.setInitialMortgageCertification({
+        promotionOptionId: promotionOption._id,
+        loanId,
+      });
 
-//       PromotionReservationService.baseUpdate(id, {
-//         $set: {
-//           status: PROMOTION_RESERVATION_STATUS.COMPLETED,
-//           reservationAgreement: {
-//             status: AGREEMENT_STATUSES.WAITING,
-//             date: new Date(),
-//           },
-//           lender: {
-//             status: PROMOTION_RESERVATION_BANK_STATUS.VALIDATED,
-//             date: new Date(),
-//           },
-//           deposit: {
-//             date: new Date(),
-//             status: DEPOSIT_STATUSES.PAID,
-//           },
-//         },
-//       });
-//     }
-//   }));
-// };
+      PromotionOptionService.baseUpdate(id, {
+        $set: {
+          status: PROMOTION_OPTION_STATUS.RESERVATION_ACTIVE,
+          reservationAgreement: {
+            status: AGREEMENT_STATUSES.WAITING,
+            date: new Date(),
+          },
+          promotionLink: { _id: promotionId },
+        },
+        $unset: {
+          proNote: true,
+          solvency: true,
+        },
+      });
+    }
+  }));
+};
 
-// export const up = async () => {
-//   await handlePromotions();
-//   await handleBookedLots();
-//   await handleSoldLots();
-// };
+const handleSoldLots = async () => {
+  const soldPromotionLots = PromotionLotService.fetch({
+    $filters: { status: PROMOTION_LOT_STATUS.SOLD },
+    loan: { promotionLinks: { _id: 1 } },
+    attributedTo: {
+      promotionOptions: {
+        promotionLotLinks: 1,
+        createdAt: 1,
+        loan: { promotionLinks: { _id: 1 } },
+      },
+    },
+  });
 
-// export const down = async () => {
-//   await PromotionReservations.rawCollection().remove({});
-// };
+  return Promise.all(soldPromotionLots.map(async ({ _id, attributedTo: { promotionOptions = [] } }) => {
+    const promotionOption = promotionOptions.find(({ promotionLotLinks }) =>
+      promotionLotLinks[0] && promotionLotLinks[0]._id === _id);
+    if (promotionOption) {
+      const id = await PromotionOptionService.activateReservation({
+        promotionOptionId: promotionOption._id,
+        startDate: new Date(),
+        withAgreement: false,
+      });
 
-// Migrations.add({
-//   version: 25,
-//   name: 'Add reservations on all promotions',
-//   up,
-//   down,
-// });
+      const {
+        loan: {
+          _id: loanId,
+          promotionLinks: { _id: promotions = [] } = {},
+        },
+      } = promotionOption;
+
+      const [promotionId] = promotions;
+
+      PromotionOptionService.baseUpdate(promotionOption._id, {
+        $set: {
+          status: PROMOTION_OPTION_STATUS.SOLD,
+          reservationAgreement: {
+            status: AGREEMENT_STATUSES.WAITING,
+            date: new Date(),
+          },
+          bank: {
+            status: PROMOTION_OPTION_BANK_STATUS.VALIDATED,
+            date: new Date(),
+          },
+          deposit: {
+            date: new Date(),
+            status: DEPOSIT_STATUSES.PAID,
+          },
+          promotionLink: { _id: promotionId },
+        },
+        $unset: {
+          proNote: true,
+          solvency: true,
+        },
+      });
+
+      PromotionOptionService.setInitialMortgageCertification({
+        promotionOptionId: promotionOption._id,
+        loanId,
+      });
+    }
+  }));
+};
+
+export const up = async () => {
+  await handlePromotions();
+  await handleBookedLots();
+  await handleSoldLots();
+};
+
+export const down = async () => {
+  const promotionOptions = PromotionOptionService.fetch({
+    mortgageCertification: { status: 1 },
+  });
+
+  return Promise.all(promotionOptions.map((promotionOption) => {
+    const { mortgageCertification: { status } = {} } = promotionOption;
+    return PromotionOptions.rawCollection().update(
+      {},
+      {
+        $unset: {
+          status: true,
+          reservationAgreement: true,
+          bank: true,
+          deposit: true,
+          promotionLink: true,
+        },
+        $set: { solvency: status },
+      },
+      { multi: true },
+    );
+  }));
+};
+
+Migrations.add({
+  version: 25,
+  name: 'Add reservations on all promotions',
+  up,
+  down,
+});
--- core/api/migrations/server/test/25.test.js
+++ core/api/migrations/server/test/25.test.js
@@ -1,154 +1,185 @@
-// // @flow
-// /* eslint-env mocha */
-// import { expect } from 'chai';
-// import { resetDatabase } from 'meteor/xolvio:cleaner';
+// @flow
+/* eslint-env mocha */
+import { expect } from 'chai';
+import { resetDatabase } from 'meteor/xolvio:cleaner';
 
-// import PromotionService from '../../../promotions/server/PromotionService';
-// import PromotionReservationService from '../../../promotionReservations/server/PromotionReservationService';
-// import generator from '../../../factories/index';
-// import {
-//   PROMOTION_LOT_STATUS,
-//   PROMOTION_RESERVATION_STATUS,
-//   AGREEMENT_STATUSES,
-//   PROMOTION_RESERVATION_BANK_STATUS,
-//   DEPOSIT_STATUSES,
-// } from '../../../constants';
-// import { up, down } from '../25';
+import PromotionService from '../../../promotions/server/PromotionService';
+import PromotionOptionService from '../../../promotionOptions/server/PromotionOptionService';
+import generator from '../../../factories/index';
+import {
+  PROMOTION_LOT_STATUS,
+  PROMOTION_OPTION_STATUS,
+  AGREEMENT_STATUSES,
+  PROMOTION_OPTION_BANK_STATUS,
+  DEPOSIT_STATUSES,
+} from '../../../constants';
+import { up, down } from '../25';
 
-// describe('Migration 25', () => {
-//   beforeEach(() => resetDatabase());
+describe('Migration 25', () => {
+  beforeEach(() => resetDatabase());
 
-//   describe('up', () => {
-//     it('sets agreementDuration on all promotions', async () => {
-//       await PromotionService.collection.rawCollection().insert({ _id: 'a' });
-//       await PromotionService.collection.rawCollection().insert({ _id: 'b' });
+  describe('up', () => {
+    it('sets agreementDuration on all promotions', async () => {
+      await PromotionService.collection.rawCollection().insert({ _id: 'a' });
+      await PromotionService.collection.rawCollection().insert({ _id: 'b' });
 
-//       await up();
+      await up();
 
-//       const promotions = PromotionService.find().fetch();
-//       expect(promotions.length).to.equal(2);
-//       promotions.forEach((p) => {
-//         expect(p.agreementDuration).to.equal(30);
-//       });
-//     });
+      const promotions = PromotionService.find().fetch();
+      expect(promotions.length).to.equal(2);
+      promotions.forEach((p) => {
+        expect(p.agreementDuration).to.equal(30);
+      });
+    });
 
-//     it('creates a promotionReservation for each booked or sold promotionLot', async () => {
-//       generator({
-//         properties: [{ _id: 'a' }, { _id: 'b' }, { _id: 'c' }],
-//         promotions: {
-//           promotionLots: [
-//             {
-//               status: PROMOTION_LOT_STATUS.AVAILABLE,
-//               propertyLinks: [{ _id: 'a' }],
-//             },
-//             {
-//               status: PROMOTION_LOT_STATUS.BOOKED,
-//               propertyLinks: [{ _id: 'b' }],
-//               promotionOptions: { loan: { _id: 'loan1' } },
-//               attributedTo: { _id: 'loan1' },
-//             },
-//             {
-//               status: PROMOTION_LOT_STATUS.SOLD,
-//               propertyLinks: [{ _id: 'c' }],
-//               promotionOptions: { loan: { _id: 'loan2' } },
-//               attributedTo: { _id: 'loan2' },
-//             },
-//           ],
-//         },
-//       });
+    it('sets the reservation for each booked or sold promotionLot', async () => {
+      generator({
+        properties: [{ _id: 'a' }, { _id: 'b' }, { _id: 'c' }],
+        promotions: {
+          _id: 'promo',
+          promotionLots: [
+            {
+              status: PROMOTION_LOT_STATUS.AVAILABLE,
+              propertyLinks: [{ _id: 'a' }],
+            },
+            {
+              status: PROMOTION_LOT_STATUS.BOOKED,
+              propertyLinks: [{ _id: 'b' }],
+              promotionOptions: {
+                loan: { _id: 'loan1' },
+                promotion: { _id: 'promo' },
+              },
+              attributedTo: { _id: 'loan1' },
+            },
+            {
+              status: PROMOTION_LOT_STATUS.SOLD,
+              propertyLinks: [{ _id: 'c' }],
+              promotionOptions: {
+                loan: { _id: 'loan2' },
+                promotion: { _id: 'promo' },
+              },
+              attributedTo: { _id: 'loan2' },
+            },
+          ],
+          loans: [{ _id: 'loan1' }, { _id: 'loan2' }],
+        },
+      });
 
-//       await up();
+      await up();
 
-//       const pRs = PromotionReservationService.find(
-//         {},
-//         { sort: { status: 1 } },
-//       ).fetch();
-//       expect(pRs.length).to.equal(2);
-//       expect(pRs[0].status).to.equal(PROMOTION_RESERVATION_STATUS.ACTIVE);
-//       expect(pRs[1].status).to.equal(PROMOTION_RESERVATION_STATUS.COMPLETED);
-//     });
+      const pOs = PromotionOptionService.find(
+        {},
+        { sort: { status: 1 } },
+      ).fetch();
+      expect(pOs.length).to.equal(2);
+      expect(pOs[0].status).to.equal(PROMOTION_OPTION_STATUS.RESERVATION_ACTIVE);
+      expect(pOs[1].status).to.equal(PROMOTION_OPTION_STATUS.SOLD);
+    });
 
-//     it('sets agreement to waiting on booked lots', async () => {
-//       generator({
-//         properties: { _id: 'a' },
-//         promotions: {
-//           promotionLots: {
-//             status: PROMOTION_LOT_STATUS.BOOKED,
-//             propertyLinks: [{ _id: 'b' }],
-//             promotionOptions: { loan: { _id: 'loan1' } },
-//             attributedTo: { _id: 'loan1' },
-//           },
-//         },
-//       });
+    it('sets agreement to waiting on booked lots', async () => {
+      generator({
+        properties: { _id: 'a' },
+        promotions: {
+          _id: 'promo',
+          promotionLots: {
+            status: PROMOTION_LOT_STATUS.BOOKED,
+            propertyLinks: [{ _id: 'b' }],
+            promotionOptions: {
+              loan: { _id: 'loan1' },
+              promotion: { _id: 'promo' },
+            },
+            attributedTo: { _id: 'loan1' },
+          },
+          loans: [{ _id: 'loan1' }],
+        },
+      });
 
-//       await up();
+      await up();
 
-//       const pRs = PromotionReservationService.find({}).fetch();
-//       expect(pRs[0].reservationAgreement).to.deep.include({
-//         status: AGREEMENT_STATUSES.WAITING,
-//       });
-//     });
+      const pOs = PromotionOptionService.find({}).fetch();
+      expect(pOs[0].reservationAgreement).to.deep.include({
+        status: AGREEMENT_STATUSES.WAITING,
+      });
+    });
 
-//     it('sets statuses on sold lots', async () => {
-//       generator({
-//         properties: { _id: 'a' },
-//         promotions: {
-//           promotionLots: {
-//             status: PROMOTION_LOT_STATUS.SOLD,
-//             propertyLinks: [{ _id: 'b' }],
-//             promotionOptions: { loan: { _id: 'loan1' } },
-//             attributedTo: { _id: 'loan1' },
-//           },
-//         },
-//       });
+    it('sets statuses on sold lots', async () => {
+      generator({
+        properties: { _id: 'a' },
+        promotions: {
+          _id: 'promo',
+          promotionLots: {
+            status: PROMOTION_LOT_STATUS.SOLD,
+            propertyLinks: [{ _id: 'b' }],
+            promotionOptions: {
+              loan: { _id: 'loan1' },
+              promotion: { _id: 'promo' },
+            },
+            attributedTo: { _id: 'loan1' },
+          },
+          loans: [{ _id: 'loan1' }],
+        },
+      });
 
-//       await up();
+      await up();
 
-//       const pRs = PromotionReservationService.find({}).fetch();
-//       expect(pRs[0].reservationAgreement).to.deep.include({
-//         status: AGREEMENT_STATUSES.WAITING,
-//       });
-//       expect(pRs[0].lender).to.deep.include({
-//         status: PROMOTION_RESERVATION_BANK_STATUS.VALIDATED,
-//       });
-//       expect(pRs[0].deposit).to.deep.include({
-//         status: DEPOSIT_STATUSES.PAID,
-//       });
-//     });
-//   });
+      const pOs = PromotionOptionService.find({}).fetch();
+      expect(pOs[0].reservationAgreement).to.deep.include({
+        status: AGREEMENT_STATUSES.WAITING,
+      });
+      expect(pOs[0].bank).to.deep.include({
+        status: PROMOTION_OPTION_BANK_STATUS.VALIDATED,
+      });
+      expect(pOs[0].deposit).to.deep.include({
+        status: DEPOSIT_STATUSES.PAID,
+      });
+    });
+  });
 
-//   describe('down', () => {
-//     it('removes all promotionReservations', async () => {
-//       generator({
-//         properties: [{ _id: 'a' }, { _id: 'b' }, { _id: 'c' }],
-//         promotions: {
-//           promotionLots: [
-//             {
-//               status: PROMOTION_LOT_STATUS.AVAILABLE,
-//               propertyLinks: [{ _id: 'a' }],
-//             },
-//             {
-//               status: PROMOTION_LOT_STATUS.BOOKED,
-//               propertyLinks: [{ _id: 'b' }],
-//               promotionOptions: { loan: { _id: 'loan1' } },
-//               attributedTo: { _id: 'loan1' },
-//             },
-//             {
-//               status: PROMOTION_LOT_STATUS.SOLD,
-//               propertyLinks: [{ _id: 'c' }],
-//               promotionOptions: { loan: { _id: 'loan2' } },
-//               attributedTo: { _id: 'loan2' },
-//             },
-//           ],
-//         },
-//       });
+  describe('down', () => {
+    it('resets all promotion options', async () => {
+      generator({
+        properties: [{ _id: 'a' }, { _id: 'b' }, { _id: 'c' }],
+        promotions: {
+          promotionLots: [
+            {
+              status: PROMOTION_LOT_STATUS.AVAILABLE,
+              propertyLinks: [{ _id: 'a' }],
+            },
+            {
+              status: PROMOTION_LOT_STATUS.BOOKED,
+              propertyLinks: [{ _id: 'b' }],
+              promotionOptions: {
+                loan: { _id: 'loan1' },
+                promotion: { _id: 'promo' },
+              },
+              attributedTo: { _id: 'loan1' },
+            },
+            {
+              status: PROMOTION_LOT_STATUS.SOLD,
+              propertyLinks: [{ _id: 'c' }],
+              promotionOptions: {
+                loan: { _id: 'loan2' },
+                promotion: { _id: 'promo' },
+              },
+              attributedTo: { _id: 'loan2' },
+            },
+          ],
+        },
+        loans: [{ _id: 'loan1' }, { _id: 'loan2' }],
+      });
 
-//       await up();
-//       await down();
+      await up();
+      await down();
 
-//       const pRs = PromotionReservationService.find({}).fetch();
+      const pOs = PromotionOptionService.find({}).fetch();
 
-//       expect(pRs.length).to.equal(0);
-//     });
-//   });
-// });
+      pOs.forEach((pO) => {
+        expect(pO.status).to.equal(undefined);
+        expect(pO.reservationAgreement).to.equal(undefined);
+        expect(pO.bank).to.equal(undefined);
+        expect(pO.deposit).to.equal(undefined);
+        expect(pO.promotionLink).to.equal(undefined);
+      });
+    });
+  });
+});
