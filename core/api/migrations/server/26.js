c8654e6c188dec7320ba7c49a25d1d9a0a4bff99
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-10-18 16:23:23 +0200

    Add migration for promotions on promotionOptions

---
+++ core/api/migrations/server/26.js
@@ -0,0 +1,38 @@
+import { Migrations } from 'meteor/percolate:migrations';
+
+import PromotionOptionService from '../../promotionOptions/server/PromotionOptionService';
+
+export const up = () => {
+  const allPromotionOptions = PromotionOptionService.fetch({
+    _id: 1,
+    promotionLots: {
+      promotion: { _id: 1 },
+    },
+  });
+
+  return Promise.all(allPromotionOptions.map(({ _id, promotionLots = [] }) =>
+    PromotionOptionService.rawCollection.update(
+      { _id },
+      { $set: { promotionLink: { _id: promotionLots[0].promotion._id } } },
+    )));
+};
+
+export const down = () => {
+  const allPromotionOptions = PromotionOptionService.fetch({
+    _id: 1,
+    promotion: 1,
+  });
+
+  return Promise.all(allPromotionOptions.map(({ _id }) =>
+    PromotionOptionService.rawCollection.update(
+      { _id },
+      { $unset: { promotionLink: 1 } },
+    )));
+};
+
+Migrations.add({
+  version: 26,
+  name: 'Add promotion link on all promotionOptions',
+  up,
+  down,
+});
