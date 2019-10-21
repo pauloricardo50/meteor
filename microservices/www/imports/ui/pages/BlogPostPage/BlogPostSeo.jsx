6a15231fc7d3b0645054ce65ce0c4b99b376d607
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-10-21 19:07:08 +0200

    Fix SEO on blog

--- microservices/www/imports/ui/pages/BlogPostPage/BlogPostSeo.jsx
+++ microservices/www/imports/ui/pages/BlogPostPage/BlogPostSeo.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
 import { Meteor } from 'meteor/meteor';
 
-export default ({ title, excerpt, post_thumbnail: postThumbnail, url }) => (
+import React from 'react';
+
+import { defaultOgTags } from 'core/components/MicroserviceHead/MicroserviceHead';
+
+export default ({
+  post: { title, excerpt, post_thumbnail: postThumbnail },
+  url,
+}) => (
   <>
-    <meta property="og:url" content={`${Meteor.settings.public.subdomains.www}${url}`} />
     <meta
-      property="og:image"
-      content="https://d2gb1cl8lbi69k.cloudfront.net/facebook-img.png"
+      property="og:url"
+      content={`${Meteor.settings.public.subdomains.www}${url}`}
     />
-    <meta property="og:type" content="website" />
+    <meta property="og:type" content={defaultOgTags.type} />
     <meta property="og:title" content={title} />
     <meta property="og:description" content={excerpt} />
-    <meta property="fb:app_id" content="1868218996582233" />
+    <meta property="fb:app_id" content={defaultOgTags.app_id} />
     <meta property="og:image" content={postThumbnail.URL} />
     <meta property="og:image:height" content={postThumbnail.height} />
     <meta property="og:image:width" content={postThumbnail.width} />
