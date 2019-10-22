83576d4e847878a2f831e128a67b8517ed2ad1c8
Quentin Herzig <quentin@e-potek.ch>
2019-10-22 14:27:17 +0200

    Remove promotionReservations folder

--- core/api/promotionReservations_old/queries.js
+++
@@ -1,8 +0,0 @@
-// import { proPromotionReservation } from '../fragments';
-// import { PROMOTION_RESERVATIONS_QUERIES } from './promotionReservationConstants';
-// import PromotionReservations from '.';
-
-// export const promotionReservations = PromotionReservations.createQuery(
-//   PROMOTION_RESERVATIONS_QUERIES.PROMOTION_RESERVATIONS,
-//   proPromotionReservation(),
-// );
