876f8c8b70c3467eb1d0573d23cee596f4801c09
Quentin Herzig <quentin@e-potek.ch>
2019-10-25 16:47:04 +0200

    Display ratios in loanProgress

--- core/components/LoanProgress/LoanProgressHeader.jsx
+++ core/components/LoanProgress/LoanProgressHeader.jsx
@@ -4,10 +4,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTasks } from '@fortawesome/pro-light-svg-icons/faTasks';
 import { faFile } from '@fortawesome/pro-light-svg-icons/faFile';
-import { faEye } from '@fortawesome/pro-light-svg-icons/faEye';
 import Tooltip from '../Material/Tooltip';
-
-import Icon from '../Icon';
 
 import T from '../Translation';
 
@@ -23,14 +20,6 @@ const LoanProgressHeader = (props: LoanProgressHeaderProps) => (
       <Tooltip title="Documents uploadés" enterTouchDelay={0}>
         <FontAwesomeIcon icon={faFile} className="icon" />
       </Tooltip>
-
-      <Tooltip title="Statut de la vérification e-Potek" enterTouchDelay={0}>
-        <FontAwesomeIcon icon={faEye} className="icon" />
-      </Tooltip>
-
-      {/* <Icon type="info" tooltip="Formulaires remplis" /> */}
-      {/* <Icon type="attachFile" tooltip="Documents uploadés" /> */}
-      {/* <Icon type="eye" tooltip="Statut de la vérification e-Potek" /> */}
     </div>
   </div>
 );
