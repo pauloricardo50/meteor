174bf50a229d9767659cd523054090511b27cea6
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-11-15 15:01:48 +0100

    Fix widget1 options tabs

--- core/components/widget1/Widget1Options/Widget1OptionsContainer.js
+++ core/components/widget1/Widget1Options/Widget1OptionsContainer.js
@@ -7,7 +7,7 @@ const mapStateToProps = ({ widget1: { purchaseType } }) => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  setPurchaseType: value => {
+  setPurchaseType: (_, value) => {
     dispatch({ type: commonTypes.SET_VALUE('purchaseType'), value });
     if (value === widget1Constants.PURCHASE_TYPE.REFINANCING) {
       dispatch({ type: widget1Types.SET_AUTO('property'), auto: false });
