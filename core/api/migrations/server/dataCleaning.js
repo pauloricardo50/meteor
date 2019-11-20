b5c74c75c3dec617e1e90f5fa9e139cf598cddc5
Quentin Herzig <quentin@e-potek.ch>
2019-11-20 14:56:21 +0100

    Copy dataCleaning from promotion reservation PR

--- core/api/migrations/server/dataCleaning.js
+++ core/api/migrations/server/dataCleaning.js
@@ -1,11 +1,14 @@
 import { Mongo } from 'meteor/mongo';
-
 import omit from 'lodash/omit';
 
 const skippedCollections = ['_cacheMigrations', 'grapher_counts'];
 const skippedFields = ['_id', 'createdAt', 'updatedAt'];
-
-const makeCleanDocument = (collection, schema) => ({ _id, ...doc }) => {
+const makeCleanDocument = collection => doc => {
+  const { _id } = doc;
+  const schema = collection.instance.simpleSchema(doc);
+  if (!schema) {
+    console.log(collection.name, doc);
+  }
   const cleanDoc = schema.clean(doc, {
     mutate: true,
     filter: true,
@@ -14,20 +17,16 @@ const makeCleanDocument = (collection, schema) => ({ _id, ...doc }) => {
     trimStrings: true,
     getAutoValues: true,
   });
-
   const withoutSkippedFields = omit(cleanDoc, skippedFields);
-
   //   Sometimes empty documents can slip through, and update will fail because $set is empty
   if (!withoutSkippedFields || Object.keys(withoutSkippedFields).length === 0) {
     console.log(`empty ${collection.name} document: ${_id}`);
     return Promise.resolve();
   }
-
   return collection.instance
     .rawCollection()
     .update({ _id }, { $set: withoutSkippedFields });
 };
-
 const cleanCollection = async collection => {
   if (
     !collection.name ||
@@ -35,22 +34,17 @@ const cleanCollection = async collection => {
     !collection.instance._c2
   ) {
     console.log(`Skipping ${collection.name}`);
-
     return;
   }
   console.log(`Cleaning ${collection.name}`);
-
-  const schema = collection.instance._c2._simpleSchema;
-
   const allDocuments = collection.instance.find({}).fetch();
-
-  await Promise.all(allDocuments.map(makeCleanDocument(collection, schema)));
-
+  const cleanDocument = makeCleanDocument(collection);
+  await Promise.all(allDocuments.map(cleanDocument));
   console.log(`Cleaned ${allDocuments.length} docs for ${collection.name}`);
 };
-
 export const cleanAllData = async () => {
+  console.log('Data cleaning Start...');
   const collections = Mongo.Collection.getAll();
-
   await Promise.all(collections.map(cleanCollection));
+  console.log('Data cleaning Complete! :)');
 };
