aa090ae7c92e4f893bd8c03c78bdc79175211d4a
Quentin Herzig <quentin@e-potek.ch>
2019-12-02 11:47:18 +0100

    Add pro only documents

--- core/components/PromotionPage/client/PromotionAdministration/PromotionAdministrationContainer.jsx
+++ core/components/PromotionPage/client/PromotionAdministration/PromotionAdministrationContainer.jsx
@@ -38,6 +38,7 @@ const promotionDocuments = [
   },
   { id: 'logos', acl: S3_ACLS.PUBLIC_READ, noTooltips: true },
   { id: 'promotionGuide', acl: S3_ACLS.PUBLIC_READ, noTooltips: true },
+  { id: 'proDocuments', acl: S3_ACLS.PUBLIC_READ, noTooltips: true },
 ];
 
 export const promotionLotSchema = new SimpleSchema({
