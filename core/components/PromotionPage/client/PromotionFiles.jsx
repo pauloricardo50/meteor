089d58243fc0e43fc737d363ee0efbd540ad5058
Quentin Herzig <quentin@e-potek.ch>
2019-12-02 14:57:18 +0100

    Separate promotion pro docs from other docs

--- core/components/PromotionPage/client/PromotionFiles.jsx
+++ core/components/PromotionPage/client/PromotionFiles.jsx
@@ -1,6 +1,8 @@
 // @flow
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock } from '@fortawesome/pro-light-svg-icons/faLock';
 
 import DocumentDownloadList from '../../DocumentDownloadList';
 
@@ -9,15 +11,25 @@ type PromotionFilesProps = {};
 const isAdmin = Meteor.microservice === 'admin';
 const isPro = Meteor.microservice === 'pro';
 
-const getDocuments = documents => {
-  const { promotionDocuments = [], proDocuments = [] } = documents;
-
-  return [...promotionDocuments, ...(isAdmin || isPro ? proDocuments : [])];
-};
-
 const PromotionFiles = ({ promotion: { documents } }: PromotionFilesProps) => (
-  <div className="animated fadeIn">
-    <DocumentDownloadList files={getDocuments(documents)} />
+  <div
+    className="animated fadeIn"
+    style={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch' }}
+  >
+    <DocumentDownloadList files={documents && documents.promotionDocuments} />
+    {(isAdmin || isPro) && (
+      <>
+        <h2 style={{ alignSelf: 'center' }}>
+          <FontAwesomeIcon
+            icon={faLock}
+            className="icon"
+            style={{ marginRight: '8px' }}
+          />
+          Documents Pros
+        </h2>
+        <DocumentDownloadList files={documents && documents.proDocuments} />
+      </>
+    )}
   </div>
 );
 
