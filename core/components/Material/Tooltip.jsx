42275aeac6c2d57c80da9d209d7753d0521be53a
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-12-04 13:34:26 +0100

    Always allow tooltips to be interactive

--- core/components/Material/Tooltip.jsx
+++ core/components/Material/Tooltip.jsx
@@ -1,1 +1,6 @@
-export { default } from '@material-ui/core/Tooltip';
+import DefaultTooltip from '@material-ui/core/Tooltip';
+import { withProps } from 'recompose';
+
+export default withProps({ leaveDelay: 100, interactive: true })(
+  DefaultTooltip,
+);
