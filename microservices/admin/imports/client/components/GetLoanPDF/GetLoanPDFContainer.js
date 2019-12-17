ea7d32af005fe4f2e9855d1a9bae21c991b81016
Quentin Herzig <quentin@e-potek.ch>
2019-12-17 16:37:40 +0100

    Add template for backgroundInfo

--- microservices/admin/imports/client/components/GetLoanPDF/GetLoanPDFContainer.js
+++ microservices/admin/imports/client/components/GetLoanPDF/GetLoanPDFContainer.js
@@ -4,6 +4,7 @@ import { compose, withProps } from 'recompose';
 import { PDF_TYPES } from 'core/api/constants';
 import { generatePDF } from 'core/api/pdf/methodDefinitions';
 import { base64ToBlob } from './base64-to-blob';
+import { makeGenerateBackgroundInfo } from './helpers';
 
 const savePdf = ({ base64, pdfName }) => {
   if (!base64) {
@@ -30,28 +31,50 @@ const saveHtmlFile = ({ html, pdfName }) => {
 };
 
 export default compose(
-  withProps(({ loan: { _id: loanId } }) => ({
-    handlePDF: ({ anonymous, organisationId, structureIds, backgroundInfo }) =>
-      generatePDF
-        .run({
-          type: PDF_TYPES.LOAN,
-          params: { loanId, organisationId, structureIds, backgroundInfo },
-          options: { anonymous },
-        })
-        .then(savePdf)
-        .catch(error => {
-          import('../../../core/utils/message').then(({ default: message }) => {
-            message.error(error.message, 5);
+  withProps(({ loan }) => {
+    const { _id: loanId } = loan;
+    const generateBackgroundInfo = makeGenerateBackgroundInfo(loan);
+
+    return {
+      handlePDF: ({ anonymous, ...model }) => {
+        const { organisationId, structureIds } = model;
+        return generatePDF
+          .run({
+            type: PDF_TYPES.LOAN,
+            params: {
+              loanId,
+              organisationId,
+              structureIds,
+              backgroundInfo: generateBackgroundInfo(model),
+            },
+            options: { anonymous },
+          })
+          .then(savePdf)
+          .catch(error => {
+            import('../../../core/utils/message').then(
+              ({ default: message }) => {
+                message.error(error.message, 5);
+              },
+            );
           });
-        }),
-    handleHTML: ({ anonymous, organisationId, structureIds, backgroundInfo }) =>
-      generatePDF
-        .run({
-          type: PDF_TYPES.LOAN,
-          params: { loanId, organisationId, structureIds, backgroundInfo },
-          options: { anonymous },
-          htmlOnly: true,
-        })
-        .then(saveHtmlFile),
-  })),
+      },
+      handleHTML: ({ anonymous, ...model }) => {
+        const { organisationId, structureIds } = model;
+
+        return generatePDF
+          .run({
+            type: PDF_TYPES.LOAN,
+            params: {
+              loanId,
+              organisationId,
+              structureIds,
+              backgroundInfo: generateBackgroundInfo(model),
+            },
+            options: { anonymous },
+            htmlOnly: true,
+          })
+          .then(saveHtmlFile);
+      },
+    };
+  }),
 );
