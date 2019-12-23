6f84dad43167d631ceb8fd6b09f3825003e575e4
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-12-23 11:18:04 +0100

    Allow adding custom insurance documents

--- core/components/UploaderArray/Uploader/AdditionalDocModifier.jsx
+++ core/components/UploaderArray/Uploader/AdditionalDocModifier.jsx
@@ -9,7 +9,6 @@ import Button from '../../Button';
 import {
   PROPERTIES_COLLECTION,
   BORROWERS_COLLECTION,
-  LOANS_COLLECTION,
 } from '../../../api/constants';
 
 type AdditionalDocModifierProps = {
@@ -27,6 +26,7 @@ export const getAdditionalDocSchema = collection => {
     allowedValues = [
       'PERSONAL_DOCUMENTS',
       'INCOMES_AND_EXPENSES',
+      'INSURANCE',
       'FORTUNE_AND_OWN_FUNDS',
       'RETIREMENT',
       'OTHER',
