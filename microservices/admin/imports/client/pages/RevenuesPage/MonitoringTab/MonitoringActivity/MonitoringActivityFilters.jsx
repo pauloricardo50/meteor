cd43368861ed01fc94b5a62d534eeae10e475b06
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-01-08 20:50:14 +0100

    Add shortcuts for some of the reports

--- microservices/admin/imports/client/pages/RevenuesPage/MonitoringTab/MonitoringActivity/MonitoringActivityFilters.jsx
+++ microservices/admin/imports/client/pages/RevenuesPage/MonitoringTab/MonitoringActivity/MonitoringActivityFilters.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import moment from 'moment';
 
 import DateRangePicker from 'core/components/DateInput/DateRangePicker';
+import Button from 'core/components/Button';
 
 type MonitoringActivityFiltersProps = {};
 
@@ -15,27 +16,67 @@ const MonitoringActivityFilters = ({
   createdAtRange,
   setCreatedAtRange,
 }: MonitoringActivityFiltersProps) => (
-  <div className="flex">
-    <div className="flex-col mr-16">
-      <label htmlFor="activityRange">Date de l'activité</label>
-      <DateRangePicker
-        range={activityRange}
-        onChange={setActivityRange}
-        numberOfMonths={3}
-        isDayBlocked={() => false}
-        id="activityRange"
-      />
+  <div className="flex-col">
+    <div className="flex mb-8">
+      <Button
+        raised
+        primary
+        className="mr-8"
+        onClick={() => {
+          setActivityRange({ startDate: null, endDate: null });
+          setCreatedAtRange({
+            startDate: moment(minDate).toDate(),
+            endDate: moment().toDate(),
+          });
+        }}
+      >
+        Performance totale des conseillers
+      </Button>
+      <Button
+        raised
+        primary
+        onClick={() => {
+          setActivityRange({
+            startDate: moment()
+              .subtract(30, 'd')
+              .toDate(),
+            endDate: moment().toDate(),
+          });
+          setCreatedAtRange({
+            startDate: null,
+            endDate: null,
+          });
+        }}
+      >
+        Activité dans les 30 derniers jours
+      </Button>
     </div>
-    <div className="flex-col">
-      <label htmlFor="createdAt">Date de création du dossier</label>
-      <DateRangePicker
-        range={createdAtRange}
-        onChange={setCreatedAtRange}
-        numberOfMonths={3}
-        id="createdAt"
-        enableOutsideDays={false}
-        isOutsideRange={date => date.isBefore(minDate)}
-      />
+
+    <div className="flex">
+      <div className="flex-col mr-16">
+        <label htmlFor="activityRange">Date de l'activité</label>
+        <DateRangePicker
+          range={activityRange}
+          onChange={(...args) => {
+            console.log(args);
+            setActivityRange(...args);
+          }}
+          numberOfMonths={3}
+          isDayBlocked={() => false}
+          id="activityRange"
+        />
+      </div>
+      <div className="flex-col">
+        <label htmlFor="createdAt">Date de création du dossier</label>
+        <DateRangePicker
+          range={createdAtRange}
+          onChange={setCreatedAtRange}
+          numberOfMonths={3}
+          id="createdAt"
+          enableOutsideDays={false}
+          isOutsideRange={date => date && date.isBefore(minDate)}
+        />
+      </div>
     </div>
   </div>
 );
