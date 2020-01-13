c8cdde485853d87d76f9825e4780cbb7ed8f74e8
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-01-13 14:17:28 +0100

    Add elise to round robin

--- core/api/users/server/roundRobinAdvisors.js
+++ core/api/users/server/roundRobinAdvisors.js
@@ -1,3 +1,7 @@
-const roundRobinAdvisors = ['lydia@e-potek.ch', 'max@e-potek.ch'];
+const roundRobinAdvisors = [
+  'lydia@e-potek.ch',
+  'max@e-potek.ch',
+  'elise@e-potek.ch',
+];
 
 export default roundRobinAdvisors;
