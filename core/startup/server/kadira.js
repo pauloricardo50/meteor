6df79acf75d208775a4ac34039997d244b20dd46
zodern <zodern@users.noreply.github.com>
2020-01-22 17:05:21 +0100

    Switch backend to Monti APM

--- core/startup/server/kadira.js
+++ core/startup/server/kadira.js
@@ -1,12 +1,19 @@
 import { Meteor } from 'meteor/meteor';
-import { Kadira } from 'meteor/lmachens:kadira';
 
 if (Meteor.settings.Kadira) {
-  const { endpoint } = Meteor.settings.Kadira;
+  let options = {};
+
+  if (Package['lmachens:kadira']) {
+    import { Kadira } from 'meteor/lmachens:kadira';
+    options.endpoint = Meteor.settings.Kadira.endpoint;
+  } else {
+    import { Kadira } from 'meteor/montiapm:agent';
+  }
+
   const { microservice } = Meteor;
 
   if (Meteor.settings.Kadira[microservice]) {
     const { appId, appSecret } = Meteor.settings.Kadira[microservice];
-    Kadira.connect(appId, appSecret, { endpoint });
+    Kadira.connect(appId, appSecret, options);
   }
 }
