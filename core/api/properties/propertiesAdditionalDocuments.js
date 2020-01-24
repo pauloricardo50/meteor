a07c061f3df7b2989951f671a604b2e920c61b28
Quentin Herzig <quentin@e-potek.ch>
2020-01-24 14:27:37 +0100

    Refactor property documents

--- core/api/properties/propertiesAdditionalDocuments.js
+++ core/api/properties/propertiesAdditionalDocuments.js
@@ -24,31 +24,51 @@ const getLoanResidenceType = propertyId => {
 
 export const conditionalDocuments = [
   {
-    id: DOCUMENTS.PROPERTY_VOLUME,
+    id: DOCUMENTS.PROPERTY_WORKS_QUOTE,
+    condition: ({ doc: { renovationYear } }) => !!renovationYear,
+  },
+  {
+    id: DOCUMENTS.INVESTMENT_PROPERTY_CONDOMINIUM_REGULATION,
+    condition: ({ doc: { isCoproperty } }) => !!isCoproperty,
+  },
+  {
+    id: DOCUMENTS.INVESTMENT_PROPERTY_CONDOMINIUM_OWNERSHIP_DIVISION_REGISTER,
+    condition: ({ doc: { isCoproperty, propertyType } }) =>
+      [
+        propertyConstants.PROPERTY_TYPE.FLAT,
+        propertyConstants.PROPERTY_TYPE.INVESTMENT_BUILDING,
+      ].includes(propertyType) && isCoproperty,
+  },
+  {
+    id: DOCUMENTS.PROPERTY_PURCHASE_DEED,
     condition: ({ doc: { propertyType } }) =>
-      propertyType === propertyConstants.PROPERTY_TYPE.HOUSE,
+      propertyType === propertyConstants.PROPERTY_TYPE.INVESTMENT_BUILDING,
+  },
+  {
+    id: DOCUMENTS.INVESTEMENT_PROPERTY_RENTAL_STATEMENT,
+    condition: ({ doc: { propertyType } }) =>
+      propertyType === propertyConstants.PROPERTY_TYPE.INVESTMENT_BUILDING,
+  },
+  {
+    id: DOCUMENTS.PROPERTY_RENTAL_AGREEMENT,
+    condition: ({ doc: { propertyType, isNew } }) =>
+      propertyType === propertyConstants.PROPERTY_TYPE.INVESTMENT_BUILDING &&
+      !isNew,
+  },
+  {
+    id: DOCUMENTS.PROPERTY_RENTAL_PROJECT,
+    condition: ({ doc: { propertyType, isNew } }) =>
+      propertyType === propertyConstants.PROPERTY_TYPE.INVESTMENT_BUILDING &&
+      isNew,
+  },
+  {
+    id: DOCUMENTS.PROPERTY_VOLUME,
+    condition: ({ doc: { propertyType, isNew } }) =>
+      propertyType === propertyConstants.PROPERTY_TYPE.HOUSE && isNew,
   },
   {
     id: DOCUMENTS.PROPERTY_MINERGIE_CERTIFICATE,
     condition: ({ doc: { minergie } }) =>
       minergie !== propertyConstants.MINERGIE_CERTIFICATE.WITHOUT_CERTIFICATE,
-  },
-  {
-    id: DOCUMENTS.COOWNERSHIP_AGREEMENT,
-    condition: ({ doc: { isCoproperty } }) => isCoproperty === true,
-  },
-  {
-    id: DOCUMENTS.COOWNERSHIP_ALLOCATION_AGREEMENT,
-    condition: ({ doc: { isCoproperty } }) => isCoproperty === true,
-  },
-  {
-    id: DOCUMENTS.INVESTEMENT_PROPERTY_RENTAL_STATEMENT,
-    condition: ({ doc: { _id: propertyId } }) =>
-      getLoanResidenceType(propertyId) === RESIDENCE_TYPE.INVESTMENT,
-  },
-  {
-    id: DOCUMENTS.INVESTEMENT_PROPERTY_SERVICE_CHARGE_SETTLEMENT,
-    condition: ({ doc: { _id: propertyId } }) =>
-      getLoanResidenceType(propertyId) === RESIDENCE_TYPE.INVESTMENT,
   },
 ];
