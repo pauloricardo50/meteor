934224821f38abaf490ee4a71e0f8cd73ca795e7
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-01-31 14:09:51 +0100

    Unify server only collections organisation
    Make liveSync and analysisReports the same

--- core/api/server/serverCollections.js
+++ core/api/server/serverCollections.js
@@ -1,5 +1,6 @@
 // These are server-only quickly written collections. They don't use all
 // the fancy tools for our heavy collections
 import '../liveSync';
+import '../liveSync/server/methods';
 import '../analysisReports';
 import '../analysisReports/server/methods';
