909b4c102b4e1f9ff3e4b414cea740fea01ee43f
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-02-05 00:32:09 +0100

    Add back all files in www

--- microservices/www/imports/ui/pages/HomePage/HomePage.jsx
+++ microservices/www/imports/ui/pages/HomePage/HomePage.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
 import PageHead from 'core/components/PageHead';
-// import HomePageInterestRates from './HomePageInterestRates';
 import HomePageHeader from './HomePageHeader';
-// import HomePagePartners from './HomePagePartners';
 import HomePageDescription from './HomePageDescription';
 import HomePageReviews from './HomePageReviews';
-// import HomePageNewsletter from './HomePageNewsletter';
 import WwwLayout from '../../WwwLayout';
 import { VARIANTS } from '../../WwwLayout/WwwTopNav';
 
@@ -16,14 +13,11 @@ const HomePage = () => (
       titleId="HomePageHeader.title"
       descriptionId="HomePageHeader.description"
     />
-    {/* <HomePageInterestRates /> */}
     <WwwLayout.TopNav variant={VARIANTS.WHITE} />
     <WwwLayout.Content>
       <HomePageHeader />
-      {/* <HomePagePartners /> */}
       <HomePageDescription />
       <HomePageReviews />
-      {/* <HomePageNewsletter /> */}
     </WwwLayout.Content>
     <WwwLayout.Footer />
   </WwwLayout>
