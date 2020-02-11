9de9ed93a294f192a4a071fb9dcaf03ac28240c6
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-02-11 12:20:20 +0100

    Refactor popup components with global queries

--- core/api/loans/reducers.js
+++ core/api/loans/reducers.js
@@ -72,8 +72,15 @@ Loans.addReducers({
       adminNotes.filter(({ isSharedWithPros }) => isSharedWithPros),
   },
   mainAssignee: {
-    body: { assignees: { name: 1, email: 1 } },
-    reduce: ({ assignees = [] }) =>
-      assignees.find(({ $metadata: { isMain } }) => isMain),
+    body: { assigneeLinks: 1, assignees: { name: 1, email: 1 } },
+    reduce: ({ assigneeLinks = [], assignees = [] }) => {
+      // FIXME: For some weird reason, the $metadata is not included
+      // in the assignees array
+      const mainAssignee = assigneeLinks.find(({ isMain }) => isMain);
+
+      return mainAssignee
+        ? assignees.find(({ _id }) => mainAssignee._id === _id)
+        : undefined;
+    },
   },
 });
