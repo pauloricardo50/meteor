962a0bbed1037ab60dfd9699bce2975c0a967775
zodern <zodern@users.noreply.github.com>
2020-02-12 18:38:07 +0100

    Fix www mup config

--- mup/staging/www-staging.mup.js
+++ mup/staging/www-staging.mup.js
@@ -2,5 +2,5 @@ const generateConfig = require('./mup.staging-base.js');
 
 module.exports = generateConfig({
   microservice: 'www',
-  subDomains: ['www.staging-2.e-potek.net'],
+  subDomains: ['www'],
 });
