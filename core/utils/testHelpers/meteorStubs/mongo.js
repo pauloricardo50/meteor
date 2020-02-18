7f68a985c0202bbf3494d940b5b9376ed3a33e82
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-02-18 14:40:03 +0100

    Improve wallaby-resiliency

--- core/utils/testHelpers/meteorStubs/mongo.js
+++ core/utils/testHelpers/meteorStubs/mongo.js
@@ -1,8 +1,13 @@
+class SimpleSchemaStub {
+  extend() {}
+}
+
 export const Mongo = {
   Collection: () => ({
     deny: () => {},
     allow: () => {},
     attachSchema: () => {},
     createQuery: name => ({ queryName: name }),
+    simpleSchema: () => new SimpleSchemaStub(),
   }),
 };
