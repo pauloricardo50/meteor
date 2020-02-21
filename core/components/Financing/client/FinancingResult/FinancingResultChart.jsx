ddfd4b3bf55ff8156d0eb2b0807200602032bb81
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-02-21 14:39:35 +0100

    Remove chart from results

--- core/components/Financing/client/FinancingResult/FinancingResultChart.jsx
+++ core/components/Financing/client/FinancingResult/FinancingResultChart.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { compose } from 'recompose';
 import cx from 'classnames';
 
-import DonutChart from 'core/components/charts/DonutChart';
 import { toMoney } from 'core/utils/conversionFunctions';
 import SingleStructureContainer from '../containers/SingleStructureContainer';
 import FinancingDataContainer from '../containers/FinancingDataContainer';
@@ -19,39 +18,10 @@ const FinancingResultChart = ({ className, ...props }) => {
 
   return (
     <div className={cx('financing-structures-result-chart', className)}>
-      <DonutChart
-        data={[
-          { value: interests, id: 'interests' },
-          { value: amortization, id: 'amortization' },
-          { value: propertyExpenses, id: 'maintenance' },
-        ]}
-        intlPrefix="general"
-        config={{
-          chart: {
-            width: 100,
-            height: 100,
-            spacingBottom: 0,
-            spacingTop: 0,
-            spacingRight: 0,
-            spacingLeft: 0,
-          },
-          plotOptions: {
-            pie: {
-              tooltip: {
-                headerFormat: '<b>{point.key}</b><br />',
-                pointFormat: 'CHF {point.y:,.0f}',
-              },
-              showInLegend: false,
-              animation: false,
-            },
-          },
-          legend: { enabled: false },
-        }}
-        title=""
-      />
       <span className="total">
         <span className="chf">CHF </span>
         {toMoney(amortization + interests + propertyExpenses)}
+        <small>&nbsp;/mois</small>
       </span>
     </div>
   );
