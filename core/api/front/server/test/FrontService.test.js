f2e4245296145b2637f737b4730823129dfec918
Quentin Herzig <quentin@e-potek.ch>
2020-02-26 11:33:37 +0100

    Store front user id on users

--- core/api/front/server/test/FrontService.test.js
+++ core/api/front/server/test/FrontService.test.js
@@ -4,6 +4,7 @@ import { resetDatabase } from 'meteor/xolvio:cleaner';
 import { expect } from 'chai';
 import sinon from 'sinon';
 
+import UserService from 'core/api/users/server/UserService';
 import generator from '../../../factories/server';
 import { LOANS_TAG_URL, FrontService } from '../FrontService';
 import { ROLES } from '../../../users/userConstants';
@@ -378,6 +379,39 @@ describe('FrontService', () => {
         expect(parsedBody).to.deep.equal({ assignee_id: 'admin' });
       });
 
+      it('sets the front user id on the assignee', async () => {
+        listTeamStub.resolves({
+          _results: [{ id: 'admin', email: 'admin@e-potek.ch' }],
+        });
+
+        generator({
+          users: {
+            emails: [{ address: 'user@e-potek.ch', verified: true }],
+            roles: [ROLES.USER],
+            assignedEmployee: {
+              emails: [{ address: 'admin@e-potek.ch', verified: true }],
+            },
+          },
+        });
+
+        await service.handleWebhook({
+          body: {
+            conversation: {
+              id: 'conversationId',
+              last_message: {
+                recipients: [{ handle: 'user@e-potek.ch', role: 'from' }],
+              },
+            },
+          },
+          webhookName: 'auto-assign',
+        });
+
+        const { frontUserId } = UserService.getByEmail('admin@e-potek.ch', {
+          frontUserId: 1,
+        });
+        expect(frontUserId).to.equal('admin');
+      });
+
       it('assigns a conversation to the user only loan main assignee', async () => {
         listTeamStub.resolves({
           _results: [
@@ -587,6 +621,46 @@ describe('FrontService', () => {
         });
 
         expect(fetchStub.callCount).to.equal(0);
+      });
+
+      it('does not call front list team if the assignee has already a front user id stored in db', async () => {
+        // listTeamStub.resolves({
+        //   _results: [{ id: 'admin', email: 'admin@e-potek.ch' }],
+        // });
+
+        generator({
+          users: {
+            emails: [{ address: 'user@e-potek.ch', verified: true }],
+            roles: [ROLES.USER],
+            assignedEmployee: {
+              emails: [{ address: 'admin@e-potek.ch', verified: true }],
+              frontUserId: 'admin',
+            },
+          },
+        });
+
+        await service.handleWebhook({
+          body: {
+            conversation: {
+              id: 'conversationId',
+              last_message: {
+                recipients: [{ handle: 'user@e-potek.ch', role: 'from' }],
+              },
+            },
+          },
+          webhookName: 'auto-assign',
+        });
+
+        expect(listTeamStub.callCount).to.equal(0);
+
+        const [endpoint, { body, method }] = fetchStub.getCall(-1).args;
+        const parsedBody = JSON.parse(body);
+
+        expect(endpoint).to.equal(
+          'https://api2.frontapp.com/conversations/conversationId/assignee',
+        );
+        expect(method).to.equal('PUT');
+        expect(parsedBody).to.deep.equal({ assignee_id: 'admin' });
       });
     });
   });
