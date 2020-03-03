e389770a0a029b7f3e063b21e66a24a2fedcff88
zodern <zodern@users.noreply.github.com>
2020-03-03 04:42:51 +0100

    Use locally installed version of mup

--- mup/utils/run-mup.js
+++ mup/utils/run-mup.js
@@ -4,7 +4,7 @@ const path = require('path');
 // Force mup to use color in output
 process.env.FORCE_COLOR = 1;
 
-const mupPath = 'mup' || path.resolve(__dirname, '../../node_modules/.bin/mup');
+const mupPath = path.resolve(__dirname, '../../node_modules/.bin/mup');
 
 function getFullCommand(command) {
   return `${mupPath} ${command}`;
