2d4aec87a941cf149235d103fb774162f3d6465c
Quentin Herzig <quentin@e-potek.ch>
2020-03-05 09:58:03 +0100

    Disable exoscale DB backups

--- .backup/scripts/backup_db/remove_old_db_backups.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl https://cronitor.link/qDOCtx/run -m 10
-s3cmd ls s3://production-backups | sort | awk -F' ' '{print $NF}' | grep -E  `date +%Y-%m-%d -d "14 day ago"` | xargs -I {} s3cmd del -r "{}"
-curl https://cronitor.link/qDOCtx/complete -m 10
--- .backup/scripts/cf/cf-connect-ssh-tunnel.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-cf ssh -L $1:kubernetes-service-node.service.consul:$1 e-potek-backup-manager-$2
