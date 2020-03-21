6f976d37b7218482401b37294f26131b2ae5c728
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-03-21 13:07:33 +0100

    Update circle CI config

--- .circleci/generateConfig.js
+++ .circleci/generateConfig.js
@@ -14,7 +14,7 @@ const defaultJobValues = {
         LANGUAGE: 'C.UTF-8',
         LC_ALL: 'C.UTF-8',
         LC_NUMERIC: 'en_US.UTF-8',
-        METEOR_VERSION: '1.9',
+        METEOR_VERSION: '1.10.1',
         NODE_ENV: 'development', // Some packages require this during tests
         TOOL_NODE_FLAGS:
           '--max_old_space_size=8192 --optimize_for_size --gc_interval=100 --min_semi_space_size=8 --max_semi_space_size=256', // NodeJS kung-fu to make your builds run faster, without running out of memory
