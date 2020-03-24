c99964463b03433169bfd0cd82108504756ee7ca
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-03-24 10:10:00 +0100

    Add CSS normalizer

--- microservices/www2/gatsby-browser.js
+++ microservices/www2/gatsby-browser.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Layout from './src/components/Layout';
 import meteorClient, { MeteorClientContext } from './src/utils/meteorClient';
 
+import './src/styles/main.scss';
+
 export const wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
 );
