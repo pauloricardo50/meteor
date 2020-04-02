1d4966b2fd0d4fcd5944e14e54db76ee04fb407f
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-04-02 12:33:59 +0200

    Don't import server-side collections

--- microservices/www/imports/startup/server/index.js
+++ microservices/www/imports/startup/server/index.js
@@ -1,9 +1,5 @@
 // This is SSR, so also initialize here as well as on the client
-import 'core/api/initialization';
-
 import '../shared/setup';
 
-import 'core/api/server';
-import 'core/fixtures';
 import './ssr-server';
 import './monti';
