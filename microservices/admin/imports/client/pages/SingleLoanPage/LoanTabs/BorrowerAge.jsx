2ca7020bfc7b128d0541d78107b8ca955e850419
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-04-04 21:27:52 +0200

    Don't calculate age if there is no birthdate

--- microservices/admin/imports/client/pages/SingleLoanPage/LoanTabs/BorrowerAge.jsx
+++ microservices/admin/imports/client/pages/SingleLoanPage/LoanTabs/BorrowerAge.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import cx from 'classnames';
 
-import T from 'core/components/Translation';
 import Icon from 'core/components/Icon';
+import T from 'core/components/Translation';
 
 const BorrowerAge = ({ borrower }) => {
   const { age } = borrower;
