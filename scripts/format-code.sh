67b9d80b1d9107d4a83970e48a6e0e1c5c64f327
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-04-06 14:52:24 +0200

    Split up commands to get quicker feedback

--- scripts/format-code.sh
+++ scripts/format-code.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 echo "Formatting all files with eslint config..."
-npx eslint --fix --ext .jsx,.js ..
+npx eslint --fix --ext .jsx,.js ../core
+npx eslint --fix --ext .jsx,.js ../microservices
 
 echo "Formatting all CSS files"
-# Check if csscomb is installed
-if ! type "csscomb" > /dev/null; then
-  # install csscomb if not
-  echo Installing csscomb
-  meteor npm i -g csscomb
-fi
-
-csscomb ../core
-csscomb ../microservices
+npx csscomb ../core
+npx csscomb ../microservices
 
 echo "Sorting all imports"
 npx import-sort --write "../core/**/*.js?(x)"
