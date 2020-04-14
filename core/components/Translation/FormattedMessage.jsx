58627e00d50b22d10ab1ffc7a00f20b39fecb95c
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-04-14 18:07:41 +0200

    Allow displaying a tooltip icon next to text

--- core/components/Translation/FormattedMessage.jsx
+++ core/components/Translation/FormattedMessage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
+import Icon from '../Icon';
+import Tooltip from '../Material/Tooltip';
 import { Consumer } from './withTranslationContext';
 
 // For some "select" strings that need a default value
@@ -8,17 +10,27 @@ const defaultTranslationValues = {
   purchaseType: 'ACQUISITION',
 };
 
-export default ({ values = {}, ...props }) => (
+export default ({ values = {}, tooltip, ...props }) => (
   <Consumer>
     {(translationValues = {}) => (
-      <FormattedMessage
-        {...props}
-        values={{
-          ...defaultTranslationValues,
-          ...translationValues,
-          ...values,
-        }}
-      />
+      <>
+        <FormattedMessage
+          {...props}
+          values={{
+            ...defaultTranslationValues,
+            ...translationValues,
+            ...values,
+          }}
+        />
+        {tooltip && (
+          <Tooltip title={tooltip}>
+            <Icon
+              type="help"
+              style={{ margin: '0 4px', fontSize: 'inherit' }}
+            />
+          </Tooltip>
+        )}
+      </>
     )}
   </Consumer>
 );
