699959d979f8f1b5b9ce60ae8c8be974b10a19c1
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-04-14 18:36:07 +0200

    Reuse form in admin

--- core/arrays/RefinancingFormArray.js
+++ core/arrays/RefinancingFormArray.js
@@ -1,10 +1,6 @@
-import {
-  MORTGAGE_NOTE_CATEGORIES,
-  MORTGAGE_NOTE_TYPES,
-} from '../api/helpers/sharedSchemaConstants';
 import { INTEREST_RATES } from '../api/interestRates/interestRatesConstants';
 
-const getRefinancingFormArray = ({ loan }) => [
+const getRefinancingFormArray = () => [
   {
     id: 'previousLender',
     type: 'textInput',
@@ -22,24 +18,6 @@ const getRefinancingFormArray = ({ loan }) => [
       { id: 'value', type: 'textInput', money: true },
       { id: 'dueDate', type: 'dateInput' },
       { id: 'rate', type: 'textInput', percent: true },
-    ],
-  },
-  {
-    id: 'mortgageNotes',
-    type: 'arrayInput',
-    inputs: [
-      { id: 'value', type: 'textInput', money: true },
-      { id: 'rank', type: 'textInput', number: true },
-      {
-        id: 'type',
-        type: 'selectInput',
-        options: Object.values(MORTGAGE_NOTE_TYPES),
-      },
-      {
-        id: 'category',
-        type: 'selectInput',
-        options: Object.values(MORTGAGE_NOTE_CATEGORIES),
-      },
     ],
   },
 ];
