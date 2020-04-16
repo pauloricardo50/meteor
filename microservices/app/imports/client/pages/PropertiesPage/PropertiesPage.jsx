b0afd8ba0778cbf9697fb25ae880c925f90821f9
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-04-16 11:25:03 +0200

    Automatically redirect to a single property when possible

--- microservices/app/imports/client/pages/PropertiesPage/PropertiesPage.jsx
+++ microservices/app/imports/client/pages/PropertiesPage/PropertiesPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 
-import { APPLICATION_TYPES } from 'core/api/loans/loanConstants';
+import { APPLICATION_TYPES, PURCHASE_TYPE } from 'core/api/loans/loanConstants';
+import { createRoute } from 'core/utils/routerUtils';
 
+import APP_ROUTES from '../../../startup/client/appRoutes';
 import PageApp from '../../components/PageApp';
 import withSimpleAppPage from '../../components/SimpleAppPage/SimpleAppPage';
 import PropertiesPageAdder from './PropertiesPageAdder';
@@ -10,7 +13,26 @@ import PropertiesPageDetail from './PropertiesPageDetail';
 import PropertiesPagePromotions from './PropertiesPagePromotions';
 
 const PropertiesPage = ({ loan, currentUser }) => {
-  const { _id: loanId, properties = [], hasPromotion, applicationType } = loan;
+  const {
+    _id: loanId,
+    properties = [],
+    hasPromotion,
+    applicationType,
+    purchaseType,
+  } = loan;
+
+  if (properties.length === 1 && purchaseType !== PURCHASE_TYPE.ACQUISITION) {
+    // In loans that never need multiple properties, route to the first one
+    return (
+      <Redirect
+        to={createRoute(APP_ROUTES.PROPERTY_PAGE.path, {
+          ':propertyId': properties[0]._id,
+          ':loanId': loanId,
+        })}
+      />
+    );
+  }
+
   return (
     <PageApp
       id="PropertiesPage"
