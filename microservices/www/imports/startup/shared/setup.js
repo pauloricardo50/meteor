c67f686e333751e4285b1f3f2fb89557cc84178f
Quentin Herzig <quentin@e-potek.ch>
2020-04-17 14:33:41 +0200

    Fix formatMessage on www

--- microservices/www/imports/startup/shared/setup.js
+++ microservices/www/imports/startup/shared/setup.js
@@ -2,9 +2,12 @@ import { Meteor } from 'meteor/meteor';
 
 import { localizationStartup } from 'core/utils/localization';
 
+import messagesFR from '../../../lang/fr.json';
+
 localizationStartup({
   setupAccounts: false,
   setupCountries: false,
+  messages: messagesFR,
 });
 
 Meteor.microservice = 'www';
