7c85b2f68e092f1b9751617ae68e7900c7d72845
Quentin Herzig <quentin@e-potek.ch>
2020-04-21 16:23:45 +0200

    Assign patrick to neho

--- core/api/users/server/assigneesByOrg.js
+++ core/api/users/server/assigneesByOrg.js
@@ -1,4 +1,8 @@
+import { employeesByEmail } from '../../../arrays/epotekEmployees';
+
 export const assigneesByOrg = {
   // For tests
   testOrgId: 'testAdminId',
+  // Neho -> Patrick
+  G7hs4JN9pfCAZzXdN: employeesByEmail['patrick@e-potek.ch']._id,
 };
