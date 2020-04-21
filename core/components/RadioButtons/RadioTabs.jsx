b1255f6dcb49abf42c015cb96347c962447c7f2b
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-04-21 17:41:42 +0200

    Add tabs component to our repo for customization

--- core/components/RadioButtons/RadioTabs.jsx
+++ core/components/RadioButtons/RadioTabs.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Tab from '@material-ui/core/Tab';
-import Tabs from '@material-ui/core/Tabs';
 
 import colors from '../../config/colors';
+import Tab from '../Material/Tab';
+import Tabs from '../Material/Tabs';
 
 const StyledTabs = withStyles({
   root: {
