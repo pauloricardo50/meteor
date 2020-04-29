c45a6fa08e4fec4870cebe7b38385d6dd29c71c2
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-04-29 10:55:14 +0200

    Only render ClickAwayListener when needed
    Avoids race conditions on touch devices

--- core/components/ContactButton/SimpleContactButtonContainer.js
+++ core/components/ContactButton/SimpleContactButtonContainer.js
@@ -1,14 +1,14 @@
-import {withProps} from 'recompose';
+import { withProps } from 'recompose';
 
-import { EPOTEK_NUMBER, employeesByEmail } from 'core/arrays/epotekEmployees';
+import { employeesByEmail } from 'core/arrays/epotekEmployees';
 
 export default withProps(({ currentUser }) => {
-    let staff;
-    if (currentUser && currentUser.assignedEmployee) {
-      staff = employeesByEmail[currentUser.assignedEmployee.email];
-    }
-    if (!staff) {
-      staff = employeesByEmail['financement@e-potek.ch'];
-    }
-    return { staff };
-  });
+  let staff;
+  if (currentUser && currentUser.assignedEmployee) {
+    staff = employeesByEmail[currentUser.assignedEmployee.email];
+  }
+  if (!staff) {
+    staff = employeesByEmail['team@e-potek.ch'];
+  }
+  return { staff };
+});
