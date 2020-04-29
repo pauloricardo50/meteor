ddefd1b8bce7c097e7eeb4ae6427bff124bc48b5
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-04-29 22:12:29 +0200

    Update formatting script

--- scripts/format-code.sh
+++ scripts/format-code.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 echo "Formatting core files with eslint config..."
-npx eslint --debug --fix --ext .jsx,.js ../core/
+# npx eslint --debug --fix --ext .jsx,.js --ignore-pattern "../core/assets/**" "../core/**"
 
 echo "Formatting microservices files with eslint config..."
-# TODO: This script loops over core again for each microservice
-npx eslint --debug --fix --ext .jsx,.js ../microservices/
+npx eslint --debug --fix --ext .jsx,.js --ignore-pattern "../microservices/www/imports/startup/client/**" "../microservices/www/imports/startup/**"
+# npx eslint --debug --fix --ext .jsx,.js --ignore-pattern "../microservices/pro/imports/core/**/*.js?(x)" "../microservices/pro/imports/**"
 
-echo "Formatting all CSS files"
-npx csscomb ../core
-npx csscomb ../microservices
+# echo "Formatting all CSS files"
+# npx csscomb ../core
+# npx csscomb ../microservices
 
-echo "Sorting all imports"
-npx import-sort --write "../core/**/*.js?(x)"
-# TODO: This script loops over core again for each microservice
-npx import-sort --write "../microservices/www/**/*.js?(x)"
+# echo "Sorting all imports"
+# npx import-sort --write "../core/**/*.js?(x)"
+# npx import-sort --write "../microservices/**/*.js?(x)" "!../microservices/**/core/**/*.js?(x)"
