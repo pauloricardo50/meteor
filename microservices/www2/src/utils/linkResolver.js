d548f08e0bf8d2491fc34fb9e9ec02a7f3931d64
Brad Hover <dev@tekha.us>
2020-05-01 13:30:06 -0700

    add prismic internal link handling

---
+++ microservices/www2/src/utils/linkResolver.js
@@ -0,0 +1,18 @@
+// -- The Link Resolver
+// This function will be used to generate links to Prismic documents
+
+exports.linkResolver = function linkResolver(doc) {
+  const { lang, type, uid } = doc;
+
+  const shortLang = {
+    'fr-ch': 'fr',
+    'en-us': 'en',
+  }[lang];
+
+  const resolvedLink = {
+    post: `/${shortLang}/blog/${uid}`,
+    page: `/${shortLang}/${uid}`,
+  }[type];
+
+  return resolvedLink || '/';
+};
