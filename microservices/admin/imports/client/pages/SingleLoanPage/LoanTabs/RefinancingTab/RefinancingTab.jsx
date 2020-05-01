e8fffd0128ac726030613d4667816396f743a2ee
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-05-01 17:58:26 +0200

    Update AutoForm to use _collection

--- microservices/admin/imports/client/pages/SingleLoanPage/LoanTabs/RefinancingTab/RefinancingTab.jsx
+++ microservices/admin/imports/client/pages/SingleLoanPage/LoanTabs/RefinancingTab/RefinancingTab.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { LOANS_COLLECTION } from 'core/api/loans/loanConstants';
 import getRefinancingFormArray from 'core/arrays/RefinancingFormArray';
 import AutoForm from 'core/components/AutoForm';
 import { Money } from 'core/components/Translation';
@@ -25,7 +24,6 @@ const RefinancingTab = ({ loan }) => {
         <AutoForm
           formClasses="user-form user-form__info"
           inputs={getRefinancingFormArray({ loan })}
-          collection={LOANS_COLLECTION}
           doc={loan}
           docId={loan._id}
         />
