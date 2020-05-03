448aadcb50e96dba8d21c1672f9f7aff8ae6462a
Brad Hover <dev@tekha.us>
2020-05-03 00:18:21 -0700

    use isomorphic-ws package because ssr cannot use WebSocket

--- microservices/www2/src/utils/meteorClient.js
+++ microservices/www2/src/utils/meteorClient.js
@@ -1,23 +1,9 @@
-import React from 'react';
 import SimpleDDP from 'simpleddp';
+import ws from 'isomorphic-ws';
 
-export const MeteorClientContext = React.createContext();
-
-// Client library to easily talk to our main backend
-//
-// Use it as follows:
-// meteorClient.call('named_query_PROMOTIONS_LIST').then(result => {})
-// meteorClient.call('getGpsStats').then(result => {})
-
-// By default it connects to the dev/production backend based on your environment
-// But it's ok to simply connect to the production URL if you don't need/want
-// to run a backend server. In that case, switch the URL here:
 const opts = {
-  endpoint:
-    process.env.NODE_ENV === 'production'
-      ? 'ws://backend.e-potek.ch/websocket'
-      : 'ws://localhost:5500/websocket',
-  SocketConstructor: WebSocket,
+  endpoint: 'wss://backend.e-potek.ch/websocket',
+  SocketConstructor: ws,
 };
 
 const meteorClient = new SimpleDDP(opts);
