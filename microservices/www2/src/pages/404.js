160328953fdb5954dd77a7bddd9a214b348cd649
Brad Hover <dev@tekha.us>
2020-05-04 14:12:56 -0700

    add basic 404 styling

--- microservices/www2/src/pages/404.js
+++ microservices/www2/src/pages/404.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
+// TODO: localize the content in this page
+// TODO: make sure home redirect is localized as well
 export default () => (
   <Layout>
     <div className="not-found">
       <h1>404</h1>
-      <h2>Document not found</h2>
+      <h2>Page not found</h2>
       <p>
         <a href="/">Return to homepage</a>
       </p>
