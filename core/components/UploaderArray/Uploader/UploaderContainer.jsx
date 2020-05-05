9d237d3ecdb59d77254e1c27d4c38e5e31f3d060
Quentin Herzig <quentin@e-potek.ch>
2020-05-05 08:56:09 +0200

    Use _collection in deleteFile. Closes #2411

--- core/components/UploaderArray/Uploader/UploaderContainer.jsx
+++ core/components/UploaderArray/Uploader/UploaderContainer.jsx
@@ -4,14 +4,15 @@ import { deleteFile } from '../../../api/files/methodDefinitions';
 import SecurityService from '../../../api/security/Security';
 import UploaderController from './UploaderController';
 
-const UploaderContainer = withProps(({ collection, docId, disabled, doc }) => {
+const UploaderContainer = withProps(({ docId, disabled, doc }) => {
   const userIsAdmin = SecurityService.currentUserIsAdmin();
+  const collection = doc?._collection;
 
   return {
     deleteFile: fileKey => deleteFile.run({ collection, docId, fileKey }),
     userIsAdmin,
     disabled: userIsAdmin ? false : disabled,
-    collection: doc?._collection,
+    collection,
   };
 });
 
