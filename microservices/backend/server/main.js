82e36dd03196206777f4a51c3f9bdcb3f60c23a6
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-05-06 18:32:21 +0200

    Improve startup script

--- microservices/backend/server/main.js
+++ microservices/backend/server/main.js
@@ -1,14 +1,9 @@
-import '../imports/startup/server';
-
-import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 
-console.log('SETTING UP METEOR.STARTUP');
-Meteor.startup(() => {
-  console.log('--------------------------');
-  console.log('MIGRATING USERS');
-  console.log('--------------------------');
+console.log('--------------------------');
+console.log('MIGRATING USERS');
+console.log('--------------------------');
+Roles._forwardMigrate();
+console.log('DONE MIGRATING USERS');
 
-  Roles._forwardMigrate();
-  console.log('DONE MIGRATING USERS');
-});
+import '../imports/startup/server';
