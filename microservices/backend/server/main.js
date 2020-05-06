62de7e9bb2153212f89ee7007069cf207c63cc4a
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-05-06 18:59:37 +0200

    Remove migration

--- microservices/backend/server/main.js
+++ microservices/backend/server/main.js
@@ -1,9 +1,1 @@
-import { Roles } from 'meteor/alanning:roles';
-
-console.log('--------------------------');
-console.log('MIGRATING USERS');
-console.log('--------------------------');
-Roles._forwardMigrate();
-console.log('DONE MIGRATING USERS');
-
 import '../imports/startup/server';
