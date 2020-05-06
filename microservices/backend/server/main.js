6ed897de4ebdcbac29304b2a53264694b1297308
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-05-06 13:33:42 +0200

    Migrate roles on startup

--- microservices/backend/server/main.js
+++ microservices/backend/server/main.js
@@ -1,1 +1,8 @@
 import '../imports/startup/server';
+
+import { Meteor } from 'meteor/meteor';
+import { Roles } from 'meteor/alanning:roles';
+
+Meteor.startup(() => {
+  Roles._forwardMigrate();
+});
