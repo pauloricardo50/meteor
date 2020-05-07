4762e80b6df35e18d0c6723b5c2dbc0310d59a60
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-05-07 14:15:51 +0200

    Allow finding loans by assignee

--- microservices/admin/imports/client/pages/LoansPage/LoansPageContainer.jsx
+++ microservices/admin/imports/client/pages/LoansPage/LoansPageContainer.jsx
@@ -1,16 +1,22 @@
-import { withSmartQuery } from 'core/api/containerToolkit';
-import { adminLoans } from 'core/api/loans/queries';
+import { compose, withState } from 'recompose';
 
-export default withSmartQuery({
-  query: adminLoans,
-  params: {
-    $body: {
-      name: 1,
-      user: { name: 1 },
-      status: 1,
+import { withSmartQuery } from 'core/api/containerToolkit';
+import { LOANS_COLLECTION } from 'core/api/loans/loanConstants';
+
+export default compose(
+  withState('assignees', 'setAssignees', []),
+  withSmartQuery({
+    query: LOANS_COLLECTION,
+    params: ({ assignees }) => ({
+      $filters: assignees.length
+        ? { 'assigneeLinks._id': { $in: assignees } }
+        : {},
       createdAt: 1,
-      updatedAt: 1,
+      name: 1,
+      status: 1,
       step: 1,
+      updatedAt: 1,
+      user: { name: 1 },
       structures: {
         id: 1,
         wantedLoan: 1,
@@ -24,8 +30,9 @@ export default withSmartQuery({
         value: 1,
         promotionLots: { properties: { totalValue: 1, value: 1 } },
       },
-    },
-  },
-  queryOptions: { reactive: false },
-  dataName: 'loans',
-});
+      $options: { sort: { createdAt: -1 } },
+    }),
+    queryOptions: { reactive: false },
+    dataName: 'loans',
+  }),
+);
