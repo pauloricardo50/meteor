71fa5fcabcc49bc8a38784c77a1873bd0c7e0978
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-05-11 14:43:08 +0200

    Prepare reproduction

--- microservices/admin/imports/client/pages/SingleLoanPage/SingleLoanPageContainer.jsx
+++ microservices/admin/imports/client/pages/SingleLoanPage/SingleLoanPageContainer.jsx
@@ -15,6 +15,10 @@ import {
   injectCalculator,
   withCalculator,
 } from 'core/containers/withCalculator';
+import {
+  useReactiveMeteorData,
+  useStaticMeteorData,
+} from 'core/hooks/useMeteorData';
 
 import PremiumSingleLoanPage from './PremiumSingleLoanPage';
 
@@ -42,21 +46,41 @@ const keysToOmit = [
 const fullLoanFragment = {
   ...omit(adminLoan({ withSort: true }), keysToOmit),
   revenues: { _id: 1, status: 1 },
+  // category: 1,
 };
 
 export default compose(
-  updateForProps(['match.params.loanId']),
-  withSmartQuery({
-    query: LOANS_COLLECTION,
-    params: ({ match, loanId }) => ({
-      $filters: {
-        _id: loanId || match.params.loanId,
+  updateForProps(['match.params.loanId', 'loanId']),
+  // withSmartQuery({
+  //   query: LOANS_COLLECTION,
+  //   params: ({ match, loanId }) => ({
+  //     $filters: {
+  //       _id: loanId || match.params.loanId,
+  //     },
+  //     ...fullLoanFragment,
+  //   }),
+  //   queryOptions: { reactive: true, single: true },
+  //   dataName: 'loan',
+  // }),
+  Component => props => {
+    const { loanId, match } = props;
+    const _id = loanId || match?.params?.loanId;
+
+    const { data, loading } = useStaticMeteorData(
+      {
+        query: LOANS_COLLECTION,
+        params: { $filters: { _id }, ...fullLoanFragment },
+        type: 'single',
       },
-      ...fullLoanFragment,
-    }),
-    queryOptions: { reactive: true, single: true },
-    dataName: 'loan',
-  }),
+      [_id],
+    );
+
+    if (loading) {
+      return <h2>LOADING</h2>;
+    }
+
+    return <Component {...props} loan={data} />;
+  },
   injectCalculator(),
   withTranslationContext(({ loan = {} }) => ({
     purchaseType: loan.purchaseType,
@@ -72,14 +96,14 @@ export default compose(
     renderComponent(PremiumSingleLoanPage),
   ),
   // FIXME: This is a serious Meteor bug, no idea what's going on
-  Component =>
-    class extends React.Component {
-      componentDidCatch(err) {
-        console.log('err:', err);
-      }
+  // Component =>
+  //   class extends React.Component {
+  //     componentDidCatch(err) {
+  //       console.log('err:', err);
+  //     }
 
-      render() {
-        return <Component {...this.props} />;
-      }
-    },
+  //     render() {
+  //       return <Component {...this.props} />;
+  //     }
+  //   },
 );
