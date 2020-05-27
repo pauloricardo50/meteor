1e5b23ee9f44f65a611a921060d05918d03fbf7f
Quentin Herzig <quentin@e-potek.ch>
2020-05-27 10:36:14 +0200

    Add all impersonate notification logic in useImpersonatedSession

--- core/hooks/useImpersonatedSession.jsx
+++ core/hooks/useImpersonatedSession.jsx
@@ -1,3 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+
+import { useEffect, useState } from 'react';
+
+import { setUserConnected } from '../api/sessions/methodDefinitions';
 import { userImpersonatedSession } from '../api/sessions/queries';
 import { useReactiveMeteorData } from './useMeteorData';
 
@@ -8,7 +13,49 @@ const useImpersonatedSession = () => {
     type: 'single',
   });
 
-  return { impersonatedSession, loading };
+  const { connectionId, userIsConnected, shared, followed } =
+    impersonatedSession || {};
+
+  const currentSessionId = Meteor.connection._lastSessionId;
+  const shouldRenderAdminNotification =
+    connectionId === currentSessionId && userIsConnected;
+  const shouldRenderUserNotification = shared;
+
+  const [followAdmin, setFollowAdmin] = useState(false);
+  const [showUserDialog, setShowUserDialog] = useState(false);
+  const shouldRenderNotification =
+    !loading &&
+    !!impersonatedSession &&
+    (shouldRenderAdminNotification || shouldRenderUserNotification);
+
+  useEffect(() => {
+    if (
+      !!impersonatedSession &&
+      !shouldRenderAdminNotification &&
+      connectionId !== currentSessionId &&
+      !userIsConnected
+    ) {
+      setUserConnected.run({ connectionId });
+    }
+  }, [connectionId, currentSessionId, userIsConnected]);
+
+  useEffect(() => {
+    setShowUserDialog(shared && !followed);
+    setFollowAdmin(shared && followed);
+  }, [shared]);
+
+  return [
+    impersonatedSession,
+    {
+      shouldRenderNotification,
+      shouldRenderAdminNotification,
+      shouldRenderUserNotification,
+      followAdmin,
+      setFollowAdmin,
+      showUserDialog,
+      setShowUserDialog,
+    },
+  ];
 };
 
 export default useImpersonatedSession;
