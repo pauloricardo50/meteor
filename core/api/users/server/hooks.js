fd9dc1f3e45d7fd1c46ef2a0ad6a12224c621006
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-05-27 22:48:52 +0200

    Update mailchimp info when a user's info changes

--- core/api/users/server/hooks.js
+++ core/api/users/server/hooks.js
@@ -1,4 +1,33 @@
+import { Meteor } from 'meteor/meteor';
+
 import formatNumbersHook from '../../../utils/phoneFormatting';
+import MailchimpService from '../../email/server/MailchimpService';
+import ErrorLogger from '../../errorLogger/server/ErrorLogger';
 import Users from '../users';
 
 formatNumbersHook(Users, 'phoneNumbers');
+
+Users.after.update((userId, doc, fieldNames) => {
+  const fieldsToWatch = ['emails', 'firstName', 'lastName', 'phoneNumbers'];
+
+  if (
+    Meteor.isProduction &&
+    !Meteor.isStaging &&
+    fieldNames.some(fieldName => fieldsToWatch.includes(fieldName))
+  ) {
+    try {
+      const { firstName, lastName, emails, phoneNumbers } = doc;
+      MailchimpService.upsertMember({
+        firstName,
+        lastName,
+        email: emails[0].address,
+        phoneNumber: phoneNumbers?.[0],
+      });
+    } catch (error) {
+      ErrorLogger.handleError({
+        error: new Error(`Users after update hook errror: ${error.message}`),
+        userId,
+      });
+    }
+  }
+});
