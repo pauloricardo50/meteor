687936c7ef3d756d4065fffe1dd9be5f74206a73
Quentin Herzig <quentin@e-potek.ch>
2020-05-28 11:46:12 +0200

    Fix organisation logo upload issue. Closes #2518

--- core/components/UploaderArray/Uploader/UploaderContainer.jsx
+++ core/components/UploaderArray/Uploader/UploaderContainer.jsx
@@ -4,15 +4,14 @@ import { deleteFile } from '../../../api/files/methodDefinitions';
 import SecurityService from '../../../api/security/Security';
 import UploaderController from './UploaderController';
 
-const UploaderContainer = withProps(({ docId, disabled, doc }) => {
+const UploaderContainer = withProps(({ docId, disabled, doc, collection }) => {
   const userIsAdmin = SecurityService.currentUserIsAdmin();
-  const collection = doc?._collection;
 
   return {
     deleteFile: fileKey => deleteFile.run({ collection, docId, fileKey }),
     userIsAdmin,
     disabled: userIsAdmin ? false : disabled,
-    collection,
+    collection: collection || doc?._collection,
   };
 });
 
