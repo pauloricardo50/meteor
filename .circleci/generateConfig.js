6ee820df097fcbcb6c10464d45cb0460cf6dbde9
Quentin Herzig <quentin@e-potek.ch>
2020-06-04 10:05:18 +0200

    Install cypress dependencies

--- .circleci/generateConfig.js
+++ .circleci/generateConfig.js
@@ -184,6 +184,7 @@ const testMicroserviceJob = ({ name, testsType, job }) => ({
       `,
     ),
     runCommand('Generate language files', `npm run lang ${name}`),
+    runCommand('Install cypress dependencies', 'sudo apt-get install libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb'),
     runTestsCommand(name, testsType),
     saveCache(
       'Cache meteor system',
