32cd07ce3bab13a2c54f89bb0df31b5f5bcdb6ce
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-06-10 10:13:35 +0200

    Avoid sending emails outside of our domain

--- core/api/email/server/EmailService.js
+++ core/api/email/server/EmailService.js
@@ -29,7 +29,7 @@ class EmailService {
       params,
     });
     const template = getMandrillTemplate(templateOptions);
-    return sendMandrillTemplate(template).then(response => {
+    return sendMandrillTemplate(template, address).then(response => {
       this.emailLogger({ emailId, address, template, response });
       this.addEmailActivity({ address, template, emailId, response });
     });
