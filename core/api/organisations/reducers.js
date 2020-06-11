ac4644bfaf7040db5913ccac6a3ecf8721f784e7
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-06-11 18:53:33 +0200

    Remove offers reducer

--- core/api/organisations/reducers.js
+++ core/api/organisations/reducers.js
@@ -1,17 +1,8 @@
-import { fullOffer } from '../fragments';
 import addressReducer from '../reducers/addressReducer';
 import Organisations from './organisations';
 
 Organisations.addReducers({
   ...addressReducer,
-  offers: {
-    body: { lenders: { offers: fullOffer() } },
-    reduce: ({ lenders = [] }) =>
-      lenders.reduce(
-        (allOffers, { offers = [] }) => [...allOffers, ...offers],
-        [],
-      ),
-  },
   offerCount: {
     body: { lenders: { offers: { _id: 1 } } },
     reduce: ({ lenders = [] }) =>
