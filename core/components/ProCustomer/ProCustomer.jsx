536c1e6ca1b0c958bee1985f5d8ec55b6c9e7a27
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-06-12 10:58:35 +0200

    Allow component to work with a userCache

--- core/components/ProCustomer/ProCustomer.jsx
+++ core/components/ProCustomer/ProCustomer.jsx
@@ -6,12 +6,17 @@ import { USERS_COLLECTION } from '../../api/users/userConstants';
 import { CollectionIconLink } from '../IconLink';
 
 const ProCustomer = ({ user, invitedByUser }) => {
-  const { _id, name, phoneNumbers = ['-'], email, assignedEmployee } = user;
+  const {
+    _id,
+    assignedEmployee,
+    email,
+    firstName,
+    lastName,
+    name = [firstName, lastName].filter(x => x).join(' '),
+    phoneNumbers = ['-'],
+  } = user;
   const isPro = Meteor.microservice === 'pro';
-  const assigneeNumber =
-    assignedEmployee &&
-    assignedEmployee.phoneNumbers &&
-    assignedEmployee.phoneNumbers[0];
+  const assigneePhoneNumber = assignedEmployee?.phoneNumbers?.[0];
 
   return (
     <CollectionIconLink
@@ -57,7 +62,9 @@ const ProCustomer = ({ user, invitedByUser }) => {
 
                         <div>
                           <b>Tél:</b>{' '}
-                          <a href={`tel:${assigneeNumber}`}>{assigneeNumber}</a>
+                          <a href={`tel:${assigneePhoneNumber}`}>
+                            {assigneePhoneNumber}
+                          </a>
                         </div>
                       </div>
                     )
