ef83ec5b45c0c0b7ac47b2b5951f76912dd62e27
zodern <zodern@users.noreply.github.com>
2020-06-14 15:32:01 -0500

    Stop backend when build finishes

--- scripts/circleci/build_backend.sh
+++ scripts/circleci/build_backend.sh
@@ -7,7 +7,7 @@ BACKEND_PATH="$SCRIPT_PATH/../../microservices/backend"
 # 2. cd to the right place, and "spawn" a command that expect will be watching
 # 3. Wait for the meteor app to have launched with "expect 'some ouput'"
 # 4. exit the expect command, killing the meteor process
-expect -c "set timeout 1800; cd $BACKEND_PATH; spawn meteor npm run start; expect \"App running at:\"; exit"
+expect -c "set timeout 1800; cd $BACKEND_PATH; spawn meteor npm run start; expect \"Profiling: Server startup \"; exit"
 
 
 exit 0
