7575cdec1a6419b6a209c28db9f23f5004318693
Brad Hover <dev@tekha.us>
2020-06-14 18:27:24 -0700

    update social links

--- microservices/www2/src/components/SocialLinks/SocialLinks.jsx
+++ microservices/www2/src/components/SocialLinks/SocialLinks.jsx
@@ -1,6 +1,32 @@
-import React from 'react';
-// placeholder component for now
+import React, { useContext } from 'react';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import LanguageContext from '../../contexts/LanguageContext';
+import { getLanguageData } from '../../utils/languages';
+import './SocialLinks.scss';
 
-const SocialLinks = () => <div className="contact-social">Suivez-nous</div>;
+const SocialLinks = () => {
+  const [language] = useContext(LanguageContext);
+
+  return (
+    <div className="social-links">
+      <span>{getLanguageData(language).followUs}</span>
+
+      <a href="https://www.instagram.com/epotekch/" target="_new">
+        <FontAwesomeIcon icon={faInstagram} />
+      </a>
+
+      <a href="https://www.linkedin.com/company/epotek/" target="_new">
+        <FontAwesomeIcon icon={faLinkedinIn} />
+      </a>
+
+      <a href="https://www.facebook.com/epotekCH/" target="_new">
+        <FontAwesomeIcon icon={faFacebookF} />
+      </a>
+    </div>
+  );
+};
 
 export default SocialLinks;
