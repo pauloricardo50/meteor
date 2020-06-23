29ad23af743f9e8bed76b12b8f2d34c35d29d32c
zodern <zodern@users.noreply.github.com>
2020-06-23 09:24:23 -0500

    Fix test scripts for backend

--- scripts/circleci/test-ci.sh
+++ scripts/circleci/test-ci.sh
@@ -4,4 +4,11 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 BACKEND_PORT="5505"
 TEST_CMD="NODE_ICU_DATA=$PWD/node_modules/full-icu NIGHTMARE_WAIT_TIMEOUT=60000 DDP_DEFAULT_CONNECTION_URL=http://localhost:$BACKEND_PORT METEOR_PACKAGE_DIRS="packages:../../meteorPackages" SERVER_TEST_REPORTER=xunit CLIENT_TEST_REPORTER=xunit SERVER_MOCHA_OUTPUT=~/app/results/unit-server.xml CLIENT_MOCHA_OUTPUT=~/app/results/unit-client.xml TEST_BROWSER_DRIVER=nightmare meteor test --once --driver-package meteortesting:mocha --settings settings-dev.json"
 
-${SCRIPTPATH}/run-backend-ci.sh --test & ${SCRIPTPATH}/wait-port.sh $BACKEND_PORT $TEST_CMD
+microservice="${PWD##*/}"
+
+if [ "$microservice" != "backend" ]; then
+  ${SCRIPTPATH}/run-backend-ci.sh --test &
+  ${SCRIPTPATH}/wait-port.sh $BACKEND_PORT $TEST_CMD
+else
+  eval ${TEST_CMD}
+fi
