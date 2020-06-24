15678dfd55df788e6b68199d9d913804d79179ca
zodern <zodern@users.noreply.github.com>
2020-06-24 08:57:42 -0500

    Attempt to fix unit tests failing in CI

--- core/startup/tests/server.js
+++ core/startup/tests/server.js
@@ -1,3 +1,8 @@
 // Set up for running client tests
 
 import 'core/api/methods/server/test/serverMethods-app-test.js';
+
+// Add a single test so mocha wan't fail due to no tests
+describe('backend-helpers', () => {
+  it('should run', () => {});
+});
