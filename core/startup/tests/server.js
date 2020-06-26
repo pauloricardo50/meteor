0cfd110aed333ba52aaa1b8b967e177eb83f3359
zodern <zodern@users.noreply.github.com>
2020-06-26 16:56:26 -0500

    Fix running core tests

--- core/startup/tests/server.js
+++ core/startup/tests/server.js
@@ -1,8 +1,12 @@
 // Set up for running client tests
 
 import 'core/api/methods/server/test/serverMethods-app-test.js';
+import 'core/lib/test/server/lib.test.js';
 
-// Add a single test so mocha wan't fail due to no tests
-describe('backend-helpers', () => {
-  it('should run', () => {});
-});
+import 'core/api/server';
+import 'core/api/api';
+import 'core/fixtures';
+import 'core/startup/server/monti';
+import 'core/startup/accounts-config';
+import 'core/api/users/server/accounts-server-config';
+import 'core/api/sessions/server/startup';
