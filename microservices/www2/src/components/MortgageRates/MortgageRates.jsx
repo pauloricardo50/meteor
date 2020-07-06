0ad28981b0804c025f5499e8782d4cd1094a38b2
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-07-06 12:04:36 +0200

    Adjust mortgageRates styles

--- microservices/www2/src/components/MortgageRates/MortgageRates.jsx
+++ microservices/www2/src/components/MortgageRates/MortgageRates.jsx
@@ -1,31 +1,13 @@
+import './MortgageRates.scss';
+
 import React, { useContext, useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-// import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import TrendIcon from './TrendIcon';
+
 import LanguageContext from '../../contexts/LanguageContext';
 import { getLanguageData } from '../../utils/languages';
 import meteorClient from '../../utils/meteorClient';
-import './MortgageRates.scss';
+import TrendIcon from './TrendIcon';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    padding: 0,
-  },
-  content: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    margin: '23px 0 20px',
-    [theme.breakpoints.up('md')]: {
-      margin: '40px 0',
-    },
-  },
-}));
-
-const makePercent = num =>
+const makePercent = (num) =>
   Number(num).toLocaleString(undefined, {
     style: 'percent',
     minimumFractionDigits: 2,
@@ -43,14 +25,8 @@ const parseRateType = (language, rateType) => {
 };
 
 const MortgageRates = () => {
-  const [expanded, setExpanded] = useState(false);
   const [currentRates, setCurrentRates] = useState('');
   const [language] = useContext(LanguageContext);
-  const classes = useStyles();
-
-  const handleChange = panel => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
-  };
 
   useEffect(() => {
     const getCurrentRates = async () => {
@@ -75,26 +51,15 @@ const MortgageRates = () => {
 
         {currentRates &&
           currentRates.rates.map((rate, idx) => (
-            <ExpansionPanel
-              key={idx}
-              expanded={expanded === `panel-${idx}`}
-              // onChange={handleChange(`panel-${idx}`)}
-            >
-              <ExpansionPanelSummary
-                // expandIcon={<AddCircleOutlineIcon />}
-                aria-controls={`panel-${idx}-content`}
-                id={`panel-${idx}-header`}
-                classes={classes}
-              >
-                <div className="rate-type">
-                  {parseRateType(language, rate.type)}
-                </div>
-                <TrendIcon trend={rate.trend} />
-                <div className="rate-range">
-                  {makePercent(rate.rateLow)} - {makePercent(rate.rateHigh)}
-                </div>
-              </ExpansionPanelSummary>
-            </ExpansionPanel>
+            <div className="rates-table-line" key={idx}>
+              <div className="rate-type">
+                {parseRateType(language, rate.type)}
+              </div>
+              <TrendIcon trend={rate.trend} />
+              <div className="rate-range">
+                {makePercent(rate.rateLow)} - {makePercent(rate.rateHigh)}
+              </div>
+            </div>
           ))}
       </div>
     </section>
