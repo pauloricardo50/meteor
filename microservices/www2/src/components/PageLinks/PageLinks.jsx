1069fab1261460348138feda09afeb650a81eb32
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-07-06 13:43:26 +0200

    Adjust pageLinks styles

--- microservices/www2/src/components/PageLinks/PageLinks.jsx
+++ microservices/www2/src/components/PageLinks/PageLinks.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import { linkResolver } from '../../utils/linkResolver';
 import './PageLinks.scss';
 
-const PageLinks = ({ fields }) => (
-  <nav className="page-links container">
-    <Tabs centered>
-      {fields.map((field, idx) => (
-        <Tab
-          key={idx}
-          component={Link}
-          label={field.label}
-          to={linkResolver(field.link._meta)}
-        />
-      ))}
-    </Tabs>
-  </nav>
-);
+import React from 'react';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import { Link } from 'gatsby';
+
+import useMedia from 'core/hooks/useMedia';
+
+import { linkResolver } from '../../utils/linkResolver';
+
+const PageLinks = ({ fields }) => {
+  const isMobile = useMedia({ maxWidth: 768 });
+
+  return (
+    <nav className="page-links container">
+      <Tabs centered orientation={isMobile ? 'vertical' : 'horizontal'}>
+        {fields.map((field, idx) => (
+          <Tab
+            key={idx}
+            component={Link}
+            label={field.label}
+            to={linkResolver(field.link._meta)}
+          />
+        ))}
+      </Tabs>
+    </nav>
+  );
+};
 
 export default PageLinks;
--- microservices/www2/src/components/PromotionsGrid/PromotionsGrid.jsx
+++ microservices/www2/src/components/PromotionsGrid/PromotionsGrid.jsx
@@ -1,44 +1,46 @@
-import React, { useContext } from 'react';
-import Button from '../Button';
-import useAllPromotion from '../../hooks/useAllPromotion';
-import LanguageContext from '../../contexts/LanguageContext';
-import { getLanguageData } from '../../utils/languages';
 import './PromotionsGrid.scss';
 
-const PromotionsGridItem = ({ language, promotion }) => (
-  <div className="promotion-item">
-    {/* TODO: add slider for multiple images */}
-    {promotion.images && (
-      <div className="promotion-item__images">
-        {promotion.images.map(promoImage => (
-          <img
-            key={promoImage.name}
-            src={promoImage.url}
-            alt={`${promotion.name}_${promoImage.name}`}
-          />
-        ))}
-      </div>
-    )}
+import React, { useContext } from 'react';
 
-    <h3 className="promotion-item__name">
-      {promotion.name}
-      {/* <span>{promotion.address}</span> */}
-    </h3>
+import LanguageContext from '../../contexts/LanguageContext';
+import useAllPromotion from '../../hooks/useAllPromotion';
+import { getLanguageData } from '../../utils/languages';
+import Button from '../Button';
 
-    {promotion.summary && (
-      <p className="promotion-item__summary">{promotion.summary}</p>
-    )}
+const PromotionsGridItem = ({ language, promotion }) => {
+  const { name, images, summary, address } = promotion;
+  return (
+    <div className="promotion-item">
+      {/* TODO: add slider for multiple images */}
+      {images && (
+        <div className="promotion-item__images">
+          {images.map((promoImage) => (
+            <img
+              key={promoImage.name}
+              src={promoImage.url}
+              alt={`${promotion.name}_${promoImage.name}`}
+            />
+          ))}
+        </div>
+      )}
 
-    <div className="promotion-item__footer">
-      <div className="interest-button">
-        <Button raised className="button" type="submit">
-          {getLanguageData(language).promoInterest}
-        </Button>
+      <h3 className="promotion-item__name">
+        {name}
+        {/* <span>{promotion.address}</span> */}
+      </h3>
+
+      {summary && <p className="promotion-item__summary">{summary}</p>}
+
+      <div className="promotion-item__footer">
+        <div className="interest-button">
+          <Button raised className="button" type="submit">
+            {getLanguageData(language).promoInterest}
+          </Button>
+        </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 const PromotionsGrid = () => {
   const promotions = useAllPromotion();
   const [language] = useContext(LanguageContext);
@@ -48,7 +50,7 @@ const PromotionsGrid = () => {
       {/* TODO: add filtering in next phase */}
       <div className="promotions-grid">
         {promotions &&
-          promotions.map(promotion => (
+          promotions.map((promotion) => (
             <PromotionsGridItem
               key={promotion.id}
               language={language}
