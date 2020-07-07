5d0679865d6ef54defc3933f6b7d693f45b49fdc
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-07-07 13:28:26 +0200

    Improve cookie handling

--- microservices/www2/src/components/CookiesNotification/CookiesNotification.jsx
+++ microservices/www2/src/components/CookiesNotification/CookiesNotification.jsx
@@ -1,14 +1,15 @@
-import React, { useState, useContext } from 'react';
-import { RichText } from 'prismic-reactjs';
-import { useCookies } from 'react-cookie';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useContext, useState } from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import SnackbarContent from '@material-ui/core/SnackbarContent';
-import Button from '../Button';
+import { makeStyles } from '@material-ui/core/styles';
+import { RichText } from 'prismic-reactjs';
+import { useCookies } from 'react-cookie';
+
 import LanguageContext from '../../contexts/LanguageContext';
 import useContentBlock from '../../hooks/useContentBlock';
 import { getLanguageData } from '../../utils/languages.js';
 import { linkResolver } from '../../utils/linkResolver';
+import Button from '../Button';
 
 const acceptCookie = 'epotek_acceptCookie';
 
@@ -49,10 +50,20 @@ const useSnackbarContentStyles = makeStyles(theme => ({
   },
 }));
 
+const cookieOptions = {
+  maxAge: '31536000', // one year
+  domain: '.e-potek.ch',
+};
+
 const CookiesNotification = () => {
   const [language] = useContext(LanguageContext);
   const [cookies, setCookie] = useCookies(acceptCookie);
-  const [visible, setVisible] = useState(true);
+  const hasSetCookie = !!cookies[acceptCookie];
+  const [visible, setVisible] = useState(
+    process.env.NODE_ENV === 'production' && !hasSetCookie,
+  );
+  const snackbarClasses = useSnackbarStyles();
+  const contentClasses = useSnackbarContentStyles();
 
   const cookieNotification = useContentBlock({
     uid: 'cookies-notification',
@@ -64,36 +75,25 @@ const CookiesNotification = () => {
   const message = RichText.render(cookieNotification, linkResolver);
 
   const handleAccept = () => {
-    setCookie(acceptCookie, true, {
-      maxAge: '31536000', // one year
-      domain: 'e-potek.ch',
-    });
-
+    setCookie(acceptCookie, 'all', cookieOptions);
     setVisible(false);
   };
 
   const handleDecline = () => {
-    setCookie(acceptCookie, false, {
-      maxAge: '31536000', // one year
-      domain: 'e-potek.ch',
-    });
-
+    setCookie(acceptCookie, 'limited', cookieOptions);
     setVisible(false);
   };
 
   return (
-    <Snackbar
-      open={!cookies[acceptCookie] && visible}
-      classes={useSnackbarStyles()}
-    >
+    <Snackbar open={visible} classes={snackbarClasses}>
       <SnackbarContent
-        classes={useSnackbarContentStyles()}
+        classes={contentClasses}
         message={message}
         action={[
-          <Button key="decline" contained onClick={() => handleDecline()}>
+          <Button key="decline" contained onClick={handleDecline}>
             {getLanguageData(language).cookieDecline}
           </Button>,
-          <Button key="accept" raised primary onClick={() => handleAccept()}>
+          <Button key="accept" raised primary onClick={handleAccept}>
             {getLanguageData(language).cookieAccept}
           </Button>,
         ]}
