6d703549f8f88a4bb9868a2806bba6ae8ecde6a4
Quentin Herzig <quentin@e-potek.ch>
2020-07-07 08:55:33 +0200

    Add LoansMap in GPSStats

---
+++ microservices/www2/src/components/GPSStats/GPSStatsConstants.js
@@ -0,0 +1,13 @@
+export const CITIES_COORDINATES = {
+  '1001': { x: 270 / 1000, y: 610 / 1000 },
+  '1200': { x: 110 / 1000, y: 750 / 1000 },
+  '1400': { x: 290 / 1000, y: 430 / 1000 },
+  '1700': { x: 450 / 1000, y: 420 / 1000 },
+  '1950': { x: 610 / 1000, y: 790 / 1000 },
+  '2000': { x: 370 / 1000, y: 330 / 1000 },
+  '2800': { x: 510 / 1000, y: 120 / 1000 },
+};
+
+export const MARKER_SIZE_FACTOR = 0.1;
+export const MARKER_FONT_SIZE_FACTOR = 1.2 / 100;
+export const MARKER_LINE_HEIGHT_FACTOR = 1.4375 / 100;
