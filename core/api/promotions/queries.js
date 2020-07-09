7c9dada1a1e9180c213284f79d36a8ccec4d39de
Quentin Herzig <quentin@e-potek.ch>
2020-07-09 17:00:55 +0200

    Allow filtering promotionsList by canton

--- core/api/promotions/queries.js
+++ core/api/promotions/queries.js
@@ -33,10 +33,12 @@ export const proPromotionUsers = Promotions.createQuery(
 export const promotionsList = Promotions.createQuery(
   PROMOTION_QUERIES.PROMOTIONS_LIST,
   {
-    name: 1,
     address: 1,
+    canton: 1,
+    city: 1,
     documents: { promotionImage: 1 },
     lotsCount: 1,
+    name: 1,
     status: 1,
     // For future use
     description: 1,
