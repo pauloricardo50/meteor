5135ee4b1c615b8f6eda6598edd7b2a94e527826
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-07-13 20:51:09 +0200

    Improve 404 page

--- microservices/www2/src/pages/404.js
+++ microservices/www2/src/pages/404.js
@@ -1,16 +1,29 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import Layout from '../components/Layout';
+import { faHeartBroken } from '@fortawesome/pro-light-svg-icons/faHeartBroken';
+import { Link } from 'gatsby';
 
-// TODO: get updated NotFound component from core to handle localization
-export default () => (
-  <Layout pageContext={{ lang: 'en', type: '404' }} pageName="404">
+import FaIcon from 'core/components/Icon/FaIcon';
+import T from 'core/components/Translation/FormattedMessage';
+import colors from 'core/config/colors';
+
+const FourOFour = () => (
+  <div>
     <div className="not-found">
-      <h1>404</h1>
-      <h2>Page not found</h2>
+      <FaIcon icon={faHeartBroken} size="6x" color={colors.duotoneIconColor} />
+      <h1>
+        <T id="404.title" />
+      </h1>
+      <h2>
+        <T id="404.description" />
+      </h2>
       <p>
-        <a href="/">Return to homepage</a>
+        <Link to="/">
+          <T id="404.backToHome" />
+        </Link>
       </p>
     </div>
-  </Layout>
+  </div>
 );
+
+export default FourOFour;
