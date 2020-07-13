9d126b1552898415bc355c59d8d2b97525bdc3d8
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-07-13 18:49:13 +0200

    Improve button size

--- microservices/www2/src/components/WwwCalculator/WwwCalculatorPurchaseType.jsx
+++ microservices/www2/src/components/WwwCalculator/WwwCalculatorPurchaseType.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import cx from 'classnames';
 
+import AcquisitionIcon from 'core/components/Icon/AcquisitionIcon';
+import RefinancingIcon from 'core/components/Icon/RefinancingIcon';
 import T from 'core/components/Translation/FormattedMessage';
 
 import { ACTIONS, PURCHASE_TYPE } from './wwwCalculatorConstants';
 import { useWwwCalculator } from './WwwCalculatorState';
 
 const purchaseTypes = Object.values(PURCHASE_TYPE);
+
+const icons = {
+  [PURCHASE_TYPE.ACQUISITION]: <AcquisitionIcon />,
+  [PURCHASE_TYPE.REFINANCING]: <RefinancingIcon />,
+};
 
 const WwwCalculatorPurchaseType = () => {
   const [{ purchaseType }, dispatch] = useWwwCalculator();
@@ -24,9 +31,10 @@ const WwwCalculatorPurchaseType = () => {
             })
           }
         >
-          <h2>
+          {icons[type]}
+          <div className="button-text">
             <T id={`WwwCalculatorPurchaseType.${type}`} />
-          </h2>
+          </div>
         </ButtonBase>
       ))}
     </div>
