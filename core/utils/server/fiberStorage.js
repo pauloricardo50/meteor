af7f616ba62f48f7b6d5e0c272839c41a084305a
zodern <zodern@users.noreply.github.com>
2020-07-21 22:48:01 -0500

    Cache fibers import

--- core/utils/server/fiberStorage.js
+++ core/utils/server/fiberStorage.js
@@ -1,16 +1,24 @@
-// import Fibers from 'fibers';
+let _fibers;
+
+const getFibers = () => {
+  if (!_fibers) {
+    _fibers = require('fibers');
+  }
+
+  return _fibers;
+};
 
 export const storeOnFiber = (key, value) => {
-  const Fibers = require('fibers');
+  const Fibers = getFibers();
   Fibers.current[key] = value;
 };
 
 export const getFromFiber = key => {
-  const Fibers = require('fibers');
+  const Fibers = getFibers();
   return Fibers.current && Fibers.current[key];
 };
 
 export const removeFromFiber = key => {
-  const Fibers = require('fibers');
+  const Fibers = getFibers();
   delete Fibers.current[key];
 };
