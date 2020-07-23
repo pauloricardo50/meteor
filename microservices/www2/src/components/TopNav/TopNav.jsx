e5a4a9803921c0ee302966b57ae216216f99e1ff
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-07-23 20:59:25 +0200

    Dynamically hide CTA on mobile to avoid confusion

--- microservices/www2/src/components/TopNav/TopNav.jsx
+++ microservices/www2/src/components/TopNav/TopNav.jsx
@@ -1,12 +1,12 @@
 import './TopNav.scss';
 
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import { makeStyles } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import useWindowScroll from 'react-use/lib/useWindowScroll';
 
 import colors from 'core/config/colors';
-import useMedia from 'core/hooks/useMedia';
 
 import { EPOTEK_APP } from '../../constants';
 import LanguageContext from '../../contexts/LanguageContext';
@@ -27,11 +27,27 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
+const contentHeight = 400;
+
 const TopNav = () => {
   const { tracking_id: pageTrackingId } = useLayoutContext();
   const [language] = useContext(LanguageContext);
-  const matches = useMediaQuery(theme => theme.breakpoints.up('md'));
-  const isMobile = useMedia({ maxWidth: 768 });
+  const isMobile = useMediaQuery(theme => theme.breakpoints.down('md'), {
+    noSsr: true, // Otherwise it always returns false at first: https://github.com/mui-org/material-ui/issues/21142
+  });
+  const { y } = useWindowScroll();
+  const shouldUseDynamicCta = isMobile && pageTrackingId === 'Home page';
+  const [showCta, setShowCta] = useState(shouldUseDynamicCta ? y === 0 : true);
+
+  useEffect(() => {
+    if (shouldUseDynamicCta && !showCta && y > contentHeight) {
+      setShowCta(true);
+    } else if (shouldUseDynamicCta && showCta && y < contentHeight) {
+      setShowCta(false);
+    } else if (!shouldUseDynamicCta && !showCta) {
+      setShowCta(true);
+    }
+  }, [y, shouldUseDynamicCta]);
 
   return (
     <AppBar
@@ -45,30 +61,33 @@ const TopNav = () => {
         <div className="top-nav-left">
           <MainMenu />
 
-          {matches && <TopMenu menuName="top-nav" />}
+          {!isMobile && <TopMenu menuName="top-nav" />}
         </div>
 
         <TopNavLogo />
 
         <div className="top-nav-right">
-          {matches && <LoginMenu />}
+          {!isMobile && <LoginMenu />}
 
-          <Button
-            size={isMobile ? 'small' : 'medium'}
-            raised
-            primary
-            component="a"
-            href={EPOTEK_APP}
-            onTrack={() =>
-              trackCTA({
-                buttonTrackingId: 'Topnav start',
-                toPath: EPOTEK_APP,
-                pageTrackingId,
-              })
-            }
-          >
-            {getLanguageData(language).getALoanText}
-          </Button>
+          {showCta && (
+            <Button
+              size={isMobile ? 'small' : 'medium'}
+              raised
+              primary
+              component="a"
+              href={EPOTEK_APP}
+              onTrack={() =>
+                trackCTA({
+                  buttonTrackingId: 'Topnav start',
+                  toPath: EPOTEK_APP,
+                  pageTrackingId,
+                })
+              }
+              className="animated fadeIn"
+            >
+              {getLanguageData(language).getALoanText}
+            </Button>
+          )}
         </div>
       </div>
     </AppBar>
