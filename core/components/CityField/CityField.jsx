70ca3d61374b7a8118e5799ec4b9646350873e6b
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-07-27 15:24:54 +0200

    Adjust CityField
    Closes #2714

--- core/components/CityField/CityField.jsx
+++ core/components/CityField/CityField.jsx
@@ -1,13 +1,7 @@
-import { Meteor } from 'meteor/meteor';
-
 import React, { useEffect, useState } from 'react';
 import { withProps } from 'recompose';
 
-import { BORROWERS_COLLECTION } from '../../api/borrowers/borrowerConstants';
-import { borrowerUpdate } from '../../api/borrowers/methodDefinitions';
 import { getCitiesFromZipCode } from '../../api/gpsStats/methodDefinitions';
-import { propertyUpdate } from '../../api/properties/methodDefinitions';
-import { PROPERTIES_COLLECTION } from '../../api/properties/propertyConstants';
 import AutoFormSelectFieldInput from '../AutoForm/AutoFormSelectFieldInput';
 import AutoFormTextInput from '../AutoForm/AutoFormTextInput';
 import T from '../Translation';
@@ -29,12 +23,53 @@ const getCities = async (zipCode = '') => {
   return [{ id: null }];
 };
 
-const CityField = ({ updateFunc, cities, city, customCity, setCustomCity }) => (
+const CityField = ({
+  updateFunc,
+  cities,
+  city,
+  customCity,
+  setCustomCity,
+  selectValue,
+}) => (
   <>
-    {customCity ? (
+    <AutoFormSelectFieldInput
+      InputProps={{
+        currentValue: selectValue,
+        label: <T id="Forms.city" />,
+        style: { width: '100%', maxWidth: '400px' },
+        readOnly: false,
+        required: true,
+        todo: !city,
+        multiline: true,
+        id: 'city',
+        options: cities,
+        transform: c => {
+          if (c === 'other') {
+            return <T id="general.other" />;
+          }
+          return c || 'Aucun résultat trouvé';
+        },
+      }}
+      saveOnChange
+      showValidIconOnChange
+      updateFunc={value => {
+        const {
+          object: { city: newCity },
+        } = value;
+
+        if (newCity === 'other') {
+          setCustomCity(true);
+          return Promise.resolve();
+        }
+
+        setCustomCity(false);
+        return updateFunc(value);
+      }}
+    />
+    {customCity && (
       <AutoFormTextInput
         InputProps={{
-          defaultValue: city,
+          currentValue: city,
           label: <T id="Forms.city.specify" />,
           style: { width: '100%', maxWidth: '400px' },
           readOnly: false,
@@ -45,92 +80,32 @@ const CityField = ({ updateFunc, cities, city, customCity, setCustomCity }) => (
         saveOnChange
         updateFunc={updateFunc}
       />
-    ) : (
-      <AutoFormSelectFieldInput
-        InputProps={{
-          defaultValue: city,
-          label: <T id="Forms.city" />,
-          style: { width: '100%', maxWidth: '400px' },
-          readOnly: false,
-          required: true,
-          todo: !city,
-          multiline: true,
-          id: 'city',
-          options: [...cities, { id: city }],
-          transform: c => {
-            if (c === 'other') {
-              return <T id="general.other" />;
-            }
-            return c || 'Aucun résultat trouvé';
-          },
-        }}
-        saveOnChange
-        showValidIconOnChange
-        updateFunc={value => {
-          const {
-            object: { city: newCity },
-          } = value;
-          if (newCity === 'other') {
-            return Promise.resolve(setCustomCity(true));
-          }
-          setCustomCity(false);
-          return updateFunc(value);
-        }}
-      />
     )}
   </>
 );
 
-export default withProps(({ doc }) => {
+export default withProps(({ doc, updateFunc }) => {
   const [cities, setCities] = useState([]);
   const [customCity, setCustomCity] = useState(false);
-  const { _collection, zipCode, city } = doc;
+  const { _id: docId, zipCode, city } = doc;
 
   useEffect(() => {
-    getCities(zipCode).then(result => setCities([...result, { id: 'other' }]));
-  }, [zipCode, city]);
+    getCities(zipCode).then(result => {
+      setCities([...result, { id: 'other' }]);
+      if (city && !result.includes(city)) {
+        setCustomCity(true);
+      }
+    });
+  }, [zipCode]);
 
-  useEffect(() => {
-    if (cities.filter(({ id }) => ![null, 'other'].includes(id)).length === 0) {
-      setCustomCity(true);
-    } else {
-      setCustomCity(false);
-    }
-  }, [cities]);
-
-  let updateFunc = () => Promise.resolve();
-
-  switch (_collection) {
-    case BORROWERS_COLLECTION:
-      updateFunc = ({ object: { city: newCity } = {} }) =>
-        newCity
-          ? borrowerUpdate.run({
-              borrowerId: doc._id,
-              object: { city: newCity },
-            })
-          : Promise.resolve();
-      break;
-    case PROPERTIES_COLLECTION:
-      updateFunc = ({ object: { city: newCity } = {} }) =>
-        newCity
-          ? propertyUpdate.run({
-              propertyId: doc._id,
-              object: { city: newCity },
-            })
-          : Promise.resolve();
-      break;
-    default: {
-      throw new Meteor.Error(
-        `Collection ${_collection} not allowed in CityField`,
-      );
-    }
-  }
+  const isRecommendedCity = city && cities.length > 0 && cities.includes(city);
 
   return {
     cities,
-    updateFunc,
+    updateFunc: ({ object }) => updateFunc({ id: docId, object }),
     city,
     customCity,
     setCustomCity,
+    selectValue: isRecommendedCity ? city : city ? 'other' : undefined,
   };
 })(CityField);
