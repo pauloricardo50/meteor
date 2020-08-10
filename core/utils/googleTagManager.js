f09aa757483fb992157b53535c1abd31c410b437
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-08-10 11:49:31 +0200

    Pin version of react-gtm-module
    In version 2.0.9, the script is inserted in the head with a weird trick
    which appears to break the module

--- core/utils/googleTagManager.js
+++ core/utils/googleTagManager.js
@@ -4,6 +4,7 @@ const GOOGLE_TAG_MANAGER_ID = 'GTM-W8KXQ9V';
 
 export const initGoogleTagManager = () => {
   if (process.env.NODE_ENV === 'production') {
+    console.log('initialise GTM');
     TagManager.initialize({ gtmId: GOOGLE_TAG_MANAGER_ID });
   }
 };
