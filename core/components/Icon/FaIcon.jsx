89c419e0223b4d1cd9859c4fc49e77b284a81d9a
Quentin Herzig <quentin@e-potek.ch>
2020-08-20 15:32:04 +0200

    Allow tooltip in FaIcon

--- core/components/Icon/FaIcon.jsx
+++ core/components/Icon/FaIcon.jsx
@@ -1,6 +1,8 @@
 import React, { forwardRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import Tooltip from '../Material/Tooltip';
+
 const getStyle = ({ color, primaryColor, secondaryColor, style }) => ({
   ...style,
   '--fa-primary-color': primaryColor || color,
@@ -8,15 +10,30 @@ const getStyle = ({ color, primaryColor, secondaryColor, style }) => ({
 });
 
 const FaIcon = forwardRef(
-  ({ icon, color, primaryColor, secondaryColor, style, ...props }, ref) => (
-    <FontAwesomeIcon
-      icon={icon}
-      color={color}
-      style={getStyle({ color, primaryColor, secondaryColor, style })}
-      forwardedRef={ref}
-      {...props}
-    />
-  ),
+  (
+    { icon, color, primaryColor, secondaryColor, style, tooltip, ...props },
+    ref,
+  ) => {
+    const Icon = (
+      <FontAwesomeIcon
+        icon={icon}
+        color={color}
+        style={getStyle({ color, primaryColor, secondaryColor, style })}
+        forwardedRef={ref}
+        {...props}
+      />
+    );
+
+    if (tooltip) {
+      return (
+        <Tooltip title={tooltip}>
+          <span>{Icon}</span>
+        </Tooltip>
+      );
+    }
+
+    return Icon;
+  },
 );
 
 export default FaIcon;
