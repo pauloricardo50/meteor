368cf8c2073e2d85a05ebd5a695fa3e95b52fdd7
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-08-27 11:48:39 +0200

    Remove comment

--- core/utils/googleTagManager.js
+++ core/utils/googleTagManager.js
@@ -3,10 +3,7 @@ import TagManager from 'react-gtm-module';
 const GOOGLE_TAG_MANAGER_ID = 'GTM-W8KXQ9V';
 
 export const initGoogleTagManager = () => {
-  console.log('init GTM');
-
   if (process.env.NODE_ENV === 'production') {
-    console.log('init GTM production');
     TagManager.initialize({ gtmId: GOOGLE_TAG_MANAGER_ID });
   }
 };
